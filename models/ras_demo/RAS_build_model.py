79d41b84d955ddcb6405a4e3209298989707e831
John Bachman <bachmanjohn@gmail.com>
2015-02-12 10:46:05 -0500

    Cosmetic changes to RAS_build_model
    Whitespace, imports, shortcuts for accessing model monomers and
    parameters, etc.

--- models/ras_demo/RAS_build_model.py
+++ models/ras_demo/RAS_build_model.py
@@ -1,6 +1,11 @@
+import pickle
 import numpy as np
 import sys
 from pysb import *
+from pysb.integrate import Solver
+import pysb.core
+
+# Allows import of belpy in case it is not already on PYTHONPATH
 sys.path.append('../../')
 from belpy import rdf_to_pysb
 
@@ -13,27 +18,34 @@ def get_base_state(mon):
             sdict[s]=None
     return mon(sdict)
 
-def add_initial(model,pattern,value):
-    import pysb.core
+def add_initial(model, pattern, value):
+    """Add initial condition; if an initial condition for this pattern
+    already exists, override it."""
     complex_pattern = pysb.as_complex_pattern(pattern)
 
     for other_cp, other_value in model.initial_conditions:
         if complex_pattern.is_equivalent_to(other_cp):
             model.initial_conditions.remove((other_cp, other_value))
-    model.initial(complex_pattern,value)
-            
+    model.initial(complex_pattern, value)
 
 # Generate model rules via belpy
-_,model = rdf_to_pysb.rdf_to_pysb('../../data/RAS_combined.rdf')
+_, model = rdf_to_pysb.rdf_to_pysb('../../data/RAS_combined.rdf')
+
+# Useful shortcuts to access model components
+m = model.monomers
+p = model.parameters
 
 # Add ligand to the model
 model.add_component(Monomer('EGF'))
-model.add_component(Rule('EGF_activates_EGFR',model.monomers['EGF']()+model.monomers['EGFR']({'Kinase':'inactive'}) >> model.monomers['EGF']() + model.monomers['EGFR']({'Kinase':'active'}),model.parameters['kf_one_step_activate']))
-
+model.add_component(
+    Rule('EGF_activates_EGFR',
+         m['EGF']() + m['EGFR']({'Kinase':'inactive'}) >>
+         m['EGF']() + m['EGFR']({'Kinase':'active'}),
+         p['kf_one_step_activate']))
 
 # Add initial conditions
+# Values taken from Chen et al. (2009) Mol. Syst. Biol.
 model.initial_conditions = []
-
 model.add_component(Parameter('init_default',10000))
 model.add_component(Parameter('EGF_0',10000))
 model.add_component(Parameter('EGFR_0',70000))
@@ -44,30 +56,28 @@ model.add_component(Parameter('MAP2K1_0',3020000))
 model.add_component(Parameter('MAPK1_0',695000))
 model.add_component(Parameter('AKT_0',905000))
 
-add_initial(model,model.monomers['EGF'](),model.parameters['EGF_0'])
-add_initial(model,get_base_state(model.monomers['EGFR']),model.parameters['EGFR_0'])
-add_initial(model,get_base_state(model.monomers['SOS1']),model.parameters['SOS_0'])
-add_initial(model,get_base_state(model.monomers['HRAS']),model.parameters['RAS_0'])
-add_initial(model,get_base_state(model.monomers['NRAS']),model.parameters['RAS_0'])
-add_initial(model,get_base_state(model.monomers['KRAS']),model.parameters['RAS_0'])
-add_initial(model,get_base_state(model.monomers['ARAF']),model.parameters['RAF_0'])
-add_initial(model,get_base_state(model.monomers['BRAF']),model.parameters['RAF_0'])
-add_initial(model,get_base_state(model.monomers['RAF1']),model.parameters['RAF_0'])
-add_initial(model,get_base_state(model.monomers['MAP2K1']),model.parameters['MAP2K1_0'])
-add_initial(model,get_base_state(model.monomers['MAPK1']),model.parameters['MAPK1_0'])
-add_initial(model,model.monomers['RASA2'](Catalytic='active'),model.parameters['init_default'])
-add_initial(model,model.monomers['RASA3'](Catalytic='active'),model.parameters['init_default'])
+add_initial(model, m['EGF'](), p['EGF_0'])
+add_initial(model, get_base_state(m['EGFR']), p['EGFR_0'])
+add_initial(model, get_base_state(m['SOS1']), p['SOS_0'])
+add_initial(model, get_base_state(m['HRAS']), p['RAS_0'])
+add_initial(model, get_base_state(m['NRAS']), p['RAS_0'])
+add_initial(model, get_base_state(m['KRAS']), p['RAS_0'])
+add_initial(model, get_base_state(m['ARAF']), p['RAF_0'])
+add_initial(model, get_base_state(m['BRAF']), p['RAF_0'])
+add_initial(model, get_base_state(m['RAF1']), p['RAF_0'])
+add_initial(model, get_base_state(m['MAP2K1']), p['MAP2K1_0'])
+add_initial(model, get_base_state(m['MAPK1']), p['MAPK1_0'])
+add_initial(model, m['RASA2'](Catalytic='active'), p['init_default'])
+add_initial(model, m['RASA3'](Catalytic='active'), p['init_default'])
 
 # Add observables
-model.add_component(Observable("ERKact",model.monomers['MAPK1'](Kinase='active')))
-model.add_component(Observable("MEKact",model.monomers['MAP2K1'](Kinase='active')))
+model.add_component(Observable("ERKact", m['MAPK1'](Kinase='active')))
+model.add_component(Observable("MEKact", m['MAP2K1'](Kinase='active')))
 
 # Import solver and generate model equations
-from pysb.integrate import Solver
 t = np.linspace(0,25,11)
 solver = Solver(model,t)
 
-import pickle
-fh = open('RAS_combined_model.pkl','wb')
-pickle.dump(model,fh)
-fh.close()
+# Pickle the model
+with open('RAS_combined_model.pkl','wb') as fh:
+    pickle.dump(model,fh)
