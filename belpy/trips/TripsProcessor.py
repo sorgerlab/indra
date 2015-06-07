7e94f8352069d83afcacf8492d59b85e08bedf9f
Ben Gyori <ben.gyori@gmail.com>
2015-06-07 18:50:53 -0400

    Implemented phosphorylation processing

--- belpy/trips/TripsProcessor.py
+++ belpy/trips/TripsProcessor.py
@@ -6,6 +6,11 @@ from belpy.statements import *
 
 import xml.etree.ElementTree as ET
 
+residue_names = {
+    'SER':'Serine',
+    'THR':'Threonine',
+    'TYR':'Tyrosine'
+    }
 
 class TripsProcessor(object):
     def __init__(self, xml_string):
@@ -26,22 +31,60 @@ class TripsProcessor(object):
             arg2 = event.find("arg2")
             arg2_name = self.get_text(arg2)
             self.belpy_stmts.append(Complex((arg1_name,arg2_name)))
-    
+
+    def get_name_by_id(self,entity_id):
+        entity_term = self.tree.find("TERM/[@id='%s']" % entity_id)
+        name = entity_term.find("name")
+        return name.text
+
+    def get_site_by_id(self,site_id):
+        all_residues = []
+        all_pos = []
+        site_term = self.tree.find("TERM/[@id='%s']" % site_id)
+        subterms = site_term.find("subterms")
+        print subterms
+        if subterms:
+            for s in subterms.getchildren():
+                residue, pos = self.get_site_by_id(s.text)
+                all_residues.append(residue)
+                all_pos.append(pos)
+        else:
+            site_name = site_term.find("name")
+            # Example name: SER-222
+            residue, pos = site_name.text.split('-')
+            print residue, pos
+            return residue, pos
+        print all_residues
+        print all_pos
+        return all_residues, all_pos
+        
+
     def get_phosphorylation(self):
         phosphorylation_events = self.tree.findall("EVENT/[type='ONT::PHOSPHORYLATION']")
         for event in phosphorylation_events:
             sentence = self.get_text(event)
             agent = event.find(".//*[@role=':AGENT']")
-            agent_name = self.get_text(agent)
+            agent_name = self.get_name_by_id(agent.attrib['id'])
             affected = event.find(".//*[@role=':AFFECTED']")
-            affected_name = self.get_text(affected)
+            affected_name = self.get_name_by_id(affected.attrib['id'])
+            site_tag = event.find("site")
+            site_id = site_tag.attrib['id']
+            residues, mod_pos = self.get_site_by_id(site_id)
             mod = 'Phosphorylation'
-            # TODO: query site once the TRIPS output is fixed
-            site = ""
+            mod = ['Phospho'+residue_names[r] for r in residues]
             # TODO: extract more information about text to use as evidence
             citation = ''
             evidence = sentence
             annotations = ''
-            self.belpy_stmts.append(Phosphorylation(agent_name,affected_name,
-                                    mod,site,sentence,citation,evidence,annotations))
-        
+            # Assuming that multiple modifications can only happen in 
+            # distinct steps, we add a statement for each modification
+            # independently
+            for m,p in zip(mod, mod_pos):
+                self.belpy_stmts.append(Phosphorylation(agent_name,affected_name,
+                                    m,p,sentence,citation,evidence,annotations))
+    
+if __name__ == '__main__':
+    tp = TripsProcessor(open('phosphosite.xml','rt').read())
+    tp.get_phosphorylation()
+
+
