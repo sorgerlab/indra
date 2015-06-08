cc9e9a3a72ee3022cd086313d92718bd325b3cf0
Ben Gyori <ben.gyori@gmail.com>
2015-06-08 16:21:12 -0400

    Added HGNC client

---
+++ belpy/databases/hgnc_client.py
@@ -0,0 +1,11 @@
+import urllib
+import urllib2
+
+hgnc_url = 'http://www.genenames.org/cgi-bin/gene_symbol_report'
+
+def get_hgnc_entry(hgnc_id):
+    hgnc_args = urllib.urlencode({'hgnc_id': 'HGNC:%s' % hgnc_id})
+    req = urllib2.Request(hgnc_url, hgnc_args)
+    res = urllib2.urlopen(req)
+    html = res.read()
+    return html
