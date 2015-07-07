54adaed9e6e939ac4416db8cf441d419af2b86b0
Ben Gyori <ben.gyori@gmail.com>
2015-07-07 13:54:55 -0400

    Update model text and run

--- models/pysba_demo/run_model.py
+++ models/pysba_demo/run_model.py
@@ -1,12 +1,20 @@
 import pickle
 import numpy
+import time
 from pysb.integrate import Solver
 from belpy.pysb_assembler import *
 
 
 if __name__ == '__main__':
     pa = PysbAssembler()
-    tp = pickle.load(open('model_text_tp.pkl'))
+    use_pickle = False
+    if use_pickle:
+        tp = pickle.load(open('model_text_tp.pkl'))
+    else:
+        tstart = time.time()
+        tp = trips_api.process_text(open('model_text.txt').read())
+        tend = time.time()
+        print 'TRIPS parser took %d seconds.' % (tend - tstart)
     bp = biopax_api.process_owl('DUSP.owl')
     bp.get_dephosphorylation()
     pa.add_statements(tp.belpy_stmts)
