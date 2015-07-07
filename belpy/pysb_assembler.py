b1d5b18ebd5baaf1c3ac7dfb7586e40c98c6bc97
Ben Gyori <ben.gyori@gmail.com>
2015-07-07 16:18:22 -0400

    BaseAgents in pysb_assembler instead of Agents
    BaseAgents are used in the assembly process as a staging ground
    for Monomers, wherease Agents are used as entities involved in
    statements. Therefore BaseAgents are defined in pysb_assembler.py
    whereas Agents are defined in statements.py

--- belpy/pysb_assembler.py
+++ belpy/pysb_assembler.py
@@ -6,8 +6,8 @@ from trips import trips_api
 
 SelfExporter.do_export = False
 
-class AgentSet(object):
-    """A container for a set of Agents. Wraps a dict of Agent instances."""
+class BaseAgentSet(object):
+    """A container for a set of BaseAgents. Wraps a dict of BaseAgent instances."""
     def __init__(self):
         self.agents = {}
 
@@ -16,7 +16,7 @@ class AgentSet(object):
         try:
             agent = self.agents[name]
         except KeyError:
-            agent = Agent(name)
+            agent = BaseAgent(name)
             self.agents[name] = agent
         return agent
 
@@ -25,6 +25,37 @@ class AgentSet(object):
 
     def __getitem__(self, name):
         return self.agents[name]
+
+class BaseAgent(object):
+    def __init__(self, name):
+        self.name = name
+        self.sites = []
+        self.site_states = {}
+        # The list of site/state configurations that lead to this agent
+        # being active (where the agent is currently assumed to have only
+        # one type of activity)
+        self.activating_mods = []
+
+    def create_site(self, site, states=None):
+        """Create a new site on an agent if it doesn't already exist"""
+        if site not in self.sites:
+            self.sites.append(site)
+        if states is not None:
+            self.site_states.setdefault(site, [])
+            try:
+                states = list(states)
+            except TypeError:
+                return
+            self.add_site_states(site, states)
+
+    def add_site_states(self, site, states):
+        """Create new states on a agent site if the site doesn't exist"""
+        for state in states:
+            if state not in self.site_states[site]:
+                self.site_states[site].append(state)
+
+    def add_activating_modification(self, activity_pattern):
+        self.activating_mods.append(activity_pattern)
 
 def add_default_initial_conditions(model):
     try:
@@ -57,12 +88,12 @@ class PysbAssembler(object):
         model = Model()
         # Keep track of which policies we're using
         self.policies = policies
-        self.agent_set = AgentSet()
+        self.agent_set = BaseAgentSet()
         # Collect information about the monomers/self.agent_set from the
         # statements
         for stmt in self.statements:
             stmt.monomers(self.agent_set, policies=policies)
-        # Add the monomers to the model based on our AgentSet
+        # Add the monomers to the model based on our BaseAgentSet
         for agent_name, agent in self.agent_set.iteritems():
             m = Monomer(agent_name, agent.sites, agent.site_states)
             model.add_component(m)
