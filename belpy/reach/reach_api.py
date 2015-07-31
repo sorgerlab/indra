7270f8459db8ea822677de6773fbade415bc33fe
Ben Gyori <ben.gyori@gmail.com>
2015-07-31 14:10:09 -0400

    Process NXML file with REACH

--- belpy/reach/reach_api.py
+++ belpy/reach/reach_api.py
@@ -1,7 +1,25 @@
+import os
 import json
-
+import tempfile
+from belpy.java_vm import autoclass, JavaException
 from processor import ReachProcessor
 
+_nxml_fries_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                   '../../../../groups/surdeanu/nxml2fries/nxml2fries')
+
+def process_nxml(file_name):
+    base = os.path.basename(file_name)
+    file_id = os.path.splitext(base)[0]
+    tmp_dir = tempfile.mkdtemp()
+    try:
+        paper_reader = autoclass('edu.arizona.sista.bionlp.ReadPaper')
+        paper_reader.main([file_name, tmp_dir, _nxml_fries_path])
+    except JavaException:
+        print 'Could not process file %s.' % file_name
+        return None
+    
+    json_file_name = os.path.join(tmp_dir, file_id + '.uaz.events.json')
+    return process_json_file(json_file_name)
 
 def process_json_file(file_name):
     try:
