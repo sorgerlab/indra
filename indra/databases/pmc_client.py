b563c039c1d48b82c154d0f85b002a0c70bfc20e
Ben Gyori <ben.gyori@gmail.com>
2016-01-24 13:44:42 -0500

    Change dependencies in PMC client and add tests

--- indra/databases/pmc_client.py
+++ indra/databases/pmc_client.py
@@ -1,6 +1,6 @@
 import urllib
 import urllib2
-import lxml.etree as et
+import xml.etree.ElementTree as et
 
 def get_xml(pmc_id):
     pmc_url = 'http://www.ncbi.nlm.nih.gov/pmc/oai/oai.cgi'
@@ -27,7 +27,7 @@ def get_xml(pmc_id):
 def check_xml_error(xml_str):
     tree = et.fromstring(xml_str)
     xmlns = "http://www.openarchives.org/OAI/2.0/"
-    err_tag = tree.find(et.QName(xmlns, 'error'))
+    err_tag = tree.find('{%s}error' % xmlns)
     if err_tag is not None:
         err_code = err_tag.attrib['code']
         err_text = err_tag.text
---
+++ indra/tests/test_pmc_client.py
@@ -0,0 +1,11 @@
+from indra.databases import pmc_client
+
+def test_get_xml():
+    pmc_id = '4322985'
+    xml_str = pmc_client.get_xml(pmc_id)
+    assert(xml_str is not None)
+
+def test_get_xml_invalid():
+    pmc_id = '9999999'
+    xml_str = pmc_client.get_xml(pmc_id)
+    assert(xml_str is None)
