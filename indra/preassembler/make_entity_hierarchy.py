d5cd680faff3740e52299d2b0fd0b339311a6589
Ben Gyori <ben.gyori@gmail.com>
2016-02-12 00:21:10 -0500

    Add initial entity hierarchy

---
+++ indra/preassembler/make_entity_hierarchy.py
@@ -0,0 +1,29 @@
+import sys
+from rdflib import Graph, Namespace, Literal
+import csv
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        proteins_file = sys.argv[1]
+    else:
+        proteins_file = '../../data/ras_pathway_proteins.csv'
+    rn = Namespace('indra/relations/')
+    en = Namespace('indra/entities/')
+    g = Graph()
+
+    has_name = rn.term('hasName')
+    has_synonym = rn.term('hasSynonym')
+    isa = rn.term('isa')
+
+    with open(proteins_file) as tsv:
+        for line in csv.reader(tsv, dialect="excel-tab"):
+            symbol, name, synonyms_str, family = line
+            g.add((en.term(symbol), has_name, Literal(name)))
+            synonyms = synonyms_str.split(', ')
+            for s in synonyms:
+                g.add((en.term(symbol), has_synonym, Literal(s)))
+            if family.strip() != '':
+                g.add((en.term(symbol), isa, en.term(family)))
+    with open('entity_hierarchy.rdf', 'wt') as out_file:
+        out_file.write(g.serialize(format='xml'))
+    
