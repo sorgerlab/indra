5149d87a8381c986cc92d52b9bbe10f0e68f2578
Ben Gyori <ben.gyori@gmail.com>
2016-02-12 19:33:56 -0500

    Processing grounding and ActivityActivity in REACH

--- indra/reach/processor.py
+++ indra/reach/processor.py
@@ -21,14 +21,6 @@ class ReachProcessor(object):
         self.tree = objectpath.Tree(json_dict)
         self.statements = []
     
-    def _get_agent_name(self, txt):
-        '''
-        Produce valid agent name from string.
-        '''
-        name = txt.replace('-', '_')
-        name = name.replace(' ', '_')
-        return name
-
     def get_phosphorylation(self):
         citation = self.tree.execute("$.events.frames.object_meta.doc_id")
         qstr = "$.events.frames[(@.type is 'protein-modification') " + \
@@ -43,7 +35,7 @@ class ReachProcessor(object):
 
             for a in args:
                 if a['argument_label'] == 'theme':
-                    theme = a['text']
+                    theme = a['arg']
                 elif a['argument_label'] == 'site':
                     site = a['text']
             qstr = "$.events.frames[(@.type is 'regulation') and " + \
@@ -53,14 +45,14 @@ class ReachProcessor(object):
             for reg in reg_res:
                 for a in reg['arguments']:
                     if a['argument_label'] == 'controller':
-                        controller = a['text']
+                        controller = a['arg']
 
             if controller is None:
                 warnings.warn('Skipping phosphorylation with missing controller.')
                 continue
 
-            controller_agent = Agent(self._get_agent_name(controller))
-            theme_agent = Agent(self._get_agent_name(theme))
+            controller_agent = self._get_agent_from_entity(controller)
+            theme_agent = self._get_agent_from_entity(theme)
             mod = 'Phosphorylation'
             if site is not None:
                 residue, pos = self._parse_site_text(site)
@@ -85,11 +77,58 @@ class ReachProcessor(object):
             sentence = r['verbose-text']
             members = []
             for a in args:
-                agent = Agent(self._get_agent_name(a['text']))
+                agent = self._get_agent_from_entity(a['arg'])
                 members.append(agent)
             ev = Evidence(source_api='reach', text=sentence, pmid=citation)
             self.statements.append(Complex(members, ev))
+   
+    def get_activation(self):
+        citation = self.tree.execute("$.events.frames.object_meta.doc_id")
+        qstr = "$.events.frames[@.type is 'activation']"
+        res = self.tree.execute(qstr)
+        for r in res:
+            sentence = r['verbose-text']
+            ev = Evidence(source_api='reach', text=sentence, pmid=citation)
+            frame_id = r['frame_id']
+            args = r['arguments']
+            for a in args:
+                if a['argument_label'] == 'controller':
+                    controller = a['arg']
+                if a['argument_label'] == 'controlled':
+                    controlled = a['arg']
+            controller_agent = self._get_agent_from_entity(controller)
+            controlled_agent = self._get_agent_from_entity(controlled)
+            if r['subtype'] == 'positive-activation':
+                rel = 'increases'
+            else:
+                rel = 'decreases'
+            st = ActivityActivity(controller_agent, 'Activity', rel,
+                controlled_agent, 'Activity', ev)
+            self.statements.append(st)
     
+    def _get_agent_from_entity(self, entity_id):
+        qstr = "$.entities.frames[(@.frame_id is \'%s\')]" % entity_id
+        res = self.tree.execute(qstr)
+        if res is None:
+            return None
+        entity_term = res.next()
+        name = self._get_agent_name(entity_term['text'])
+        db_refs = {}
+        for xr in entity_term['xrefs']:
+            if xr['namespace'] == 'uniprot':
+                db_refs['UP'] = xr['id']
+        agent = Agent(name, db_refs=db_refs)
+        return agent
+    
+    def _get_agent_name(self, txt):
+        '''
+        Produce valid agent name from string.
+        '''
+        name = txt.replace('-', '_')
+        name = name.replace(' ', '_')
+        return name
+
+
     def _parse_site_text(self, s):
         m = re.match(r'([TYS])[-]?([0-9]+)', s)
         if m is not None:
