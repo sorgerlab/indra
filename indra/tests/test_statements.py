f3af3dbc2339fd4276a20ad904af848e5d58750a
John Bachman <bachmanjohn@gmail.com>
2016-02-13 21:58:41 -0500

    Add addnl matches tests for all stmts

--- indra/tests/test_statements.py
+++ indra/tests/test_statements.py
@@ -1,5 +1,7 @@
 from indra.statements import *
 
+# Checking for exact matching (except Evidence) between Agents/stmts ---------
+
 def test_matches():
     ras = Agent('Ras')
     raf = Agent('Raf')
@@ -13,7 +15,6 @@ def test_matches_key():
     st1 = Complex([ras, raf])
     st2 = Complex([ras, raf])
     assert(st1.matches_key() == st2.matches_key())
-
 
 def test_matches2():
     raf = Agent('Raf')
@@ -123,6 +124,110 @@ def test_matches_key_bound_multiple_order():
                                             BoundCondition(Agent('RAF1'), True)])
     assert(hras1.matches_key() == hras2.matches_key())
 
+# Check matches implementations for all statement types ---------------------
+def test_matches_selfmod():
+    """Test matching of entities only, entities match only on name."""
+    nras1 = Agent('NRAS', db_refs = {'HGNC': '7989'})
+    nras2 = Agent('NRAS', db_refs = {'HGNC': 'dummy'})
+    st1 = Autophosphorylation(nras1, 'PhosphorylationTyrosine', '32',
+                          evidence=Evidence(text='foo'))
+    st2 = Autophosphorylation(nras1, 'PhosphorylationTyrosine', '32',
+                          evidence=Evidence(text='bar'))
+    st3 = Autophosphorylation(nras2, 'Phosphorylation', None,
+                          evidence=Evidence(text='bar'))
+    assert(st1.matches(st2))
+    assert(not st1.matches(st3))
+
+def test_matches_activityactivity():
+    """Test matching of entities only, entities match only on name."""
+    src = Agent('SRC', db_refs = {'HGNC': '11283'})
+    nras1 = Agent('NRAS', db_refs = {'HGNC': '7989'})
+    nras2 = Agent('NRAS', db_refs = {'HGNC': 'dummy'})
+    st1 = ActivityActivity(src, 'Kinase1', 'increases1',
+                           nras1, 'GtpBoundActivity1',
+                           evidence=Evidence(text='foo'))
+    st2 = ActivityActivity(src, 'Kinase1', 'increases1',
+                           nras1, 'GtpBoundActivity1',
+                           evidence=Evidence(text='bar'))
+    st3 = ActivityActivity(src, 'Kinase2', 'increases2',
+                           nras2, 'GtpBoundActivity2', 
+                           evidence=Evidence(text='bar'))
+    assert(st1.matches(st2))
+    assert(not st1.matches(st3))
+
+def test_matches_activitymod():
+    """Test matching of entities only, entities match only on name."""
+    nras1 = Agent('NRAS', db_refs = {'HGNC': '7989'})
+    nras2 = Agent('NRAS', db_refs = {'HGNC': 'dummy'})
+    st1 = ActivityModification(nras1, 'PhosphorylationTyrosine', '32',
+                               'increases1', 'GtpBoundActivity1',
+                               evidence=Evidence(text='foo'))
+    st2 = ActivityModification(nras1, 'PhosphorylationTyrosine', '32',
+                               'increases1', 'GtpBoundActivity1',
+                               evidence=Evidence(text='bar'))
+    st3 = ActivityModification(nras2, 'Phosphorylation', None,
+                               'increases2', 'GtpBoundActivity2',
+                               evidence=Evidence(text='bar'))
+    assert(st1.matches(st2))
+    assert(not st1.matches(st3))
+
+def test_matches_activatingsub():
+    """Test matching of entities only, entities match only on name."""
+    nras1 = Agent('NRAS', db_refs = {'HGNC': '7989'})
+    nras2 = Agent('NRAS', db_refs = {'HGNC': 'dummy'})
+    st1 = ActivatingSubstitution(nras1, 'G', '12', 'D', 'GtpBoundActivity1',
+                                 'increases1', evidence=Evidence(text='foo'))
+    st2 = ActivatingSubstitution(nras1, 'G', '12', 'D', 'GtpBoundActivity1',
+                                 'increases1', evidence=Evidence(text='bar'))
+    st3 = ActivatingSubstitution(nras2, 'Q', '61', 'L', 'GtpBoundActivity2',
+                                 'increases2', evidence=Evidence(text='bar'))
+    assert(st1.matches(st2))
+    assert(not st1.matches(st3))
+
+def test_matches_rasgef():
+    """Test matching of entities only, entities match only on name."""
+    sos1 = Agent('SOS1', db_refs = {'HGNC': 'sos1'})
+    sos2 = Agent('SOS1', db_refs = {'HGNC': 'sos2'})
+    nras1 = Agent('NRAS', db_refs = {'HGNC': '7989'})
+    nras2 = Agent('NRAS', db_refs = {'HGNC': 'dummy'})
+    st1 = RasGef(sos1, 'GtpBoundActivity1', nras1,
+                 evidence=Evidence(text='foo'))
+    st2 = RasGef(sos1, 'GtpBoundActivity1', nras1,
+                 evidence=Evidence(text='bar'))
+    st3 = RasGef(sos2, 'GtpBoundActivity2', nras2,
+                 evidence=Evidence(text='bar'))
+    assert(st1.matches(st2))
+    assert(not st1.matches(st3))
+
+def test_matches_rasgap():
+    """Test matching of entities only, entities match only on name."""
+    rasa1 = Agent('RASA1', db_refs = {'HGNC': 'rasa1'})
+    rasa2 = Agent('RASA1', db_refs = {'HGNC': 'rasa2'})
+    nras1 = Agent('NRAS', db_refs = {'HGNC': '7989'})
+    nras2 = Agent('NRAS', db_refs = {'HGNC': 'dummy'})
+    st1 = RasGap(rasa1, 'GtpBoundActivity1', nras1,
+                 evidence=Evidence(text='foo'))
+    st2 = RasGap(rasa1, 'GtpBoundActivity1', nras1,
+                 evidence=Evidence(text='bar'))
+    st3 = RasGap(rasa2, 'GtpBoundActivity2', nras2,
+                 evidence=Evidence(text='bar'))
+    assert(st1.matches(st2))
+    assert(not st1.matches(st3))
+
+def test_matches_complex():
+    """Test matching of entities only, entities match only on name."""
+    ksr1 = Agent('KSR1', db_refs = {'HGNC': 'ksr1'})
+    ksr2 = Agent('KSR1', db_refs = {'HGNC': 'ksr2'})
+    braf1 = Agent('BRAF', db_refs = {'HGNC': 'braf1'})
+    braf2 = Agent('BRAF', db_refs = {'HGNC': 'braf2'})
+    map2k1 = Agent('MAP2K1', db_refs = {'HGNC': 'map2k1'})
+    map2k2 = Agent('MAP2K1', db_refs = {'HGNC': 'map2k2'})
+    st1 = Complex([ksr1, braf1, map2k1], evidence=Evidence(text='foo'))
+    st2 = Complex([ksr1, braf1, map2k1], evidence=Evidence(text='bar'))
+    assert(st1.matches(st2))
+
+
+# Entity matching between statements ----------------------------------------
 def test_agent_entity_match():
     """Agents match only on name."""
     nras1 = Agent('NRAS', db_refs = {'HGNC': '7989'})
@@ -220,4 +325,9 @@ def test_entities_match_complex():
     st1 = Complex([ksr1, braf1, map2k1], evidence=Evidence(text='foo'))
     st2 = Complex([ksr2, braf2, map2k2], evidence=Evidence(text='bar'))
     assert(st1.entities_match(st2))
+
+# TODO expand tests to also check for things that should NOT match (different
+# agent names)
+
+# TODO Expand match tests to 
 
