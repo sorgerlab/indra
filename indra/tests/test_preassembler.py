6fe5f4e2688275b708f67c616796372d5765c6ff
John Bachman <bachmanjohn@gmail.com>
2016-02-15 15:22:29 -0500

    Added tests for support relations based on refinements

--- indra/tests/test_preassembler.py
+++ indra/tests/test_preassembler.py
@@ -54,27 +54,62 @@ def test_combine_duplicates():
     assert(len(pa.unique_stmts[2].evidence) == 3)
     assert(len(pa.unique_stmts[3].evidence) == 1)
 
-if __name__ == '__main__':
-
+def test_superfamily_refinement():
+    """A gene-level statement should be supported by a family-level
+    statement."""
     src = Agent('SRC', db_refs = {'HGNC': '11283'})
     ras = Agent('RAS', db_refs = {'FA': '03663'})
-    gtp = Agent('GTP', db_refs = {'CHEBI': '15996'})
-    rasgtp = Agent('RAS', db_refs = {'FA': '03663'}, 
-        bound_conditions=[BoundCondition(gtp, True)])
     nras = Agent('NRAS', db_refs = {'HGNC': '7989'})
-    nrasgtp = Agent('NRAS', db_refs = {'HGNC': '7989'}, 
-        bound_conditions=[BoundCondition(gtp, True)])
-    st1 = Phosphorylation(src, rasgtp, 'PhosphorylationTyrosine', '32')
+    st1 = Phosphorylation(src, ras, 'PhosphorylationTyrosine', '32')
     st2 = Phosphorylation(src, nras, 'PhosphorylationTyrosine', '32')
-    st3 = Phosphorylation(src, nrasgtp, 'Phosphorylation', None)
-    pa = Preassembler([st1, st2, st3])
-    pa.assemble()
-    """
-    stmts = pa.unique_stmts
+    stmts = Preassembler.combine_related([st1, st2])
+    # The top-level list should contain only one statement, the gene-level
+    # one, supported by the family one.
     assert(len(stmts) == 1)
-    assert(stmts[0].enz.name == 'SRC')
-    assert(stmts[0].sub.name == 'NRAS')
-    assert(stmts[0].mod == 'PhosphorylationTyrosine')
-    assert(stmts[0].mod_pos == '32')
-    """
+    assert (stmts[0] == st2)
+    assert (stmts[0].supported_by == [st1])
+
+def test_modification_refinement():
+    """A more specific modification statement should be supported by a more
+    generic modification statement."""
+    src = Agent('SRC', db_refs = {'HGNC': '11283'})
+    nras = Agent('NRAS', db_refs = {'HGNC': '7989'})
+    st1 = Phosphorylation(src, nras, 'PhosphorylationTyrosine', '32')
+    st2 = Phosphorylation(src, nras, 'Phosphorylation', None)
+    stmts = Preassembler.combine_related([st1, st2])
+    # The top-level list should contain only one statement, the more specific
+    # modification, supported by the less-specific modification.
+    assert(len(stmts) == 1)
+    assert (stmts[0] == st1)
+    assert (stmts[0].supported_by == [st2])
+
+def test_bound_condition_refinement():
+    """A statement with more specific bound context should be supported by a
+    less specific statement."""
+    src = Agent('SRC', db_refs = {'HGNC': '11283'})
+    gtp = Agent('GTP', db_refs = {'CHEBI': '15996'})
+    nras = Agent('NRAS', db_refs = {'HGNC': '7989'})
+    nrasgtp = Agent('NRAS', db_refs = {'HGNC': '7989'},
+        bound_conditions=[BoundCondition(gtp, True)])
+    st1 = Phosphorylation(src, nras, 'PhosphorylationTyrosine', '32')
+    st2 = Phosphorylation(src, nrasgtp, 'PhosphorylationTyrosine', '32')
+    # The top-level list should contain only one statement, the more specific
+    # modification, supported by the less-specific modification.
+    stmts = Preassembler.combine_related([st1, st2])
+    assert(len(stmts) == 1)
+    assert (stmts[0] == st2)
+    assert (stmts[0].supported_by == [st1])
+
+def test_mod_sites_refinement():
+    """A statement with more specific modification context should be supported
+    by a less-specific statement."""
+    # TODO
+    assert True
+
+def test_binding_site_refinement():
+    """A statement with information about a binding site for an interaction
+    between two proteins should be supported by a statement without this
+    information."""
+    # TODO
+    assert True
 
