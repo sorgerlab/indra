c90b1127f1a8a7880af9efea958338bf23f49f05
Ben Gyori <ben.gyori@gmail.com>
2016-02-16 11:40:22 -0500

    Match by name and synonyms in hierarchy manager and add BEL family names

--- indra/preassembler/hierarchy_manager.py
+++ indra/preassembler/hierarchy_manager.py
@@ -11,15 +11,39 @@ class HierarchyManager(object):
         self.graph = rdflib.Graph()
         self.graph.parse(rdf_file)
 
+    def find_entity(self, x):
+        qstr = self.prefixes + """
+            SELECT ?x WHERE {{
+                {{
+                {{ ?x rn:hasName "{0}" . }}
+                UNION
+                {{ ?x rn:hasSynonym "{0}" . }}
+                }}
+            }}
+            """.format(x)
+        res = self.graph.query(qstr)
+        if list(res):
+            en = list(res)[0][0].toPython()
+            return en
+        else:
+            return None
+        
     def isa(self, t1, t2):
         """Both t1 and t2 are entities and we determine whether there is a
         series of "isa" edges from t1 to t2.
         """
+
+        en1 = self.find_entity(t1)
+        en2 = self.find_entity(t2)
+
+        if en1 is None or en2 is None:
+            return None
+
         qstr = self.prefixes + """ 
             SELECT (COUNT(*) as ?s) WHERE {{
-                en:{} rn:isa+ en:{} .
+                <{}> rn:isa+ <{}> .
                 }}
-            """.format(t1, t2)
+            """.format(en1, en2)
         res = self.graph.query(qstr)
         count = [r[0] for r in res][0]
         if count.toPython() == 1:
--- indra/preassembler/make_entity_hierarchy.py
+++ indra/preassembler/make_entity_hierarchy.py
@@ -1,6 +1,7 @@
 import sys
 from rdflib import Graph, Namespace, Literal
 import csv
+import urllib2
 
 if __name__ == '__main__':
     indra_ns = 'http://sorger.med.harvard.edu/indra/'
@@ -13,18 +14,42 @@ if __name__ == '__main__':
     g = Graph()
 
     has_name = rn.term('hasName')
+    has_long_name = rn.term('hasLongName')
     has_synonym = rn.term('hasSynonym')
     isa = rn.term('isa')
 
+    # Read BEL family names
+    res = urllib2.urlopen('http://resource.belframework.org/belframework/'+\
+        'latest-release/namespace/selventa-protein-families.belns')
+    belns_text = res.read()
+    start = belns_text.find('[Values]')
+    lines = belns_text[start:].split('\n')
+    bel_family_names = []
+    for l in lines:
+        if l.endswith(' Family|P'):
+            family_name = l[:-2].replace(' ', '_').replace('/', '_')
+            bel_family_names.append(family_name)
+    
+    family_names = set([])
     with open(proteins_file) as tsv:
         for line in csv.reader(tsv, dialect="excel-tab"):
             symbol, name, synonyms_str, family = line
-            g.add((en.term(symbol), has_name, Literal(name)))
+            g.add((en.term(symbol), has_long_name, Literal(name)))
+            g.add((en.term(symbol), has_name, Literal(symbol)))
             synonyms = synonyms_str.split(', ')
             for s in synonyms:
                 g.add((en.term(symbol), has_synonym, Literal(s)))
-            if family.strip() != '':
-                g.add((en.term(symbol), isa, en.term(family)))
+            family_name = family.strip()
+            if family_name != '':
+                g.add((en.term(symbol), isa, en.term(family_name)))
+                g.add((en.term(family), has_name, Literal(family_name)))
+                family_names.add(family_name)
+    for fn in family_names:
+        bel_synonyms = [bn for bn in bel_family_names 
+                        if bn.find(fn) != -1]
+        for b in bel_synonyms:
+            g.add((en.term(fn), has_synonym, Literal(b.upper())))
+
     with open('entity_hierarchy.rdf', 'wt') as out_file:
         out_file.write(g.serialize(format='xml'))
     
