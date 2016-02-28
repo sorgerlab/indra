408ff6918afd9b6eeddaf0c78a4a99b12f1053b7
Ben Gyori <ben.gyori@gmail.com>
2016-02-27 21:41:50 -0500

    Agent construction reads modifications in BioPAX

--- indra/biopax/processor.py
+++ indra/biopax/processor.py
@@ -116,7 +116,7 @@ class BiopaxProcessor(object):
                 ma = self._get_agents_from_entity(m)
                 members.append(ma)
         return members
-
+    
     @staticmethod
     def _get_modification_site(modPE):
         # Do we need to look at EntityFeatures?
@@ -299,8 +299,62 @@ class BiopaxProcessor(object):
         # references
         name = self._get_entity_name(bpe)
         db_refs = self._get_db_refs(bpe)
-        agent = Agent(name, db_refs=db_refs)
+        mods, mod_sites = self._get_entity_mods(bpe)
+        agent = Agent(name, db_refs=db_refs, mods=mods, mod_sites=mod_sites)
         return agent
+
+    def _get_entity_mods(self, bpe):
+        """Get all the modifications of an entity in INDRA format"""
+        feats = bpe.getFeature().toArray()
+        mod_types = []
+        mod_poss = []
+        for f in feats:
+            if is_modification(f):
+                mod_type, mod_pos = self._extract_mod_from_feature(f)
+                if mod_type is not None:
+                    if mod_type == 'Active':
+                        # Skip activity as a modification state for now
+                        continue
+                    mod_types.append(mod_type)
+                    mod_poss.append(mod_pos)
+        return mod_types, mod_poss
+    
+    @staticmethod
+    def _extract_mod_from_feature(mf):
+        """Extract the type of modification and the position from
+        a ModificationFeature object in the INDRA format."""
+        # ModificationFeature / SequenceModificationVocabulary
+        mf_type = mf.getModificationType()
+        if mf_type is None:
+            warnings.warn('Modification type missing for  %s' % mf.getUri())
+            return None, None
+        if len(mf_type.getTerm().toArray()) != 1:
+            warnings.warn('Other than one modification term')
+        mf_type = mf_type.getTerm().toArray()[0]
+        try:
+            mod_type = BiopaxProcessor._mftype_dict[mf_type]
+        except KeyError:
+            warnings.warn('Unknown modification type %s' % mf_type)
+            return None, None
+
+        # getFeatureLocation returns SequenceLocation, which is the
+        # generic parent class of SequenceSite and SequenceInterval.
+        # Here we need to cast to SequenceSite in order to get to
+        # the sequence position.
+        mf_pos = mf.getFeatureLocation()
+        if mf_pos is not None:
+            mf_site = cast(bp('SequenceSite'), mf_pos)
+            mf_pos_status = mf_site.getPositionStatus()
+            if mf_pos_status is None:
+                mod_pos = None
+            elif mf_pos_status and mf_pos_status.toString() != 'EQUAL':
+                warnings.warn('Modification site position is %s' %
+                              mf_pos_status.toString())
+            else:
+                mod_pos = mf_site.getSequencePosition()
+        else:
+            mod_pos = None
+        return mod_type, mod_pos
 
     def _get_db_refs(self, bpe):
         if is_protein(bpe):
@@ -347,7 +401,12 @@ class BiopaxProcessor(object):
         xrefs = bp_entref.getXref().toArray()
         uniprot_refs = [x for x in xrefs if x.getDb() == 'UniProt Knowledgebase']
         uniprot_ids = [r.getId() for r in uniprot_refs]
-        return uniprot_ids
+        if not uniprot_ids:
+            return None
+        elif len(uniprot_ids) == 1:
+            return uniprot_ids[0]
+        else:
+            return uniprot_ids
 
     @staticmethod
     def _get_hgnc_id(bpe):
@@ -390,7 +449,8 @@ class BiopaxProcessor(object):
         'O-phospho-L-serine': 'PhosphorylationSerine',
         'O-phospho-L-threonine': 'PhosphorylationThreonine',
         'O-phospho-L-tyrosine': 'PhosphorylationTyrosine',
-        'O4\'-phospho-L-tyrosine': 'PhosphorylationTyrosine'
+        'O4\'-phospho-L-tyrosine': 'PhosphorylationTyrosine',
+        'residue modification, active': 'Active'
         }
 
 # Functions for accessing frequently used java classes with shortened path
@@ -449,6 +509,12 @@ def is_small_molecule(pe):
     """Return True if the physical entity is a small molecule"""
     val = isinstance(pe, bp('SmallMolecule')) or \
             isinstance(pe, bpimpl('SmallMolecule'))
+    return val
+
+def is_modification(fe):
+    """Return True if the feature is a modification"""
+    val = isinstance(fe, bp('ModificationFeature')) or \
+            isinstance(fe, bpimpl('ModificationFeature'))
     return val
 
 def listify(lst):
