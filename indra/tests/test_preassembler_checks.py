3d26cd5d71415552509704629c7578ffb6177725
Ben Gyori <ben.gyori@gmail.com>
2016-03-29 12:32:42 -0400

    Update preassembler checks and tests

--- indra/tests/test_preassembler_checks.py
+++ indra/tests/test_preassembler_checks.py
@@ -1,78 +1,90 @@
 from indra.preassembler import check_statements, check_sequence
 from indra.preassembler import check_agent_mod
-from indra.statements import Phosphorylation, Agent
+from indra.statements import Phosphorylation, Agent, ModCondition
 
 def test_check_agent_mod_args():
     a = Agent('MAPK1', db_refs = {'UP': 'P28482'})
-    failures = check_agent_mod(a, ['PhosphorylationThreonine'], [185])
+    mc = ModCondition('phosphorylation', 'threonine', '185')
+    failures = check_agent_mod(a, [mc])
     assert(not failures)
 
 def test_check_agent_mod_multiple():
-    a = Agent('MAPK1', mods=['PhosphorylationThreonine', 'PhosphorylationTyrosine'],
-              mod_sites=[185, 187], db_refs = {'UP': 'P28482'})
+    mc1 = ModCondition('phosphorylation', 'threonine', '185')
+    mc2 = ModCondition('phosphorylation', 'tyrosine', '187')
+    a = Agent('MAPK1', mods=[mc1, mc2], db_refs = {'UP': 'P28482'})
     failures = check_agent_mod(a)
     assert(not failures)
 
 def test_check_agent_mod_wrong():
-    a = Agent('MAPK1', mods=['PhosphorylationThreonine'],
-              mod_sites=[186], db_refs = {'UP': 'P28482'})
+    mc = ModCondition('phosphorylation', 'threonine', '186')
+    a = Agent('MAPK1', mods=[mc], db_refs = {'UP': 'P28482'})
     failures = check_agent_mod(a)
     assert(failures)
 
 def test_check_agent_mod_wrong_multiple():
-    a = Agent('MAPK1', mods=['PhosphorylationThreonine', 'PhosphorylationTyrosine'],
-              mod_sites=[184, 186], db_refs = {'UP': 'P28482'})
+    mc1 = ModCondition('phosphorylation', 'threonine', '184')
+    mc2 = ModCondition('phosphorylation', 'tyrosine', '186')
+    a = Agent('MAPK1', mods=[mc1, mc2], db_refs = {'UP': 'P28482'})
     failures = check_agent_mod(a)
     assert(failures)
 
 def test_check_agent_mod_wrong_multiple2():
-    a = Agent('MAPK1', mods=['PhosphorylationThreonine', 'PhosphorylationTyrosine'],
-              mod_sites=[185, 186], db_refs = {'UP': 'P28482'})
+    mc1 = ModCondition('phosphorylation', 'threonine', '185')
+    mc2 = ModCondition('phosphorylation', 'tyrosine', '186')
+    a = Agent('MAPK1', mods=[mc1, mc2], db_refs = {'UP': 'P28482'})
     failures = check_agent_mod(a)
     assert(failures)
 
 def test_check_sequence_phos():
     s = Agent('MAPK1', db_refs = {'UP': 'P28482'})
     e = Agent('MAP2K1')
-    stmt = Phosphorylation(e, s, 'PhosphorylationThreonine', 185)
+    mc1 = ModCondition('phosphorylation', 'threonine', '185')
+    stmt = Phosphorylation(e, s, mc1)
     failures = check_sequence(stmt)
     assert(not failures)
 
 def test_check_sequence_phos_wrong():
     s = Agent('MAPK1', db_refs = {'UP': 'P28482'})
     e = Agent('MAP2K1')
-    stmt = Phosphorylation(e, s, 'PhosphorylationThreonine', 186)
+    mc1 = ModCondition('phosphorylation', 'threonine', '186')
+    stmt = Phosphorylation(e, s, mc1)
     failures = check_sequence(stmt)
     assert(failures)
 
 def test_check_sequence_phos_enz():
     s = Agent('MAPK1', db_refs = {'UP': 'P28482'})
-    e = Agent('MAP2K1', mods=['PhosphorylationSerine'], mod_sites=[222], 
-              db_refs = {'UP': 'Q02750'})
-    stmt = Phosphorylation(e, s, 'PhosphorylationThreonine', 185)
+    mc1 = ModCondition('phosphorylation', 'serine', '222')
+    mc2 = ModCondition('phosphorylation', 'threoninine', '185')
+    e = Agent('MAP2K1', mc1, db_refs = {'UP': 'Q02750'})
+    stmt = Phosphorylation(e, s, mc2)
     failures = check_sequence(stmt)
     assert(not failures)
 
 def test_check_sequence_phos_enz_wrong():
     s = Agent('MAPK1', db_refs = {'UP': 'P28482'})
-    e = Agent('MAP2K1', mods=['PhosphorylationSerine'], mod_sites=[221],
+    mc1 = ModCondition('phosphorylation', 'serine', '221')
+    mc2 = ModCondition('phosphorylation', 'threoninine', '185')
+    e = Agent('MAP2K1', mods=[mc1],
               db_refs = {'UP': 'Q02750'})
-    stmt = Phosphorylation(e, s, 'PhosphorylationThreonine', 185)
+    stmt = Phosphorylation(e, s, mc2)
     failures = check_sequence(stmt)
     assert(failures)
 
 def test_check_statements():
     stmts = []
     s = Agent('MAPK1', db_refs = {'UP': 'P28482'})
-    e = Agent('MAP2K1', mods=['PhosphorylationSerine'], mod_sites=[222], 
+    mc1 = ModCondition('phosphorylation', 'serine', '222')
+    mc2 = ModCondition('phosphorylation', 'threoninine', '185')
+    e = Agent('MAP2K1', mods=[mc1],
               db_refs = {'UP': 'Q02750'})
-    stmt = Phosphorylation(e, s, 'PhosphorylationThreonine', 185)
+    stmt = Phosphorylation(e, s, mc2)
     stmts.append(stmt)
-    
+
+    mc1 = ModCondition('phosphorylation', 'serine', '221')
+    mc2 = ModCondition('phosphorylation', 'threoninine', '185')
     s = Agent('MAPK1', db_refs = {'UP': 'P28482'})
-    e = Agent('MAP2K1', mods=['PhosphorylationSerine'], mod_sites=[221],
-              db_refs = {'UP': 'Q02750'})
-    stmt = Phosphorylation(e, s, 'PhosphorylationThreonine', 185)
+    e = Agent('MAP2K1', mods=[mc1], db_refs = {'UP': 'Q02750'})
+    stmt = Phosphorylation(e, s, mc2)
     stmts.append(stmt)
 
     p, f = check_statements(stmts)
