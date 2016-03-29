14356364864c8a3ad9c69a99f6714406ab1a9150
Ben Gyori <ben.gyori@gmail.com>
2016-03-29 11:50:17 -0400

    Update BioPAX tests

--- indra/tests/test_biopax.py
+++ indra/tests/test_biopax.py
@@ -47,7 +47,7 @@ def test_has_members_er():
                      'ProteinReference_971cec47bcd850e2b7d602f0416edacf')
     bpe = cast(bpc.bp('ProteinReference'), bpe)
     assert(bpc.has_members(bpe))
-    
+
     bpe = bp.model.getByID('http://identifiers.org/uniprot/P56159')
     bpe = cast(bpc.bp('ProteinReference'), bpe)
     assert(not bpc.has_members(bpe))
@@ -66,7 +66,7 @@ def test_is_pe():
     bpe = bp.model.getByID('http://identifiers.org/reactome/REACT_117345.2')
     bpe = cast(bpc.bp('Protein'), bpe)
     assert(bpc.is_entity(bpe))
-    
+
 def test_is_pe2():
     bpe = bp.model.getByID(uri_prefix +\
                      'ProteinReference_971cec47bcd850e2b7d602f0416edacf')
@@ -77,7 +77,7 @@ def test_is_er():
     bpe = bp.model.getByID('http://identifiers.org/reactome/REACT_117345.2')
     bpe = cast(bpc.bp('Protein'), bpe)
     assert(not bpc.is_reference(bpe))
-    
+
 def test_is_er2():
     bpe = bp.model.getByID(uri_prefix +\
                      'ProteinReference_971cec47bcd850e2b7d602f0416edacf')
@@ -127,3 +127,25 @@ def test_get_hgnc_id():
 def test_get_hgnc_name():
     hgnc_name = bp._get_hgnc_name(3432)
     assert(hgnc_name == 'ERBB4')
+
+def test_get_mod_feature():
+    bpe = bp.model.getByID(uri_prefix +\
+            'ModificationFeature_bd27a53570fb9a5094bb5929bd973217')
+    mf = cast(bpc.bp('ModificationFeature'), bpe)
+    mc = bpc.BiopaxProcessor._extract_mod_from_feature(mf)
+    assert(mc.mod_type == 'phosphorylation')
+    assert(mc.residue == 'threonine')
+    assert(mc.position == '274')
+
+def test_get_entity_mods():
+    bpe = bp.model.getByID(uri_prefix +\
+            'Protein_7aeb1631f64e49491b7a0303aaaec536')
+    protein = cast(bpc.bp('Protein'), bpe)
+    mods = bpc.BiopaxProcessor._get_entity_mods(protein)
+    assert(len(mods) == 5)
+    mod_types = set([m.mod_type for m in mods])
+    assert(mod_types == set(['phosphorylation']))
+    residues = set([m.residue for m in mods])
+    assert(residues == set(['tyrosine']))
+    mod_pos = set([m.position for m in mods])
+    assert(mod_pos == set(['1035', '1056', '1128', '1188', '1242']))
