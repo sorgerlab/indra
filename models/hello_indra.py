826c77eda739f95d499ca980fd44e530986fa34e
John Bachman <bachmanjohn@gmail.com>
2016-04-20 16:52:38 -0400

    Hello INDRA example for paper

---
+++ models/hello_indra.py
@@ -0,0 +1,48 @@
+from indra.bel import bel_api
+from indra.trips import trips_api
+from indra.biopax import biopax_api
+from indra.pysb_assembler import PysbAssembler
+from pysb.integrate import Solver
+import numpy as np
+from matplotlib import pyplot as plt
+
+# 1. TEXT
+# User defines text:
+text = ('MEK1 phosphorylates ERK2 on Thr-185 and Tyr-187. DUSP4 '
+       'dephosphorylates ERK2 on Thr-185 and Tyr-187.')
+
+# Show round trip going out to TRIPS/DRUM web service,
+# return logical form to INDRA, which is queried by
+# INDRA for relevant statements
+tp = trips_api.process_text(text)
+
+# 2. BIOPAX
+# Show round trip going out to PC web service, returning
+# Biopax model, queried by INDRA
+#bp = biopax_api.process_pc_pathsbetween(['MAP2K1', 'MAPK1'])
+#bp.get_phosphorylation()
+
+# 3. BEL
+# Show round trip to NDeX/BEL service
+#belp = bel_api.process_ndex_neighborhood(['MAP2K1'])
+
+# Statements can now be collected since they are in the same format
+#stmts = tp.statements + bp.statements + belp.statements
+
+# .... (do stuff with statements here, filter, curate, etc.)
+
+# Now generate PySB model
+stmts = tp.statements # Don't show this one
+
+pa = PysbAssembler()
+pa.add_statements(stmts)
+pa.make_model()
+t = np.linspace(0, 10000)
+sol = Solver(pa.model, t)
+sol.run()
+
+labels = [str(s) for s in pa.model.species]
+plt.ion()
+for i in range(sol.y.shape[1]):
+    plt.plot(t, sol.y[:, i], label=labels[i])
+plt.legend(loc='upper right')
