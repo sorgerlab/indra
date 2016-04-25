2697c583fd57b9cbb0b510e773b9590b23c837ad
Ben Gyori <ben.gyori@gmail.com>
2016-04-24 22:49:50 -0400

    Add scripts for running the machine and starting a new model

---
+++ models/rasmachine/run_machine.sh
@@ -0,0 +1,17 @@
+model_name=$1
+export CLASSPATH="$HOME/src/reach/*"
+platform=`uname`
+if [[ "$platform" == 'Linux' ]]; then
+    export JAVA_HOME="/usr/lib/jvm/java-8-oracle/"
+else
+    export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_74.jdk/Contents/Home"
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+export PYTHONPATH=../../../indra:..
+twitter_cred="$model_name/twitter_cred.txt"
+gmail_cred="$model_name/gmail_cred.txt"
+log_file="$model_name/log.txt"
+err_file="$model_name/err.txt"
+python rasmachine.py --model $model_name --twitter $twitter_cred --gmail $gmail_cred 1>> $log_file 2>> $err_file
---
+++ models/rasmachine/start_model.sh
@@ -0,0 +1,8 @@
+model_name="$1"
+mkdir "$model_name"
+mkdir "$model_name/jsons"
+mkdir "$model_name/jsons/abstract"
+mkdir "$model_name/jsons/full"
+touch "$model_name/search_terms.txt"
+touch "$model_name/log.txt"
+touch "$model_name/err.txt"
