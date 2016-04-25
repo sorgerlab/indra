e287a4373a08d99b27a17a769cd7c3b985936e74
Ben Gyori <ben.gyori@gmail.com>
2016-04-24 22:49:04 -0400

    Adding argparse to rasmachine.py and generalizing

--- models/rasmachine/rasmachine.py
+++ models/rasmachine/rasmachine.py
@@ -1,6 +1,8 @@
 import os
+import sys
 import time
 import shutil
+import argparse
 import gmail_client
 import twitter_client
 from incremental_model import IncrementalModel
@@ -8,7 +10,6 @@ from indra import reach
 from indra.literature import pubmed_client, get_full_text
 
 model_path = os.path.dirname(os.path.abspath(__file__))
-use_twitter = False
 
 def get_email_pmids(cred_file):    
     try:
@@ -25,9 +26,19 @@ def get_email_pmids(cred_file):
     print 'Collected %d PMIDs' % len(pmids)
     return pmids
 
+def get_searchterm_pmids(search_terms, num_days=1):
+    pmids = set([])
+    for s in search_terms:
+        ids = pubmed_client.get_ids(s, reldate=num_days)
+        pmids = pmids.union(ids)
+    return list(pmids)
+
 def process_paper(pmid):
-    abstract_path = model_path + '/jsons/abstract/PMID%s.json' % pmid
-    fulltext_path = model_path + '/jsons/full/PMID%s.json' % pmid
+    global model_name
+    abstract_path = os.path.join(model_path, model_name, 
+                                 'jsons', 'abstract', 'PMID%s.json' % pmid)
+    fulltext_path = os.path.join(model_path, model_name, 
+                                 'jsons', 'full', 'PMID%s.json' % pmid)
 
     # If the paper has been parsed, use the parse output file
     if os.path.exists(abstract_path):
@@ -38,7 +49,7 @@ def process_paper(pmid):
         txt_format = 'txt'
     # If the paper has not been parsed, download the text and parse
     else:
-        txt, txt_format = indra.literature.get_full_text(pmid)
+        txt, txt_format = get_full_text(pmid)
         if txt_format == 'nxml':
             rp = reach.process_nxml_str(txt, citation=pmid, offline=True)
             shutil.move('reach_output.json', fulltext_path)
@@ -82,6 +93,7 @@ def make_status_message(stats):
     return msg_str
 
 def extend_model(model, pmids):
+    global model_name
     npapers = 0
     nabstracts = 0
     for pmid in pmids:
@@ -101,29 +113,75 @@ def extend_model(model, pmids):
     return npapers, nabstracts
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', help='Model name', required=True)
+    parser.add_argument('--twitter', help='Twitter credentials file')
+    parser.add_argument('--gmail', help='Gmail credentials file')
+    args = parser.parse_args()
+
     print time.strftime('%c')
 
+    global model_name
+
+    if not args.model:
+        print 'Model name must be supplied as --model model_name.'
+        sys.exit()
+    else:
+        model_name = args.model
+
+    if args.twitter:
+        twitter_cred = args.twitter
+        if os.path.exists(twitter_cred):
+            use_twitter = True
+        else:
+            use_twitter = False
+    else:
+        use_twitter = False
+
+    if args.gmail:
+        gmail_cred = args.gmail
+        if os.path.exists(gmail_cred):
+            use_gmail = True
+        else:
+            use_gmail = False
+    else:
+        use_gmail = False
+
+    pmids = []
     # Get email PMIDs
-    pmids = get_email_pmids('gmail_cred.txt')
+    if use_gmail: 
+        pmids += get_email_pmids(gmail_cred)
+
+    # Get search PMIDs
+    search_terms_file = os.path.join(model_path, model_name, 'search_terms.txt')
+    if os.path.exists(search_terms_file):
+        search_terms = [l.strip() for l in
+                    open(search_terms_file, 'rt').readlines()]
+        if search_terms:
+            pmids += get_searchterm_pmids(search_terms) 
+    if not pmids:
+        print 'No PMIDs found.'
+        sys.exit()
+
     # Load the model
-    rasmodel = IncrementalModel(model_path + '/rasmodel.pkl')
-    pysb_model = rasmodel.make_model()
+    inc_model_file = os.path.join(model_path, model_name, 'model.pkl')
+    model = IncrementalModel(inc_model_file)
+    pysb_model = model.make_model()
     stats = {}
-    stats['orig_stmts'] = len(rasmodel.get_statements())
+    stats['orig_stmts'] = len(model.get_statements())
     stats['orig_monomers'] = len(pysb_model.monomers)
     stats['orig_rules'] = len(pysb_model.rules)
     # Extend the model with PMIDs
-    stats['new_papers'], stats['new_abstracts'] = extend_model(rasmodel, pmids)
-    stats['new_stmts'] = len(rasmodel.get_statements())
-    pysb_model = rasmodel.make_model()
+    stats['new_papers'], stats['new_abstracts'] = extend_model(model, pmids)
+    stats['new_stmts'] = len(model.get_statements())
+    pysb_model = model.make_model()
     stats['new_monomers'] = len(pysb_model.monomers)
     stats['new_rules'] = len(pysb_model.rules)
     # Save model
-    rasmodel.save(model_path + '/rasmodel.pkl')
+    model.save(inc_model_file)
     # Print and tweet the status message
     msg_str = make_status_message(stats)
     if msg_str is not None:
         print msg_str
         if use_twitter:
-            twitter_client.update_status(msg_str, 'twitter_cred.txt')
-    print model
+            twitter_client.update_status(msg_str, twitter_cred)
