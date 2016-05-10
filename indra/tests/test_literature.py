ab7d9cd32ac14967a50cafd7c9642b3a9adf4c2a
John Bachman <bachmanjohn@gmail.com>
2016-05-10 15:41:29 -0400

    Remove ID lookup tests from test_literature

--- indra/tests/test_literature.py
+++ indra/tests/test_literature.py
@@ -1,35 +1,5 @@
 from indra.literature import id_lookup, get_full_text
 
-def test_id_lookup_pmid():
-    ids = id_lookup('25361007')
-    assert(ids['doi'] == '10.18632/oncotarget.2555')
-    assert(ids['pmid'] == '25361007')
-    assert(ids['pmcid'] == 'PMC4322985')
-
-def test_id_lookup_pmid2():
-    ids = id_lookup('PMID25361007')
-    assert(ids['doi'] == '10.18632/oncotarget.2555')
-    assert(ids['pmid'] == '25361007')
-    assert(ids['pmcid'] == 'PMC4322985')
-
-def test_id_lookup_pmcid():
-    ids = id_lookup('PMC4322985')
-    assert(ids['doi'] == '10.18632/oncotarget.2555')
-    assert(ids['pmid'] == '25361007')
-    assert(ids['pmcid'] == 'PMC4322985')
-
-def test_id_lookup_doi():
-    ids = id_lookup('10.18632/oncotarget.2555')
-    assert(ids['doi'] == '10.18632/oncotarget.2555')
-    assert(ids['pmid'] == '25361007')
-    assert(ids['pmcid'] == 'PMC4322985')
-
-def test_id_lookup_doi2():
-    ids = id_lookup('DOI10.18632/oncotarget.2555')
-    assert(ids['doi'] == '10.18632/oncotarget.2555')
-    assert(ids['pmid'] == '25361007')
-    assert(ids['pmcid'] == 'PMC4322985')
-
 def test_get_full_text_pmc():
     txt, txt_format = get_full_text('PMC4322985')
     assert(txt_format == 'nxml')
