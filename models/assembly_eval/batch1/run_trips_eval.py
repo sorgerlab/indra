54e4e0efc57470f236e01083b4d1d302fa3dfeef
Ben Gyori <ben.gyori@gmail.com>
2016-05-12 13:13:51 -0400

    Assing correct PMID in TRIPS-extracted statements

--- models/assembly_eval/batch1/run_trips_eval.py
+++ models/assembly_eval/batch1/run_trips_eval.py
@@ -1,15 +1,20 @@
 from indra import trips
+from indra.literature import id_lookup
 from assembly_eval import have_file, run_assembly
 
 if __name__ == '__main__':
     pmc_ids = ['PMC1234335', 'PMC3178447', 'PMC3690480',
                'PMC4345513', 'PMC534114']
-
+    pmids = [id_lookup(pmcid)['pmid'] for pmcid in pmc_ids]
     # Use the existing EKB extractions.
-    for pmcid in pmc_ids:
+    for pmid, pmcid in zip(pmids, pmc_ids):
         folder = 'trips'
         prefix = folder + '/' + pmcid
         print 'Processing %s...' % pmcid
         tp = trips.process_xml(open(prefix + '-20160503T1152.ekb').read())
-
+        # PMIDs from TRIPS need to be set here because it propagates
+        # the PMCID by default
+        for s in tp.statements:
+            for e in s.evidence:
+                e.pmid = pmid
         run_assembly(tp.statements, folder, pmcid)
