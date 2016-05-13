3936cb040d9458d725d4f72535428cbae4e917d4
Ben Gyori <ben.gyori@gmail.com>
2016-05-13 12:00:38 -0400

    Update docstring format for BEL and BioPAX

--- indra/bel/bel_api.py
+++ indra/bel/bel_api.py
@@ -5,18 +5,18 @@ import ndex_client
 from processor import BelProcessor
 
 def process_ndex_neighborhood(gene_names, rdf_out='bel_output.rdf'):
-    """ Return a BelProcessor for an NDEx network neighborhood.
+    """Return a BelProcessor for an NDEx network neighborhood.
 
     Parameters
     ----------
     gene_names : list
         A list of HGNC gene symbols to search the neighborhood of.
         Example: ['BRAF', 'MAP2K1']
-    rdf_out : string, optional (default: 'bel_output.rdf')
+    rdf_out : Optional[str]
         Name of the output file to save the RDF returned by the web service.
         This is useful for debugging purposes or to repeat the same query
-        on an offline RDF file later.
-    
+        on an offline RDF file later. Default: bel_output.rdf
+
     Returns
     -------
     bp : BelProcessor
@@ -24,11 +24,9 @@ def process_ndex_neighborhood(gene_names, rdf_out='bel_output.rdf'):
 
     Notes
     -----
-    This function calls process_belrdf to the returned RDF string from the 
+    This function calls process_belrdf to the returned RDF string from the
     webservice.
     """
-
-    
     network_id = '9ea3c170-01ad-11e5-ac0f-000c29cb28fb'
     #url_suffix = '/bel2rdf/v1/network/%s/asBELRDF/query' % network_id
     url_suffix = '/network/%s/asBELRDF/query' % network_id
@@ -44,11 +42,11 @@ def process_ndex_neighborhood(gene_names, rdf_out='bel_output.rdf'):
 
 
 def process_belrdf(rdf_str):
-    """ Return a BelProcessor for a BEL/RDF string.
+    """Return a BelProcessor for a BEL/RDF string.
 
     Parameters
     ----------
-    rdf_str : string
+    rdf_str : str
         A BEL/RDF string to be processed. This will usually come from reading
         a .rdf file.
 
@@ -60,8 +58,8 @@ def process_belrdf(rdf_str):
     Notes
     -----
     This function calls all the specific get_type_of_mechanism()
-    functions of the newly constructed BelProcessor to extract 
-    INDRA Statements. 
+    functions of the newly constructed BelProcessor to extract
+    INDRA Statements.
     """
     g = rdflib.Graph()
     try:
--- indra/biopax/biopax_api.py
+++ indra/biopax/biopax_api.py
@@ -3,7 +3,7 @@ from indra.java_vm import autoclass, JavaException
 from indra.biopax import pathway_commons_client as pcc
 
 def process_pc_neighborhood(gene_names, neighbor_limit=1):
-    """ Returns a BiopaxProcessor for a PathwayCommons neighborhood query.
+    """Returns a BiopaxProcessor for a PathwayCommons neighborhood query.
 
     The neighborhood query finds the neighborhood around a set of source genes.
     http://www.pathwaycommons.org/pc2/#graph
@@ -14,22 +14,21 @@ def process_pc_neighborhood(gene_names, neighbor_limit=1):
     gene_names : list
         A list of HGNC gene symbols to search the neighborhood of.
         Examples: ['BRAF'], ['BRAF', 'MAP2K1']
-    neighbor_limit : int, optional (default: 1)
+    neighbor_limit : Optional[int]
         The number of steps to limit the size of the neighborhood around
-        the gene names being queried.
+        the gene names being queried. Default: 1
 
     Returns
     -------
     bp : BiopaxProcessor
         A BiopaxProcessor containing the obtained BioPAX model in bp.model.
-       
+
     Notes
     -----
-    As returned from this function, bp.statements is empty and one 
+    As returned from this function, bp.statements is empty and one
     has to call methods of bp (e.g. bp.get_complexes()) to
     populate bp.statements.
     """
-
     model = pcc.graph_query('neighborhood', gene_names,
                             neighbor_limit=neighbor_limit)
     if model is not None:
@@ -37,7 +36,7 @@ def process_pc_neighborhood(gene_names, neighbor_limit=1):
 
 
 def process_pc_pathsbetween(gene_names, neighbor_limit=1):
-    """ Returns a BiopaxProcessor for a PathwayCommons paths-between query.
+    """Returns a BiopaxProcessor for a PathwayCommons paths-between query.
 
     The paths-between query finds the paths between a set of genes. Here
     source gene names are given in a single list and all directions of paths
@@ -50,18 +49,18 @@ def process_pc_pathsbetween(gene_names, neighbor_limit=1):
     gene_names : list
         A list of HGNC gene symbols to search for paths between.
         Examples: ['BRAF', 'MAP2K1']
-    neighbor_limit : int, optional (default: 1)
+    neighbor_limit : Optional[int]
         The number of steps to limit the length of the paths between
-        the gene names being queried.
+        the gene names being queried. Default: 1
 
     Returns
     -------
     bp : BiopaxProcessor
         A BiopaxProcessor containing the obtained BioPAX model in bp.model.
-       
+
     Notes
     -----
-    As returned from this function, bp.statements is empty and one 
+    As returned from this function, bp.statements is empty and one
     has to call methods of bp (e.g. bp.get_complexes()) to
     populate bp.statements.
     """
@@ -72,7 +71,7 @@ def process_pc_pathsbetween(gene_names, neighbor_limit=1):
 
 
 def process_pc_pathsfromto(source_genes, target_genes, neighbor_limit=1):
-    """ Returns a BiopaxProcessor for a PathwayCommons paths-from-to query.
+    """Returns a BiopaxProcessor for a PathwayCommons paths-from-to query.
 
     The paths-from-to query finds the paths from a set of source genes to
     a set of target genes.
@@ -89,30 +88,30 @@ def process_pc_pathsfromto(source_genes, target_genes, neighbor_limit=1):
         A list of HGNC gene symbols that are the targets of paths being
         searched for.
         Examples: ['MAP2K1', 'MAP2K2']
-    neighbor_limit : int, optional (default: 1)
+    neighbor_limit : Optional[int]
         The number of steps to limit the length of the paths
-        between the source genes and target genes being queried.
+        between the source genes and target genes being queried. Default: 1
 
     Returns
     -------
     bp : BiopaxProcessor
         A BiopaxProcessor containing the obtained BioPAX model in bp.model.
-       
+
     Notes
     -----
-    As returned from this function, bp.statements is empty and one 
+    As returned from this function, bp.statements is empty and one
     has to call methods of bp (e.g. bp.get_complexes()) to
     populate bp.statements.
     """
-    model = pcc.graph_query('pathsfromto', source_genes, 
+    model = pcc.graph_query('pathsfromto', source_genes,
                              target_genes, neighbor_limit)
     if model is not None:
         return process_model(model)
 
 
 def process_owl(owl_filename):
-    """ Returns a BiopaxProcessor for a BioPAX OWL file.
-    
+    """Returns a BiopaxProcessor for a BioPAX OWL file.
+
     Parameters
     ----------
     owl_filename : string
@@ -125,7 +124,7 @@ def process_owl(owl_filename):
 
     Notes
     -----
-    As returned from this function, bp.statements is empty and one 
+    As returned from this function, bp.statements is empty and one
     has to call methods of bp (e.g. bp.get_complexes()) to
     populate bp.statements.
     """
@@ -134,8 +133,8 @@ def process_owl(owl_filename):
 
 
 def process_model(model):
-    """ Returns a BiopaxProcessor for a BioPAX model object.
-    
+    """Returns a BiopaxProcessor for a BioPAX model object.
+
     Parameters
     ----------
     model : org.biopax.paxtools.model.Model
@@ -148,7 +147,7 @@ def process_model(model):
 
     Notes
     -----
-    As returned from this function, bp.statements is empty and one 
+    As returned from this function, bp.statements is empty and one
     has to call methods of bp (e.g. bp.get_complexes()) to
     populate bp.statements.
     """
