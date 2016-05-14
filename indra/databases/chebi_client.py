16c371d7c47e75579097b08463c3896fa4cd52d3
Ben Gyori <ben.gyori@gmail.com>
2016-05-14 12:25:43 -0400

    Add ChEMBL mapping to ChEBI client

--- indra/databases/chebi_client.py
+++ indra/databases/chebi_client.py
@@ -2,18 +2,41 @@ import os
 import csv
 from functools32 import lru_cache
 
-# Based on ftp://ftp.ebi.ac.uk/pub/databases/chebi/
-#                Flat_file_tab_delimited/reference.tsv.gz
-chebi_to_pubchem_file = os.path.dirname(os.path.abspath(__file__)) + \
-                        '/../resources/chebi_to_pubchem.tsv'
-try:
-    fh = open(chebi_to_pubchem_file, 'rt')
-    rd = csv.reader(fh, delimiter='\t')
-    chebi_pubchem = {}
-    for row in rd:
-        chebi_pubchem[row[0]] = row[1]
-except IOError:
-    chebi_pubchem = {}
+def read_chebi_to_pubchem():
+    # Based on ftp://ftp.ebi.ac.uk/pub/databases/chebi/
+    #                Flat_file_tab_delimited/reference.tsv.gz
+    chebi_to_pubchem_file = os.path.dirname(os.path.abspath(__file__)) + \
+                            '/../resources/chebi_to_pubchem.tsv'
+    try:
+        fh = open(chebi_to_pubchem_file, 'rt')
+        rd = csv.reader(fh, delimiter='\t')
+        chebi_pubchem = {}
+        for row in rd:
+            chebi_pubchem[row[0]] = row[1]
+    except IOError:
+        chebi_pubchem = {}
+    return chebi_pubchem
+
+def read_chebi_to_chembl():
+    # Based on ftp://ftp.ebi.ac.uk/pub/databases/chebi/
+    #                Flat_file_tab_delimited/reference.tsv.gz
+    chebi_to_chembl_file = os.path.dirname(os.path.abspath(__file__)) + \
+                            '/../resources/chebi_to_chembl.tsv'
+    try:
+        fh = open(chebi_to_chembl_file, 'rt')
+        rd = csv.reader(fh, delimiter='\t')
+        chebi_chembl = {}
+        for row in rd:
+            chebi_chembl[row[0]] = row[1]
+    except IOError:
+        chebi_chembl = {}
+    return chebi_chembl
+
+chebi_pubchem = read_chebi_to_pubchem()
+chebi_chembl = read_chebi_to_chembl()
 
 def get_pubchem_id(chebi_id):
     return chebi_pubchem.get(chebi_id)
+
+def get_chembl_id(chebi_id):
+    return chebi_chembl.get(chebi_id)
