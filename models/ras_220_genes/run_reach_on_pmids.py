33fbbd6b05bbb66bb381162c5400480560a2aacc
John Bachman <bachmanjohn@gmail.com>
2016-05-18 15:01:27 -0400

    Create REACH configuration file

--- models/ras_220_genes/run_reach_on_pmids.py
+++ models/ras_220_genes/run_reach_on_pmids.py
@@ -1,24 +1,30 @@
 # Iterate over the PMIDs
 # Check in S3
 # If present, copy to NXML folder
+
 # Prepare temporary .conf file using appropriate number of cores
 # Run reach on NXML files, send to output folder
 # Join the resulting .json files
 # Upload the .json to S3, mark down in a folder as being run
 
+import os
 import sys
+import zlib
+import tempfile
+import shutil
 import boto3
 import botocore
 
+cleanup = True
+
 # Check the arguments
-usage = "Usage: %s pmid_list txt_dir output_dir num_cores start_index end_index" \
+usage = "Usage: %s pmid_list tmp_dir num_cores start_index end_index" \
          % sys.argv[0]
-if len(sys.argv) < 7:
+if len(sys.argv) < 6:
     print usage
     sys.exit(1)
 # Get the command line arguments
-(pmid_list_file, txt_dir, output_dir,
-                 num_cores, start_index, end_index) = sys.argv[1:]
+(pmid_list_file, tmp_dir, num_cores, start_index, end_index) = sys.argv[1:]
 start_index = int(start_index)
 end_index = int(end_index)
 num_cores = int(num_cores)
@@ -26,6 +32,13 @@ num_cores = int(num_cores)
 with open(pmid_list_file) as f:
     pmid_list = [line.strip('\n') for line in f.readlines()]
 pmids_to_read = pmid_list[start_index:end_index]
+# Create the temp directories for input and output
+base_dir = tempfile.mkdtemp(prefix='read_%s_to_%s_' % (start_index, end_index),
+                            dir=tmp_dir)
+input_dir = os.path.join(base_dir, 'input')
+output_dir = os.path.join(base_dir, 'output')
+os.makedirs(input_dir)
+os.makedirs(output_dir)
 # Initialize S3 stuff
 bucket_name ='bigmech'
 client = boto3.client('s3')
@@ -47,10 +60,69 @@ for pmid in pmids_to_read:
         else:
             raise e
     # Get the content from the object
-    xml_gz = xml_gz['Body'].read()
+    xml_gz = xml_gz_obj['Body'].read()
     # Decode the gzipped content
-    buf = cStringIO.StringIO()
+    xml = zlib.decompress(xml_gz, 16+zlib.MAX_WBITS)
+    # Write the contents to a file
+    xml_path = os.path.join(input_dir, 'PMID%s.nxml' % pmid)
+    with open(xml_path, 'w') as f:
+        f.write(xml)
 
-    # Have we already read this paper? With this version of REACH?
-    # If not, do nothing
+# Create the REACH configuration file
+conf_file_text = """
+# this is the directory that stores the raw nxml files
+# this directory *must* exist
+nxmlDir = {input_dir}
+# this is where the output files containing the extracted mentions will be
+# stored
+# if this directory doesn't exist it will be created
+friesDir = {output_dir}
+# this is where the context files will be stored
+# if this directory doesn't exist it will be created
+contextDir = {output_dir}
+# this is where the brat standoff and text files are dumped
+bratDir = {output_dir}
+# the encoding of input and output files
+encoding = "utf-8"
+# nxml2fries configuration
+nxml2fries {{
+  # this is a list of sections that we should ignore
+  ignoreSections = ["references", "materials", "materials|methods", "methods", "supplementary-material"]
+}}
+# context engine config
+contextEngine {{
+    type = Policy4
+    params = {{
+        bound = 3
+    }}
+}}
+# the output format for mentions: text, fries, indexcard (default is 'text')
+outputType = "fries"
+# this log file gets overwritten every time ReachCLI is executed
+# so you should copy it if you want to keep it around
+logFile = {base_dir}/log.txt
+# grounding configuration
+grounding: {{
+  # List of AdHoc grounding files to insert, in order, into the grounding
+  # search sequence. Each element of the list is a map of KB filename and
+  # optional meta info (not yet used):
+  #   example: {{ kb: "adhoc.tsv", source: "NMZ at CMU" }}
+  adHocFiles: [
+#     {{ kb: "adhoc.tsv", source: "NMZ at CMU" }}
+  ]
+}}
+# number of simultaneous threads to use for parallelization
+threadLimit = {num_cores}
+# ReadPapers
+ReadPapers.papersDir = src/test/resources/inputs/nxml/
+ReadPapers.serializedPapers = mentions.ser
+""".format(base_dir=base_dir, input_dir=input_dir, output_dir=output_dir,
+           num_cores=num_cores)
+# Write the configuration file to the temp directory
+conf_file_path = os.path.join(base_dir, 'indra.conf')
+with open(conf_file_path, 'w') as f:
+    f.write(conf_file_text)
+
+#if cleanup:
+#    shutil.rmtree(base_dir)
 
