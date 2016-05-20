f8c0c94231078522dba244760f72bb95f013fc33
Ben Gyori <ben.gyori@gmail.com>
2016-05-20 15:26:27 +0200

    Document ReachProcessor and reader

--- indra/reach/reach_reader.py
+++ indra/reach/reach_reader.py
@@ -1,10 +1,27 @@
 from indra.java_vm import autoclass, JavaException
 
 class ReachReader(object):
+    """The ReachReaader wraps a singleton instance of the REACH reader.
+
+    This allows calling the reader many times without having to wait for it to
+    start up each time.
+
+    Attributes
+    ----------
+    api_ruler : edu.arizona.sista.reach.apis.ApiRuler
+        An instance of the REACH ApiRuler class (java object).
+    """
     def __init__(self):
         self.api_ruler = None
 
     def get_api_ruler(self):
+        """Return the existing reader if it exists or launch a new one.
+
+        Returns
+        -------
+        api_ruler : edu.arizona.sista.reach.apis.ApiRuler
+            An instance of the REACH ApiRuler class (java object).
+        """
         if self.api_ruler is None:
             try:
                 self.api_ruler =\
