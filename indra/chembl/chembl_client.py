889f8482e9a55e6a2bab7072e38b71c1cf5da295
subkar <sub.kartik@gmail.com>
2016-06-08 15:33:37 -0400

    added chembl client

---
+++ indra/chembl/chembl_client.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+
+def query(drug_chemblid, target_chemblid):
+    
+    url = 'https://www.ebi.ac.uk/chembl/api/data/activity.json?molecule_chembl_id=%s&target_chembl_id=%s' % (
+        drug_chemblid, target_chemblid)
+
+    r = requests.get(url)
+    r.raise_for_status()
+
+    js = r.json()
+    return js
+
