52644467a2caf955542b7ed58229d4d42e9735c4
John Bachman <bachmanjohn@gmail.com>
2016-06-15 16:14:04 -0400

    Elementary monomer_pattern embedding
    Might be better off in PySB, but it's OK here for now.

--- indra/tests/test_model_checker.py
+++ indra/tests/test_model_checker.py
@@ -1,21 +1,46 @@
 from indra.statements import *
 from pysb import *
-from indra.tools.model_checker import ModelChecker
+from pysb.core import SelfExporter
+from pysb.tools import render_reactions
+from indra.tools.model_checker import ModelChecker, mp_embeds_into
+from pysb.tools import species_graph
+from pysb.bng import generate_equations
+from pysb import kappa
+from pysb.testing import with_model
 
-def test_simple_phosphorylation():
+@with_model
+def test_mp_embedding():
+    # Create a PySB model
+    Monomer('A', ['b', 'other'], {'other':['u','p']})
+    mp1 = A(other='u')
+    mp2 = A()
+    mp3 = A(other='p')
+    assert mp_embeds_into(mp1, mp2)
+    assert not mp_embeds_into(mp2, mp1)
+    assert mp_embeds_into(mp3, mp2)
+    assert not mp_embeds_into(mp2, mp3)
+    assert not mp_embeds_into(mp3, mp1)
+    assert not mp_embeds_into(mp1, mp3)
+
+@with_model
+def test_one_step_phosphorylation():
+    # Override the shutoff of self export in psyb_assembler
     # Create the statement
     a = Agent('A')
     b = Agent('B')
     st = Phosphorylation(a, b, 'T', '185')
     # Now create the PySB model
-    Model()
     Monomer('A')
     Monomer('B', ['T185'], {'T185':['u', 'p']})
     Rule('A_phos_B', A() + B(T185='u') >> A() + B(T185='p'),
          Parameter('k', 1))
-    Observable('B_phosphoT185', B(T185='p'))
     Initial(A(), Parameter('A_0', 100))
     Initial(B(T185='u'), Parameter('B_0', 100))
+    with open('model_rxn.dot', 'w') as f:
+        f.write(render_reactions.run(model))
+    with open('species_1step.dot', 'w') as f:
+        f.write(species_graph.run(model))
+    import ipdb; ipdb.set_trace()
     # Now check the model
     mc = ModelChecker(model, [st])
     results = mc.check_model()
@@ -24,7 +49,39 @@ def test_simple_phosphorylation():
     assert results[0][0] == st
     assert results[0][1] == True
 
-if __name__ == '__main__':
-    test_simple_phosphorylation()
+def test_two_step_phosphorylation():
+    # Create the statement
+    a = Agent('A')
+    b = Agent('B')
+    st = Phosphorylation(a, b, 'T', '185')
+    # Now create the PySB model
+    Monomer('A', ['b', 'other'], {'other':['u','p']})
+    Monomer('B', ['b', 'T185'], {'T185':['u', 'p']})
+    Rule('A_bind_B', A(b=None) + B(b=None, T185='u') <>
+                     A(b=1) % B(b=1, T185='u'),
+                 Parameter('kf', 1), Parameter('kr', 1))
+    Rule('A_phos_B', A(b=1) % B(b=1, T185='u') >>
+                     A(b=None) + B(b=None, T185='p'),
+                 Parameter('kcat', 1))
+    Initial(A(b=None, other='p'), Parameter('Ap_0', 100))
+    Initial(A(b=None, other='u'), Parameter('Au_0', 100))
+    Initial(B(b=None, T185='u'), Parameter('B_0', 100))
+    with open('model_rxn.dot', 'w') as f:
+        f.write(render_reactions.run(model))
+    with open('species_2step.dot', 'w') as f:
+        f.write(species_graph.run(model))
+    im = kappa.influence_map(model)
+    im.draw('im_2step.pdf', prog='dot')
+    generate_equations(model)
+    # Now check the model
+    mc = ModelChecker(model, [st])
+    results = mc.check_model()
+    assert len(results) == 1
+    assert isinstance(results[0], tuple)
+    assert results[0][0] == st
+    assert results[0][1] == True
 
+
+if __name__ == '__main__':
+    test_mp_embedding()
 
--- indra/tools/model_checker.py
+++ indra/tools/model_checker.py
@@ -1,4 +1,8 @@
-
+from pysb import kappa
+from pysb import Observable
+from indra.statements import *
+from indra.assemblers import pysb_assembler as pa
+from copy import deepcopy
 
 class ModelChecker(object):
     """Check a PySB model against a set of INDRA statements."""
@@ -8,4 +12,74 @@ class ModelChecker(object):
         self.statements = statements
 
     def check_model(self):
-        return [(self.statements[0], True)]
+        results = []
+        for stmt in self.statements:
+            result = self.check_statement(stmt)
+            results.append((stmt, result))
+        return results
+
+    def check_statement(self, stmt):
+        if isinstance(stmt, Phosphorylation):
+            return self.check_phosphorylation(stmt)
+        else:
+            return False
+
+    def check_phosphorylation(self, stmt):
+        # Identify the observable we're looking for in the model, which
+        # may not exist!
+        # The observable is the modified form of the substrate
+        enz_mp = pa.get_monomer_pattern(self.model, stmt.enz)
+        modified_sub = _add_modification_to_agent(stmt.sub, 'phosphorylation',
+                                                  stmt.residue, stmt.position)
+        sub_mp = pa.get_monomer_pattern(self.model, modified_sub)
+        # Make a copy of the model and add the observable for the modified
+        # substrate
+        new_model = deepcopy(self.model)
+        sub_obs = Observable('target', sub_mp, _export=False)
+        new_model.add_component(sub_obs)
+        return True
+
+def _add_modification_to_agent(agent, mod_type, residue, position):
+    new_mod = ModCondition(mod_type, residue, position)
+    # Check if this modification already exists
+    for old_mod in agent.mods:
+        if old_mod.equals(new_mod):
+            return agent
+    new_agent = deepcopy(agent)
+    new_agent.mods.append(new_mod)
+    return new_agent
+
+def match_lhs(cp, rules):
+    rule_matches = []
+    for rule in rules:
+        reactant_pattern = rule.rule_expression.reactant_pattern
+        for rule_cp in reactant_pattern.complex_patterns:
+            if embeds_into(rule_cp, cp):
+                rule_matches.append(rule)
+    return rule_matches
+
+def cp_embeds_into(cp1, cp2):
+    # Check that any state in cp2 is matched in cp2
+    # If the thing we're matching to is just a monomer pattern, that makes
+    # things easier--we just need to find the corresponding monomer pattern
+    # in cp1
+    if len(cp2.monomer_patterns) == 1:
+        mp2 = cp2.monomer_patterns[0]
+        # Iterate over the monomer patterns in cp1 and see if there is one
+        # that has the same name
+        for mp1 in cp1.monomer_patterns:
+            if mp_embeds_into(mp1, mp2):
+                return True
+    return False
+
+def mp_embeds_into(mp1, mp2):
+    sc_matches = []
+    if mp1.monomer.name != mp2.monomer.name:
+        return False
+    # Check that all conditions in mp2 are met in mp1
+    for site_name, site_state in mp2.site_conditions.items():
+        if site_name not in mp1.site_conditions or \
+           site_state != mp1.site_conditions[site_name]:
+            return False
+    return True
+
