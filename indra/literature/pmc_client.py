24693e235d7c6b83f55c3f63e77712d4ab8ea25c
John Bachman <bachmanjohn@gmail.com>
2016-08-04 16:02:20 -0400

    Add resource files with PMC fulltexts and filtering API

--- indra/literature/pmc_client.py
+++ indra/literature/pmc_client.py
@@ -2,10 +2,23 @@ import urllib
 import urllib2
 import xml.etree.ElementTree as et
 from indra.literature import pubmed_client
+import os.path
 
 pmc_url = 'http://www.ncbi.nlm.nih.gov/pmc/oai/oai.cgi'
 pmid_convert_url = 'http://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/'
 
+# Paths to resource files
+pmids_fulltext_path = os.path.join(os.path.dirname(__file__),
+                                   'pmids_fulltext.txt')
+pmids_oa_xml_path = os.path.join(os.path.dirname(__file__),
+                                   'pmids_oa_xml.txt')
+pmids_oa_txt_path = os.path.join(os.path.dirname(__file__),
+                                   'pmids_oa_txt.txt')
+pmids_auth_xml_path = os.path.join(os.path.dirname(__file__),
+                                   'pmids_auth_xml.txt')
+# Define global dict containing lists of PMIDs among mineable PMCs
+# to be lazily initialized
+pmids_fulltext_dict = {}
 
 def id_lookup(paper_id, idtype=None):
     """This function takes a Pubmed ID, Pubmed Central ID, or DOI
@@ -79,3 +92,32 @@ def check_xml_error(xml_str):
         err_text = err_tag.text
         return (err_code, err_text)
     return None
+
+def filter_pmids(pmid_list, source_type):
+    """Filter a list of PMIDs for ones with full text from PMC.
+
+    Parameters
+    ----------
+    pmid_list : list
+        List of PMIDs to filter.
+    source_type : string
+        One of 'fulltext', 'oa_xml', 'oa_txt', or 'auth_xml'.
+
+    Returns
+    -------
+    list of PMIDs available in the specified source/format type.
+    """
+    global pmids_fulltext_dict
+    # Check args
+    if source_type not in ('fulltext', 'oa_xml', 'oa_txt', 'auth_xml'):
+        raise ValueError("source_type must be one of: 'fulltext', 'oa_xml', "
+                         "'oa_txt', or 'auth_xml'.")
+    # Check if we've loaded this type, and lazily initialize
+    if pmids_fulltext_dict.get(source_type) is None:
+        fulltext_list_path = os.path.join(os.path.dirname(__file__),
+                                     'pmids_%s.txt' % source_type)
+        with open(fulltext_list_path) as f:
+            fulltext_list = set([line.strip('\n') for line in f.readlines()])
+            pmids_fulltext_dict[source_type] = fulltext_list
+    return list(set(pmid_list).intersection(
+                                pmids_fulltext_dict.get(source_type)))
