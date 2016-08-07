ad97bdf305710f9660c6faa89e577c0e9184f4b2
Ben Gyori <ben.gyori@gmail.com>
2016-08-07 12:24:43 +0200

    Fixes and comments in combined assembly code

--- models/assembly_eval/batch4/run_combined.py
+++ models/assembly_eval/batch4/run_combined.py
@@ -1,7 +1,7 @@
 from indra import trips, reach
 from indra.literature import id_lookup
 from assembly_eval import have_file, run_assembly
-from rasmodel import ras_model_statements
+import rasmodel
 
 if __name__ == '__main__':
     pmc_ids = [s.strip() for s in open('pmcids.txt', 'rt').readlines()]
@@ -9,13 +9,19 @@ if __name__ == '__main__':
 
     for pmid, pmcid in zip(pmids, pmc_ids):
         print 'Processing %s...' % pmcid
+        # Process TRIPS
         trips_fname = 'trips/' + pmcid + '.ekb'
         tp = trips.process_xml(open(trips_fname).read())
+        # Force the correct PMID from outside
         for s in tp.statements:
             for e in s.evidence:
                 e.pmid = pmid
+        # Process REACH
         reach_fname = 'reach/' + pmcid + '.json'
         rp = reach.process_json_file(reach_fname, citation=pmid)
-        rasmodel_stmts = ras_model_statements()
+        # Get prior statements
+        rasmodel_stmts = rasmodel.get_statements()
+        # Combine all statements
         all_statements = tp.statements + rp.statements + rasmodel_stmts
+        # Run assembly
         run_assembly(all_statements, 'combined', pmcid)
