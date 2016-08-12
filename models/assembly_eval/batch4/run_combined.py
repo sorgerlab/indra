09a21badd16d1b1aefbedfa4ae859fff29e1297a
Ben Gyori <ben.gyori@gmail.com>
2016-08-12 17:01:25 +0900

    Use index card assembler PMCID override in evaluation

--- models/assembly_eval/batch4/run_combined.py
+++ models/assembly_eval/batch4/run_combined.py
@@ -5,20 +5,15 @@ import rasmodel
 
 if __name__ == '__main__':
     pmc_ids = [s.strip() for s in open('pmcids.txt', 'rt').readlines()]
-    pmids = [id_lookup(pmcid, 'pmcid')['pmid'] for pmcid in pmc_ids]
 
-    for pmid, pmcid in zip(pmids, pmc_ids):
+    for pmcid in pmc_ids:
         print 'Processing %s...' % pmcid
         # Process TRIPS
         trips_fname = 'trips/' + pmcid + '.ekb'
         tp = trips.process_xml(open(trips_fname).read())
-        # Force the correct PMID from outside
-        for s in tp.statements:
-            for e in s.evidence:
-                e.pmid = pmid
         # Process REACH
         reach_fname = 'reach/' + pmcid + '.json'
-        rp = reach.process_json_file(reach_fname, citation=pmid)
+        rp = reach.process_json_file(reach_fname)
         # Get prior statements
         rasmodel_stmts = rasmodel.get_statements()
         # Combine all statements
