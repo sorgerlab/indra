b79890125ddfdbdc2f7b973cb4d83d21b35f2e84
John Bachman <bachmanjohn@gmail.com>
2016-08-19 17:48:11 -0400

    Check Complex statements against BioGrid

--- indra/tools/analyze_complexes.py
+++ indra/tools/analyze_complexes.py
@@ -5,6 +5,8 @@ from indra.statements import Complex
 import pickle
 from indra.preassembler import Preassembler
 from indra.preassembler.hierarchy_manager import hierarchies
+from indra.databases import biogrid_client as bg
+import csv
 
 logger = logging.getLogger('analyze_complexes')
 
@@ -15,6 +17,9 @@ def load_file(stmts_file):
         results = pickle.load(f)
     return results
 
+
+def get_biogrid_interactors(gene_list):
+    pass
 
 if __name__ == '__main__':
 
@@ -43,4 +48,36 @@ if __name__ == '__main__':
                            if all([True if 'HGNC' in ag.db_refs.keys()
                                         else False
                                         for ag in s.agent_list()])]
+
+    genes = list(set([ag.db_refs['HGNC'] for stmt in protein_complexes
+                                         for ag in stmt.members]))
+
+    # Get complexes from BioGrid and combine duplicates
+    bg_complexes = bg.get_statements(genes)
+    pa_bg = Preassembler(hierarchies, bg_complexes)
+    pa_bg.combine_duplicates()
+    bg_complexes = pa_bg.unique_stmts
+
+    reach_bg_matches = []
+    for reach_stmt in protein_complexes:
+        bg_matches = []
+        for bg_stmt in bg_complexes:
+            if bg_stmt.refinement_of(reach_stmt, hierarchies):
+                bg_matches.append(bg_stmt.evidence[0].source_id)
+                print 'BG %s refines/matches REACH %s' % (bg_stmt, reach_stmt)
+        if bg_matches:
+            reach_bg_matches.append((reach_stmt, bg_matches))
+
+    not_matched = set(protein_complexes).difference(
+                        set([t[0] for t in reach_bg_matches]))
+    not_matched = sorted(not_matched, key=lambda x: len(x.evidence),
+                         reverse=True)
+
+    rows = []
+    for stmt in not_matched:
+        rows.append([stmt.members[0].name, stmt.members[1].name,
+                     len(stmt.evidence)])
+    with open('unmatched_complexes.tsv', 'w') as f:
+        csvwriter = csv.writer(f, delimiter='\t')
+        csvwriter.writerows(rows)
 
