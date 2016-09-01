208381ab2aa5ef27ff06c829f36fbad8db32f455
John Bachman <bachmanjohn@gmail.com>
2016-09-01 17:39:37 -0400

    Fix merge conflict in grounding_mapper

--- indra/preassembler/grounding_mapper.py
+++ indra/preassembler/grounding_mapper.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import sys
 from copy import deepcopy
 from indra.databases import uniprot_client
 from itertools import groupby
@@ -104,23 +105,26 @@ class GroundingMapper(object):
 def load_grounding_map(path):
     g_map = {}
     with open(path) as f:
-        mapreader = csv.reader(f, delimiter=',', quotechar='"')
-        for row in mapreader:
-            key = row[0]
-            db_refs = {'TEXT': key}
-            for pair_ix in range(0, 2):
-                col_ix = (pair_ix * 2) + 1
-                db = row[col_ix]
-                db_id = row[col_ix + 1]
-                if db == '' or db == 'None' or db_id == '' or db_id == 'None':
-                    continue
-                else:
-                    db_refs[db] = db_id
+        mapreader = csv.reader(f, delimiter='\t')
+        rows = [row for row in mapreader]
+
+    for row in rows:
+        key = row[0]
+        db_refs = {'TEXT': key}
+        keys = [entry for entry in row[1::2] if entry != '']
+        values = [entry for entry in row[2::2] if entry != '']
+        if len(keys) != len(values):
+            logger.info('ERROR: Mismatched keys and values in row %s' %
+                        str(row))
+            continue
+        else:
+            db_refs.update(dict(zip(keys, values)))
             if len(db_refs.keys()) > 1:
                 g_map[key] = db_refs
             else:
                 g_map[key] = None
     return g_map
+
 
 # Some useful functions for analyzing the grounding of sets of statements
 # Put together all agent texts along with their grounding
@@ -156,11 +160,14 @@ def agent_texts_with_grounding(stmts):
     refs_counter = Counter(refs)
     refs_counter_dict = [(dict(entry[0]), entry[1])
                          for entry in refs_counter.items()]
+    # First, sort by text
     refs_counter_dict_sorted = \
             refs_counter_dict.sort(key=lambda x: x[0].get('TEXT'))
 
+    # Then group by text
     grouped_by_text = []
     for k, g in groupby(refs_counter_dict, key=lambda x: x[0].get('TEXT')):
+        # Total occurrences of this agent text
         total = 0
         entry = [k]
         db_ref_list = []
@@ -175,11 +182,14 @@ def agent_texts_with_grounding(stmts):
                 else:
                     db_ref_list.append((db, id, count))
             total += count
+        # Sort the db_ref_list by the occurrences of each grounding
         entry.append(tuple(sorted(db_ref_list, key=lambda x: x[2],
                      reverse=True)))
+        # Now add the total frequency to the entry
         entry.append(total)
+        # And add the entry to the overall list
         grouped_by_text.append(tuple(entry))
-
+    # Sort the list by the total number of occurrences of each unique key
     grouped_by_text.sort(key=lambda x: x[2], reverse=True)
     return grouped_by_text
 
@@ -210,12 +220,15 @@ def save_base_map(filename, grouped_by_text):
                 name = uniprot_client.get_mnemonic(id)
             else:
                 name = ''
-            rows.append([text_string.encode('utf8'), db, id, count, name])
+            row = [text_string.encode('utf8'), db, id, count, name]
+            rows.append(row)
+
     with open(filename, 'w') as f:
         csvwriter = csv.writer(f, delimiter=',', quotechar='"',
-                               quoting=csv.QUOTE_MINIMAL)
+                               quoting=csv.QUOTE_MINIMAL,
+                               lineterminator='\r\n')
         csvwriter.writerows(rows)
-
+        f.write('\r\n')
 
 default_grounding_map_path = os.path.join(os.path.dirname(__file__),
                                   '../../bioentities/grounding_map.csv')
@@ -226,7 +239,13 @@ gm = default_grounding_map
 if __name__ == '__main__':
     import pickle
 
-    with open('trips_b4_stmts.pkl') as f:
+    if len(sys.argv) != 2:
+        print "Usage: %s stmt_file" % sys.argv[0]
+        sys.exit()
+    statement_file = sys.argv[1]
+
+    print "Opening statement file", statement_file
+    with open(statement_file) as f:
         st = pickle.load(f)
 
     stmts = []
@@ -239,5 +258,5 @@ if __name__ == '__main__':
     filtered_twg = [entry for entry in twg
                     if entry[0] not in default_grounding_map.keys()]
 
-    #save_base_map('trips_batch4_base_map.txt', filtered_twg)
+    save_base_map('%s_filtered_twg.csv' % statement_file, filtered_twg)
 
