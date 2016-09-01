eae7e5b2b5b020bb768bf2284a46f97e37879665
Ben Gyori <ben.gyori@gmail.com>
2016-09-01 13:17:39 -0400

    Fix typo in SBGNAssembler

--- indra/assemblers/sbgn_assembler.py
+++ indra/assemblers/sbgn_assembler.py
@@ -120,7 +120,7 @@ class SBGNAssembler(object):
                 class_name = 'process'
                 consumed = [s.obj]
             elif isinstance(s, ist.ActiveForm):
-                class_name = 'proccess'
+                class_name = 'process'
                 consumed = [s.agent]
             else:
                 logger.warning("WARNING: skipping %s" % type(s))
