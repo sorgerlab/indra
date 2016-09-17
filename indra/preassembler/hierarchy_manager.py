0dae02de5b1f8523b5cc4e8d6b095a6648bf10f2
John Bachman <bachmanjohn@gmail.com>
2016-09-16 18:54:58 -0600

    Update isa to use db ids with namespaces

--- indra/preassembler/hierarchy_manager.py
+++ indra/preassembler/hierarchy_manager.py
@@ -2,6 +2,16 @@ import os
 import rdflib
 import functools32
 
+def get_term(ns, id):
+    if ns == 'HGNC':
+        return 'http://identifiers.org/hgnc.symbol/' + id
+    elif ns == 'UP':
+        return 'http://identifiers.org/uniprot/' + id
+    elif ns == 'BE' or ns == 'INDRA':
+        return 'http://sorger.med.harvard.edu/indra/entities/' + id
+    else:
+        raise ValueError('Unknown namespace %s' % ns)
+
 class HierarchyManager(object):
     """Store hierarchical relationships between different types of entities.
 
@@ -20,52 +30,49 @@ class HierarchyManager(object):
     """
     prefixes = """
         PREFIX rn: <http://sorger.med.harvard.edu/indra/relations/>
-        PREFIX en: <http://sorger.med.harvard.edu/indra/entities/>
         """
 
     def __init__(self, rdf_file):
         """Initialize with the path to an RDF file"""
         self.graph = rdflib.Graph()
         self.graph.parse(rdf_file)
-        self.transitive_closure = {}
+        self.isa_closure = {}
+        self.partof_closure = {}
 
-    def build_transitive_closure(self, rel='isa'):
-        """Build the transitive closure of the hierarchy.
+    def build_transitive_closures(self):
+        """Build the transitive closures of the hierarchy.
 
-        This method constructs a dictionary which contains terms in the
-        hierarchy as keys and all the "isa+" related terms as values.
-
-        Parameters
-        ----------
-        rel : string
-            The relationship along which to build the transitive closure.
-            By default this is "isa" but it can also be "partof".
+        This method constructs dictionaries which contain terms in the
+        hierarchy as keys and either all the "isa+" or "partof+" related terms
+        as values.
         """
-        qstr = self.prefixes + """
-            SELECT ?x ?y WHERE {{
-                {{?x rn:{0}+ ?y .}}
-                }}
-            """.format(rel)
-        res = self.graph.query(qstr)
-        self.transitive_closure = {}
-        for x, y in res:
-            xs = x.toPython()
-            ys = y.toPython()
-            try:
-                self.transitive_closure[xs].append(ys)
-            except KeyError:
-                self.transitive_closure[xs] = [ys]
+        for rel, tc_dict in (('isa', self.isa_closure),
+                             ('partof', self.partof_closure)):
+            qstr = self.prefixes + """
+                SELECT ?x ?y WHERE {{
+                    {{?x rn:{0}+ ?y .}}
+                    }}
+                """.format(rel)
+            res = self.graph.query(qstr)
+            for x, y in res:
+                xs = x.toPython()
+                ys = y.toPython()
+                try:
+                    tc_dict[xs].append(ys)
+                except KeyError:
+                    tc_dict[xs] = [ys]
 
+    """
     @functools32.lru_cache(maxsize=100000)
     def find_entity(self, x):
-        """Get the entity that has the specified name (or synonym).
+        Get the entity that has the specified name (or synonym).
 
         Parameters
         ----------
         x : string
             Name or synonym for the target entity.
-        """
-        qstr = self.prefixes + """
+
+        qstr = self.prefixes +
             SELECT ?x WHERE {{
                 {{
                 {{ ?x rn:hasName "{0}" . }}
@@ -75,23 +82,28 @@ class HierarchyManager(object):
                 {{ ?x rn:hasId "{0}" . }}
                 }}
             }}
-            """.format(x)
+            .format(x)
         res = self.graph.query(qstr)
         if list(res):
             en = list(res)[0][0].toPython()
             return en
         else:
             return None
+    """
 
     @functools32.lru_cache(maxsize=100000)
-    def isa(self, t1, t2):
+    def isa(self, ns1, id1, ns2, id2):
         """Indicate whether one entity has an "isa" relationship to another.
 
         Parameters
         ----------
-        t1 : string
+        ns1 : string
+            Namespace code for an entity.
+        id1 : string
             URI for an entity.
-        t2 : string
+        ns2 : string
+            Namespace code for an entity.
+        id2 : string
             URI for an entity.
 
         Returns
@@ -100,15 +112,12 @@ class HierarchyManager(object):
             True if t1 has an "isa" relationship with t2, either directly or
             through a series of intermediates; False otherwise.
         """
-        en1 = self.find_entity(t1)
-        en2 = self.find_entity(t2)
+        term1 = get_term(ns1, id1)
+        term2 = get_term(ns2, id2)
 
-        if en1 is None or en2 is None:
-            return None
-
-        if self.transitive_closure:
-            ec = self.transitive_closure.get(en1)
-            if ec and en2 in ec:
+        if self.isa_closure:
+            ec = self.isa_closure.get(term1)
+            if ec is not None and term2 in ec:
                 return True
             else:
                 return False
@@ -117,7 +126,7 @@ class HierarchyManager(object):
             SELECT (COUNT(*) as ?s) WHERE {{
                 <{}> rn:isa+ <{}> .
                 }}
-            """.format(en1, en2)
+            """.format(term1, term2)
         res = self.graph.query(qstr)
         count = [r[0] for r in res][0]
         if count.toPython() == 1:
@@ -179,15 +188,15 @@ ccomp_file_path = os.path.join(os.path.dirname(__file__),
 """Default entity hierarchy loaded from the RDF file at
 `resources/entity_hierarchy.rdf`."""
 entity_hierarchy = HierarchyManager(entity_file_path)
-entity_hierarchy.build_transitive_closure()
+entity_hierarchy.build_transitive_closures()
 """Default modification hierarchy loaded from the RDF file at
 `resources/modification_hierarchy.rdf`."""
 modification_hierarchy = HierarchyManager(mod_file_path)
-modification_hierarchy.build_transitive_closure()
+modification_hierarchy.build_transitive_closures()
 """Default activity hierarchy loaded from the RDF file at
 `resources/activity_hierarchy.rdf`."""
 activity_hierarchy = HierarchyManager(act_file_path)
-activity_hierarchy.build_transitive_closure()
+activity_hierarchy.build_transitive_closures()
 """Default cellular_component hierarchy loaded from the RDF file at
 `resources/cellular_component_hierarchy.rdf`."""
 ccomp_hierarchy = HierarchyManager(ccomp_file_path)
