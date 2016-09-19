82e265a8b82dcb67b0f252265162d3088e8fc5f3
John Bachman <bachmanjohn@gmail.com>
2016-09-19 10:13:11 -0400

    Change hierarchy paths, eliminate hasName

--- indra/preassembler/make_modification_hierarchy.py
+++ indra/preassembler/make_modification_hierarchy.py
@@ -9,20 +9,12 @@ if __name__ == '__main__':
     g = Graph()
 
     isa = rn.term('isa')
-    has_name = rn.term('hasName')
 
-    g.add((en.term('Modification'), has_name, Literal('modification')))
-    g.add((en.term('Phosphorylation'), has_name, Literal('phosphorylation')))
-    g.add((en.term('Ubiquitination'), has_name, Literal('ubiquitination')))
-    g.add((en.term('Sumoylation'), has_name, Literal('sumoylation')))
-    g.add((en.term('Acetylation'), has_name, Literal('acetylation')))
-    g.add((en.term('Hydroxylation'), has_name, Literal('hydroxylation')))
+    g.add((en.term('phosphorylation'), isa, en.term('modification')))
+    g.add((en.term('ubiquitination'), isa, en.term('modification')))
+    g.add((en.term('sumoylation'), isa, en.term('modification')))
+    g.add((en.term('acetylation'), isa, en.term('modification')))
+    g.add((en.term('hydroxylation'), isa, en.term('modification')))
 
-    g.add((en.term('Phosphorylation'), isa, en.term('Modification')))
-    g.add((en.term('Ubiquitination'), isa, en.term('Modification')))
-    g.add((en.term('Sumoylation'), isa, en.term('Modification')))
-    g.add((en.term('Acetylation'), isa, en.term('Modification')))
-    g.add((en.term('Hydroxylation'), isa, en.term('Modification')))
-
-    with open('modification_hierarchy.rdf', 'wt') as out_file:
+    with open('../resources/modification_hierarchy.rdf', 'wt') as out_file:
         out_file.write(g.serialize(format='xml'))
