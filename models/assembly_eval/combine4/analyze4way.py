bca35664606a1295db1e2e0be28477ef03855735
Ben Gyori <ben.gyori@gmail.com>
2016-09-20 16:22:24 -0400

    Update plotting script

--- models/assembly_eval/combine4/analyze4way.py
+++ models/assembly_eval/combine4/analyze4way.py
@@ -1,6 +1,8 @@
 import glob
 import pickle
+import matplotlib_venn as mv
 import matplotlib.pyplot as plt
+import indra.tools.plot_formatting as pf
 
 fnames = glob.glob('combined/other_outputs/*.pkl')
 all_stmts = []
@@ -31,14 +33,16 @@ data = []
 for k in keys:
     data.append(belief[k])
 
+pf.set_fig_params()
 plt.boxplot(data)
-labels = []
-for k in keys:
-    k = k.replace('nactem', 'nactem/isi')
-    names = k.split('_')
-    names = [n.upper() for n in names]
-    labels.append('+'.join(names))
 labels = ['RE', 'TR', 'NI', 'RE + TR', 'NI + TR', 'NI + RE', 'NI + RE + TR']
 plt.ylabel('Belief probability of INDRA Statement')
 plt.xticks(range(1,8), labels, rotation='vertical')
 plt.show()
+
+plt.figure(figsize=(4, 4), dpi=150)
+venn_entries = []
+for k in keys:
+    venn_entries.append(count[k])
+mv.venn3(venn_entries, ['REACH', 'TRIPS', 'NACTEM / ISI'])
+
