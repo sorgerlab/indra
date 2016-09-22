9e5fd92b32ab1163257597c33ad4c2ef061695e3
John Bachman <bachmanjohn@gmail.com>
2016-09-22 11:34:23 -0400

    Function to check for unicode strings in objs

--- indra/util/__init__.py
+++ indra/util/__init__.py
@@ -1,17 +1,30 @@
-def has_str(obj):
+from __future__ import absolute_import, division, print_function, \
+                       unicode_literals
+
+def unicode_strs(obj):
     if type(obj) == str:
-        return True
+        return False
     # Check for an iterable
     if hasattr(obj, '__iter__'):
         for item in obj:
-            item_has_str = has_str(item)
-            if item_has_str:
-                return True
+            has_unicode_strs = unicode_strs(item)
+            if not has_unicode_strs:
+                return False
     if hasattr(obj, '__dict__'):
         for item in obj.__dict__.values():
-            item_has_str = has_str(item)
-            if item_has_str:
-                return True
-    return False
+            has_unicode_strs = unicode_strs(item)
+            if not has_unicode_strs:
+                return False
+    if isinstance(obj, dict):
+        for k, v in obj.items():
+            k_has_unicode_strs = unicode_strs(k)
+            v_has_unicode_strs = unicode_strs(v)
+            if not k_has_unicode_strs or not v_has_unicode_strs:
+                return False
+    return True
 
 
+#foo = {u'a':u'b', u'c':[u'a', u'b', u'c']}
+foo = {'a':'b', 'c':['a', 'b', 'c']}
+
+print(unicode_strs(foo))
