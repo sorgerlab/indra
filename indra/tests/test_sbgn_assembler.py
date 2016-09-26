c9a6733d67823f967ae7f30fc8d4736843f70f82
John Bachman <bachmanjohn@gmail.com>
2016-09-26 00:17:23 -0400

    Skip SHA1 SBGN test for Python 3
    The SHA1 changes each time due to non-deterministic ordering of the tags
    (but the content is otherwise the same from run to run).

--- indra/tests/test_sbgn_assembler.py
+++ indra/tests/test_sbgn_assembler.py
@@ -1,4 +1,5 @@
 from __future__ import print_function, unicode_literals
+import sys
 from os.path import dirname, join
 from hashlib import sha1
 from indra.assemblers.sbgn_assembler import text_to_sbgn
@@ -8,6 +9,12 @@ test_small_file = join(dirname(__file__), 'test_small.xml')
 # Simple smoke test to ensure the test_small file produces known-correct output.
 def test_assembler():
     sbgn_output = text_to_sbgn(trips_xml=open(test_small_file).read())
-    assert (sha1(sbgn_output).hexdigest() ==
-            '99f74446e6e5f0be4a37666c7ac6b74afe5290d0')
+    # The Python3 implementation of lxml produces XML with the tags in
+    # different orders each time, so the SHA1 isn't constant
+    if sys.version_info > (3, 0):
+        pass
+    # This still works for Python 2
+    else:
+        assert (sha1(sbgn_output).hexdigest() ==
+                '99f74446e6e5f0be4a37666c7ac6b74afe5290d0')
 
