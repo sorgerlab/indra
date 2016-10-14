066ac690d9d4537b58fce4e9c00c8e5855e67aca
John Bachman <bachmanjohn@gmail.com>
2016-10-14 14:02:15 -0400

    Commit of Degradation statement

--- indra/statements.py
+++ indra/statements.py
@@ -1442,6 +1442,70 @@ class Translocation(Statement):
                 str(self.to_location))
         return str(key)
 
+@python_2_unicode_compatible
+class Degradation(Statement):
+    """Degradation of a protein, possibly mediated by another protein.
+
+    Parameters
+    ----------
+    subj : :py:class`indra.statement.Agent`
+        The protein mediating the degradation.
+    obj : :py:class:`indra.statement.Agent`
+        The protein that is degraded.
+    evidence : list of :py:class:`Evidence`
+        Evidence objects in support of the degradation statement.
+    """
+    def __init__(self, subj, obj, evidence=None):
+        super(Statement, self).__init__(evidence)
+        self.subj = subj
+        self.obj = obj
+
+    def matches_key(self):
+        if self.subj is None:
+            subj_key = None
+        else:
+            subj_key = self.subj.matches_key()
+        key = (type(self), subj_key, self.obj.matches_key())
+        return str(key)
+
+    def agent_list(self):
+        return [self.subj, self.obj]
+
+    def set_agent_list(self, agent_list):
+        if len(agent_list) != 2:
+            raise ValueError("Degradation has two agents in agent_list.")
+        self.subj = agent_list[0]
+        self.obj = agent_list[1]
+
+    def refinement_of(self, other, hierarchies):
+        # Make sure the statement types match
+        if type(self) != type(other):
+            return False
+
+        # Check agent arguments
+        if self.subj is None and other.subj is None:
+            subj_refinement = True
+        elif self.subj is None and other.subj is not None:
+            subj_refinement = False
+        elif self.subj is not None and other.subj is None:
+            subj_refinement = True
+        else:
+            subj_refinement = self.subj.refinement_of(other.subj, hierarchies)
+        obj_refinement = self.obj.refinement_of(other.obj, hierarchies)
+        if subj_refinement and obj_refinement:
+            return True
+        else:
+            return False
+
+    def equals(self, other):
+        matches = super(Statement, self).equals(other)
+        return matches
+
+    def __str__(self):
+        s = ("%s(%s, %s)" %
+                  (type(self).__name__, self.enz, self.sub))
+        return s
+
 
 def get_valid_residue(residue):
     """Check if the given string represents a valid amino acid residue."""
