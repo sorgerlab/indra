631e6c0d8bd196568792293cdc29bb935865f141
John Bachman <bachmanjohn@gmail.com>
2016-10-15 09:52:15 -0400

    Initial implementation of children lookup

--- indra/tests/test_expand_families.py
+++ indra/tests/test_expand_families.py
@@ -5,6 +5,25 @@ from indra.preassembler.hierarchy_manager import hierarchies
 from indra.statements import Agent, Phosphorylation
 from indra.tools import expand_families as ef
 
+def test_component_children_lookup():
+    raf = Agent('RAF', db_refs={'BE':'RAF'})
+    braf = Agent('BRAF', db_refs={'HGNC':'1097'})
+    # Look up RAF
+    import ipdb; ipdb.set_trace()
+    exp = ef.Expander(hierarchies)
+    rafs = exp.get_children(raf)
+    assert isinstance(rafs, list)
+    assert len(rafs) == 3
+    # The lookup of a gene-level entity should not return any additional
+    # entities
+    brafs = exp.get_children(braf)
+    assert isinstance(brafs, list)
+    assert len(brafs) == 0
+    # The lookup for a top-level family (e.g., MAPK, which has as children
+    # both the intermediate family ERK as well as all MAPK1-15 members)
+    # should not return the intermediate families when a filter is applied.
+    #mapk = Agent('MAPK', db_refs={'BE':'MAPK'})
+    #mapks = exp.get_children(mapks)
 
 def test_expand_families():
     raf = Agent('RAF', db_refs={'BE':'RAF'})
@@ -13,3 +32,5 @@ def test_expand_families():
     exp = ef.Expander(hierarchies)
     expanded_stmts = exp.expand_families([st])
 
+if __name__ == '__main__':
+    test_component_children_lookup()
--- indra/tools/expand_families.py
+++ indra/tools/expand_families.py
@@ -1,9 +1,39 @@
 from __future__ import print_function, unicode_literals, absolute_import
 from builtins import dict, str
+import logging
+
+logger = logging.getLogger('expand_families')
 
 class Expander(object):
     def __init__(self, hierarchies):
-        self.hierarchies = hierarchies
+        self.entities = hierarchies['entity']
+        # Build reverse lookup dict from the entity hierarchy
+        self.families = {}
+        self.complexes = {}
+        logger.info('Generating reverse lookup table for families')
+        for child, parents in self.entities.isa_closure.items():
+            for parent in parents:
+                children = self.families.get(parent, [])
+                children.append(child)
+                self.families[parent] = children
+        logger.info('Generating reverse lookup table for complexes')
+        for subunit, complexes in self.entities.partof_closure.items():
+            for complex in complexes:
+                subunits = self.complexes.get(complex, [])
+                subunits.append(subunit)
+                self.complexes[complex] = subunits
+
+    def get_children(self, agent):
+        # Get the grounding for the agent
+        (ns, id) = agent.get_grounding()
+        # Get URI for agent
+        ag_uri = self.entities.get_uri(ns, id)
+        # Look up the children for this family
+        children = self.families.get(ag_uri)
+        if children is None:
+            return []
+        else:
+            return children
 
     def expand_families(self, stmts):
         pass
