09ac99407dc3bd3004e4f0fb26d7bf821b32a2f4
Ben Gyori <ben.gyori@gmail.com>
2016-10-17 16:31:48 -0400

    Update run_machine script

--- models/rasmachine/run_machine.sh
+++ models/rasmachine/run_machine.sh
@@ -11,10 +11,6 @@ fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 export PYTHONPATH=../../../indra:..
-twitter_cred="$model_name/twitter_cred.txt"
-gmail_cred="$model_name/gmail_cred.txt"
-ndex_cred="$model_name/ndex_cred.txt"
 log_file="$model_name/log.txt"
-err_file="$model_name/err.txt"
-belief_file="$model_name/belief.txt"
-python rasmachine.py --model $model_name --twitter $twitter_cred --gmail $gmail_cred --ndex $ndex_cred --belief $belief_file 1>> $log_file 2>> $err_file
+config_file="$model_name/config.yaml"
+python rasmachine.py $config_file >> $log_file 2>> $log_file
