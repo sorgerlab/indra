bd475aeda114c60a714761664d215f3b957b88b8
John Bachman <bachmanjohn@gmail.com>
2016-10-17 11:16:44 -0400

    Working lookup of children from entity hierarchy

--- indra/tests/test_expand_families.py
+++ indra/tests/test_expand_families.py
@@ -4,26 +4,40 @@ from indra.preassembler.hierarchy_manager import hierarchies
 
 from indra.statements import Agent, Phosphorylation
 from indra.tools import expand_families as ef
+from indra.util import unicode_strs
 
 def test_component_children_lookup():
     raf = Agent('RAF', db_refs={'BE':'RAF'})
     braf = Agent('BRAF', db_refs={'HGNC':'1097'})
     # Look up RAF
-    import ipdb; ipdb.set_trace()
     exp = ef.Expander(hierarchies)
     rafs = exp.get_children(raf)
+    # Should get three family members
     assert isinstance(rafs, list)
     assert len(rafs) == 3
+    assert unicode_strs(rafs)
     # The lookup of a gene-level entity should not return any additional
     # entities
     brafs = exp.get_children(braf)
     assert isinstance(brafs, list)
     assert len(brafs) == 0
+    assert unicode_strs(brafs)
     # The lookup for a top-level family (e.g., MAPK, which has as children
     # both the intermediate family ERK as well as all MAPK1-15 members)
     # should not return the intermediate families when a filter is applied.
-    #mapk = Agent('MAPK', db_refs={'BE':'MAPK'})
-    #mapks = exp.get_children(mapks)
+    mapk = Agent('MAPK', db_refs={'BE':'MAPK'})
+    mapks = exp.get_children(mapk, ns_filter=None)
+    assert len(mapks) == 3
+    assert ('HGNC', 'MAPK1') in mapks
+    assert ('HGNC', 'MAPK3') in mapks
+    assert ('BE', 'ERK') in mapks
+    assert unicode_strs(mapks)
+    # Now do the same expansion with a namespace filter
+    mapks = exp.get_children(mapk, ns_filter='HGNC')
+    assert unicode_strs(mapks)
+    assert len(mapks) == 2
+    assert ('HGNC', 'MAPK1') in mapks
+    assert ('HGNC', 'MAPK3') in mapks
 
 def test_expand_families():
     raf = Agent('RAF', db_refs={'BE':'RAF'})
--- indra/tools/expand_families.py
+++ indra/tools/expand_families.py
@@ -1,6 +1,8 @@
 from __future__ import print_function, unicode_literals, absolute_import
 from builtins import dict, str
 import logging
+from indra.preassembler.make_entity_hierarchy import ns_map
+import rdflib.namespace
 
 logger = logging.getLogger('expand_families')
 
@@ -8,32 +10,52 @@ class Expander(object):
     def __init__(self, hierarchies):
         self.entities = hierarchies['entity']
         # Build reverse lookup dict from the entity hierarchy
-        self.families = {}
-        self.complexes = {}
+        self._children = {}
         logger.info('Generating reverse lookup table for families')
-        for child, parents in self.entities.isa_closure.items():
+        all_children = set(self.entities.isa_closure.keys()).union(
+                       self.entities.partof_closure.keys())
+        for child in all_children:
+            parents = self._get_parents(child)
             for parent in parents:
-                children = self.families.get(parent, [])
-                children.append(child)
-                self.families[parent] = children
-        logger.info('Generating reverse lookup table for complexes')
-        for subunit, complexes in self.entities.partof_closure.items():
-            for complex in complexes:
-                subunits = self.complexes.get(complex, [])
-                subunits.append(subunit)
-                self.complexes[complex] = subunits
+                children_list = self._children.get(parent, [])
+                children_list.append(child)
+                self._children[parent] = children_list
 
-    def get_children(self, agent):
+    def _get_parents(self, child):
+        immediate_parents = set(self.entities.isa_closure.get(child, [])).union(
+                      set(self.entities.partof_closure.get(child, [])))
+        all_parents = set([])
+        for parent in immediate_parents:
+            grandparents = self._get_parents(parent)
+            all_parents = all_parents.union(grandparents)
+        return all_parents.union(immediate_parents)
+
+    def get_children(self, agent, ns_filter='HGNC'):
         # Get the grounding for the agent
         (ns, id) = agent.get_grounding()
         # Get URI for agent
         ag_uri = self.entities.get_uri(ns, id)
         # Look up the children for this family
-        children = self.families.get(ag_uri)
-        if children is None:
+        children_uris = self._children.get(ag_uri)
+        if children_uris is None:
             return []
-        else:
-            return children
+        # Parse children URI list into namespaces and ID
+        children_parsed = []
+        for child_uri in children_uris:
+            (child_ns, child_id) = rdflib.namespace.split_uri(child_uri)
+            child_ns_name = ns_map.get(child_ns)
+            # This shouldn't happen, so generate a logging message if it does
+            if child_ns_name is None:
+                logger.warning('Unmatched namespace %s in URI %s, skipping' %
+                               (child_ns, child_uri))
+                continue
+            # If ns_filter is None, add in all children
+            if ns_filter is None:
+                children_parsed.append((child_ns_name, child_id))
+            # Otherwise, only add children with a matching namespace
+            elif child_ns_name == ns_filter:
+                children_parsed.append((child_ns_name, child_id))
+        return children_parsed
 
     def expand_families(self, stmts):
         pass
