63e38c1f0e04338e0115fdb0771e9e6896ad6a85
Ben Gyori <ben.gyori@gmail.com>
2016-10-21 09:54:20 -0400

    Implement caching reading output for p53 models

--- models/word_models/run_p53_model.py
+++ models/word_models/run_p53_model.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 from indra import trips
@@ -5,8 +6,21 @@ from indra.assemblers import PysbAssembler
 from pysb import Observable, Parameter
 from pysb.integrate import Solver
 
-def assemble_model(model_name):
-    tp = trips.process_text(open(model_name + '.txt').read())
+def assemble_model(model_name, reread=False):
+    xml_fname = model_name + '.xml'
+    if not reread:
+        print('Processing %s' % xml_fname)
+        if os.path.exists(xml_fname):
+            with open(xml_fname, 'rb') as fh:
+                tp = trips.process_xml(fh.read())
+        else:
+            reread = True
+    if reread:
+        fname = model_name + '.txt'
+        print('Reading %s' % fname)
+        with open(fname, 'rb') as fh:
+            tp = trips.process_text(fh.read(), xml_fname)
+
     pa = PysbAssembler()
     pa.add_statements(tp.statements)
     model = pa.make_model()
@@ -33,14 +47,12 @@ def assemble_model(model_name):
         model.initial(atm(act='active'),
                       model.parameters['ATMa_0'])
         model.parameters['kf_pa_act_1'].value = 1e-04
-        model.parameters['ATM_0'].value = 99.0
 
     if model_name == 'p53_ATR':
         model.add_component(Parameter('ATRa_0', 1))
         atr = model.monomers['ATR']
         model.initial(atr(act='active'),
                       model.parameters['ATRa_0'])
-        model.parameters['ATR_0'].value = 99.0
 
     pa.model = model
     pa.save_model('%s.py' % model_name)
@@ -51,15 +63,17 @@ def run_model(model):
     ts = np.linspace(0, sim_hours*3600, sim_hours*60)
     solver = Solver(model, ts)
     solver.run()
+    plt.figure()
     plt.plot(ts, solver.yobs['P53_active'], 'r')
     plt.xticks([])
-    plt.xlabel('time (a.u)', fontsize=15)
+    plt.xlabel('Time (a.u.)', fontsize=15)
     plt.ylabel('Active p53')
     plt.yticks([])
     plt.savefig(model_name + '.png')
     return ts, solver
 
 if __name__ == '__main__':
-    model_name = 'p53_ATR'
-    model = assemble_model(model_name)
-    ts, solver = run_model(model)
+    model_names = ['p53_ATR', 'p53_ATM']
+    for model_name in model_names:
+        model = assemble_model(model_name, reread=False)
+        ts, solver = run_model(model)
