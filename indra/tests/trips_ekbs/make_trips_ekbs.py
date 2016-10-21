f5e90c5e4e9bc672ae290861d468fc6972ed786c
Ben Gyori <ben.gyori@gmail.com>
2016-10-21 15:06:32 -0400

    Refactor making EKB tests to add single new test

--- indra/tests/trips_ekbs/make_trips_ekbs.py
+++ indra/tests/trips_ekbs/make_trips_ekbs.py
@@ -1,11 +1,15 @@
 import re
 from indra import trips
 
-lines = open('trips_ekb_sentences.txt', 'rt').readlines()
-for i, l in enumerate(lines):
-    txt = l.strip()
+def make_ekb(txt):
     fname = re.sub('[^a-zA-Z0-9]', '_', txt[:-1]) + '.ekb'
     tp = trips.process_text(txt, save_xml_name=fname)
     print(txt)
     print(tp.statements)
-    print('---')
+
+if __name__ == '__main__':
+    lines = open('trips_ekb_sentences.txt', 'rt').readlines()
+    for i, l in enumerate(lines):
+        txt = l.strip()
+        make_ekb(txt)
+        print('---')
