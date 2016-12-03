5db3b6d8596844f153e2fcf5477af5bd0fb5d4d7
Ben Gyori <ben.gyori@gmail.com>
2016-12-02 19:42:43 -0500

    Extract detailed Phosphorylation and Agents

--- indra/sparser/processor.py
+++ indra/sparser/processor.py
@@ -1,7 +1,9 @@
 from __future__ import absolute_import, print_function, unicode_literals
 from builtins import dict, str
 import logging
+import collections
 from indra.statements import Phosphorylation, Agent, Evidence
+from indra.databases import uniprot_client
 
 logger = logging.getLogger('sparser')
 
@@ -10,21 +12,74 @@ class SparserProcessor(object):
     def __init__(self, xml_etree):
         self.tree = xml_etree
         self.statements = []
+        self._sems = collections.defaultdict(list)
+        for interp in self.tree.findall('interpretation'):
+            sentence = interp.find('sentence-text').text
+            sems = interp.findall('sem')
+            for sem in sems:
+                category = sem.find('ref').attrib['category']
+                self._sems[category].append((sem, sentence))
 
     def get_phosphorylations(self):
-        phos_events = self.tree.findall("sem/ref/[@category='phosphorylate']")
-        for event in phos_events:
-            enzyme = event.find("var/[@name='agent']/ref")
+        phos_events = self._sems.get('phosphorylate')
+        for event, sentence in phos_events:
+            # Get enzyme agent
+            enzyme = event.find("ref/var/[@name='agent']/ref")
             if enzyme is None:
                 enz = None
             else:
-                enz_name = enzyme.attrib.get('name')
-                enz = Agent(enz_name)
-            substrate = event.find("var/[@name='substrate']/ref")
-            sub_name = substrate.attrib.get('name')
-            sub = Agent(sub_name)
-            st = Phosphorylation(enz, sub)
+                enz = self._get_agent_from_ref(enzyme)
+
+            # Get substrate agent
+            substrate = event.find("ref/var/[@name='substrate']/ref")
+            sub = self._get_agent_from_ref(substrate)
+
+            # Get site
+            residue = None
+            position = None
+            site = event.find("ref/var/[@name='site']")
+            if site is not None:
+                residue, position = self._get_site(site)
+
+            st = Phosphorylation(enz, sub, residue, position)
             self.statements.append(st)
+
+    def _get_agent_from_ref(self, ref):
+        name = ref.attrib.get('name')
+        if name is None:
+            name = ref.find("var/[@name='name']").text
+            uid = ref.find("var/[@name='uid']").text
+        else:
+            uid = ref.attrib.get('uid')
+        db_refs = {}
+        if uid.startswith('UP:'):
+            up_mnemonic = uid[3:]
+            up_id = uniprot_client.get_id_from_mnemonic(up_mnemonic)
+            name = uniprot_client.get_gene_name(up_id)
+            db_refs['UP'] = up_id
+
+        agent = Agent(name, db_refs=db_refs)
+        return agent
+
+    @staticmethod
+    def _get_amino_acid(ref):
+        name_tag = ref.find("var/[@name='name']")
+        if name_tag is not None:
+            return name_tag.text
+        return None
+
+    @staticmethod
+    def _get_site(site):
+        residue = None
+        position = None
+        residue_tag = site.find("ref/[@category='residue-on-protein']")
+        if residue_tag is not None:
+            residue_aa_tag = residue_tag.find("var/[@name='amino-acid']/ref")
+            residue = SparserProcessor._get_amino_acid(residue_aa_tag)
+            position_tag = residue_tag.find("var/[@name='position']")
+            if position_tag is not None:
+                position = position_tag.text.strip()
+        return residue, position
 
     def _get_evidence(self, sem):
         ev = Evidence(source_api='sparser')
--- indra/tests/test_sparser.py
+++ indra/tests/test_sparser.py
@@ -1,6 +1,7 @@
 from indra import sparser
 
-xml_str = '''
+xml_str1 = '''
+<article>
  <interpretation>
  <sentence-text>MEK1 phosphorylates ERK1</sentence-text>
  <sem>
@@ -15,13 +16,61 @@ xml_str = '''
  </ref>
  </sem>
 </interpretation>
+</article>
 '''
+
+xml_str2 = '''
+<article>
+<interpretation>
+  <sentence-text>Hence ASPP2 can be phosphorylated at serine 827 by MAPK1 in vitro</sentence-text>
+  <sem>
+    <ref category="phosphorylate">
+      <var name="subordinate-conjunction">
+          <ref category="subordinate-conjunction"><var name="word">hence</var></ref></var>
+      <var name="substrate">
+          <ref category="protein" name="ASPP2_HUMAN" uid="UP:ASPP2_HUMAN"/>
+      </var>
+      <var name="agent">
+        <ref category="protein">
+          <var name="context">
+            <ref category="in-vitro"></ref>
+          </var>
+          <var name="uid">UP:MK01_HUMAN</var>
+          <var name="name">MK01_HUMAN</var>
+        </ref>
+      </var>
+      <var name="site">
+        <ref category="residue-on-protein">
+          <var name="amino-acid">
+            <ref category="amino-acid"><var name="name">serine</var></ref>
+          </var>
+          <var name="position"> 827</var>
+        </ref>
+      </var>
+      <var name="modal"><ref category="can"></ref></var>
+    </ref>
+  </sem>
+</interpretation>
+</article>
+'''
+
 
 def test_invalid_xml():
     sp = sparser.process_xml('xyz')
     assert(sp is None)
 
+
 def test_phosphorylation():
-    sp = sparser.process_xml(xml_str)
-    print(sp.tree)
+    sp = sparser.process_xml(xml_str1)
     assert(len(sp.statements) == 1)
+    assert(sp.statements[0].enz.name == 'MAP2K1')
+    assert(sp.statements[0].sub.name == 'MAPK3')
+
+
+def test_phosphorylation2():
+    sp = sparser.process_xml(xml_str2)
+    assert(len(sp.statements) == 1)
+    assert(sp.statements[0].enz.name == 'MAPK1')
+    assert(sp.statements[0].sub.name == 'TP53BP2')
+    assert(sp.statements[0].residue == 'S')
+    assert(sp.statements[0].position == '827')
