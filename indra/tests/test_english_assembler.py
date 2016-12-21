18f41cbf6d1cb3459efe84a8c7e7c34998a7b06a
John Bachman <bachmanjohn@gmail.com>
2016-12-21 15:19:13 -0500

    Update english assembler

--- indra/tests/test_english_assembler.py
+++ indra/tests/test_english_assembler.py
@@ -198,12 +198,20 @@ def test_autophos():
     assert(s == 'EGFR phosphorylates itself on tyrosine.')
 
 def test_activation():
-    st = Activation(Agent('MEK'), Agent('ERK'), True)
+    st = Activation(Agent('MEK'), Agent('ERK'))
     e = ea.EnglishAssembler()
     e.add_statements([st])
     s = e.make_model()
     print(s)
     assert(s == 'MEK activates ERK.')
+
+def test_inhibition():
+    st = Inhibition(Agent('MEK'), Agent('ERK'))
+    e = ea.EnglishAssembler()
+    e.add_statements([st])
+    s = e.make_model()
+    print(s)
+    assert(s == 'MEK inhibits ERK.')
 
 def test_agent_loc():
     a = Agent('BRAF', location='cytoplasm')
