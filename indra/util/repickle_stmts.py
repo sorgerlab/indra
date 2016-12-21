04640bedd60639a5319bd7f288f0b6ca15b539af
John Bachman <bachmanjohn@gmail.com>
2016-12-21 14:14:21 -0500

    Fix problems with repickle_stmts

--- indra/util/repickle_stmts.py
+++ indra/util/repickle_stmts.py
@@ -1,11 +1,14 @@
-import sys
-import pickle
+from __future__ import absolute_import, print_function, unicode_literals
+from builtins import dict, str
 
-filename = sys.argv[1]
+if __name__ == '__main__':
+    import sys
+    import pickle
 
-with open(filename, 'rb') as f:
-    stmts = pickle.load(f)
+    filename = sys.argv[1]
 
-with open('%s.new' % filename, 'wb') as f:
-    pickle.dump(stmts, f)
+    with open(filename, 'rb') as f:
+        stmts = pickle.load(f)
 
+    with open('%s.new' % filename, 'wb') as f:
+        pickle.dump(stmts, f, protocol=2)
