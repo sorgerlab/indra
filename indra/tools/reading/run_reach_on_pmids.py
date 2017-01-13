a74d217c9a1c4099ef23faf5fc738cbee4551239
John Bachman <bachmanjohn@gmail.com>
2017-01-13 16:06:46 -0500

    Turn off force reading

--- indra/tools/reading/run_reach_on_pmids.py
+++ indra/tools/reading/run_reach_on_pmids.py
@@ -69,7 +69,7 @@ if __name__ == '__main__':
     path_to_reach = '/pmc/reach/target/scala-2.11/reach-gordo-1.3.3-SNAPSHOT.jar'
     #path_to_reach = '/Users/johnbachman/Dropbox/1johndata/Knowledge File/Biology/Research/Big Mechanism/reach/target/scala-2.11/reach-gordo-1.3.3-SNAPSHOT.jar'
     reach_version = '1.3.3-5af7782c76'
-    force_read = True
+    force_read = False
     force_fulltext = False
 
     # Check the arguments
