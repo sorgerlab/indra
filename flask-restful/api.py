c4482bfa20a29314df04fe01f8ee15726036989b
Petar Todorov <petar.v.todorov@gmail.com>
2017-02-18 11:42:54 -0500

    Parse and load
    Split the API into parse and load resources.
    
    Examples using requests
    In [1]: from requests import post
    In [2]: from indra.statements import *
    In [3]: api_parse = post('http://localhost:5000/parse',
                             data={'txt': 'Ras phosphorylates Raf.'}).json()
    This will return a json dumped into a string.
    
    In [4]: post('http://localhost:5000/load',data={'json':api_parse})
    This will return a REST 201 response to the user. On the server side it
    loads the JSON string and uses Statement.from_json() to generate a list
    of INDRA statements. Indra statements will be printed in the console.

--- flask-restful/api.py
+++ flask-restful/api.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 api = Api(app)
 parser = reqparse.RequestParser()
 parser.add_argument('txt')
+parser.add_argument('json')
 
 class InputText(Resource):
     def post(self):
@@ -20,9 +21,25 @@ class InputText(Resource):
 	for s in st:
             s_json = s.to_json()
             json_statements['statements'].append(s_json)
+        json_statements = json.dumps(json_statements)
         return json_statements, 201
 
-api.add_resource(InputText, '/')
+api.add_resource(InputText, '/parse')
+
+class InputStmtJSON(Resource):
+    def post(self):
+        args = parser.parse_args()
+        print(args)
+        json_data = args['json']
+        json_dict = json.loads(json_data)
+	st = []
+	for j in json_dict['statements']:
+            s = Statement.from_json(j)
+            print(s)
+            st.append(s)
+        return 201
+
+api.add_resource(InputStmtJSON, '/load')
 
 if __name__ == '__main__':
     app.run(debug=True)
