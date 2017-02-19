277f6b2d0e00ea7260e7963143bf90bef3003dd6
Ben Gyori <ben.gyori@gmail.com>
2017-02-19 12:29:59 -0500

    Add more endpoints and some cleanup

--- rest_api/api.py
+++ rest_api/api.py
@@ -1,8 +1,12 @@
 import json
-from bottle import route, run, request, post, default_app
+import logging
+from bottle import route, run, request, default_app
 from indra import trips, reach, bel, biopax
 from indra.statements import *
-from indra.assemblers import PysbAssembler
+from indra.assemblers import PysbAssembler, CxAssembler, GraphAssembler
+
+logger = logging.getLogger('rest_api')
+logger.setLevel(logging.DEBUG)
 
 ###### INPUT PROCESSING #######
 
@@ -13,8 +17,7 @@ def trips_process_text():
     text = body.get('text')
     tp = trips.process_text(text)
     if tp and tp.statements:
-        stmts = json.dumps([json.loads(st.to_json()) for st
-                            in tp.statements])
+        stmts = _stmts_to_json(tp.statements)
         res = {'statements': stmts}
         return res
     else:
@@ -29,8 +32,7 @@ def reach_process_text():
     text = body.get('text')
     rp = reach.process_text(text)
     if rp and rp.statements:
-        stmts = json.dumps([json.loads(st.to_json()) for st
-                            in rp.statements])
+        stmts = _stmts_to_json(rp.statements)
         res = {'statements': stmts}
         return res
     else:
@@ -43,14 +45,43 @@ def reach_process_pmc():
     pmcid = body.get('pmcid')
     rp = reach.process_pmc(pmcid)
     if rp and rp.statements:
-        stmts = json.dumps([json.loads(st.to_json()) for st
-                            in rp.statements])
+        stmts = _stmts_to_json(rp.statements)
         res = {'statements': stmts}
         return res
     else:
         res = {'statements': []}
     return res
 ##################
+
+### BEL ###
+@route('/bel/process_ndex_neighborhood', method='POST')
+def bel_process_ndex_neighborhood():
+    body = json.load(request.body)
+    genes = body.get('genes')
+    logger.debug(genes)
+    bp = bel.process_ndex_neighborhood(genes)
+    if bp and bp.statements:
+        stmts = _stmts_to_json(bp.statements)
+        res = {'statements': stmts}
+        return res
+    else:
+        res = {'statements': []}
+    return res
+
+### BioPAX ###
+@route('/biopax/process_pc_pathsbetween', method='POST')
+def biopax_process_pc_pathsbetween():
+    body = json.load(request.body)
+    genes = body.get('genes')
+    logger.debug(genes)
+    bp = bel.process_ndex_neighborhood(genes)
+    if bp and bp.statements:
+        stmts = _stmts_to_json(bp.statements)
+        res = {'statements': stmts}
+        return res
+    else:
+        res = {'statements': []}
+    return res
 
 ### OUTPUT ASSEMBLY ####################
 
@@ -60,14 +91,46 @@ def reach_process_pmc():
 def assemble_pysb():
     body = json.load(request.body)
     stmts_str = body.get('statements')
-    stmts_json = json.loads(stmts_str)
-    stmts = [Statement.from_json(json.dumps(stj)) for stj in stmts_json]
+    stmts = _stmts_from_json(stmts_str)
     pa = PysbAssembler()
     pa.add_statements(stmts)
     pa.make_model()
     model_str = pa.print_model()
     res = {'model': model_str}
     return res
+
+### CX ###
+
+@route('/assemblers/cx', method='POST')
+def assemble_cx():
+    body = json.load(request.body)
+    stmts_str = body.get('statements')
+    stmts = _stmts_from_json(stmts_str)
+    ca = CxAssembler(stmts)
+    model_str = ca.make_model()
+    res = {'model': model_str}
+    return res
+
+### GRAPH ###
+
+@route('/assemblers/graph', method='POST')
+def assemble_grapg():
+    body = json.load(request.body)
+    stmts_str = body.get('statements')
+    stmts = _stmts_from_json(stmts_str)
+    ga = GraphAssembler(stmts)
+    model_str = ga.make_model()
+    res = {'model': model_str}
+    return res
+
+def _stmts_to_json(stmts):
+    stj = json.dumps([json.loads(st.to_json()) for st in stmts])
+    return stj
+
+def _stmts_from_json(stmts_str):
+    stmts_json = json.loads(stmts_str)
+    stmts = [Statement.from_json(json.dumps(stj)) for stj in stmts_json]
+    return stmts
 
 if __name__ == '__main__':
     app = default_app()
