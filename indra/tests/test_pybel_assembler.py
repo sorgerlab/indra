9256a0fc95eac0f8c8e9621f8362c62e67fe3911
John Bachman <bachmanjohn@gmail.com>
2017-02-23 16:43:49 -0500

    Fix test so it passes now

--- indra/tests/test_pybel_assembler.py
+++ indra/tests/test_pybel_assembler.py
@@ -1,4 +1,6 @@
 from indra.assemblers import pybel_assembler as pa
+from indra.statements import *
+import pybel
 
 def test_simple_phosphorylation():
     braf = Agent('BRAF', db_refs={'HGNC': '1097', 'UP': 'P15056'})
@@ -6,4 +8,8 @@ def test_simple_phosphorylation():
     stmt = Phosphorylation(braf, mek, 'S', '218')
     pba = pa.PybelAssembler([stmt])
     belgraph = pba.make_model()
-    belgraph.to_bel('test.bel')
+    pybel.to_bel(belgraph)
+    assert len(belgraph.nodes()) == 2
+    assert ('Protein', 'HGNC', 'BRAF') in belgraph.nodes()
+    assert ('Protein', 'HGNC', 'MAP2K1') in belgraph.nodes()
+    assert len(belgraph.edges()) == 1
