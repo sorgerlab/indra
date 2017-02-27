824715e9b42d99a54230f493cc6a724183745356
Ben Gyori <ben.gyori@gmail.com>
2017-02-27 18:59:16 -0500

    Unicode fixes and improvements in read phosphosite

--- models/phase3_eval/read_phosphosite.py
+++ models/phase3_eval/read_phosphosite.py
@@ -2,6 +2,7 @@ from __future__ import print_function, absolute_import, unicode_literals
 from builtins import dict, str
 import pandas
 from copy import deepcopy
+from indra.util import unicode_strs
 from indra.databases import hgnc_client, uniprot_client
 from indra.statements import Agent, Phosphorylation, ModCondition, Evidence
 
@@ -12,17 +13,23 @@ def read_phosphosite(fname):
     for _, row in df.iterrows():
         sub_upid = row['SUB_ID']
         if not pandas.isnull(sub_upid):
+            sub_upid = sub_upid.decode('utf-8') # to guarantee unicode
+            if sub_upid.find('-') != -1:
+                sub_upid = sub_upid.split('-')[0]
             sub_hgnc_symbol = uniprot_client.get_gene_name(sub_upid)
             sub_hgnc = hgnc_client.get_hgnc_id(sub_hgnc_symbol)
         else:
             sub_hgnc_symbol = row['SUB_GENE']
+            sub_hgnc_symbol = sub_hgnc_symbol.decode('utf-8')
             sub_hgnc_id = hgnc_client.get_hgnc_id(sub_hgnc_symbol)
             sub_upid = hgnc_client.get_uniprot_id(sub_hgnc_id)
+            if sub_upid is None:
+                continue
         sub = Agent(sub_hgnc_symbol,
                     db_refs={'UP': sub_upid,'HGNC': sub_hgnc})
-        residue = row['Actual_site'][0]
+        residue = row['Actual_site'][0].decode('utf-8')
         if len(row['Actual_site']) > 1:
-            position = row['Actual_site'][1:]
+            position = row['Actual_site'][1:].decode('utf-8')
         else:
             position = None
 
@@ -44,17 +51,24 @@ def read_phosphosite(fname):
 
         kin_upid = row['KIN_ID']
         if not pandas.isnull(kin_upid):
+            kin_upid = kin_upid.decode('utf-8') # to guarantee unicode
+            if kin_upid.find('-') != -1:
+                kin_upid = kin_upid.split('-')[0]
             if not uniprot_client.is_human(kin_upid):
-                print('%s non human' % kin_upid)
                 continue
             kin_hgnc_symbol = uniprot_client.get_gene_name(kin_upid)
             kin_hgnc = hgnc_client.get_hgnc_id(kin_hgnc_symbol)
         else:
             kin_hgnc_symbol = row['KINASE_GENE_SYMBOL']
+            if pandas.isnull(kin_hgnc_symbol):
+                continue
+            kin_hgnc_symbol = kin_hgnc_symbol.decode('utf-8')
             kin_hgnc_id = hgnc_client.get_hgnc_id(kin_hgnc_symbol)
             kin_upid = hgnc_client.get_uniprot_id(kin_hgnc_id)
+            if kin_upid is None:
+                continue
         kin = Agent(kin_hgnc_symbol,
-                    db_refs={'UP': kin_upid,'HGNC': kin_hgnc})
+                    db_refs={'UP': kin_upid, 'HGNC': kin_hgnc})
 
         ev = Evidence(source_api='phosphosite')
         st = Phosphorylation(kin, sub, residue, position, evidence = [ev])
