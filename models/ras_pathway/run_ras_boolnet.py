e86b9fb27b333607635b55dcfeada74e45ec2632
Ben Gyori <ben.gyori@gmail.com>
2017-03-20 00:00:57 -0400

    Update RAS pathway Boolean simulation script

--- models/ras_pathway/run_ras_boolnet.py
+++ models/ras_pathway/run_ras_boolnet.py
@@ -3,6 +3,7 @@ import boolean2
 import matplotlib.pyplot as plt
 from indra.util import plot_formatting as pf
 from indra.assemblers import SifAssembler
+from indra.tools import assemble_corpus as ac
 
 def get_sim_avgs(bn_str, nsim=100, nsteps=20, off=None, on=None):
     if off is None:
@@ -24,52 +25,63 @@ def get_sim_avgs(bn_str, nsim=100, nsteps=20, off=None, on=None):
     return avgs
 
 
-st = pickle.load(open('statements.pkl', 'rb'))
-sa = SifAssembler(st)
-sa.make_model()
-bn_str = sa.print_boolean_net('ras_pathway_bn.txt')
-# Condition 1
-off = []
-on = ['GROWTH-FACTOR']
-avgs = get_sim_avgs(bn_str, off=off, on=on)
-jun_basic_noinh = avgs['JUN']
-# Condition 2
-off = ['MAP2K1', 'MAP2K2']
-on = ['GROWTH-FACTOR']
-avgs = get_sim_avgs(bn_str, off=off, on=on)
-jun_basic_inh = avgs['JUN']
+if __name__ == '__main__':
+    # Build Boolean net for basic pathway
+    st = ac.load_statements('ras_pathway.pkl')
+    sa = SifAssembler(st)
+    sa.make_model(use_name_as_key=True)
+    sa.save_model('ras_pathway.sif')
+    bn_str = sa.print_boolean_net('ras_pathway_bn.txt')
 
-st_ext = pickle.load(open('extension.pkl', 'rb'))
-sa = SifAssembler(st + st_ext)
-sa.make_model()
-bn_str = sa.print_boolean_net('ras_pathway_ext_bn.txt')
-# Condition 1
-off = []
-on = ['GROWTH-FACTOR']
-avgs = get_sim_avgs(bn_str, off=off, on=on)
-jun_ext_noinh = avgs['JUN']
-# Condition 2
-off = ['MAP2K1', 'MAP2K2']
-on = ['GROWTH-FACTOR']
-avgs = get_sim_avgs(bn_str, off=off, on=on)
-jun_ext_inh = avgs['JUN']
-# Condition 3
-off = ['MAP2K1', 'MAP2K2', 'MAPK8', 'MAPK9']
-on = ['GROWTH-FACTOR']
-avgs = get_sim_avgs(bn_str, off=off, on=on)
-jun_ext_inh2 = avgs['JUN']
+    # Build Boolean net for extended pathway
+    st_ext = ac.load_statements('ras_pathway_extension.pkl')
+    sa = SifAssembler(st + st_ext)
+    sa.make_model(use_name_as_key=True)
+    sa.save_model('ras_pathway_extension.sif')
+    bn_str = sa.print_boolean_net('ras_pathway_extension_bn.txt')
 
-pf.set_fig_params()
-plt.figure(figsize=(4,4), dpi=300)
-plt.ion()
-plt.plot(jun_basic_noinh, 'r', linewidth=2, label='Basic model, no inhibitor')
-plt.plot(jun_basic_inh, 'r--', linewidth=2, label='Basic model, MEK inhibitor')
-plt.plot(jun_ext_noinh, 'b', linewidth=2, label='Extended model, no inhibitor')
-plt.plot(jun_ext_inh, 'b--', linewidth=2, label='Extended model, MEK inhibitor')
-plt.plot(jun_ext_inh2, 'g--', linewidth=2, label='Extended model, MEK+JNK inhibitor')
-plt.ylim(-0.01, 1.01)
-plt.ylabel('Average JUN activity')
-plt.xlabel('Time steps')
-plt.legend()
-pf.format_axis(plt.gca())
-plt.show()
+    # Condition 1
+    off = []
+    on = ['GROWTH-FACTOR']
+    avgs = get_sim_avgs(bn_str, off=off, on=on)
+    jun_basic_noinh = avgs['JUN']
+
+    # Condition 2
+    off = ['MAP2K1', 'MAP2K2']
+    on = ['GROWTH-FACTOR']
+    avgs = get_sim_avgs(bn_str, off=off, on=on)
+    jun_basic_inh = avgs['JUN']
+
+    # Condition 1
+    off = []
+    on = ['GROWTH-FACTOR']
+    avgs = get_sim_avgs(bn_str, off=off, on=on)
+    jun_ext_noinh = avgs['JUN']
+
+    # Condition 2
+    off = ['MAP2K1', 'MAP2K2']
+    on = ['GROWTH-FACTOR']
+    avgs = get_sim_avgs(bn_str, off=off, on=on)
+    jun_ext_inh = avgs['JUN']
+
+    # Condition 3
+    off = ['MAP2K1', 'MAP2K2', 'MAPK8', 'MAPK9']
+    on = ['GROWTH-FACTOR']
+    avgs = get_sim_avgs(bn_str, off=off, on=on)
+    jun_ext_inh2 = avgs['JUN']
+
+    # Plot figure
+    pf.set_fig_params()
+    plt.figure(figsize=(4,4), dpi=300)
+    plt.ion()
+    plt.plot(jun_basic_noinh, 'r', linewidth=2, label='Basic model, no inhibitor')
+    plt.plot(jun_basic_inh, 'r--', linewidth=2, label='Basic model, MEK inhibitor')
+    plt.plot(jun_ext_noinh, 'b', linewidth=2, label='Extended model, no inhibitor')
+    plt.plot(jun_ext_inh, 'b--', linewidth=2, label='Extended model, MEK inhibitor')
+    plt.plot(jun_ext_inh2, 'g--', linewidth=2, label='Extended model, MEK+JNK inhibitor')
+    plt.ylim(-0.01, 1.01)
+    plt.ylabel('Average JUN activity')
+    plt.xlabel('Time steps')
+    plt.legend()
+    pf.format_axis(plt.gca())
+    plt.show()
