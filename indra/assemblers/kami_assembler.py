380ce1daf43723e502d5615f3dd4713357b3b569
Ben Gyori <ben.gyori@gmail.com>
2017-04-10 09:51:56 -0400

    Refactoring of assembler around Nugget class

--- indra/assemblers/kami_assembler.py
+++ indra/assemblers/kami_assembler.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 from builtins import dict, str
 import logging
 import itertools
+from collections import defaultdict
 import indra.statements as ist
 from indra.assemblers.pysb_assembler import \
         PysbAssembler,\
@@ -115,6 +116,89 @@ class KamiAssembler(PysbAssembler):
         return func(stmt, *args)
 
 
+class Nugget(object):
+    def __init__(self, id, name, rate):
+        self.counters = defaultdict(int)
+        self.id = id
+        self.name = name
+        self.rate = rate
+        self.nodes = []
+        self.edges = []
+        self.typings = {}
+
+    def add_agent(self, agent):
+        agent_id = self.add_node(agent.name)
+        self.add_typing(agent_id, 'agent')
+        # Handle bound conditions
+        for bc in agent.bound_conditions:
+            # Here we make the assumption that the binding site
+            # is simply named after the binding partner
+            if bc.is_bound:
+                test_type = 'is_bnd'
+            else:
+                test_type = 'is_free'
+            bound_name = bc.agent.name
+            agent_bs = get_binding_site_name(bc.agent)
+            test_name = '%s_bound_to_%s_test' % (agent_id, bound_name)
+            agent_bs_id = self.add_node(agent_bs)
+            test_id = self.add_node(test_name)
+            self.add_edge(agent_bs_id, agent_id)
+            self.add_edge(agent_bs_id, test_id)
+            self.add_typing(agent_bs_id, 'locus')
+            self.add_typing(test_id, test_type)
+
+        for mod in agent.mods:
+            mod_site_str = abbrevs[mod.mod_type]
+            if mod.residue is not None:
+                mod_site_str = mod.residue
+            mod_pos_str = mod.position if mod.position is not None else ''
+            mod_site = ('%s%s' % (mod_site_str, mod_pos_str))
+            site_states = states[mod.mod_type]
+            if mod.is_modified:
+                val = site_states[1]
+            else:
+                val = site_states[0]
+            mod_site_id = self.add_node(mod_site, {'val': val})
+            self.add_edge(mod_site_id, agent_id)
+            self.add_typing(mod_site_id, 'state')
+        return agent_id
+
+    def add_node(self, name_base, attrs=None):
+        if name_base not in self.counters:
+            node_id = name_base
+        else:
+            node_id = '%s%d' % (name_base, self.counters[name_base])
+        node = {'id': node_id}
+        if attrs:
+            node['attrs'] = attrs
+        self.nodes.append(node)
+        self.counters[node_id] += 1
+        return node_id
+
+    def add_edge(self, from_node, to_node):
+        self.edges.append({'from': from_node, 'to': to_node})
+
+    def add_typing(self, node_id, typing):
+        self.typings[node_id] = typing
+
+    def get_nugget_dict(self):
+        nugget_dict = \
+            {'id': self.id,
+             'graph': {
+                 'nodes': self.nodes,
+                 'edges': self.edges
+                 },
+             'attrs': {
+                 'name': self.name,
+                 'rate': self.rate
+                 }
+            }
+        return nugget_dict
+
+    def get_typing_dict(self):
+        return self.typings
+
+
 # COMPLEX ############################################################
 
 def complex_monomers_one_step(stmt, agent_set):
@@ -132,76 +216,57 @@ def complex_assemble_one_step(stmt, model, agent_set):
         nugget_name += '_bind'
         action_name =  nugget_name + '_act'
         kf_bind = 1e-6
-        nugget_dict = {'id': nugget_name,
-                       'graph': {},
-                       'attrs': {'name': nugget_name,
-                                  'rate': kf_bind}}
+        nugget = Nugget(nugget_name, nugget_name, kf_bind)
+        action_id = nugget.add_node(action_name)
         # Initialize dicts/lists for this nugget
-        nodes = [{'id': action_name}]
-        edges = []
-        typing_dict = {action_name: 'bnd'}
+        nugget.add_typing(action_id, 'bnd')
         for agent in pair:
-            agent_nodes, agent_edges, agent_types = get_agent_conditions(agent)
-            nodes += agent_nodes
-            edges += agent_edges
-            typing_dict.update(agent_types)
-            # Construct full patterns of each agent with conditions
-            #agent1_pattern = get_monomer_pattern(model, agent1)
+            agent_id = nugget.add_agent(agent)
             agent_bs = get_binding_site_name(agent)
-            nodes.append({'id': agent.name})
-            nodes.append({'id': agent_bs})
-            edges.append({'from': agent_bs, 'to': agent.name})
-            edges.append({'from': agent_bs, 'to': action_name})
+            agent_bs_id = nugget.add_node(agent_bs)
+            nugget.add_edge(agent_bs_id, agent_id)
+            nugget.add_edge(agent_bs_id, action_id)
             # Add to the Kami typing dict
-            typing_dict.update({agent.name: 'agent', agent_bs: 'locus'})
-        nugget_dict['graph']['nodes'] = nodes
-        nugget_dict['graph']['edges'] = edges
+            nugget.add_typing(agent_bs_id, 'locus')
         # Typing dicts linking the nugget to the Action Graph and to the
         # Kami graph
         typing_dict_ag = {'from': nugget_name, 'to': 'action_graph',
                           'mapping': {}, 'total': False,
                           'ignore_attrs': False}
         typing_dict_kami = {'from': nugget_name, 'to': 'kami',
-                            'mapping': typing_dict, 'total': True,
+                            'mapping': nugget.get_typing_dict(), 'total': True,
                             'ignore_attrs': True}
         # Add the graphs for this nugget to the graphs and typing lists
         model['typing'] += [typing_dict_ag, typing_dict_kami]
-        model['graphs'].append(nugget_dict)
+        model['graphs'].append(nugget.get_nugget_dict())
 
         # In reverse reaction, assume that dissocition is unconditional
         nugget_name = '_'.join([get_agent_rule_str(m) for m in pair])
         nugget_name += '_dissociate'
         action_name =  nugget_name + '_act'
         kr_bind = 1e-1
-        nugget_dict = {'id': nugget_name,
-                       'attrs': {'name': nugget_name,
-                                 'rate': kr_bind},
-                       'graph': {}}
-        # Initialize dicts/lists for this nugget
-        nodes = [{'id': action_name}]
-        edges = []
-        typing_dict = {action_name: 'brk'}
+        nugget = Nugget(nugget_name, nugget_name, kr_bind)
+        action_id = nugget.add_node(action_name)
+        nugget.add_typing(action_id, 'brk')
         for agent in pair:
             agent_bs = get_binding_site_name(agent)
-            nodes.append({'id': agent.name})
-            nodes.append({'id': agent_bs})
-            edges.append({'from': agent_bs, 'to': agent.name})
-            edges.append({'from': agent_bs, 'to': action_name})
-            # Add to the Kami typing dict
-            typing_dict.update({agent.name: 'agent', agent_bs: 'locus'})
-        nugget_dict['graph']['nodes'] = nodes
-        nugget_dict['graph']['edges'] = edges
+            agent_id = nugget.add_node(agent.name)
+            agent_bs_id = nugget.add_node(agent_bs)
+            nugget.add_edge(agent_bs_id, agent_id)
+            nugget.add_edge(agent_bs_id, action_id)
+            nugget.add_typing(agent_id, 'agent')
+            nugget.add_typing(agent_bs_id, 'locus')
         # Typing dicts linking the nugget to the Action Graph and to the
         # Kami graph
         typing_dict_ag = {'from': nugget_name, 'to': 'action_graph',
                           'mapping': {}, 'total': False,
                           'ignore_attrs': False}
         typing_dict_kami = {'from': nugget_name, 'to': 'kami',
-                            'mapping': typing_dict, 'total': True,
+                            'mapping': nugget.get_typing_dict(), 'total': True,
                             'ignore_attrs': True}
         # Add the graphs for this nugget to the graphs and typing lists
         model['typing'] += [typing_dict_ag, typing_dict_kami]
-        model['graphs'].append(nugget_dict)
+        model['graphs'].append(nugget.get_nugget_dict())
 
 
 complex_assemble_default = complex_assemble_one_step
@@ -229,40 +294,18 @@ def modification_assemble_one_step(stmt, model, agent_set):
         (rule_enz_str, mod_condition_name, rule_sub_str, mod_site)
     action_name =  nugget_name + '_act'
     kf_mod = 1e-6
-    nugget_dict = {'id': nugget_name,
-                   'graph': {},
-                   'attrs': {'name': nugget_name,
-                             'rate': kf_mod}}
-
-    # Initialize dicts/lists for this nugget
-    nodes = []
-    edges = []
-    typing_dict = {action_name: 'bnd'}
-
-    # Add enzyme conditions
-    enz_nodes, enz_edges, enz_types = get_agent_conditions(stmt.enz)
-    nodes += enz_nodes
-    edges += enz_edges
-    typing_dict.update(enz_types)
-
-    # Add substrate conditions
-    sub_nodes, sub_edges, sub_types = get_agent_conditions(stmt.sub)
-    nodes += sub_nodes
-    edges += sub_edges
-    typing_dict.update(sub_types)
+    nugget = Nugget(nugget_name, nugget_name, kf_mod)
+    enz_id = nugget.add_agent(stmt.enz)
+    sub_id = nugget.add_agent(stmt.sub)
 
     # Add nodes/edges/types for the modification itself
-    nodes.append({'id': stmt.enz.name})
-    nodes.append({'id': stmt.sub.name})
-    nodes.append({'id': mod_site, 'attrs': {'val': unmod_site_state}})
-    nodes.append({'id': action_name, 'attrs': {'val': mod_site_state}})
-    edges.append({'from': mod_site, 'to': stmt.sub.name})
-    edges.append({'from': action_name, 'to': mod_site})
-    edges.append({'from': stmt.enz.name, 'to': action_name})
-    typing_dict.update({stmt.enz.name: 'agent', stmt.sub.name: 'agent',
-                        mod_site: 'site', action_name: 'mod'})
-    nugget_dict['graph']['nodes'] = nodes
-    nugget_dict['graph']['edges'] = edges
+    mod_site_id = nugget.add_node(mod_site, {'val': unmod_site_state})
+    nugget.add_typing(mod_site_id, 'site')
+    action_id = nugget.add_node(action_name, {'val': mod_site_state})
+    nugget.add_typing(action_id, 'mod')
+    nugget.add_edge(mod_site_id, sub_id)
+    nugget.add_edge(action_id, mod_site_id)
+    nugget.add_edge(enz_id, action_id)
 
     # Typing dicts linking the nugget to the Action Graph and to the
     # Kami graph
@@ -270,11 +313,11 @@ def modification_assemble_one_step(stmt, model, agent_set):
                       'mapping': {}, 'total': False,
                       'ignore_attrs': False}
     typing_dict_kami = {'from': nugget_name, 'to': 'kami',
-                        'mapping': typing_dict, 'total': True,
+                        'mapping': nugget.get_typing_dict(), 'total': True,
                         'ignore_attrs': True}
     # Add the graphs for this nugget to the graphs and typing lists
     model['typing'] += [typing_dict_ag, typing_dict_kami]
-    model['graphs'].append(nugget_dict)
+    model['graphs'].append(nugget.get_nugget_dict())
 
 
 def demodification_monomers_one_step(stmt, agent_set):
@@ -379,42 +422,3 @@ for mc, func_type, pol in itertools.product(demod_classes,
     exec(code)
 
 
-def get_agent_conditions(agent):
-    nodes = []
-    edges = []
-    types = {}
-    # Handle bound conditions
-    for bc in agent.bound_conditions:
-        # Here we make the assumption that the binding site
-        # is simply named after the binding partner
-        if bc.is_bound:
-            test_type = 'is_bnd'
-        else:
-            test_type = 'is_free'
-        bound_name = bc.agent.name
-        agent_bs = get_binding_site_name(bc.agent)
-        test_name = '%s_bound_to_%s_test' % (agent.name, bound_name)
-        nodes += [{'id': agent_bs},
-                  {'id': test_name}]
-        edges += [{'from': agent_bs, 'to': agent.name},
-                  {'from': agent_bs, 'to': test_name}]
-        types.update({agent_bs: 'locus',
-                      test_name: test_type})
-
-    for mod in agent.mods:
-        mod_site_str = abbrevs[mod.mod_type]
-        if mod.residue is not None:
-            mod_site_str = mod.residue
-        mod_pos_str = mod.position if mod.position is not None else ''
-        mod_site = ('%s%s' % (mod_site_str, mod_pos_str))
-        site_states = states[mod.mod_type]
-        if mod.is_modified:
-            val = site_states[1]
-        else:
-            val = site_states[0]
-        nodes += [{'id': mod_site, 'attrs': {'val': val}}]
-        edges += [{'from': mod_site, 'to': agent.name}]
-        types.update({mod_site: 'state'})
-
-    return nodes, edges, types
-    #TODO: locations, mutations, activity states
