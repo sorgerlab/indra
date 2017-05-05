a25fb42cf668ae8629fb768ab950928f26a99305
John Bachman <bachmanjohn@gmail.com>
2017-05-05 10:52:46 -0400

    Change structure of S3 keys

--- indra/tools/reading/submit_reading_pipeline_aws.py
+++ indra/tools/reading/submit_reading_pipeline_aws.py
@@ -11,9 +11,10 @@ if __name__ == '__main__':
     s3_client = boto3.client('s3')
     job_name = sys.argv[1]
     pmid_list_filename = sys.argv[2]
-    pmid_list_key = 'pmid_lists/' + pmid_list_filename
+    pmid_list_key = 'reading_results/%s/pmids' % job_name
     # Upload the pmid_list to Amazon S3
     s3_client.upload_file(pmid_list_filename, 'bigmech', pmid_list_key)
+    pmids_per_job = 100
 
     # Get AWS credentials
     # http://stackoverflow.com/questions/36287720/boto3-get-credentials-dynamically
@@ -31,16 +32,25 @@ if __name__ == '__main__':
              'value': access_key},
             {'name': 'AWS_SECRET_ACCESS_KEY',
              'value': secret_key}]
+
     # The command to run
     base_command_list = ['python', '-m',
                          'indra.tools.reading.run_reach_on_pmids_aws',
-                         job_name, pmid_list_filename, '/tmp', '16', '0', '10']
+                         job_name, '/tmp', '16']
+
     # Submit the reading job
     batch_client = boto3.client('batch')
     # Iterate over the list of PMIDs and submit the job in chunks
-    batch_client.submit_job(jobName=job_name,
+    with open(pmid_list_filename, 'rt') as f:
+        lines = f.readlines()
+        num_pmids = len(lines)
+
+    for start_ix in range(0, num_pmids, pmids_per_job):
+        end_ix = start_ix + pmids_per_job
+        command_list = base_command_list + [str(start_ix), str(end_ix)]
+        batch_client.submit_job(jobName=job_name,
                             jobQueue='run_reach_queue',
                             jobDefinition='run_reach_jobdef',
                             containerOverrides={'environment': environment_vars,
-                                                'command': base_command_list})
+                                                'command': command_list})
 
