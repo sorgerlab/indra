e4010da68c3b71031b418ac7f47ff4d1d55608ff
John Bachman <bachmanjohn@gmail.com>
2017-05-06 15:34:06 -0400

    Script to assemble batch stmts into single pkl

--- indra/tools/reading/assemble_reach_stmts_aws.py
+++ indra/tools/reading/assemble_reach_stmts_aws.py
@@ -2,6 +2,52 @@
 from __future__ import absolute_import, print_function, unicode_literals
 from builtins import dict, str
 
+def assemble_batch_results(result_type):
+    # The trailing slash here is important
+    prefix = 'reading_results/%s/%s/' % (basename, result_type)
+    # Get all keys associated with reading results
+    result_file_keys = []
+    marker = ''
+    # Page through the files
+    while True:
+        res = client.list_objects(Bucket=bucket_name, Prefix=prefix,
+                                  Delimiter='/', Marker=marker)
+        if not res.get('Contents'):
+            break
+        result_file_keys += [item['Key'] for item in res['Contents']]
+        # If the response is truncated, get another round of results
+        if res.get('IsTruncated'):
+            marker = res['NextMarker']
+        else:
+            break
+    # Now that we have the keys, get and unpickle each of the pickles
+    results = {}
+    for key in result_file_keys[0:3]:
+        try:
+            logger.info('Downloading and unpickling %s' % key)
+            result_obj = client.get_object(Bucket=bucket_name, Key=key)
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] =='NoSuchKey':
+                logger.debug('Key %s not found on S3' % key)
+                continue
+            # If there was some other kind of problem, re-raise the exception
+            else:
+                raise e
+        result_bytes = result_obj['Body'].read()
+        result_subset = pickle.loads(result_bytes)
+        # Add results into master list
+        results.update(result_subset)
+
+    # Write out the final statement set
+    # Pickle the statements to a bytestring
+    if results:
+        pickle_key_name = 'reading_results/%s/%s.pkl' % (basename, result_type)
+        logger.info('Pickling combined file %s' % pickle_key_name)
+        results_bytes = pickle.dumps(results)
+        logger.info('Uploading combined file %s' % pickle_key_name)
+        client.put_object(Key=pickle_key_name, Body=results_bytes,
+                          Bucket=bucket_name)
+
 if __name__ == '__main__':
     from indra.tools.reading import run_reach_on_pmids as rr
     import boto3
@@ -11,48 +57,13 @@ if __name__ == '__main__':
     import pickle
     import logging
 
+    logger = logging.getLogger('assemble_reach')
+
     client = boto3.client('s3')
     bucket_name = 'bigmech'
     basename = sys.argv[1]
 
-    # The trailing slash here is important
-    prefix = 'reading_results/%s/stmts/' % basename
-    # Get all keys associated with reading results
-    stmt_file_keys = []
-    marker = ''
-    while True:
-        res = client.list_objects(Bucket=bucket_name, Prefix=prefix,
-                                  Delimiter='/', Marker=marker)
-        if not res.get('Contents'):
-            break
-        keys = [item['Key'] for item in res['Contents']]
-        stmt_file_keys += keys
-        # If the response is truncated, get another round of results
-        if not res.get('IsTruncated'):
-            marker = res['NextMarker']
-        else:
-            break
-    # Now that we have the keys, get and unpickle each of the pickles
-    stmts = {}
-    for key in stmt_file_keys:
-        try:
-            stmt_obj = client.get_object(Bucket=bucket_name, Key=key)
-        except botocore.exceptions.ClientError as e:
-            if e.response['Error']['Code'] =='NoSuchKey':
-                logger.debug('key %s not in S3' % key)
-                continue
-            # If there was some other kind of problem, re-raise the exception
-            else:
-                raise e
-        stmt_bytes = stmt_obj['Body'].read()
-        stmt_subset = pickle.loads(stmt_bytes)
-        # Add stmts into master list
-        stmts.update(stmt_subset)
+    result_types = ('content_types', 'stmts')
+    for rt in result_types:
+        assemble_batch_results(rt)
 
-    # Write out the final statement set
-    # Pickle the statements to a bytestring
-    if stmts:
-        pickle_key_name = 'reading_results/%s/stmts.pkl' % basename
-        stmts_bytes = pickle.dumps(stmts)
-        client.put_object(Key=pickle_key_name, Body=stmts_bytes,
-                          Bucket=bucket_name)
