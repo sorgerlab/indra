3215aa7934af3f2c86416c6e270e687eda7358ca
John Bachman <bachmanjohn@gmail.com>
2017-05-08 07:01:58 -0400

    Refactor job submission to support full pipeline

--- indra/tools/reading/submit_reading_pipeline_aws.py
+++ indra/tools/reading/submit_reading_pipeline_aws.py
@@ -20,6 +20,56 @@ def get_environment():
              'value': secret_key}]
     return environment_vars
 
+
+def submit_run_reach(basename, pmid_list_filename, start_ix=0, end_ix=None,
+                 pmids_per_job=3000):
+    # Upload the pmid_list to Amazon S3
+    pmid_list_key = 'reading_results/%s/pmids' % basename
+    s3_client = boto3.client('s3')
+    s3_client.upload_file(pmid_list_filename, 'bigmech', pmid_list_key)
+
+    # If no end index is specified, read all the PMIDs
+    if end_ix is None:
+        with open(pmid_list_filename, 'rt') as f:
+            lines = f.readlines()
+            end_ix = len(lines)
+
+    # Get environment variables
+    environment_vars = get_environment()
+
+    # Iterate over the list of PMIDs and submit the job in chunks
+    job_list = []
+    for job_start_ix in range(start_ix, end_ix, pmids_per_job):
+        job_end_ix = job_start_ix + pmids_per_job
+        if job_end_ix > end_ix:
+            job_end_ix = end_ix
+        job_name = '%s_%d_%d' % (basename, job_start_ix, job_end_ix)
+        command_list = ['python', '-m',
+                        'indra.tools.reading.run_reach_on_pmids_aws',
+                        basename, '/tmp', '16', str(job_start_ix),
+                        str(job_end_ix)]
+        print(command_list)
+        job_info = batch_client.submit_job(jobName=job_name,
+                       jobQueue='run_reach_queue',
+                       jobDefinition='run_reach_jobdef',
+                       containerOverrides={'environment': environment_vars,
+                                           'command': command_list})
+        job_list.append(job_info)
+    return job_list
+
+def submit_combine(basename):
+    job_name = '%s_combine_reach' % basename
+    command_list = ['python', '-m',
+                    'indra.tools.reading.assemble_reach_stmts_aws',
+                    basename]
+    print(command_list)
+    batch_client.submit_job(jobName=job_name, jobQueue='run_reach_queue',
+                       jobDefinition='run_reach_jobdef',
+                       containerOverrides={'environment': environment_vars,
+                                           'command': command_list,
+                                           'memory': 60000,
+                                           'vcpus': 1})
+
 if __name__ == '__main__':
     import sys
     import boto3
@@ -27,58 +77,35 @@ if __name__ == '__main__':
     from indra.literature import elsevier_client as ec
 
     bucket_name = 'bigmech'
-    s3_client = boto3.client('s3')
     job_type =sys.argv[1]
 
     # Submit the reading job
     batch_client = boto3.client('batch')
 
-    environment_vars = get_environment()
-
-    if job_type == 'run_reach':
+    if job_type == 'read':
         basename = sys.argv[2]
         pmid_list_filename = sys.argv[3]
         main_start_ix = int(sys.argv[4])
         main_end_ix = int(sys.argv[5])
-        pmid_list_key = 'reading_results/%s/pmids' % basename
-        # Upload the pmid_list to Amazon S3
-        s3_client.upload_file(pmid_list_filename, 'bigmech', pmid_list_key)
-        pmids_per_job = 3000
-
-        #with open(pmid_list_filename, 'rt') as f:
-        #    lines = f.readlines()
-        #    num_pmids = len(lines)
-
-        # Iterate over the list of PMIDs and submit the job in chunks
-        for start_ix in range(main_start_ix, main_end_ix, pmids_per_job):
-            end_ix = start_ix + pmids_per_job
-            if end_ix > main_end_ix:
-                end_ix = main_end_ix
-            job_name = '%s_%d_%d' % (basename, start_ix, end_ix)
-            command_list = ['python', '-m',
-                            'indra.tools.reading.run_reach_on_pmids_aws',
-                            basename, '/tmp', '16', str(start_ix), str(end_ix)]
-            print(command_list)
-            batch_client.submit_job(jobName=job_name,
-                           jobQueue='run_reach_queue',
-                           jobDefinition='run_reach_jobdef',
-                           containerOverrides={'environment': environment_vars,
-                                               'command': command_list})
-    elif job_type == 'assemble_reach':
+        pmids_per_job = int(sys.argv[6])
+        # force_read
+        # force_fulltext
+        job_ids = submit_reach(basename, pmid_list_filename, start_ix,
+                               end_ix, pmids_per_job)
+    elif job_type == 'combine':
         basename = sys.argv[2]
-        job_name = '%s_assemble_reach' % basename
-        command_list = ['python', '-m',
-                        'indra.tools.reading.assemble_reach_stmts_aws',
-                        basename]
-        print(command_list)
-        batch_client.submit_job(jobName=job_name, jobQueue='run_reach_queue',
-                           jobDefinition='run_reach_jobdef',
-                           containerOverrides={'environment': environment_vars,
-                                               'command': command_list,
-                                               'memory': 60000,
-                                               'vcpus': 1})
+        submit_combine(basename)
+    elif job_type == 'full':
+        basename = sys.argv[2]
+        pmid_list_filename = sys.argv[3]
+        main_start_ix = int(sys.argv[4])
+        main_end_ix = int(sys.argv[5])
+        pmids_per_job = int(sys.argv[6])
+        job_ids = submit_reach(basename, pmid_list_filename, start_ix,
+                               end_ix, pmids_per_job)
+        submit_combine(basename, job_ids)
     else:
-        print('job type must be run_reach or assemble_reach')
+        print('job type must be "read", "combine", or "full"')
         sys.exit(1)
 
 
