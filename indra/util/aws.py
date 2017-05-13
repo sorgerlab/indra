a4a595d650b6481669cb801c9655b09692dd5c43
John Bachman <bachmanjohn@gmail.com>
2017-05-13 12:42:53 -0400

    Funcs to get logs for jobs

--- indra/util/aws.py
+++ indra/util/aws.py
@@ -1,7 +1,6 @@
 import boto3
 
 def kill_all(job_queue, reason='None given'):
-    import ipdb; ipdb.set_trace()
     batch = boto3.client('batch')
     runnable = batch.list_jobs(jobQueue=job_queue, jobStatus='RUNNABLE')
     job_info = runnable.get('jobSummaryList')
@@ -16,4 +15,59 @@ def kill_all(job_queue, reason='None given'):
         job_ids = [job['jobId'] for job in job_info]
         for job_id in job_ids:
             batch.terminate_job(jobId=job_id, reason=reason)
+
+
+def get_jobs(job_queue='run_reach_queue', job_status='RUNNING'):
+    batch = boto3.client('batch')
+    jobs = batch.list_jobs(jobQueue=job_queue, jobStatus=job_status)
+    return jobs.get('jobSummaryList')
+
+
+def get_job_log(job_info, log_group_name='/aws/batch/job',
+                write_file=True):
+    job_name = job_info['jobName']
+    job_id = job_info['jobId']
+    logs = boto3.client('logs')
+    stream_resp = logs.describe_log_streams(
+                            logGroupName=log_group_name,
+                            logStreamNamePrefix='%s/%s' % (job_name, job_id))
+    streams = stream_resp.get('logStreams')
+    if not streams:
+        print('No streams for job')
+        return None
+    elif len(streams) > 1:
+        print('More than 1 stream for job, returning first')
+    log_stream_name = streams[0]['logStreamName']
+    lines = []
+    kwargs = {'logGroupName': log_group_name,
+              'logStreamName': log_stream_name,
+              'startFromHead': True}
+    print("Getting log for %s/%s" % (job_name, job_id))
+    while True:
+        response = logs.get_log_events(**kwargs)
+        # If we've gotten all the events already, the nextForwardToken for
+        # this call will be the same as the last one
+        if response.get('nextForwardToken') == kwargs.get('nextToken'):
+            break
+        else:
+            events = response.get('events')
+            if events:
+                lines += ['%s: %s' % (evt['timestamp'], evt['message'])
+                          for evt in events]
+            kwargs['nextToken'] = response.get('nextForwardToken')
+        print('%d %s' % (len(lines), lines[-1]))
+    if write_file:
+        with open('%s_%s.log' % (job_name, job_id), 'wt') as f:
+            for line in lines:
+                f.write(line)
+    return lines
+
+
+def dump_logs(job_queue='run_reach_queue', job_status='RUNNING'):
+    jobs = get_jobs(job_queue, job_status)
+    for job in jobs:
+        get_job_log(job)
+
+if __name__ == '__main__':
+    pass
 
