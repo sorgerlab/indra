e1be27ba64972aa688b3aa3abc5ab2e86d043aa1
John Bachman <bachmanjohn@gmail.com>
2017-05-14 11:38:39 -0400

    Add docs to aws funcs

--- indra/util/aws.py
+++ indra/util/aws.py
@@ -1,6 +1,7 @@
 import boto3
 
 def kill_all(job_queue, reason='None given'):
+    """Terminates/cancels all RUNNING and RUNNABLE jobs, respectively."""
     batch = boto3.client('batch')
     runnable = batch.list_jobs(jobQueue=job_queue, jobStatus='RUNNABLE')
     job_info = runnable.get('jobSummaryList')
@@ -18,6 +19,8 @@ def kill_all(job_queue, reason='None given'):
 
 
 def get_jobs(job_queue='run_reach_queue', job_status='RUNNING'):
+    """Returns a list of dicts with jobName and jobId for each job with the
+    given status."""
     batch = boto3.client('batch')
     jobs = batch.list_jobs(jobQueue=job_queue, jobStatus=job_status)
     return jobs.get('jobSummaryList')
@@ -25,6 +28,25 @@ def get_jobs(job_queue='run_reach_queue', job_status='RUNNING'):
 
 def get_job_log(job_info, log_group_name='/aws/batch/job',
                 write_file=True):
+    """Gets the Cloudwatch log associated with the given job.
+
+    Parameters
+    ----------
+    job_info : dict
+        dict containing entries for 'jobName' and 'jobId', e.g., as returned
+        by get_jobs()
+    log_group_name : string
+        Name of the log group; defaults to '/aws/batch/job'
+    write_file : boolean
+        If True, writes the downloaded log to a text file with the filename
+        '%s_%s.log' % (job_name, job_id)
+
+
+    Returns
+    -------
+    list of strings
+        The event messages in the log, with the earliest events listed first.
+    """
     job_name = job_info['jobName']
     job_id = job_info['jobId']
     logs = boto3.client('logs')
@@ -52,7 +74,7 @@ def get_job_log(job_info, log_group_name='/aws/batch/job',
         else:
             events = response.get('events')
             if events:
-                lines += ['%s: %s' % (evt['timestamp'], evt['message'])
+                lines += ['%s: %s\n' % (evt['timestamp'], evt['message'])
                           for evt in events]
             kwargs['nextToken'] = response.get('nextForwardToken')
         print('%d %s' % (len(lines), lines[-1]))
@@ -64,10 +86,8 @@ def get_job_log(job_info, log_group_name='/aws/batch/job',
 
 
 def dump_logs(job_queue='run_reach_queue', job_status='RUNNING'):
+    """Write logs for all jobs with given the status to files."""
     jobs = get_jobs(job_queue, job_status)
     for job in jobs:
-        get_job_log(job)
-
-if __name__ == '__main__':
-    pass
+        get_job_log(job, write_file=True)
 
