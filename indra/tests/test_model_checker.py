0206819a6ad030049b95cedc15e76a62e503fdb0
John Bachman <bachmanjohn@gmail.com>
2017-05-17 16:55:26 -0400

    Test (failing) of grounded mps in check_regulate_activity

--- indra/tests/test_model_checker.py
+++ indra/tests/test_model_checker.py
@@ -544,6 +544,51 @@ def test_phosphorylation_annotations():
     assert results[2][1] == False
 
 
+@with_model
+def test_activation_annotations():
+    # Create the statement
+    a = Agent('MEK1', db_refs={'HGNC': '6840'})
+    b = Agent('ERK2', db_refs={'HGNC': '6871'})
+    st1 = Phosphorylation(a, b, 'T', '185')
+    st2 = Activation(a, b)
+    st3 = Activation(a, b, 'kinase')
+    # Now create the PySB model
+    Monomer('A_monomer')
+    Monomer('B_monomer', ['Thr185', 'Y187'],
+            {'Thr185':['un', 'phos'], 'Y187': ['u', 'p']})
+    Rule('A_phos_B', A_monomer() + B_monomer(Thr185='un') >>
+                     A_monomer() + B_monomer(Thr185='phos'),
+         Parameter('k', 1))
+    Initial(A_monomer(), Parameter('A_0', 100))
+    Initial(B_monomer(Thr185='un', Y187='u'), Parameter('B_0', 100))
+    # Add agent grounding
+    Annotation(A_monomer, 'http://identifiers.org/hgnc/HGNC:6840')
+    Annotation(B_monomer, 'http://identifiers.org/hgnc/HGNC:6871')
+    Annotation('A_phos_B', 'A_monomer', 'rule_has_subject')
+    Annotation('A_phos_B', 'B_monomer', 'rule_has_object')
+    Annotation('B_monomer', {'Thr185':'phos'}, 'has_active_pattern')
+    # Add annotations to the sites/states of the Monomer itself
+    B_annot = [
+        Annotation('Thr185', 'T', 'is_residue'),
+        Annotation('Thr185', '185', 'is_position'),
+        Annotation(('Thr185', 'phos'), 'phosphorylation', 'is_modification'),
+        Annotation('Y187', 'Y', 'is_residue'),
+        Annotation('Y187', '187', 'is_position'),
+        Annotation(('Y187', 'p'), 'phosphorylation', 'is_modification'),
+    ]
+    B_monomer.site_annotations = B_annot
+    mc = ModelChecker(model, [st1, st2, st3])
+    results = mc.check_model()
+    assert len(results) == 3
+    assert isinstance(results[0], tuple)
+    assert results[0][0] == st1
+    assert results[0][1] == ['A_phos_B', 'B_monomer_Thr185_phos_obs']
+    assert results[1][0] == st2
+    assert results[1][1] == ['A_phos_B', 'B_monomer_Thr185_phos_obs']
+    assert results[2][0] == st3
+    assert results[1][1] == ['A_phos_B', 'B_monomer_Thr185_phos_obs']
+
+
 def test_multitype_path():
     """Test causal chain involving Complex, RasGef, Activation"""
     egfr = Agent('EGFR', db_refs={'HGNC':'3236'})
@@ -1040,4 +1085,8 @@ def test_check_transphosphorylation():
 #
 # When Ras machine finds a new finding, it can be checked to see if it's
 # satisfied by the model.
+
+
+if __name__ == '__main__':
+    test_activation_annotations()
 
