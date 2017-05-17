a1c97db3df6b79991c39e56672d74f22b9c85929
John Bachman <bachmanjohn@gmail.com>
2017-05-17 14:35:45 -0400

    Remove decode calls

--- models/phase3_eval/read_phosphosite.py
+++ models/phase3_eval/read_phosphosite.py
@@ -9,36 +9,34 @@ from indra.statements import Agent, Phosphorylation, ModCondition, Evidence
 phosphosite_file = 'sources/annotated_kinases_v5.csv'
 
 def read_phosphosite(fname):
-    df = pandas.read_csv(fname, index_col=None, sep='\t')
+    df = pandas.read_csv(fname, index_col=None, sep='\t', encoding='utf8')
     statements = []
     antibody_map = {}
     for _, row in df.iterrows():
         sub_upid = row['SUB_ID']
         if not pandas.isnull(sub_upid):
-            sub_upid = sub_upid.decode('utf-8') # to guarantee unicode
             if sub_upid.find('-') != -1:
                 sub_upid = sub_upid.split('-')[0]
             sub_hgnc_symbol = uniprot_client.get_gene_name(sub_upid)
             sub_hgnc = hgnc_client.get_hgnc_id(sub_hgnc_symbol)
         else:
             sub_hgnc_symbol = row['SUB_GENE']
-            sub_hgnc_symbol = sub_hgnc_symbol.decode('utf-8')
             sub_hgnc_id = hgnc_client.get_hgnc_id(sub_hgnc_symbol)
             sub_upid = hgnc_client.get_uniprot_id(sub_hgnc_id)
             if sub_upid is None:
                 continue
         sub = Agent(sub_hgnc_symbol,
                     db_refs={'UP': sub_upid,'HGNC': sub_hgnc})
-        residue = row['Actual_site'][0].decode('utf-8')
+        residue = row['Actual_site'][0]
         if len(row['Actual_site']) > 1:
-            position = row['Actual_site'][1:].decode('utf-8')
+            position = row['Actual_site'][1:]
         else:
             position = None
 
         sub_readout = deepcopy(sub)
         mc = ModCondition('phosphorylation', residue, position)
         sub_readout.mods = [mc]
-        ps = row['phosphosite'].decode('utf-8')
+        ps = row['phosphosite']
         if ps in antibody_map:
             found = False
             for p in antibody_map[ps]:
@@ -53,7 +51,6 @@ def read_phosphosite(fname):
 
         kin_upid = row['KIN_ID']
         if not pandas.isnull(kin_upid):
-            kin_upid = kin_upid.decode('utf-8') # to guarantee unicode
             if kin_upid.find('-') != -1:
                 kin_upid = kin_upid.split('-')[0]
             if not uniprot_client.is_human(kin_upid):
@@ -64,7 +61,6 @@ def read_phosphosite(fname):
             kin_hgnc_symbol = row['KINASE_GENE_SYMBOL']
             if pandas.isnull(kin_hgnc_symbol):
                 continue
-            kin_hgnc_symbol = kin_hgnc_symbol.decode('utf-8')
             kin_hgnc_id = hgnc_client.get_hgnc_id(kin_hgnc_symbol)
             kin_upid = hgnc_client.get_uniprot_id(kin_hgnc_id)
             if kin_upid is None:
