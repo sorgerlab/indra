33cee1e0cc3985bded93689b07aa57714049737b
Ben Gyori <ben.gyori@gmail.com>
2017-06-29 13:07:13 -0400

    Print coverage stats table and fixes

--- indra/benchmarks/benchmark_db_coverage.py
+++ indra/benchmarks/benchmark_db_coverage.py
@@ -15,20 +15,38 @@ def get_biopax_statistics(bp):
     num_all, num_extracted = bp.get_coverage()
     return num_all, num_extracted
 
-signaling = ['MAPK1', 'AKT1', 'JAK1', 'MAPK8', 'CTNNB1']
-genereg = ['MYC', 'TP53', 'STAT3', 'FOXO3', 'JUN']
-metabolism = ['IDH1', 'PFKL', 'DHFR', 'GLUL', 'NOS1']
+def print_stats(stats):
+    header = ['Gene', 'BPXtot', 'BPXext', 'BPX\%', 'BELtot', 'BELext', 'BEL\%']
+    print(' & '.join(header) + '\\\\')
+    for gene, gene_stats in stats.items():
+        bpx_vals = ['%d' % gene_stats['biopax'][0],
+                    '%d' % gene_stats['biopax'][1],
+                    ('%.1f\\%%' % (100.0*gene_stats['biopax'][1] /
+                                   gene_stats['biopax'][0])
+                                 if gene_stats['biopax'][0] else 'N/A')]
+        bel_vals = ['%d' % gene_stats['bel'][0],
+                    '%d' % gene_stats['bel'][1],
+                    ('%.1f\\%%' % (100.0*gene_stats['bel'][1] /
+                                   gene_stats['bel'][0])
+                                if gene_stats['bel'][0] else 'N/A')]
+        print(' & '.join([gene] + bpx_vals + bel_vals) + '\\\\')
 
-#all_genes = signaling + genereg + metabolism
-all_genes = metabolism
-stats = {'bel': {}, 'biopax': {}}
-for gene in all_genes:
-    print('%s\n=========' % gene)
-    belp = bel.process_ndex_neighborhood([gene])
-    num_all, num_extracted = get_bel_statistics(belp)
-    stats['bel'][gene] = (num_all, num_extracted)
-    print(num_all, num_extracted)
-    biopp = biopax.process_pc_neighborhood([gene])
-    num_all, num_extracted = get_biopax_statistics(biopp)
-    print(num_all, num_extracted)
-    stats['biopax'][gene] = (num_all, num_extracted)
+if __name__ == '__main__':
+    signaling = ['MAPK1', 'AKT1', 'JAK1', 'GNAS', 'CTNNB1']
+    genereg = ['MYC', 'TP53', 'STAT3', 'FOXO3', 'JUN']
+    metabolism = ['IDH1', 'PFKL', 'DHFR', 'GLUL', 'NOS1']
+
+    all_genes = signaling + genereg + metabolism
+    stats = {g: {} for g in all_genes}
+    for gene in all_genes:
+        print('%s\n======' % gene)
+        belp = bel.process_ndex_neighborhood([gene])
+        num_all, num_extracted = get_bel_statistics(belp)
+        stats[gene]['bel'] = (num_all, num_extracted)
+        print(num_all, num_extracted)
+        biopp = biopax.process_pc_neighborhood([gene])
+        num_all, num_extracted = get_biopax_statistics(biopp)
+        print(num_all, num_extracted)
+        stats[gene]['biopax'] = (num_all, num_extracted)
+
+    print_stats(stats)
