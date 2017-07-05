b373608b57e3a53955017b28549f269fb9dde055
Ben Gyori <ben.gyori@gmail.com>
2017-07-05 00:19:39 -0400

    Refactor statistics data structure

--- indra/benchmarks/benchmark_db_coverage.py
+++ indra/benchmarks/benchmark_db_coverage.py
@@ -1,6 +1,7 @@
+import json
 from indra import bel
 from indra import biopax
-
+from collections import OrderedDict
 
 def get_bel_statistics(bp):
     if bp is None:
@@ -18,35 +19,42 @@ def get_biopax_statistics(bp):
 def print_stats(stats):
     header = ['Gene', 'BPXtot', 'BPXext', 'BPX\%', 'BELtot', 'BELext', 'BEL\%']
     print(' & '.join(header) + '\\\\')
-    for gene, gene_stats in stats.items():
-        bpx_vals = ['%d' % gene_stats['biopax'][0],
-                    '%d' % gene_stats['biopax'][1],
-                    ('%.1f\\%%' % (100.0*gene_stats['biopax'][1] /
-                                   gene_stats['biopax'][0])
-                                 if gene_stats['biopax'][0] else 'N/A')]
-        bel_vals = ['%d' % gene_stats['bel'][0],
-                    '%d' % gene_stats['bel'][1],
-                    ('%.1f\\%%' % (100.0*gene_stats['bel'][1] /
-                                   gene_stats['bel'][0])
-                                if gene_stats['bel'][0] else 'N/A')]
-        print(' & '.join([gene] + bpx_vals + bel_vals) + '\\\\')
+    for group, gene_stats in stats.items():
+        for gene, gene_stats in gene_stats.items():
+            bpx_vals = ['%d' % gene_stats['biopax'][0],
+                        '%d' % gene_stats['biopax'][1],
+                        ('%.1f\\%%' % (100.0*gene_stats['biopax'][1] /
+                                       gene_stats['biopax'][0])
+                                     if gene_stats['biopax'][0] else 'N/A')]
+            bel_vals = ['%d' % gene_stats['bel'][0],
+                        '%d' % gene_stats['bel'][1],
+                        ('%.1f\\%%' % (100.0*gene_stats['bel'][1] /
+                                       gene_stats['bel'][0])
+                                    if gene_stats['bel'][0] else 'N/A')]
+            print(' & '.join([gene] + bpx_vals + bel_vals) + '\\\\')
+        print('\\hline')
 
 if __name__ == '__main__':
-    signaling = ['MAPK1', 'AKT1', 'JAK1', 'GNAS', 'CTNNB1']
-    genereg = ['MYC', 'TP53', 'STAT3', 'FOXO3', 'JUN']
-    metabolism = ['IDH1', 'PFKL', 'DHFR', 'GLUL', 'NOS1']
+    all_genes = \
+        OrderedDict({'signaling': ['MAPK1', 'AKT1', 'JAK1', 'GNAS', 'CTNNB1'],
+                     'genereg': ['MYC', 'TP53', 'STAT3', 'FOXO3', 'JUN'],
+                     'metabolism': ['IDH1', 'PFKL', 'DHFR', 'GLUL', 'NOS1']}
 
-    all_genes = signaling + genereg + metabolism
-    stats = {g: {} for g in all_genes}
-    for gene in all_genes:
-        print('%s\n======' % gene)
-        belp = bel.process_ndex_neighborhood([gene])
-        num_all, num_extracted = get_bel_statistics(belp)
-        stats[gene]['bel'] = (num_all, num_extracted)
-        print(num_all, num_extracted)
-        biopp = biopax.process_pc_neighborhood([gene])
-        num_all, num_extracted = get_biopax_statistics(biopp)
-        print(num_all, num_extracted)
-        stats[gene]['biopax'] = (num_all, num_extracted)
+    stats = {group: {g: {} for g in genes}
+             for group, genes in all_genes.items()}
+    for group, genes in all_genes.items():
+        for gene in genes:
+            print('%s\n======' % gene)
+            belp = bel.process_ndex_neighborhood([gene])
+            num_all, num_extracted = get_bel_statistics(belp)
+            stats[group][gene]['bel'] = (num_all, num_extracted)
+            print(num_all, num_extracted)
+            biopp = biopax.process_pc_neighborhood([gene])
+            num_all, num_extracted = get_biopax_statistics(biopp)
+            print(num_all, num_extracted)
+            stats[group][gene]['biopax'] = (num_all, num_extracted)
+
+    with open('db_coverage_stats.json', 'w') as fh:
+        json.dump(fh, stats)
 
     print_stats(stats)
