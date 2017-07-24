2dc97c7ddaf251539b381af4051d715cff78cf1d
John Bachman <bachmanjohn@gmail.com>
2017-07-24 09:10:27 +0200

    Get mod statements from omnipath

---
+++ indra/databases/omnipath_client.py
@@ -0,0 +1,55 @@
+from __future__ import unicode_literals
+from builtins import dict, str
+import logging
+import requests
+from indra.databases import hgnc_client, uniprot_client
+from indra.statements import *
+
+logger = logging.getLogger("omnipath")
+
+op_url = 'http://omnipathdb.org'
+
+def _agent_from_up_id(up_id):
+    db_refs = {'UP': up_id}
+    gene_name = uniprot_client.get_gene_name(up_id)
+    hgnc_id = hgnc_client.get_hgnc_id(gene_name)
+    if hgnc_id:
+        db_refs['HGNC'] = hgnc_id
+    ag = Agent(gene_name, db_refs=db_refs)
+    return ag
+
+
+def _stmts_from_op_mods(mod_list):
+    stmt_list = []
+    for mod_entry in mod_list:
+        enz = _agent_from_up_id(mod_entry['enzyme'])
+        sub = _agent_from_up_id(mod_entry['substrate'])
+        res = mod_entry['residue_type']
+        pos = mod_entry['residue_offset']
+        ref_list = mod_entry['references']
+        evidence = [Evidence('omnipath', None, pmid)
+                    for pmid in mod_entry['references']]
+        if mod_entry['modification'] == 'phosphorylation':
+            stmt = Phosphorylation(enz, sub, res, pos, evidence)
+        stmt_list.append(stmt)
+    return stmt_list
+
+
+def get_all_modifications():
+    params = {'format': 'json', 'fields':['sources', 'references']}
+    ptm_url = '%s/ptms' % op_url
+    res = requests.get(ptm_url, params=params)
+    if not res.status_code == 200 or not res.text:
+        return None
+    return _stmts_from_op_mods(res.json())
+
+
+def get_modifications(up_list):
+    params = {'format': 'json', 'fields':['sources', 'references']}
+    gene_str = ','.join(up_list)
+    ptm_url = '%s/ptms/%s' % (op_url, gene_str)
+    res = requests.get(ptm_url, params=params)
+    if not res.status_code == 200 or not res.text:
+        return None
+    return _stmts_from_op_mods(res.json())
+
--- indra/tests/test_omnipath.py
+++ indra/tests/test_omnipath.py
@@ -1,8 +1,13 @@
 from __future__ import unicode_literals
 from builtins import dict, str
-
+from indra.statements import Phosphorylation
 from indra.databases import omnipath as op
 
 def test_query_ptms():
-    op.get_ptms(['MAP2K1'])
-
+    stmts = op.get_ptms(['Q13873'])
+    assert len(stmts) == 1
+    assert isinstance(stmts[0], Phosphorylation)
+    assert stmts[0].enz.name == 'CSNK2A1'
+    assert stmts[0].sub.name == 'BMPR2'
+    assert stmts[0].residue == 'S'
+    assert stmts[0].position == '757'
