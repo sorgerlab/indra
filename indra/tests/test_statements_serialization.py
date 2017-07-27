68a5e968777cb41ce7b65d835fc72f5c794173a1
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-07-28 00:27:08 +0200

    Lint code
    Remove unused imports, unnecessary imports of builtins. Also note that
    assert is a keyword and not a function so it gets whitespace before
    parentheses

--- indra/tests/test_statements_serialization.py
+++ indra/tests/test_statements_serialization.py
@@ -1,52 +1,58 @@
 from __future__ import absolute_import, print_function, unicode_literals
-from builtins import dict, str
+
 from indra.statements import *
-from indra.util import unicode_strs
 
 ev = Evidence(source_api='bel', pmid='12345', epistemics={'direct': True},
               text='This is the evidence.')
 
+
 def test_mod_condition_from():
     jd = {'mod_type': 'phosphorylation', 'residue': 'S'}
     mc = ModCondition._from_json(jd)
-    assert(mc.residue == 'S')
-    assert(mc.mod_type == 'phosphorylation')
-    assert(mc.position is None)
+    assert (mc.residue == 'S')
+    assert (mc.mod_type == 'phosphorylation')
+    assert (mc.position is None)
+
 
 def test_agent_mod_condition():
     a = Agent('MAP2K1', mods=[ModCondition('phosphorylation', 'serine', 218),
                               ModCondition('phosphorylation', 'serine', 222)])
     jd = a.to_json()
     jd2 = Agent._from_json(jd).to_json()
-    assert(jd == jd2)
+    assert (jd == jd2)
+
 
 def test_modification():
     stmt = Phosphorylation(Agent('a'), Agent('b'), 'S', evidence=[ev])
     jd = stmt.to_json()
     g = stmt.to_graph()
     jd2 = Statement._from_json(jd).to_json()
-    assert(jd == jd2)
+    assert (jd == jd2)
+
 
 def test_selfmodification():
     stmt = Autophosphorylation(Agent('a'), 'Y', '1234', evidence=[ev])
     jd = stmt.to_json()
     g = stmt.to_graph()
     jd2 = Statement._from_json(jd).to_json()
-    assert(jd == jd2)
+    assert (jd == jd2)
+
 
 def test_activation():
     stmt = Activation(Agent('a'), Agent('b'), 'kinase', evidence=[ev])
     jd = stmt.to_json()
     g = stmt.to_graph()
     jd2 = Statement._from_json(jd).to_json()
-    assert(jd == jd2)
+    assert (jd == jd2)
+
 
 def test_amount():
     stmt = IncreaseAmount(Agent('a'), Agent('b'), evidence=[ev])
     jd = stmt.to_json()
     g = stmt.to_graph()
     jd2 = Statement._from_json(jd).to_json()
-    assert(jd == jd2)
+    assert (jd == jd2)
+
 
 def test_active_form():
     stmt = ActiveForm(Agent('a', location='nucleus'), 'kinase', False,
@@ -54,35 +60,40 @@ def test_active_form():
     jd = stmt.to_json()
     g = stmt.to_graph()
     jd2 = Statement._from_json(jd).to_json()
-    assert(jd == jd2)
+    assert (jd == jd2)
+
 
 def test_complex():
     stmt = Complex([Agent('a'), Agent('b')], evidence=[ev])
     jd = stmt.to_json()
     g = stmt.to_graph()
     jd2 = Statement._from_json(jd).to_json()
-    assert(jd == jd2)
+    assert (jd == jd2)
+
 
 def test_translocation():
     stmt = Translocation(Agent('a'), 'cytoplasm', 'nucleus', evidence=[ev])
     jd = stmt.to_json()
     g = stmt.to_graph()
     jd2 = Statement._from_json(jd).to_json()
-    assert(jd == jd2)
+    assert (jd == jd2)
+
 
 def test_gap():
     stmt = Gap(Agent('a'), Agent('b'), evidence=[ev])
     jd = stmt.to_json()
     g = stmt.to_graph()
     jd2 = Statement._from_json(jd).to_json()
-    assert(jd == jd2)
+    assert (jd == jd2)
+
 
 def test_gef():
     stmt = Gef(Agent('a'), Agent('b'), evidence=[ev])
     jd = stmt.to_json()
     g = stmt.to_graph()
     jd2 = Statement._from_json(jd).to_json()
-    assert(jd == jd2)
+    assert (jd == jd2)
+
 
 def test_supports():
     stmt1 = Gap(Agent('B'), Agent('B'), evidence=[ev])
@@ -93,15 +104,15 @@ def test_supports():
     jd2 = stmt2.to_json()
     jds = [jd1, jd2]
     stmts = stmts_from_json(jds)
-    assert(len(stmts[0].supports) == 1)
-    assert(len(stmts[1].supported_by) == 1)
-    assert(stmts[0].supports[0] == stmts[1])
-    assert(stmts[1].supported_by[0] == stmts[0])
+    assert (len(stmts[0].supports) == 1)
+    assert (len(stmts[1].supported_by) == 1)
+    assert (stmts[0].supports[0] == stmts[1])
+    assert (stmts[1].supported_by[0] == stmts[0])
     jds2 = stmts_to_json(stmts)
     stmts2 = stmts_from_json(jds2)
-    assert(len(stmts2[0].supports) == 1)
-    assert(len(stmts2[1].supported_by) == 1)
-    assert(stmts2[0].supports[0] == stmts2[1])
-    assert(stmts2[1].supported_by[0] == stmts2[0])
+    assert (len(stmts2[0].supports) == 1)
+    assert (len(stmts2[1].supported_by) == 1)
+    assert (stmts2[0].supports[0] == stmts2[1])
+    assert (stmts2[1].supported_by[0] == stmts2[0])
     g = stmt1.to_graph()
     g = stmt2.to_graph()
