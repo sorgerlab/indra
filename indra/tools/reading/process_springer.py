cc073655fc8860a684729b077af23fa15c161a57
pagreene <patrick.anton.greene@gmail.com>
2017-08-10 14:14:07 -0400

    Mostly completed method to upload springer data to AWS.

--- indra/tools/reading/process_springer.py
+++ indra/tools/reading/process_springer.py
@@ -1,28 +1,22 @@
 from __future__ import absolute_import, print_function, unicode_literals
 from builtins import dict, str
-
 import re
-
-from os import listdir, path, walk
+import shutil
+import gzip
+import lxml.etree.ElementTree as ET
+from os import path, walk
 from subprocess import call
+from collections import namedtuple
+
+from indra.db import insert_text_ref, insert_text_content
+from indra.util import zip_string
+from indra.tools.reading.pmc_upload_to_s3 import content_type
 
 RE_PATT_TYPE = type(re.compile(''))
+# TODO: finish this
+SP_INFO = namedtuple('springer_info', ('File', 'date'))
 
-def pdftotext(pdf_file_path, txt_file_path = None):
-    '''Wrapper around the command line function of the same name'''
-    if txt_file_path is None:
-        txt_file_path = pdf_file_path.replace('.pdf', '.txt')
-    elif callable(txt_file_path):
-        txt_file_path = txt_file_path(pdf_file_path)
-
-    call(['pdftotext', pdf_file_path, txt_file_path])
-    assert path.exists(txt_file_path),\
-         "A txt file was not created or name is unknown!"
-
-    return txt_file_path
-
-
-def find(top_dir, patt):
+def _find(top_dir, patt):
     '''Find files that match `patt` recursively down from `top_dir`
     
     Note: patt may be a regex string or a regex pattern object.
@@ -40,21 +34,56 @@ def find(top_dir, patt):
     return matches
 
 
-def convert_pdfs(dir_path, name_rule):
-    '''Convert the pdfs in the springer directory to text.'''
-    pdf_path_list = find(dir_path, '.*?\.pdf')
-    txt_path_list = []
-    for pdf_path in pdf_path_list:
-        print("Converting %s to txt." % path.basename(pdf_path))
-        txt_path_list = pdftotext(pdf_path, name_rule)
-    return txt_path_list
+def _pdftotext(pdf_file_path, txt_file_path = None):
+    '''Wrapper around the command line function of the same name'''
+    if txt_file_path is None:
+        txt_file_path = pdf_file_path.replace('.pdf', '.txt')
+    elif callable(txt_file_path):
+        txt_file_path = txt_file_path(pdf_file_path)
 
-def default_name_rule(pdf_path):
-    '''This is a default name rule for use with convert_pdfs'''
-    pass # TODO: Actually write this. Must have access to group data first.
+    call(['pdftotext', pdf_file_path, txt_file_path])
+    assert path.exists(txt_file_path),\
+         "A txt file was not created or name is unknown!"
+
+    return txt_file_path
 
 
-def add_to_db():
-    '''Add the text files to the database'''
-    # Use XML files for metadata
-    pass
+def upload_springer(springer_dir):
+    '''Convert the pdfs to text and upload data to AWS'''
+    # TODO: This probably shouldn't be hard-coded.
+    txt_path = 'tmp.txt'
+    for pdf_path in _find(springer_dir, '.*?\.pdf'):
+        pdf_name = path.basename(pdf_path)
+        
+        # This is where the xml /should/ be.
+        art_dirname = path.abspath(pdf_path + '/'.join(4*['..']))
+        xml_path_list = _find(art_dirname, pdf_name + '\.xml.*?')
+        assert len(xml_path_list) > 0, "We have no metadata"
+        if len(xml_path_list) == 1:
+            xml = ET.parse(xml_path_list[0])
+        elif len(xml_path_list) > 1:
+            #TODO: we really should be more intelligent about this
+            xml = ET.parse(xml_path_list[0])
+        text_ref_id = insert_text_ref(
+            source = 'springer',
+            doi = xml.find('.//ArticleDOI').text)
+        
+        #TODO: define the content_type
+        content_type = None
+        
+        txt_path = _pdftotext(pdf_path, txt_path)
+        with open(txt_path, 'rb') as f_in:
+            with gzip.open(txt_path + '.gz', 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+        with open(txt_path, 'rb') as f:
+            content = zip_string(f.read())
+            
+        insert_text_content(text_ref_id, content_type, content)
+    return
+
+
+if __name__ == "__main__":
+    #TODO: we should probably support reading from a different
+    # directory.
+    default_dir = '/groups/lsp/darpa/springer/content/data'
+    upload_springer(default_dir)
