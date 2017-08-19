7b5514cfe75422758ea330813c018ba4262547ee
John Bachman <bachmanjohn@gmail.com>
2017-08-19 01:29:38 -0400

    More tests, including failing test of sampling
    Error in test_sampling is IndexError: Cannot choose from an empty sequence
    
    Not sure if it is permissible for sampling to arrive at a node where
    there is no valid path to the target (no successors of v containing tags
    associated with all elements in the current path). If this is
    permissible, then the path needs to be discarded. If not, then there is
    a problem with the calculation of the dictionary.

--- indra/tests/test_cycle_free_paths.py
+++ indra/tests/test_cycle_free_paths.py
@@ -11,9 +11,6 @@ g2_uns.add_edges_from((('A', 'B'), ('B', 'A'), ('B', 'D'), ('A', 'D')))
 g3_uns = nx.DiGraph()
 g3_uns.add_edges_from((('A', 'B'), ('B', 'A'), ('B', 'C'),
                       ('C', 'D'), ('A', 'D')))
-source = 'A'
-target = 'D'
-length = 3
 
 def draw(g, filename):
     ag = nx.nx_agraph.to_agraph(g)
@@ -39,6 +36,9 @@ def test_prune():
                 ((2, 'C'), (3, 'D')), ((3, 'D'), (4, 'T'))])
 
 def test_pg_0():
+    source = 'A'
+    target = 'D'
+    length = 3
     # We first run the pg_0 calculation on a simple graph with no cycles
     # involving the source or target
     (f_level, b_level) = pg.get_reachable_sets(g1_uns, source, target,
@@ -71,6 +71,47 @@ def test_pg_0():
     assert tags == {(0, 'A'): [], (1, 'B'): ['A'], (2, 'C'): ['A'],
                     (3, 'D'): ['A']}
 
+    g4_uns = nx.DiGraph()
+    g4_uns.add_edges_from(((0, 1), (1, 0), (0, 2), (2, 0), (1, 2), (2, 1)))
+    source, target, length = (0, 2, 2)
+    (f_level, b_level) = pg.get_reachable_sets(g4_uns, source, target,
+                                               max_depth=length)
+    pg_raw = pg.paths_graph(g4_uns, source, target, length, f_level, b_level)
+    (pg_0, tags) = cfp.PG_0(pg_raw, (0, source), (length, target))
+    assert pg_0
+    assert tags
+
+
+def test_pg():
+    g4_uns = nx.DiGraph()
+    g4_uns.add_edges_from(((0, 1), (1, 0), (0, 2), (2, 0), (1, 2), (2, 1)))
+    source, target, length = (0, 2, 2)
+    (f_level, b_level) = pg.get_reachable_sets(g4_uns, source, target,
+                                               max_depth=length)
+    pg_raw = pg.paths_graph(g4_uns, source, target, length, f_level, b_level)
+    (pg_0, tags) = cfp.PG_0(pg_raw, (0, source), (length, target))
+    dic_PG = cfp.PG((pg_0, tags), (0, source), (length, target), length)
+    import ipdb; ipdb.set_trace()
+    assert len(dic_PG) == length - 1
+    assert dic_PG[0][0]
+    assert dic_PG[0][1]
+
+
+def test_sampling():
+    g = nx.DiGraph()
+    g.add_edges_from([(0, 1), (0, 3), (0, 4), (0, 5), (1, 4), (2, 4), (2, 5),
+                      (3, 0), (3, 2), (3, 4), (3, 5), (4, 2), (4, 3), (4, 5)])
+    source, target, length = (0, 5, 5)
+    (f_level, b_level) = pg.get_reachable_sets(g, source, target,
+                                               max_depth=length)
+    pg_raw = pg.paths_graph(g, source, target, length, f_level, b_level)
+    src = (0, source)
+    tgt = (length, target)
+    pg_0 = cfp.PG_0(pg_raw, src, tgt)
+    dic_PG = cfp.PG(pg_0, src, tgt, length)
+    G_cf, T = dic_PG[length - 1]
+    P = cfp.cf_sample_many_paths(src, tgt, G_cf, T, 10000)
+
+
 if __name__ == '__main__':
-    test_prune()
-    #test_pg_0()
+    test_sampling()
