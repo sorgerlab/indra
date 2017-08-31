bb9f9671a2120309ddb802e467003e0cc01b0af6
Petar Todorov <petar.v.todorov@gmail.com>
2017-08-31 14:09:28 -0400

    Added test for get_profile_data()

--- indra/tests/test_cbio_client.py
+++ indra/tests/test_cbio_client.py
@@ -51,3 +51,16 @@ def test_get_mutations_ccle():
     assert 'I208V' not in muts['A101D_SKIN']['BRAF']
     assert len(muts['LOXIMVI_SKIN']['AKT1']) == 0
     assert len(muts['A101D_SKIN']['AKT1']) == 0
+
+
+def test_get_profile_data():
+    profile_data = cbio_client.get_profile_data(cbio_client.ccle_study,
+                                                ['BRAF', 'PTEN'],
+                                                'COPY_NUMBER_ALTERATION',
+                                                'all')
+    assert profile_data['BT20_BREAST']['PTEN'] == -2
+    assert profile_data['BT20_BREAST']['BRAF'] == 1
+    assert profile_data['LOXIMVI_SKIN']['PTEN'] == 0
+    assert profile_data['LOXIMVI_SKIN']['BRAF'] == 0
+
+
