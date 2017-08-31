a72dfcd5728e6ab36ece3737ca1ae39ac8a65686
Petar Todorov <petar.v.todorov@gmail.com>
2017-08-31 14:12:37 -0400

    PEP8 fixes

--- indra/tests/test_cbio_client.py
+++ indra/tests/test_cbio_client.py
@@ -2,23 +2,28 @@ from __future__ import absolute_import, print_function, unicode_literals
 from builtins import dict, str
 from indra.databases import cbio_client
 
+
 def test_get_cancer_studies():
     study_ids = cbio_client.get_cancer_studies('paad')
     assert(len(study_ids) > 0)
     assert('paad_tcga' in study_ids)
 
+
 def test_get_cancer_types():
     type_ids = cbio_client.get_cancer_types('lung')
     assert(len(type_ids) > 0)
+
 
 def test_get_genetic_profiles():
     genetic_profiles = \
         cbio_client.get_genetic_profiles('paad_icgc', 'mutation')
     assert(len(genetic_profiles) > 0)
 
+
 def test_get_num_sequenced():
     num_case = cbio_client.get_num_sequenced('paad_tcga')
     assert(num_case > 0)
+
 
 def test_send_request_ccle():
     """Sends a request and gets back a dataframe of all cases in ccle study.
