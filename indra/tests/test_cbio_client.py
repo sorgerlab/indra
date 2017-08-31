5860b830316e6b0a2292556c8dbf6d404baa2372
Petar Todorov <petar.v.todorov@gmail.com>
2017-08-31 15:48:09 -0400

    Update tests for cbio_client

--- indra/tests/test_cbio_client.py
+++ indra/tests/test_cbio_client.py
@@ -67,9 +67,14 @@ def test_get_profile_data():
     assert profile_data['BT20_BREAST']['BRAF'] == 1
     assert profile_data['LOXIMVI_SKIN']['PTEN'] == 0
     assert profile_data['LOXIMVI_SKIN']['BRAF'] == 0
+    assert len(profile_data) > 0
 
 
 def test_get_ccle_cna():
-    profile_data = cbio_client.get_ccle_cna(['BRAF'])
-    assert profile_data['BT20_BREAST']['BRAF'] == 1
+    profile_data = cbio_client.get_ccle_cna(['BRAF', 'AKT1'],
+                                            ['LOXIMVI_SKIN', 'SKMEL30_SKIN'])
+    assert profile_data['SKMEL30_SKIN']['BRAF'] == 1
+    assert profile_data['SKMEL30_SKIN']['AKT1'] == -1
     assert profile_data['LOXIMVI_SKIN']['BRAF'] == 0
+    assert profile_data['LOXIMVI_SKIN']['AKT1'] == 0
+    assert len(profile_data) == 2
