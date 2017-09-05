24728b534f7d406976683ba57e17f76019bda60a
John Bachman <bachmanjohn@gmail.com>
2017-09-05 12:16:02 -0400

    Node information is now correct

--- indra/assemblers/pybel_assembler.py
+++ indra/assemblers/pybel_assembler.py
@@ -5,7 +5,9 @@ from indra.databases import hgnc_client
 import logging
 import pybel
 import pybel.constants as pc
-from pybel.parser.modifiers import PmodParser
+from copy import deepcopy
+from pybel.parser.language import pmod_namespace
+from pybel.parser.parse_bel import canonicalize_variant
 
 # Python 2
 try:
@@ -26,74 +28,125 @@ class PybelAssembler(object):
 
         self.model = None
 
-
     def make_model(self, **kwargs):
         self.model = pybel.BELGraph(**kwargs)
         for stmt in self.statements:
             # Convert phosphorylation statements into
             # kin(p(agent)) => pmod(p(agent))
-            if isinstance(stmt, Phosphorylation):
-                # Define the enzyme node
-                (enz_node, enz_attr) = _get_agent_node(stmt.enz)
-                (sub_node, sub_attr) = _get_agent_node(stmt.sub)
-                sub_attr[pc.VARIANTS] = [
-                                    {pc.KIND: pc.PMOD,
-                                     pc.IDENTIFIER: {
-                                       pc.NAMESPACE: pc.BEL_DEFAULT_NAMESPACE,
-                                       pc.NAME: 'Ph'},
-                                     PmodParser.CODE: stmt.residue,
-                                     PmodParser.POSITION: stmt.position }]
-                self.model.add_node(enz_node, enz_attr)
-                self.model.add_node(sub_node, sub_attr)
-                # Create an edge for each evidence object
-                edge = {pc.SUBJECT: {
-                        pc.EFFECT: {pc.NAME: 'kin',
-                                    pc.NAMESPACE: pc.BEL_DEFAULT_NAMESPACE},
-                        pc.MODIFIER: pc.ACTIVITY},
-                        pc.RELATION: pc.INCREASES,
-                        pc.OBJECT: {}}
-                        #'object': {
-                        #    'effect': {'name': 'kin', 'namespace': 'bel'},
-                        #    'modifier': 'Activity'}}
-                # If there's no evidence for this statement, add node without
-                # any evidence info
-                import ipdb; ipdb.set_trace()
-                if not stmt.evidence:
-                    edge[pc.ANNOTATIONS] = {}
-                    edge[pc.CITATION] = {}
-                    edge[pc.EVIDENCE] = ''
-                    self.model.add_edge(enz_node, sub_node, attr_dict=edge)
-                # Otherwise, add an edge for each piece of evidence.
-                else:
-                    for ev in stmt.evidence:
-                        edge[pc.ANNOTATIONS] = {}
-                        # FIXME Retrieve citation information from pubmed_client
-                        edge[pc.CITATION] = {'authors': '', 'comments': '',
-                                             'date': '', 'name': '',
-                                             'reference': ev.pmid,
-                                             'type': 'PubMed'},
-                        edge[pc.EVIDENCE] = ev.text
-                        self.model.add_edge(enz_node, sub_node, attr_dict=edge)
+            if isinstance(stmt, Modification):
+                self._assemble_modification(stmt)
         return self.model
 
+    def _assemble_modification(self, stmt):
+        #(enz_node, enz_attr) = _get_agent_node(stmt.enz)
+        #(sub_node, sub_attr) = _get_agent_node(stmt.sub)
+        (enz_node, enz_attr) = _get_agent_node(stmt.enz)
+        sub_agent = _get_modified_substrate(stmt)
+        (sub_node, sub_attr) = _get_agent_node(sub_agent)
+        self.model.add_node(enz_node, attr_dict=enz_attr)
+        self.model.add_node(sub_node, attr_dict=sub_attr)
+        # Create an edge for each evidence object
+        edge = {pc.SUBJECT: {
+                pc.EFFECT: {pc.NAME: 'kin',
+                            pc.NAMESPACE: pc.BEL_DEFAULT_NAMESPACE},
+                pc.MODIFIER: pc.ACTIVITY},
+                pc.RELATION: pc.INCREASES,
+                pc.OBJECT: {}}
+                #'object': {
+                #    'effect': {'name': 'kin', 'namespace': 'bel'},
+                #    'modifier': 'Activity'}}
+        # If there's no evidence for this statement, add node without
+        # any evidence info
+        if not stmt.evidence:
+            edge[pc.ANNOTATIONS] = {}
+            edge[pc.CITATION] = {}
+            edge[pc.EVIDENCE] = ''
+            self.model.add_edge(enz_node, sub_node, attr_dict=edge)
+        # Otherwise, add an edge for each piece of evidence.
+        else:
+            for ev in stmt.evidence:
+                edge[pc.ANNOTATIONS] = {}
+                # FIXME Retrieve citation information from pubmed_client
+                edge[pc.CITATION] = {'authors': '', 'comments': '',
+                                     'date': '', 'name': '',
+                                     'reference': ev.pmid,
+                                     'type': 'PubMed'},
+                edge[pc.EVIDENCE] = ev.text
+                self.model.add_edge(enz_node, sub_node, attr_dict=edge)
+
+
+def _get_modified_substrate(mod_stmt):
+    mod_agent = deepcopy(mod_stmt.sub)
+    modtype = modclass_to_modtype[mod_stmt.__class__]
+    res = mod_stmt.residue
+    pos = mod_stmt.position
+    mc = ModCondition(mod_stmt.__class__.__name__.lower(), res, pos)
+    mod_agent.mods.append(mc)
+    return mod_agent
+
+
 def _get_agent_node(agent):
-    if agent is None:
-        return (None, None)
-    # FIXME: For now, deal only with agents having HGNC grounding
-    # FIXME: For now, don't look at bound/mod/mut/loc/activity conditions
-    hgnc_id = agent.db_refs.get('HGNC')
-    if hgnc_id is None:
-        return (None, None)
-    hgnc_name = hgnc_client.get_hgnc_name(hgnc_id)
-    node = (pc.PROTEIN, 'HGNC', hgnc_name)
-    node_attr = {pc.FUNCTION: pc.PROTEIN, pc.NAMESPACE: 'HGNC',
-                 pc.NAME: hgnc_name}
-    return (node, node_attr)
+    (db_ns, db_id) = _agent_grounding(agent)
+    if db_ns == None:
+        logging.warning('Agent %s has no grounding.', stmt)
+        return None
+    node_attr = {pc.FUNCTION: pc.PROTEIN,
+                     pc.NAMESPACE: db_ns,
+                     pc.NAME: db_id}
+    variants = []
+    for mod in agent.mods:
+        var = {pc.KIND: pc.PMOD,
+               pc.IDENTIFIER: {
+                   pc.NAMESPACE: pc.BEL_DEFAULT_NAMESPACE,
+                   pc.NAME: pmod_namespace[mod.mod_type]}}
+        if mod.residue is not None:
+            res = amino_acids[mod.residue]['short_name'].capitalize()
+            var[pc.PMOD_CODE] = res
+        if mod.position is not None:
+            var[pc.PMOD_POSITION] = int(mod.position)
+        variants.append(var)
+    # TODO: Handle other types of variants, e.g. Mutations
+    if variants:
+        node_attr[pc.VARIANTS] = variants
+    node_tuple = _make_node_tuple(node_attr)
+    return (node_tuple, node_attr)
+
+
+def _agent_grounding(agent):
+        hgnc_id = agent.db_refs.get('HGNC')
+        up_id = agent.db_refs.get('UP')
+        # If no HGNC, check for Uniprot (in case is not a human gene)
+        if hgnc_id:
+            hgnc_name = hgnc_client.get_hgnc_name(hgnc_id)
+            return ('HGNC', hgnc_name)
+        elif up_id:
+            return ('UNIPROT', up_id)
+        else:
+            return (None, None)
+
+
+def _make_node_tuple(node_attr):
+    if pc.VARIANTS in node_attr:
+        variants = tuple(sorted([canonicalize_variant(token)
+                                 for token in node_attr[pc.VARIANTS]]))
+        return _make_simple_tuple(node_attr) + variants
+    return _make_simple_tuple(node_attr)
+
+
+def _make_simple_tuple(node_attr):
+    return (node_attr[pc.FUNCTION], node_attr[pc.NAMESPACE], node_attr[pc.NAME])
+
 
 def _get_citation(evidence):
     pass
 
 """
+Representation of PTM reactions in PyBEL/BEL
+--------------------------------------------
+- Kin -> pmod(Sub) vs.
+- Kin -> rxn(Sub + ATP, pmod(Sub) + ADP)
+
+
 structure of bel graph.
 - node/tuple, maps to
   - dict with node/tuples as keys, each one mapped to
--- indra/tests/test_pybel_assembler.py
+++ indra/tests/test_pybel_assembler.py
@@ -5,30 +5,49 @@ import networkx as nx
 import pybel.constants as pc
 
 phostuple = (pc.PMOD, (pc.BEL_DEFAULT_NAMESPACE, 'Ph'), 'Ser', 218)
+ubtuple = (pc.PMOD, (pc.BEL_DEFAULT_NAMESPACE, 'Ub'), 'Lys', 218)
+braf_node = (pc.PROTEIN, 'HGNC', 'BRAF')
+map2k1_node = (pc.PROTEIN, 'HGNC', 'MAP2K1')
 
 def draw(g, filename):
     ag = nx.nx_agraph.to_agraph(g)
     ag.draw(filename, prog='dot')
 
 def test_simple_phosphorylation_no_evidence():
-    braf = Agent('BRAF', db_refs={'HGNC': '1097', 'UP': 'P15056'})
-    mek = Agent('MAP2K1', db_refs={'HGNC': '6840', 'UP': 'Q02750'})
-    stmt = Phosphorylation(braf, mek, 'S', '218')
-    pba = pa.PybelAssembler([stmt])
-    belgraph = pba.make_model()
-    #pybel.to_bel(belgraph)
-    assert len(belgraph.nodes()) == 2
-    assert ('Protein', 'HGNC', 'BRAF') in belgraph
-    assert ('Protein', 'HGNC', 'MAP2K1', phostuple) in belgraph
-    assert len(belgraph.edges()) == 1
-    _, _, edge_data = belgraph.edges(data=True)[0]
-    assert edge_data[pc.SUBJECT] == {
+    for modclass, modtuple in ((Phosphorylation, phostuple),
+                               (Ubiquitination, ubtuple)):
+        braf = Agent('BRAF', db_refs={'HGNC': '1097', 'UP': 'P15056'})
+        mek = Agent('MAP2K1', db_refs={'HGNC': '6840', 'UP': 'Q02750'})
+        stmt = modclass(braf, mek, 'S', '218')
+        pba = pa.PybelAssembler([stmt])
+        belgraph = pba.make_model()
+        import ipdb; ipdb.set_trace()
+        assert len(belgraph.nodes()) == 2
+        assert braf_node in belgraph
+        map2k1_mod_node = map2k1_node + tuple([modtuple])
+        assert map2k1_mod_node in belgraph
+        assert belgraph.node[braf_node] == {
+                pc.FUNCTION: pc.PROTEIN,
+                pc.NAMESPACE: 'HGNC',
+                pc.NAME: 'BRAF'}
+        assert belgraph.node[map2k1_mod_node] == {
+                pc.FUNCTION: pc.PROTEIN,
+                pc.NAMESPACE: 'HGNC',
+                pc.NAME: 'MAP2K1',
+                pc.VARIANTS: [{
+                    pc.KIND: pc.PMOD,
+                    pc.IDENTIFIER: {
+                        pc.NAMESPACE: pc.BEL_DEFAULT_NAMESPACE,
+                        pc.NAME: modtuple[1][1]},
+                    pc.PMOD_CODE: modtuple[2],
+                    pc.PMOD_POSITION: modtuple[3]}]}
+        assert belgraph.number_of_edges() == 1
+        _, _, edge_data = belgraph.edges(data=True)[0]
+        assert edge_data[pc.SUBJECT] == {
                 pc.MODIFIER: pc.ACTIVITY,
                 pc.EFFECT: {
                     pc.NAME: 'kin', pc.NAMESPACE: pc.BEL_DEFAULT_NAMESPACE}}
-    assert edge_data[pc.RELATION] == pc.DIRECTLY_INCREASES
-
-    draw(belgraph, 'belgraph.pdf')
+        assert edge_data[pc.RELATION] == pc.DIRECTLY_INCREASES
 
 def test_simple_phosphorylation_with_evidence():
     braf = Agent('BRAF', db_refs={'HGNC': '1097', 'UP': 'P15056'})
