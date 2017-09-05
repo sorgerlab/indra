54f5257f7c4b15f0dd9bb95e330ee1e1e7832119
John Bachman <bachmanjohn@gmail.com>
2017-09-05 10:37:27 -0400

    Initial simple phos test (failing)

--- indra/tests/test_pybel_assembler.py
+++ indra/tests/test_pybel_assembler.py
@@ -1,6 +1,14 @@
 from indra.assemblers import pybel_assembler as pa
 from indra.statements import *
 import pybel
+import networkx as nx
+import pybel.constants as pc
+
+phostuple = (pc.PMOD, (pc.BEL_DEFAULT_NAMESPACE, 'Ph'), 'Ser', 218)
+
+def draw(g, filename):
+    ag = nx.nx_agraph.to_agraph(g)
+    ag.draw(filename, prog='dot')
 
 def test_simple_phosphorylation_no_evidence():
     braf = Agent('BRAF', db_refs={'HGNC': '1097', 'UP': 'P15056'})
@@ -8,11 +16,19 @@ def test_simple_phosphorylation_no_evidence():
     stmt = Phosphorylation(braf, mek, 'S', '218')
     pba = pa.PybelAssembler([stmt])
     belgraph = pba.make_model()
-    pybel.to_bel(belgraph)
+    #pybel.to_bel(belgraph)
     assert len(belgraph.nodes()) == 2
-    assert ('Protein', 'HGNC', 'BRAF') in belgraph.nodes()
-    assert ('Protein', 'HGNC', 'MAP2K1') in belgraph.nodes()
+    assert ('Protein', 'HGNC', 'BRAF') in belgraph
+    assert ('Protein', 'HGNC', 'MAP2K1', phostuple) in belgraph
     assert len(belgraph.edges()) == 1
+    _, _, edge_data = belgraph.edges(data=True)[0]
+    assert edge_data[pc.SUBJECT] == {
+                pc.MODIFIER: pc.ACTIVITY,
+                pc.EFFECT: {
+                    pc.NAME: 'kin', pc.NAMESPACE: pc.BEL_DEFAULT_NAMESPACE}}
+    assert edge_data[pc.RELATION] == pc.DIRECTLY_INCREASES
+
+    draw(belgraph, 'belgraph.pdf')
 
 def test_simple_phosphorylation_with_evidence():
     braf = Agent('BRAF', db_refs={'HGNC': '1097', 'UP': 'P15056'})
@@ -29,5 +45,5 @@ def test_simple_phosphorylation_with_evidence():
     assert len(belgraph.edges()) == 2
 
 if __name__ == '__main__':
-     test_simple_phosphorylation_with_evidence()
+     test_simple_phosphorylation_no_evidence()
 
