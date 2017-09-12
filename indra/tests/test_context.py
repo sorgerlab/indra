35dd439d0aa2ba2be8c467992d38bb7dc0af347a
Ben Gyori <ben.gyori@gmail.com>
2017-09-12 12:42:28 -0400

    Update context tests for new API

--- indra/tests/test_context.py
+++ indra/tests/test_context.py
@@ -4,18 +4,18 @@ from indra.databases import context_client
 from indra.util import unicode_strs
 
 def test_get_protein_expression():
-    res = context_client.get_protein_expression('EGFR', 'BT20_BREAST')
+    res = context_client.get_protein_expression(['EGFR'], ['BT20_BREAST'])
     assert(res is not None)
-    assert(res.get('EGFR') is not None)
-    assert(res['EGFR'].get('BT20_BREAST') is not None)
-    assert(res['EGFR']['BT20_BREAST'] > 1000)
+    assert(res.get('BT20_BREAST') is not None)
+    assert(res['BT20_BREAST'].get('EGFR') is not None)
+    assert(res['BT20_BREAST']['EGFR'] > 1000)
     assert unicode_strs(res)
 
 def test_get_mutations():
-    res = context_client.get_mutations('BRAF', 'A375_SKIN')
+    res = context_client.get_mutations(['BRAF'], ['A375_SKIN'])
     assert(res is not None)
-    assert(res.get('BRAF') is not None)
-    assert(res['BRAF'].get('A375_SKIN') is not None)
-    assert(res['BRAF']['A375_SKIN'] == 1.0)
+    assert(res.get('A375_SKIN') is not None)
+    assert(res['A375_SKIN'].get('BRAF') is not None)
+    assert(res['A375_SKIN']['BRAF'] == ['V600E'])
     assert unicode_strs(res)
 
