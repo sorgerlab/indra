fe6cb6db30eae6b90213152442191af9691f681f
John Bachman <bachmanjohn@gmail.com>
2017-09-22 23:41:17 -0400

    Function to get evidence/epistemics/annotations

--- indra/sources/signor.py
+++ indra/sources/signor.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, print_function, unicode_literals
 from builtins import dict, str
 import logging
-from collections import namedtuple
+from collections import namedtuple, Counter, defaultdict
 
 from indra.statements import *
 from indra.util import read_unicode_csv
@@ -53,6 +53,45 @@ _type_db_map = {
     ('chemical', 'PUBCHEM'): 'PUBCHEM',
 }
 
+_mechanism_map = {
+    'catalytic activity': None, # Conversion
+    'oxidoreductase activity': None,
+    'transcriptional activation': None, # IncreaseAmount by tscript
+    'transcriptional repression': None, # DecreaseAmount by tscript
+    'Farnesylation': Farnesylation,
+    'gtpase-activating protein': Gap,
+    'deacetylation': Deacetylation,
+    'demethylation': Demethylation,
+    'dephosphorylation': Dephosphorylation,
+    'destabilization': DecreaseAmount,
+    'guanine nucleotide exchange factor': Gef,
+    'acetylation': Acetylation,
+    'binding': Complex,
+    'cleavage': None, # Important!
+    'desumoylation': Desumoylation,
+    'deubiquitination': Deubiquitination,
+    'glycosylation': Glycosylation,
+    'hydroxylation': Hydroxylation,
+    'neddylation': None, # Neddylation,
+    'chemical activation': Activation,
+    'chemical inhibition': Inhibition,
+    'trimethylation': Methylation,
+    'ubiquitination': Ubiquitination,
+    'post transcriptional regulation': None,
+    'relocalization': Translocation,
+    'small molecule catalysis': None,
+    's-nitrosylation': None,
+    'transcriptional regulation': RegulateAmount, # Subject has tscript activity
+    'translation regulation': None,
+    'tyrosination': None,
+    'lipidation': None,
+    'oxidation': None,
+    'methylation': Methylation,
+    'palmitoylation': Palmitoylation,
+    'phosphorylation': Phosphorylation,
+    'stabilization': IncreaseAmount,
+    'sumoylation': Sumoylation
+}
 
 SignorRow = namedtuple('SignorRow', _signor_fields)
 
@@ -80,6 +119,19 @@ class SignorProcessor(object):
         next(data_iter)
         # Process into a list of SignorRow namedtuples
         self._data = [SignorRow(*r) for r in data_iter]
+        self.statements = []
+
+    def make_model(self):
+        mechs = []
+        mech_map = defaultdict(list)
+        for row in self._data:
+            #self.statements.append(self._process_row(row))
+            key = (row.EFFECT, row.MECHANISM)
+            mechs.append(key)
+            mech_map[key].append(row)
+        mech_ctr = Counter(mechs)
+        mech_ctr = sorted(mech_ctr.items(), key=lambda x: x[1], reverse=True)
+        return (mech_ctr, mech_map)
 
     @staticmethod
     def _get_agent(ent_name, ent_type, id, database):
@@ -102,7 +154,34 @@ class SignorProcessor(object):
             db_refs = {}
         return Agent(name, db_refs=db_refs)
 
-    def _process_row():
-        agent_a = _entity_a(row)
+    #@staticmethod(agent_a, agent_b, mechanism, effect,
+    @staticmethod
+    def _get_evidence(row):
+        # Get epistemics (direct/indirect)
+        epistemics = {}
+        epistemics['direct'] = True if row.DIRECT == 'YES' else False
+        # Get annotations
+        _n = lambda s: s if s else None
+        annotations = {
+                'SEQUENCE': _n(row.SEQUENCE),
+                'TAX_ID': _n(row.TAX_ID),
+                'CELL_DATA': _n(row.CELL_DATA),
+                'TISSUE_DATA': _n(row.TISSUE_DATA),
+                'MODULATOR_COMPLEX': _n(row.MODULATOR_COMPLEX),
+                'TARGET_COMPLEX': _n(row.TARGET_COMPLEX),
+                'MODIFICATIONA': _n(row.MODIFICATIONA),
+                'MODASEQ': _n(row.MODASEQ),
+                'MODIFICATIONB': _n(row.MODIFICATIONB),
+                'MODBSEQ': _n(row.MODBSEQ),
+                'NOTES': _n(row.NOTES),
+                'ANNOTATOR': _n(row.ANNOTATOR)}
+        return Evidence(source_api='SIGNOR', source_id=row.SIGNOR_ID,
+                        pmid=row.PMID, text=row.SENTENCE,
+                        epistemics=epistemics, annotations=annotations)
 
 
+    @staticmethod
+    def _process_row(row):
+        agent_a = _get_agent(row.ENTITYA, row.TYPEA, row.IDA, row.DATABASEA)
+        agent_b = _get_agent(row.ENTITYB, row.TYPEB, row.IDB, row.DATABASEB)
+        
--- indra/tests/test_signor.py
+++ indra/tests/test_signor.py
@@ -20,46 +20,80 @@ test_row = SignorRow(ENTITYA='RELA', TYPEA='protein', IDA='Q04206',
         TAX_ID='10090', CELL_DATA='BTO:0002895', TISSUE_DATA='',
         MODULATOR_COMPLEX='', TARGET_COMPLEX='', MODIFICATIONA='', MODASEQ='',
         MODIFICATIONB='', MODBSEQ='', PMID='19530226', DIRECT='YES', NOTES='',
-        ANNOTATOR='gcesareni', SENTENCE="""Together, these results indicate that
-        the Met gene is a direct target of NFkappaB and that Met participates
-        in NFkappaB-mediated cell survival.""", SIGNOR_ID='SIGNOR-241929')
+        ANNOTATOR='gcesareni',
+        SENTENCE="Together, these results indicate that the Met gene is a "\
+                 "direct target of NFkappaB and that Met participates "\
+                 "in NFkappaB-mediated cell survival.",
+        SIGNOR_ID='SIGNOR-241929')
 
 
 def test_parse_csv():
     sp = SignorProcessor(signor_test_path)
     assert isinstance(sp._data, list)
     assert isinstance(sp._data[0], SignorRow)
-    globals().update(locals())
 
 
 def test_get_agent():
     # Protein/gene
     test_ag = Agent('RELA', db_refs={'HGNC': _id('RELA'), 'UP': 'Q04206'})
-    sp_ag = sp._get_agent(test_row.ENTITYA, test_row.TYPEA, test_row.IDA,
-                          test_row.DATABASEA)
+    sp_ag = SignorProcessor._get_agent(test_row.ENTITYA, test_row.TYPEA,
+                                       test_row.IDA, test_row.DATABASEA)
     assert test_ag.matches(sp_ag)
     # Chemical
     test_ag = Agent('AZD1480', db_refs={'PUBCHEM': 'CID:16659841'})
-    sp_ag = sp._get_agent('AZD1480', 'chemical', 'CID:16659841', 'PUBCHEM')
+    sp_ag = SignorProcessor._get_agent('AZD1480', 'chemical', 'CID:16659841',
+                                       'PUBCHEM')
     assert test_ag.matches(sp_ag)
     # Signor phenotype
     test_ag = Agent('Cell cycle progr.', db_refs={'SIGNOR': 'SIGNOR-PH42'})
-    sp_ag = sp._get_agent('Cell cycle progr.', 'phenotype', 'SIGNOR-PH42',
-                          'SIGNOR')
+    sp_ag = SignorProcessor._get_agent('Cell cycle progr.', 'phenotype',
+                                       'SIGNOR-PH42', 'SIGNOR')
     assert test_ag.matches(sp_ag)
     # Ungrounded -- couldn't find a real example in the dataset
     test_ag = Agent('Foobar', db_refs={})
-    sp_ag = sp._get_agent('Foobar', 'pathway', None, None)
+    sp_ag = SignorProcessor._get_agent('Foobar', 'pathway', None, None)
     assert test_ag.matches(sp_ag)
-    sp_ag = sp._get_agent('Foobar', 'antibody', None, None)
+    sp_ag = SignorProcessor._get_agent('Foobar', 'antibody', None, None)
     assert test_ag.matches(sp_ag)
+
 
 @raises(KeyError)
 def test_get_agent_keyerror():
-    sp_ag = sp._get_agent('foo', 'bar', None, None)
+    sp_ag = SignorProcessor._get_agent('foo', 'bar', None, None)
+
+
+def test_get_evidence():
+    ev = SignorProcessor._get_evidence(test_row)
+    assert isinstance(ev, Evidence)
+    assert ev.pmid == '19530226'
+    assert ev.annotations == {
+            'SEQUENCE': None,
+            'TAX_ID': '10090',
+            'CELL_DATA': 'BTO:0002895',
+            'TISSUE_DATA': None,
+            'MODULATOR_COMPLEX': None,
+            'TARGET_COMPLEX': None,
+            'MODIFICATIONA': None,
+            'MODASEQ': None,
+            'MODIFICATIONB': None,
+            'MODBSEQ': None,
+            'NOTES': None,
+            'ANNOTATOR': 'gcesareni',
+        }
+    assert ev.epistemics['direct']
+    assert ev.source_api == 'SIGNOR'
+    assert ev.source_id == 'SIGNOR-241929'
+    assert ev.text == "Together, these results indicate that the Met gene " \
+                      "is a direct target of NFkappaB and that Met " \
+                      "participates in NFkappaB-mediated cell survival."
+
+#def test_get_mechanism():
+#    sp = SignorProcessor(signor_test_path)
+#    mechs, mech_map = sp.make_model()
 
 
 if __name__ == '__main__':
     test_parse_csv()
     test_get_agent()
     test_get_agent_keyerror()
+    test_get_evidence()
