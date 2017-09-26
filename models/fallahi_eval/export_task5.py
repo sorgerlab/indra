e39ac751f72fc8283824f4b4c771ed44de88f731
John Bachman <bachmanjohn@gmail.com>
2017-09-26 14:57:16 -0400

    Update export_task scripts to print path groups

--- models/fallahi_eval/export_task5.py
+++ models/fallahi_eval/export_task5.py
@@ -1,11 +1,20 @@
 from util import *
 import indra.tools.assemble_corpus as ac
 from run_task5 import export_paths, report_paths
+import group_paths as gp
 
-# This should be done for all cell lines
-cell_lines = ['C32', 'RVH421']
-for cell_line in cell_lines:
-    scored_paths, models = pklload('task5_scored_paths')
-    model = models[cell_line]
-    stmts = pklload('pysb_stmts_%s' % cell_line)
-    report_paths(scored_paths[cell_line], model, stmts, cell_line)
+stmts = pklload('pysb_stmts')
+
+for pkl_name in ('task5_scored_paths', 'task5_scored_paths_inverse'):
+    if pkl_name.endswith('_inverse'):
+        print("\n\n -- INVERSE -------------------------------------\n\n")
+    scored_path_dict, models = pklload(pkl_name)
+    for cell_line in ['C32', 'RVH421']:
+        model = models[cell_line]
+        report_paths(scored_path_dict[cell_line], model, stmts, cell_line)
+
+        for drug_name, scored_paths in scored_path_dict[cell_line].items():
+            print("Grouping paths for %s, %s" % (cell_line, drug_name))
+            gp.print_top_group_scores(scored_paths, model, stmts)
+
+
