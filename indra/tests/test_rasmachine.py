1e783d2f7d18accdede8bb11cb752084b8898fd8
Ben Gyori <ben.gyori@gmail.com>
2017-09-27 00:50:53 -0400

    Update Ras Machine status tests

--- indra/tests/test_rasmachine.py
+++ indra/tests/test_rasmachine.py
@@ -7,10 +7,8 @@ stats['new_abstracts'] = 0
 stats['new_papers'] = 0
 stats['orig_stmts'] = 10
 stats['new_stmts'] = 10
-stats['orig_top'] = 10
-stats['orig_likely'] = 10
-stats['new_top'] = 10
-stats['new_likely'] = 10
+stats['orig_final'] = 10
+stats['new_final'] = 10
 
 def test_noabs_nopaper():
     s = stats.copy()
@@ -20,7 +18,7 @@ def test_noabs_nopaper():
 def test_absonly():
     s = stats.copy()
     s['new_abstracts'] = 7
-    s['new_likely'] = 12
+    s['new_final'] = 12
     status_msg = make_status_message(s)
     assert(status_msg == 'Today I read 7 abstracts, ' +\
                          'and learned 2 new mechanisms!')
@@ -28,7 +26,7 @@ def test_absonly():
 def test_papersonly():
     s = stats.copy()
     s['new_papers'] = 3
-    s['new_likely'] = 12
+    s['new_final'] = 12
     status_msg = make_status_message(s)
     assert(status_msg == 'Today I read 3 papers, ' +\
                          'and learned 2 new mechanisms!')
@@ -37,7 +35,7 @@ def test_abs_and_paper():
     s = stats.copy()
     s['new_papers'] = 1
     s['new_abstracts'] = 1
-    s['new_likely'] = 12
+    s['new_final'] = 12
     status_msg = make_status_message(s)
     assert(status_msg == 'Today I read 1 paper and 1 abstract, ' +\
                          'and learned 2 new mechanisms!')
@@ -46,7 +44,7 @@ def test_abs_and_papers():
     s = stats.copy()
     s['new_papers'] = 2
     s['new_abstracts'] = 2
-    s['new_likely'] = 12
+    s['new_final'] = 12
     status_msg = make_status_message(s)
     assert(status_msg == 'Today I read 2 papers and 2 abstracts, ' +\
                          'and learned 2 new mechanisms!')
