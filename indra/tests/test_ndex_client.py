fa0b1dac9a830f31c247a2bc835a2e03dc7eaa14
Ben Gyori <ben.gyori@gmail.com>
2017-09-26 21:12:40 -0400

    Separate out ndex client tests

---
+++ indra/tests/test_ndex_client.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import, print_function, unicode_literals
+from builtins import dict, str
+from indra.databases import ndex_client
+
+
+def test_ndex_ver():
+    assert(ndex_client._increment_ndex_ver(None) == '1.0')
+    assert(ndex_client._increment_ndex_ver('') == '1.0')
+    assert(ndex_client._increment_ndex_ver('1.0') == '1.1')
+    assert(ndex_client._increment_ndex_ver('1.9') == '1.10')
+    assert(ndex_client._increment_ndex_ver('2.10') == '2.11')
+
