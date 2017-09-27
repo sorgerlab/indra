241d70fdccd73c3bdf52ad2f9354d8b8b13cb9db
Ben Gyori <ben.gyori@gmail.com>
2017-09-27 00:54:25 -0400

    Update some other comments to new machine location

--- indra/tools/machine/__main__.py
+++ indra/tools/machine/__main__.py
@@ -1,5 +1,5 @@
 """
-Entrypoint module, in case you use `python -m indra`
+Entrypoint module, in case you use `python -m indra.tools.machine`
 
 Why does this file exist, and why __main__? For more info, read:
 
--- indra/tools/machine/cli.py
+++ indra/tools/machine/cli.py
@@ -4,11 +4,11 @@ Module that contains the command line app
 Why does this file exist, and why not put this in __main__?
 You might be tempted to import things from __main__ later, but that will cause
 problems--the code will get executed twice:
- - When you run `python3 -m indra` python will execute
+ - When you run `python3 -m indra.tools.machine` python will execute
    ``__main__.py`` as a script. That means there won't be any
-   ``indra.__main__`` in ``sys.modules``.
+   ``indra.tools.machine__main__`` in ``sys.modules``.
  - When you import __main__ it will get executed again (as a module) because
-   there's no ``indra.__main__`` in ``sys.modules``.
+   there's no ``indra.tools.machine__main__`` in ``sys.modules``.
 Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
 """
 
