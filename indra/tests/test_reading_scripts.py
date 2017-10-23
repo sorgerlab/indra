153ac85401d3b512800c8782bb09d845a75d8299
pagreene <patrick.anton.greene@gmail.com>
2017-10-23 17:57:15 -0400

    Add test for test enrichement of reading data.

--- indra/tests/test_reading_scripts.py
+++ indra/tests/test_reading_scripts.py
@@ -8,7 +8,8 @@ from nose import SkipTest
 from indra.tools.reading.read_pmids import READER_DICT, get_proc_num,\
     get_mem_total
 from indra.tools.reading.read_db import _convert_id_entry, get_content,\
-    get_clauses, post_reading_output, read_content, make_statements
+    get_clauses, post_reading_output, read_content, make_statements,\
+    enrich_reading_data
 
 from indra.tests.test_db import get_db as get_test_db
 from indra.tests.test_db import get_db_with_content
@@ -35,7 +36,7 @@ TMP_DIR_FMT = '%s_%%s' % BASENAME
 OUTPUT_FILE_FMT = '%s_stmts_0-10.pkl' % TMP_DIR_FMT
 READINGS_PKL = 'sample_reach_outputs.pkl'
 
-
+''' These test a depricated feature, and take FOREVER.
 def _call_reader(reader, num_cores):
     out_dir = TMP_DIR_FMT % reader
     if not path.exists(out_dir):
@@ -102,6 +103,7 @@ def test_sparser_two_core():
         raise SkipTest("Not enough processes.")
     stmts = _call_reader('sparser', 2)
     _check_result(stmts)
+'''
 
 # ==============================================================================
 # Tests for NEW reading pipeline which uses the database.
@@ -109,12 +111,14 @@ def test_sparser_two_core():
 
 
 def test_convert_id_entry():
+    "Test that we correctly conver the id's given us."
     id_entry = 'pmid\t: 12345\n'
     res = _convert_id_entry(id_entry)
     assert len(res) == 2 and res[0] == 'pmid' and res[1] == '12345'
 
 
 def test_get_clauses():
+    "Test that the clauses are correctly created."
     db = get_test_db()
     id_str_list = ['pmid:17399955']
     clauses = get_clauses(id_str_list, db.TextRef)
@@ -122,6 +126,7 @@ def test_get_clauses():
 
 
 def test_get_content():
+    "Test that we get content from the database successfully."
     id_str_list = ['pmid:17399955']
     content = get_content(id_str_list)
     assert len(list(content)) == 1, "Failed to get correct content."
@@ -163,5 +168,22 @@ def test_make_statements():
     "Test that statements can be created from reading output."
     with open(READINGS_PKL, 'rb') as f:
         reading_output = pickle.load(f)
-    make_statements(reading_output)
+    stmts = make_statements(reading_output)
+    assert len(stmts), 'No statements created.'
 
+
+def test_enrichment():
+    "Test that reading data can be enriched with ids from the database."
+    db = get_db_with_content()
+    with open(READINGS_PKL, 'rb') as f:
+        reading_output = pickle.load(f)
+    rid_list = []
+    for rd in reading_output.values():
+        reading_id = db.insert('readings',
+                               **dict(zip(rd.get_cols(), rd.make_tuple())))
+        rid_list.append(reading_id)
+    assert all([rd.reading_id is None for rd in reading_output.values()]), \
+        "No readings should have reading_ids already."
+    enrich_reading_data(reading_output.values(), db=db)
+    assert all([rd.reading_id in rid_list for rd in reading_output.values()]),\
+        "Some reading data objects didn't have reading_ids after enrichment."
