aaa9cf11d6e5d8a320bcf9fcea7dc944b573a545
Ben Gyori <ben.gyori@gmail.com>
2017-11-01 17:06:59 -0400

    Improve simulation script for Task 1

--- models/fallahi_eval/simulate_task1.py
+++ models/fallahi_eval/simulate_task1.py
@@ -1,7 +1,8 @@
+import copy
 import numpy
 from pysb.bng import generate_equations
 from pysb.simulator import ScipyOdeSimulator
-from pysb import Observable, ReactionPattern
+from pysb import Observable, ReactionPattern, ComplexPattern
 
 from util import pkldump, pklload
 from process_data import *
@@ -12,39 +13,36 @@ time_steps = 100
 # ##########################
 
 
-def run_simulation_condition(model, ts, inhib_monomers):
+def run_simulation_condition(sim, inhib_monomers):
     if inhib_monomers:
         print('Setting inhibition condition for %s' %
               ', '.join(inhib_monomers))
-    old_init = {}
+    old_params = {p.name: p.value for p in sim.model.parameters}
+    sim_params = copy.copy(old_params)
     for monomer_name in inhib_monomers:
-        init_name = monome_name + '_0'
-        old_init[monomer_name] = model.parameters[init_name].value
-        model.parameters[init_name].value = 0
-
-    print('Instantiating simulator')
-    sim = ScipyOdeSimulator(model, ts)
+        init_name = monomer_name + '_0'
+        sim_params[init_name] = 0
 
     print('Running simulation')
-    res = sim.run()
+    res = sim.run(param_values=sim_params)
 
     print('Reverting change to model initial conditions')
     for monomer_name in inhib_monomers:
-        init_name = monome_name + '_0'
-        model.parameters[init_name].value = old_init[monomer_name]
+        init_name = monomer_name + '_0'
+        model.parameters[init_name].value = old_params[init_name]
     return res
 
 
 def set_model_observables(model):
+    print('Setting model observables')
     s6 = model.monomers['RPS6']
-    site_patterns = [
-        {'S235': 'p', 'S236': 'u'}
-        {'S235': 'u', 'S236': 'p'}
-        {'S235': 'p', 'S236': 'p'}
-        ]
-    pattern_terms = [s6(**pat) for pat in site_patterns]
+    site_patterns = [{'S235': 'p', 'S236': 'u'},
+                     {'S235': 'u', 'S236': 'p'},
+                     {'S235': 'p', 'S236': 'p'}]
+    pattern_terms = [ComplexPattern([s6(**pat)], None)
+                     for pat in site_patterns]
     obs_pattern = ReactionPattern(pattern_terms)
-    obs = Observable('pS6', obs_pattern)
+    obs = Observable('pS6', obs_pattern, _export=False)
     model.add_component(obs)
 
 
@@ -55,9 +53,16 @@ if __name__ == '__main__':
     for cell_line  in ('C32', 'LOXIMVI', 'MMACSF', 'MZ7MEL', 'RVH421'):
         print('Loading model for %s' % cell_line)
         model = pklload('pysb_model_%s' % cell_line)
-        print('Generating model equations for %s' % cell_line)
+
+        set_model_observables(model)
+
+        print('Generating model equations')
         generate_equations(model)
-        res_nodrug = run_simulation_condition(model, ts, [])
-        res_drug = run_simulation_condition(model, ts, ['ARAF', 'BRAF', 'RAF1'])
+
+        print('Instantiating simulator')
+        sim = ScipyOdeSimulator(model, ts, use_theano=True, verbose=True)
+
+        res_drug = run_simulation_condition(sim, ['ARAF', 'BRAF', 'RAF1'])
+        res_nodrug = run_simulation_condition(sim, [])
         break
 
