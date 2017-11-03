ed79aed61b28bc635ce87908b4959f18d1020715
Ben Gyori <ben.gyori@gmail.com>
2017-11-03 14:29:24 -0400

    Remove full PMC reading for now

--- indra/tests/test_reach.py
+++ indra/tests/test_reach.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, print_function, unicode_literals
 from builtins import dict, str
+import unittest
 from indra.sources import reach
 from indra.sources.reach.processor import ReachProcessor
 from indra.util import unicode_strs
@@ -169,6 +170,7 @@ def test_process_unicode():
         rp = reach.process_text('MEK1 binds ERK2\U0001F4A9.', offline=offline)
         assert unicode_strs(rp.statements)
 
+@unittest.skip('Taking too long on Travis')
 def test_process_pmc():
     for offline in offline_modes:
         rp = reach.process_pmc('PMC4338247', offline=offline)
