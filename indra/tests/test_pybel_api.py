567a618ac2a3d627b960d7e02ef835eb9989d91b
John Bachman <bachmanjohn@gmail.com>
2017-12-14 13:30:50 -0500

    Test for get_agent

--- indra/tests/test_pybel_api.py
+++ indra/tests/test_pybel_api.py
@@ -1,8 +1,38 @@
 import pybel
+from pybel.dsl import *
+import pybel.constants as pc
 from pybel.examples import egf_graph, sialic_acid_graph
+from indra.statements import *
 from indra.sources import pybel as pb
+from indra.databases import hgnc_client
+
 
 def test_process_pybel():
     pbp = pb.process_pybel_graph(egf_graph)
     assert pbp.statements
 
+
+def test_increase_amount():
+    mek = protein(name='MAP2K1', namespace='HGNC')
+    erk = protein(name='MAPK1', namespace='HGNC')
+    g = pybel.BELGraph()
+    g.add_qualified_edge(mek, erk, relation=pc.INCREASES,
+                         evidence="Some evidence.", citation='123456')
+    pbp = pb.process_pybel_graph(g)
+    assert pbp.statements
+    assert len(pbp.statements) == 1
+    assert isinstance(pbp.statements[0], IncreaseAmount)
+
+def test_get_agent():
+    mek = protein(name='MAP2K1', namespace='HGNC')
+    agent = pb._get_agent(mek)
+    assert isinstance(agent, Agent)
+    assert agent.name == 'MAP2K1'
+    hgnc_id = hgnc_client.get_hgnc_id('MAP2K1')
+    up_id = hgnc_client.get_uniprot_id(hgnc_id)
+    assert agent.db_refs('HGNC') == hgnc_id
+    assert agent.db_refs('UP') == up_id
+
+
+if __name__ == '__main__':
+    test_get_agent()
