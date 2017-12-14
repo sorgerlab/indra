50670adfe7fb0eda7008c0f53dbaf4510e25e37b
John Bachman <bachmanjohn@gmail.com>
2017-12-14 13:47:08 -0500

    Pass test for get_agent

--- indra/sources/pybel.py
+++ indra/sources/pybel.py
@@ -1,5 +1,6 @@
+import pybel.constants as pc
 from indra.statements import *
-
+from indra.databases import hgnc_client
 
 def process_pybel_graph(graph):
     proc = PybelProcessor(graph)
@@ -26,6 +27,58 @@ class PybelProcessor(object):
 
     def get_statements(self):
         for u, v, d in self.graph.edges_iter(data=True):
-            pass
+            u_data = self.graph.node[u]
+            v_data = self.graph.node[v]
+
+            if v_data[pc.FUNCTION] == pc.PROTEIN and \
+               d[pc.RELATION] in pc.CAUSAL_RELATIONS:
+                stmt = self._get_regulate_amount(u_data, v_data, d)
+
         st = Phosphorylation(Agent('A'), Agent('B'))
         self.statements = [st]
+
+    def _get_regulate_amount(self, u_data, v_data, edge_data):
+        is_direct = _rel_is_direct(d)
+        subj = _get_agent(u_data)
+        #stmt = IncreaseAmount(
+
+
+def _get_agent(node_data):
+    name = node_data.get(pc.NAME)
+    ns = node_data[pc.NAMESPACE]
+    ident = node_data.get(pc.IDENTIFIER)
+    # No ID present, get identifier using the name, namespace
+    if not ident:
+        assert name, "Node must have a name if lacking an identifier."
+        if ns == 'HGNC':
+            hgnc_id = hgnc_client.get_hgnc_id(name)
+            if not hgnc_id:
+                raise ValueError("Invalid HGNC name: %s" % name)
+            db_refs = {'HGNC': hgnc_id, 'UP': _get_up_id(hgnc_id)}
+            # Now get the uniprot ID
+    # We've already got an identifier, look up other identifiers if necessary
+    else:
+        pass
+
+    ag = Agent(name, db_refs=db_refs)
+    return ag
+
+    # name
+    # namespace
+    # identifier
+
+    # INDRA:
+    # name
+    # db_refs
+    # conditions: muts, mods, bound_conds, loc_conds, activity
+
+def _rel_is_direct(d):
+    return d[pc.RELATION] in (pc.DIRECTLY_INCREASES, pc.DIRECTLY_DECREASES)
+
+
+def _get_up_id(hgnc_id):
+    up_id = hgnc_client.get_uniprot_id(hgnc_id)
+    if not up_id:
+        raise ValueError("No Uniprot ID for HGNC ID %s" % hgnc_id)
+    return up_id
+
