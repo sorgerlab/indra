a0ba0190fc9c57100f09767ab12e8de3b4730976
John Bachman <bachmanjohn@gmail.com>
2018-01-10 15:38:46 -0500

    Refactoring and docs up to _split_graph

--- indra/explanation/cycle_free_paths.py
+++ indra/explanation/cycle_free_paths.py
@@ -338,8 +338,10 @@ def _backward(v, H):
         L[k] = [w for w in h if w[0] == k]
     return h
 
+
 def PG_cf(src, tgt, g, t):
     """
+    Generate a cycle free paths graph (CFPG).
 
     Implements the major step (the outer loop) for constructing G_cf. We do so
     by computing dic_CF, a dictionary based version of G_cf.  dic_CF[i] will be
@@ -347,166 +349,141 @@ def PG_cf(src, tgt, g, t):
 
     V_i will be the set of nodes at level i.
 
-    A node -after dic_CF[i] has been computed- will be of the form (i, n, c) where i is the level, n is the name and c is 
-    the copy number of the node (i,n) in G_0. In other words, each node in G_0 will be split into 
-    one or more copies to implement our memoryless sampling strategy.
+    A node--after dic_CF[i] has been computed--will be of the form (i, n, c)
+    where i is the level, n is the name and c is the copy number of the node
+    (i,n) in G_0. In other words, each node in G_0 will be split into one or
+    more copies to implement our memoryless sampling strategy.
 
-    next_i is the successor relation
+    next_i is the successor relation for the CFPG.
 
-    pred_i[v] is the set of predecessors of of v in V_i. The construction proceeds from 
-    the target to source. At stage i of the construction we convert nodes of the form (i, n) into 
-    nodes of the form (i,n,c). For any such new node pred_i[v] will be nodes of the form 
-    (i-1,n) at level i-1.
+    pred_i[v] is the set of predecessors of v in V_i. The construction
+    proceeds from the target to source. At stage i of the construction we
+    convert nodes of the form (i, n) into nodes of the form (i,n,c). For any
+    such new node pred_i[v] will be nodes of the form (i-1,n) at level i-1.
 
-    t_i[v] will be the new tags of the node v. They will be pairs of the form (j,n).
-    In other words their type will be the same as of T_0.
-    (Note: In T_0,  I assign nodes of G_0 as tags rather than their names. This turns out 
+    t_i[v] will be the new tags of the node v. They will be pairs of the form
+    (j,n). In other words their type will be the same as of T_0.  (Note: In
+    T_0, I assign nodes of G_0 as tags rather than their names. This turns out
     to be convenient for the construction of G_cf)
 
     Once the construction of PG_cf is complete we will no onger 
     require pred_i and t_i.
-  
-
-"""
-
-    We first hardwire dic_CF[tgt[0]]. We just need to create one instance of tgt.
     """
-    
-    next_tgt = {}
-    pred_tgt = {}
-    tgt_0 = (tgt[0], tgt[1], 0)
-    next_tgt[tgt_0] = []
-    pred_tgt[tgt_0] = g.predecessors(tgt)
-    t_cf_tgt = {}
-    t_cf_tgt[tgt_0] = t[tgt]
-    
+    # We first hardwire dic_CF[tgt[0]].
+    tgt_0 = (tgt[0], tgt[1], 0) # 3-tuple version of tgt
+    next_tgt = {tgt_0: []}
+    pred_tgt = {tgt_0: g.predecessors(tgt)}
+    t_cf_tgt = {tgt_0: t[tgt]}
     dic_CF = {tgt[0]: ([tgt_0], next_tgt, pred_tgt, t_cf_tgt)}
-    
-    """here is the main construction
-    """
+
+    # If we were given an empty pre-CFPG, then the CFPG should also be empty
     empty = ([],{}, {},{})
+    if not g:
+        for j in range(0, tgt[0]+1):
+            dic_CF[j] = empty
+        return dic_CF
+    # Iterate from level n-1 (one "above" the target) back to the source
     for i in reversed(range(1, tgt[0])):
-        V_i_1, next_i_1, pred_i_1, t_cf_i_1 = dic_CF[i+1]
-        
-        if V_i_1 == []:
-            for j in range(0, tgt[0]+1):
-                dic_CF[j] = empty
-            return dic_CF
+        # Get the information for level i+1 (one level closer to the target)
+        V_ip1, next_ip1, pred_ip1, t_cf_ip1 = dic_CF[i+1]
+        # Because we are working off of a non-empty G_0, we should never end
+        # with a level in the graph with no nodes
+        assert V_ip1 != []
+        # TODO: Can V_current be replaced simply by the nodes in g at level i?
+        # TODO: Rename V_current -> V_i_old, V_i -> V_i_new?
         V_current = []
-        for v in V_i_1:
-            V_current.extend(pred_i_1[v])
+        for v in V_ip1:
+            V_current.extend(pred_ip1[v])
             V_current = list(set(V_current))
-        
-        """
-        Thus V_current is the set of nodes (which will be pairs)  at level i to be processed.
-        the converted  nodes(which will be triples)  will be binned into V_i
-        """
-        if V_current == []:
-            for j in range(0, tgt[0]+1):
-                dic_CF[j] = empty
-            return dic_CF
-            
-        else:
-            V_i = []
-            next_i = {}
-            pred_i = {}
-            t_cf_i = {}
-            """ Now comes the heart of the construction. We take a node x in V_current
-                 and split it into -in general- multiple copies to ensure that 
-                 if (u,v) is an edge in G_cf then the set of tags of u is included in the
-                 set of tags of v"""
-            for x in V_current:
-                """X_up is the set of nodes at the level above (remember tgt is above src!).
-                These nodes will be triples.
-                
-                X_down is the nodes at the level below. They will be pairs
-                """
-                X_up = [w for w in V_i_1 if x in pred_i_1[w]]
-                X_down = g.predecessors(x)
-                if X_up == []:
-                    continue
-                """The actual splitting up and how to connect the resulting copies of x to its neighbors 
-                above and below is carried out by the split_graph function. We explain this function later
-                """
-                
-                V_x, next_x, pred_x, t_cf_x = split_graph(src, tgt, x,  X_up, X_down, t_cf_i_1, t, g)
-                
-                """We now extend V_i, next_i, pred_i and t_i in the obvious way.
-                """
-                
-                V_i.extend(V_x)
-                for u in V_x:
-                    next_i[u] = next_x[u]
-                    pred_i[u] = pred_x[u]
-                    t_cf_i[u] = t_cf_x[u]
-            dic_CF[i] = (V_i, next_i, pred_i, t_cf_i)               
-    """Finally we hardwire dic_CF[0]
-    """
-         
+        # V_current should never be empty by construction of G_0
+        assert V_current != []
+        # Thus V_current is the set of nodes (which will be 2-tuples) at level
+        # i to be processed. The converted  nodes(which will be 3-tuples,
+        # including the copy number) will be binned into V_i.
+        V_i = []
+        next_i = {}
+        pred_i = {}
+        t_cf_i = {}
+        # Now comes the heart of the construction. We take a node x in
+        # V_current and split it into -in general- multiple copies to ensure
+        # that if (u,v) is an edge in G_cf then the set of tags of u is
+        # included in the set of tags of v
+        for x in V_current:
+            # X_ip1 is the set of nodes at the level i+1 to which x is connected
+            # via the pred_ip1 function. These nodes, already processed, will
+            # be 3-tuples. X_im1 is the nodes at the level i-1. They are
+            # unprocessed 2-tuples.
+            X_ip1 = [w for w in V_ip1 if x in pred_ip1[w]]
+            X_im1 = g.predecessors(x)
+            assert X_ip1 != []
+            # The actual splitting up and how to connect the resulting copies
+            # of x to its neighbors above and below is carried out by the
+            # _split_graph function, below.
+            V_x, next_x, pred_x, t_cf_x = \
+                    _split_graph(src, tgt, x,  X_ip1, X_im1, t_cf_ip1, t, g)
+            # We now extend V_i, next_i, pred_i and t_i in the obvious way.
+            V_i.extend(V_x)
+            next_i.update(next_x)
+            pred_i.update(pred_x)
+            t_cf_i.update(t_cf_x)
+        dic_CF[i] = (V_i, next_i, pred_i, t_cf_i)
+    # Finally we hardwire dic_CF[0]
     V_1 = dic_CF[1][0]
-    src_0 = (src[0], src[1], 0)
-    tgt_0 = (tgt[0], tgt[1], 0)
+    src_0 = (src[0], src[1], 0) # 3-tuple version of src
     V_0 = [src_0]
-    next_src = {}    
-    next_src[src_0] = V_1
-    pred_src = {}
-    pred_src[src_0] = []
-    t_cf_src = {}
-    t_cf_src[src_0] = t[src] 
-    
-    dic_CF[0] = (V_0 , next_src, pred_src, t_cf_src)        
-    
+    next_src = {src_0: V_1}
+    pred_src = {src_0: []}
+    t_cf_src = {src_0: t[src]}
+    dic_CF[0] = (V_0 , next_src, pred_src, t_cf_src)
     return dic_CF
 
-def split_graph(src, tgt, x,  X_up, X_down, t_cf, t, g):
+
+def _split_graph(src, tgt, x,  X_ip1, X_im1, t_cf, t, g):
+    """Splits a node x from G_0 into multiple copies for the CFPG.
+
+    The nodes in X_ip1 represent the possible successor nodes to x in the CFPG.
+    For each successor w of x in X_ip1, we first obtain the set of possible
+    antecedent nodes lying on paths from the source up to the edge x->w. We
+    obtain this by finding the intersection between the tags of x and the tags
+    of w. This is the set X_wx below for each w in X_ip1.
+
+    However X_wx is the set of nodes (in G_0) from which we can reach x->w
+    without encountering x[1] AND without encountering w[1]. As a result some
+    nodes in X_wx may be isolated.  Hence we prune them away.
+    """
     V_x = []
     next_x = {}
     pred_x = {}
     t_x = {}
-    S_up = {}
-    
-    
-    """
-    For each member of X_up we create one copy of x in terms of the tags it is going to get.
-    This is the set X_wx below for each w in X_up. However X_wx is the set of nodes (in G_0) from which we can reach x
-    without encountering x[1] AND without encountering w[1]. As result some nodes in X_wx may be isolated.
-    Hence we prune them away. 
-    """
-    for w in X_up:
+    S_ip1 = {}
+    for w in X_ip1:
         X_wx =  set(t_cf[w]) & set(t[x])
         N_wx = list(X_wx)
+        # TODO: Reimplement pruning so as to avoid inducing a subgraph?
         g_wx = g.subgraph(N_wx)
-        nodes_prune = [v for v in g_wx if (v != x and g_wx.successors(v) == []) or (v!= src and g_wx.predecessors(v) == [])]
+        nodes_prune = [v for v in g_wx
+                         if (v != x and g_wx.successors(v) == []) or
+                            (v!= src and g_wx.predecessors(v) == [])]
         g_wx_pruned = _prune(g_wx, nodes_prune, src, x)
+        # If the pruned graph still contains both src and x itself, there is
+        # at least one path from the source to x->w. The nodes in this subgraph
+        # constitute the new set of tags of the copy of x that lies on a path
+        # between src and w.
         if x in g_wx_pruned and src in g_wx_pruned:
             s = frozenset(g_wx_pruned.nodes())
-            S_up[w] = s
-     
-    
-    """ Thus S_up[w] are the elements of S that point to w
-    """
-    S = []               
-    for m in range(len(X_up)):
-        w = X_up[m]
-        if w in S_up.keys():
-            S.append(S_up[w])
-    S = list(set(S))
-        
-    """
-each  element of S will be a pruned list of tags. We will creat one copy x_r
-of x  for each element r in S. We assign r to be the set of tags of x_r.
-next of x_r is assemebled using S_up and pred is defined in the expected way using X_down.
-    """             
-        
-    c = 0
-    for r in S:
+            S_ip1[w] = s
+    S = set([S_ip1.values())
+    # Each element of the set S will be a unique, (frozen) set of tags. We will
+    # create one copy x_r of x for each unique tag set r in S, and we assign r
+    # to be the set of tags of the new, split node x_r. The successors of x_r
+    # are assembled using S_ip1; pred is defined in the expected way using
+    # X_im1.
+    for c, r in enumerate(S):
         x_c = (x[0], x[1], c)
         V_x.append(x_c)
-        next_x[x_c] = [w for w in X_up if w in S_up.keys() and r == S_up[w]]
-        pred_x[x_c] = [u for u in X_down if u in r]
+        next_x[x_c] = [w for w in S_ip1.keys() if r == S_ip1[w]]
+        pred_x[x_c] = [u for u in X_im1 if u in r]
         t_x[x_c] = r
-        c += 1
-
     return (V_x, next_x, pred_x, t_x)
 
 def dic_to_graph(dic):
@@ -521,7 +498,7 @@ def dic_to_graph(dic):
     G.add_edges_from(E)
     return G  
 
- 
+
 def name_paths(Q):
     Q_names = []
     for q in Q:
@@ -546,6 +523,8 @@ def dc_prev(src_0,tgt_0, dic):
                 prev_k[v] = [u for u in V_k_1 if v in next_k_1[u]]
             dic_prev[k] = (V_k, next_k, prev_k)
     return dic_prev
+
+
 def PG_cf_pruned(src_0, tgt_0, dic):
     l = len(dic.keys())
     current = dic.copy()
