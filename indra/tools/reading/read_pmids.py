6b7b4b87fe468a3f60c41ca48769e160e2b422c6
Ben Gyori <ben.gyori@gmail.com>
2018-01-11 16:14:32 -0500

    Add Xmx option to specify Java heap size

--- indra/tools/reading/read_pmids.py
+++ indra/tools/reading/read_pmids.py
@@ -649,7 +649,8 @@ def run_reach(pmid_list, base_dir, num_cores, start_index, end_index,
 
         # Run REACH!
         logger.info("Beginning reach.")
-        args = ['java', '-Dconfig.file=%s' % conf_file_path, '-jar', reach_ex]
+        args = ['java', '-Xmx24000m' '-Dconfig.file=%s' % conf_file_path,
+                '-jar', reach_ex]
         p = subprocess.Popen(args, stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE)
         if verbose:
