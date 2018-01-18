d0f1114fdcee63d65c5dd74501b3e329a12f8e53
Ben Gyori <ben.gyori@gmail.com>
2018-01-18 00:15:08 -0500

    Make Eidos reader instantiate when first reading

--- indra/sources/eidos/eidos_reader.py
+++ indra/sources/eidos/eidos_reader.py
@@ -11,11 +11,11 @@ class EidosReader(object):
     Attributes
     ----------
     eidos_reader : org.clulab.wm.AgroSystem
-        A Scala object, an instance of the Eidos reading system
+        A Scala object, an instance of the Eidos reading system. It is
+        instantiated only when first processing text.
     """
     def __init__(self):
-        eidos = autoclass('org.clulab.wm.AgroSystem')
-        self.eidos_reader = eidos(autoclass('java.lang.Object')())
+        self.eidos_reader = None
 
     def process_text(self, text):
         """Return a mentions JSON object given text.
@@ -30,6 +30,10 @@ class EidosReader(object):
         json_dict : dict
             A JSON object of mentions extracted from text.
         """
+        if self.eidos_reader is None:
+            eidos = autoclass('org.clulab.wm.AgroSystem')
+            self.eidos_reader = eidos(autoclass('java.lang.Object')())
+
         mentions = self.eidos_reader.extractFrom(text)
         ser = autoclass('org.clulab.wm.serialization.json.WMJSONSerializer')
         mentions_json = ser.jsonAST(mentions)
