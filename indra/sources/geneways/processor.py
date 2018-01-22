4717848019f58a55b792920654f72887335e4f0b
Ben Gyori <ben.gyori@gmail.com>
2018-01-22 15:07:51 -0500

    Merge action type mapper into processor

--- indra/sources/geneways/processor.py
+++ indra/sources/geneways/processor.py
@@ -17,9 +17,8 @@ import logging
 from indra.statements import Evidence, Agent
 import indra.databases.hgnc_client as hgc
 from indra.literature import *
+from indra.statements import Complex, Phosphorylation
 from indra.sources.geneways.geneways_action_parser import GenewaysActionParser
-from indra.sources.geneways.geneways_action_type_mapper import \
-        geneways_action_to_indra_statement_type
 try:
     from indra.sources.geneways._find_full_text_sentence import FullTextMention
     get_ft_mention = True
@@ -157,3 +156,36 @@ class GenewaysProcessor(object):
 
         # Make the statement
         return statement_generator(upstream_agent, downstream_agent, evidence)
+
+
+def geneways_action_to_indra_statement_type(actiontype, plo):
+    """This function provides the mapping from Geneways statements
+    to Indra statements. It is used by GenewaysProcessor.
+    Inputs:
+    * actiontype: the verb extracted by the Geneways processor
+    * plo: A one character string designating whether Geneways classifies
+    this verb as a physical, logical, or other interaction
+
+    Outputs:
+    * statement_generator: If there is no mapping to Indra statements
+    from this action type, None.
+        If there is such a mapping, statement_generator is an anonymous
+        function that takes in the subject agent, object agent, and evidence,
+        in that order, and returns an Indra statement object.
+    """
+    actiontype = actiontype.lower()
+
+    statement_generator = None
+    is_direct = (plo == 'P')
+
+    if actiontype == 'bind':
+        statement_generator = lambda substance1, substance2, evidence: \
+                Complex([substance1, substance2], evidence=evidence) 
+        is_direct = True
+    elif actiontype == 'phosphorylate':
+        statement_generator = lambda substance1, substance2, evidence: \
+                Phosphorylation(substance1, substance2, evidence=evidence)
+        is_direct = True
+
+    return (statement_generator, is_direct)
+
