75b1fdc9f290c85b4d469cdce5e5d1154aed4881
pagreene <patrick.anton.greene@gmail.com>
2018-01-30 08:10:55 -0700

    Implement test for basic graph.

--- indra/tests/test_util.py
+++ indra/tests/test_util.py
@@ -1,9 +1,12 @@
 from __future__ import absolute_import, print_function, unicode_literals
 from builtins import dict, str
+
+import json
 import xml.etree.ElementTree as ET
-from indra.util import UnicodeXMLTreeBuilder as UTB
+from indra.util import UnicodeXMLTreeBuilder as UTB, kappy_json_to_graph
 from indra.util import unicode_strs
 from io import BytesIO
+
 
 def test_unicode_tree_builder():
     xml = u'<html><bar>asdf</bar></html>'.encode('utf-8')
@@ -11,4 +14,17 @@ def test_unicode_tree_builder():
     tree = ET.parse(xml_io, parser=UTB())
     bar = tree.find('.//bar')
     assert unicode_strs(bar)
+
+
+def test_kappy_influence_json_to_graph():
+    with open('kappy_influence.json', 'r') as f:
+        imap = json.load(f)
+    graph = kappy_json_to_graph(imap)
+    assert graph is not None, 'No graph produced.'
+    n_nodes = len(graph.nodes)
+    n_edges = len(graph.edges)
+    assert n_nodes == 4, \
+        'Wrong number (%d vs. %d) of nodes on the graph.' % (n_nodes, 4)
+    assert n_edges == 6, \
+        "Wrong number (%d vs. %d) of edges on graph." % (n_edges, 4)
 
