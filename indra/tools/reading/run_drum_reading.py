c5cf8df78106e15a81f976f99d26d361b036318a
Ben Gyori <ben.gyori@gmail.com>
2018-01-31 11:29:45 -0700

    Improve batch Drum reading implementation

--- indra/tools/reading/run_drum_reading.py
+++ indra/tools/reading/run_drum_reading.py
@@ -1,25 +1,49 @@
 import sys
 import json
-from indra.sources.trips.drum_reader import DrumReader
+import time
+import pickle
 from indra.sources.trips import process_xml
+from indra.sources.trips.drum_reader import DrumReader
 
-def read_content(content):
-    sentences = []
-    for k, v in content.items():
-        sentences += v
-    dr = DrumReader(to_read=sentences)
-    try:
-        dr.start()
-    except SystemExit:
-        pass
-    statements = []
-    for extraction in dr.extractions:
-        statements += process_xml(extraction).statements
-    return statements
+
+def set_pmid(statements, pmid):
+    for stmt in statements:
+        for evidence in stmt.evidence:
+            evidence.pmid = pmid
+
+
+def read_content(content, host):
+    all_statements = []
+    for pmid, sentences in content.items():
+        print('================================')
+        print('Processing %d sentences for %s' % (len(sentences), pmid))
+        ts = time.time()
+        dr = DrumReader(to_read=sentences, host=host)
+        try:
+            dr.start()
+        except SystemExit:
+            pass
+        statements = []
+        for extraction in dr.extractions:
+            tp = process_xml(extraction)
+            statements += tp.statements
+        set_pmid(statements, pmid)
+        te = time.time()
+        print('Reading took %d seconds and produced %d Statements.' %
+              (te-ts, len(statements)))
+        all_statements += statements
+    return all_statements
+
+
+def save_results(statements, out_fname):
+    with open(out_fname, 'wb') as fh:
+        pickle.dump(statements, fh)
+
 
 if __name__ == '__main__':
-    file_name = sys.argv[1]
+    host = sys.argv[1]
+    file_name = sys.argv[2]
     with open(file_name, 'rt') as fh:
         content = json.load(fh)
-    statements = read_content(content)
-    print(statements)
+    statements = read_content(content, host)
+    save_results(statements, 'results.pkl')
