e1ea7109c303ccc643566445b1c3c70f3d7a2215
Ben Gyori <ben.gyori@gmail.com>
2018-02-05 11:25:58 -0500

    Add test and test file for kappy contact map

--- indra/tests/test_kappa_util.py
+++ indra/tests/test_kappa_util.py
@@ -3,7 +3,8 @@ from builtins import dict, str
 
 import json
 from os import path
-from indra.util.kappa_util import im_json_to_graph
+from indra.util.kappa_util import im_json_to_graph, cm_json_to_graph
+
 
 def test_kappy_influence_json_to_graph():
     with open(path.join(path.dirname(path.abspath(__file__)),
@@ -17,4 +18,18 @@ def test_kappy_influence_json_to_graph():
         'Wrong number (%d vs. %d) of nodes on the graph.' % (n_nodes, 13)
     assert n_edges == 6, \
         "Wrong number (%d vs. %d) of edges on graph." % (n_edges, 4)
+
+
+def test_kappy_contact_json_to_graph():
+    with open(path.join(path.dirname(path.abspath(__file__)),
+                        'kappy_contact.json'), 'r') as f:
+        cmap = json.load(f)
+    graph = cm_json_to_graph(cmap)
+    assert graph is not None, 'No graph produced.'
+    n_nodes = len(graph.nodes())
+    n_edges = len(graph.edges())
+    assert n_nodes == 5, \
+        'Wrong number (%d vs. %d) of nodes on the graph.' % (n_nodes, 5)
+    assert n_edges == 3, \
+        "Wrong number (%d vs. %d) of edges on graph." % (n_edges, 3)
 
