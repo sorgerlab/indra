aaa109172b973b566d29f207344df2a60fbdcfa1
Daniel Milstein <djmilstein@gmail.com>
2018-02-06 13:19:14 -0500

    Process IncreaseAmount statements

--- indra/sources/tees/processor.py
+++ indra/sources/tees/processor.py
@@ -10,7 +10,8 @@ from __future__ import absolute_import, print_function, unicode_literals
 from builtins import dict, str
 from future.utils import python_2_unicode_compatible
 
-from indra.statements import Phosphorylation, Dephosphorylation, Complex
+from indra.statements import Phosphorylation, Dephosphorylation, Complex, \
+        IncreaseAmount
 from indra.sources.tees.parse_tees import run_and_parse_tees
 
 class TEESProcessor(object):
@@ -47,6 +48,7 @@ class TEESProcessor(object):
         self.statements = []
         self.statements.extend(self.process_phosphorylation_statements())
         self.statements.extend(self.process_binding_statements())
+        self.statements.extend(self.process_positive_regulation())
 
     def node_has_edge_with_label(self, node_name, edge_label):
         """Looks for an edge from node_name to some other node with the specified
@@ -158,6 +160,25 @@ class TEESProcessor(object):
             if to_relation == relation and not G.node[to]['is_event']:
                 return G.node[to]['text']
         return None
+
+    def process_positive_regulation(self):
+        """Looks for Positive_Regulation events with a specified Cause
+        and a Gene_Expression theme, and processes them into INDRA statements.
+        """
+        statements = []
+
+        pwcs = self.find_event_parent_with_event_child('Positive_regulation',
+                'Gene_expression')
+        for pair in pwcs:
+            pos_reg = pair[0]
+            expression = pair[1]
+
+            cause = self.get_entity_text_for_relation(pos_reg, 'Cause')
+            target = self.get_entity_text_for_relation(expression, 'Theme')
+
+            if cause is not None and target is not None:
+                statements.append(IncreaseAmount(cause, target))
+        return statements
 
     def process_phosphorylation_statements(self):
         """Looks for Phosphorylation events in the graph and extracts them into
