8d539bf5e42780055555dcef97c8691d4ac1898c
pagreene <patrick.anton.greene@gmail.com>
2018-02-27 12:20:01 -0500

    Improve error reporting for test_api.

--- indra_db_api/test_api.py
+++ indra_db_api/test_api.py
@@ -15,14 +15,16 @@ class DbApiTestCase(unittest.TestCase):
     def test_blank_response(self):
         """Test the response to an empty request."""
         resp = self.app.get('/statements/')
-        assert resp.status_code == 400, 'Got clean response with no input.'
+        assert resp.status_code == 400, \
+            ('Got unexpected response with code %d: %s.'
+             % (resp.status_code, resp.data.decode()))
 
     def test_specific_query(self):
         """Test whether we can get a "fully" specified statement."""
         resp = self.app.get('/statements/?object=MAP2K1&subject=MAPK1'
                             '&action=Phosphorylation')
         assert resp.status_code == 200, \
-            'Got error code %d.' % resp.status_code
+            'Got error code %d: \"%s\".' % (resp.status_code, resp.data.decode())
         assert len(json.loads(resp.data.decode())) is not 0, \
             'Did not get any statements.'
 
