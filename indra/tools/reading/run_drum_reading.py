c441ebff0926b150de280807ff20fdbdeb2b0983
Ben Gyori <ben.gyori@gmail.com>
2018-03-02 16:49:18 -0500

    Start generalizing DRUM reading script

--- indra/tools/reading/run_drum_reading.py
+++ indra/tools/reading/run_drum_reading.py
@@ -6,19 +6,37 @@ from indra.sources.trips import process_xml
 from indra.sources.trips.drum_reader import DrumReader
 
 
-def set_pmid(statements, pmid):
-    for stmt in statements:
-        for evidence in stmt.evidence:
-            evidence.pmid = pmid
+def read_pmid_sentences(pmid_sentences, **drum_args):
+    """Read sentences from a PMID-keyed dictonary and return all Statements
 
+    Parameters
+    ----------
+    pmid_sentences : dict[str, list[str]]
+        A dictonary where each key is a PMID pointing to a list of sentences
+        to be read.
 
-def read_content(content, host):
+    **drum_args
+        Keyword arguments passed directly to the DrumReader. Typical
+        things to specify are 'host' and 'port'.
+
+    Returns
+    -------
+    all_statements : list[indra.statement.Statement]
+        A list of INDRA Statements resulting from the reading
+    """
+    def _set_pmid(statements, pmid):
+        for stmt in statements:
+            for evidence in stmt.evidence:
+                evidence.pmid = pmid
+
     all_statements = []
-    for pmid, sentences in content.items():
+    for pmid, sentences in pmid_sentences.items():
         print('================================')
         print('Processing %d sentences for %s' % (len(sentences), pmid))
         ts = time.time()
-        dr = DrumReader(to_read=sentences, host=host)
+        dr = DrumReader(**drum_args)
+        for sentence in sentences:
+            dr.read_text(sentence)
         try:
             dr.start()
         except SystemExit:
@@ -27,7 +45,7 @@ def read_content(content, host):
         for extraction in dr.extractions:
             tp = process_xml(extraction)
             statements += tp.statements
-        set_pmid(statements, pmid)
+        _set_pmid(statements, pmid)
         te = time.time()
         print('Reading took %d seconds and produced %d Statements.' %
               (te-ts, len(statements)))
@@ -35,18 +53,40 @@ def read_content(content, host):
     return all_statements
 
 
+def read_text(text, **drum_args):
+    """Read sentences from a PMID-keyed dictonary and return all Statements
+
+    Parameters
+    ----------
+    pmid_sentences : dict[str, list[str]]
+        A dictonary where each key is a PMID pointing to a list of sentences
+        to be read.
+
+    **drum_args
+        Keyword arguments passed directly to the DrumReader. Typical
+        things to specify are 'host' and 'port'.
+
+    Returns
+    -------
+    all_statements : list[indra.statement.Statement]
+        A list of INDRA Statements resulting from the reading
+    """
+
+
+
+def read_pmc(pmcid, **drum_args):
+    
+
+
 def save_results(statements, out_fname):
     with open(out_fname, 'wb') as fh:
         pickle.dump(statements, fh)
 
 
-# TODO: this currently assumes that a list of sentences is to be read
-# for a given PMID. Other usage modes to support are reading a full
-# NXML paper (from PMC), and reading the abstract for a given PMID.
 if __name__ == '__main__':
     host = sys.argv[1]
     file_name = sys.argv[2]
     with open(file_name, 'rt') as fh:
         content = json.load(fh)
-    statements = read_content(content, host)
+    statements = read_content(content, host=host, port=port)
     save_results(statements, 'results.pkl')
