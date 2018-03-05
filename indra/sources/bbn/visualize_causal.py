d3a947811b91b2eb8fed2396fdb18baf0aafc9a2
Daniel Milstein <djmilstein@gmail.com>
2018-03-05 08:27:46 -0500

    Add some annotations

--- indra/sources/bbn/visualize_causal.py
+++ indra/sources/bbn/visualize_causal.py
@@ -26,6 +26,28 @@ def shorter_name(key):
             key_short = key_short
     return key_short.replace('-', '_').replace('.', '_')
 
+def add_event_property_edges(event_entity, entries):
+    """Adds edges to the graph for event properties."""
+    do_not_log = ['@type', '@id',
+            'http://worldmodelers.com/DataProvenance#sourced_from']
+
+    for prop in event_entity:
+        if prop not in do_not_log:
+            value = event_entity[prop]
+            value_entry = None
+            value_str = None
+            if '@id' in value[0]:
+                value = value[0]['@id']
+
+                if value in entries:
+                    value_str = get_entry_compact_text_repr(entries[value], entries)
+                #get_entry_compact_text_repr(entry, entries)
+
+            if value_str is not None:
+                edges.append([shorter_name(event_entity['@id']), shorter_name(value), shorter_name(prop)])
+                node_labels[shorter_name(value)] = value_str
+            #print('\t', prop, value, value_str)
+
 def get_shortest_string(string_list):
     """From a list of strings, returns the string with the shortest length.
 
@@ -48,7 +70,58 @@ def get_shortest_string(string_list):
             shortest_string = s
     return shortest_string
 
-def type_and_shortest_text_value(entry):
+def get_shortest_text_value(entry):
+    """Given a JSON-LD entry, returns the text attribute that has the
+    shortest length.
+
+    Parameters
+    ----------
+    entry: dict
+        A JSON-LD entry parsed into a nested python directionary via the json
+        module
+
+    Returns
+    -------
+    short_text: str
+        Of the text values, the shortest one
+    """
+    text_attr = 'http://www.ontologyrepository.com/CommonCoreOntologies/has_text_value'
+    if text_attr in entry:
+        text_values = entry[text_attr]
+        text_values = [i['@value'] for i in text_values]
+        return get_shortest_string(text_values)
+    else:
+        return None
+
+def get_sourced_from(entry):
+    """Get a list of values from the source_from attribute"""
+    sourced_from = 'http://worldmodelers.com/DataProvenance#sourced_from'
+
+    if sourced_from in entry:
+        values = entry[sourced_from]
+        values = [i['@id'] for i in values]
+        return values
+
+def get_entry_compact_text_repr(entry, entries):
+    """If the entry has a text value, return that.
+    If the entry has a source_from value, return the text value of the source.
+    Otherwise, return None."""
+    text = get_shortest_text_value(entry)
+    if text is not None:
+        return text
+    else:
+        sources = get_sourced_from(entry)
+        # There are a lot of references to this entity, each of which refer
+        # to it by a different text label. For the sake of visualization,
+        # let's pick one of these labels (in this case, the shortest one)
+        if sources is not None:
+            texts = []
+            for source in sources:
+                source_entry = entries[source]
+                texts.append(get_shortest_text_value(source_entry))
+            return get_shortest_string(texts)
+
+def get_entity_type(entry):
     """Given a JSON-LD entry, returns the abbreviated @type and the 
     text attribute that has the shortest length.
 
@@ -69,19 +142,10 @@ def type_and_shortest_text_value(entry):
     entry_type = [shorter_name(t) for t in entry_type]
     entry_type = repr(entry_type)
 
-    text_values = entry[text_attr]
-    text_values = [i['@value'] for i in text_values]
+    
+    return entry_type
 
-    return (entry_type, get_shortest_string(text_values))
-
-if __name__ == '__main__':
-    # First argument is the JSON file to visualize
-    args = sys.argv
-    if len(args) != 2:
-        print('Expecting exactly one argument: the name of the JSON-LD file')
-        sys.exit(1)
-    visualize_file = args[1]
-
+def load_json(visualize_file):
     # Load in the entries of the JSON-LD file and bucket by id
     entries = {}
     with open(visualize_file, 'r') as f:
@@ -92,6 +156,17 @@ if __name__ == '__main__':
             entry_id = entry['@id']
             assert(entry_id not in entries) #IDs should be unique
             entries[entry_id] = entry
+    return entries
+
+if __name__ == '__main__':
+    # First argument is the JSON file to visualize
+    args = sys.argv
+    if len(args) != 2:
+        print('Expecting exactly one argument: the name of the JSON-LD file')
+        sys.exit(1)
+    visualize_file = args[1]
+
+    entries = load_json(visualize_file)
 
     # Find all the causal assertion events
     causal_assertions = []
@@ -122,7 +197,8 @@ if __name__ == '__main__':
             cause_uri = assertion[cause_attr][0]['@id']
             cause_entry = entries[cause_uri]
 
-            (cause_type, text) = type_and_shortest_text_value(cause_entry)
+            cause_type = get_entity_type(cause_entry)
+            text = get_shortest_text_value(cause_entry)
             cause_uri = shorter_name(cause_uri)
 
             node_labels[cause_uri] = text
@@ -130,6 +206,9 @@ if __name__ == '__main__':
 
             edges.append([cause_uri, cause_uri + '_type', 'type'])
             node_labels[cause_uri + '_type'] = cause_type
+
+
+            add_event_property_edges(cause_entry, entries) 
         else:
             cause_uri = None
 
@@ -139,7 +218,8 @@ if __name__ == '__main__':
             effect_uri = assertion[effect_attr][0]['@id']
             effect_entry = entries[effect_uri]
 
-            (effect_type, text) = type_and_shortest_text_value(effect_entry)
+            effect_type = get_entity_type(effect_entry)
+            text = get_shortest_text_value(effect_entry)
             effect_uri = shorter_name(effect_uri)
 
             node_labels[effect_uri] = text
@@ -147,6 +227,8 @@ if __name__ == '__main__':
 
             edges.append([effect_uri, effect_uri + '_type', 'type'])
             node_labels[effect_uri + '_type'] = effect_type
+
+            add_event_property_edges(effect_entry, entries) 
         else:
             effect_uri = None
 
