bf80bb4df272e6d88038de0bdba53904a39277b0
Daniel Milstein <djmilstein@gmail.com>
2018-03-15 14:43:53 -0400

    Rename cwms to cwms_rdf

--- indra/sources/cwms_rdf/processor.py
+++ indra/sources/cwms_rdf/processor.py
@@ -4,7 +4,7 @@ import logging
 import rdflib
 from indra.statements import Influence, Agent, Evidence
 
-logger = logging.getLogger('cwms')
+logger = logging.getLogger('cwms_rdf')
 
 prefixes = """
 PREFIX role: <http://www.cs.rochester.edu/research/trips/role#>
