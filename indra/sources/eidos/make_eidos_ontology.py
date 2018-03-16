a4f185fa8aa0c835548f5f3d82a920cf24e5babd
Ben Gyori <ben.gyori@gmail.com>
2018-03-16 18:20:10 -0400

    Change Eidos ont NS and fix construction bug

--- indra/sources/eidos/make_eidos_ontology.py
+++ indra/sources/eidos/make_eidos_ontology.py
@@ -6,7 +6,7 @@ from rdflib import Graph, Namespace, Literal
 eidos_ont_url = 'https://raw.githubusercontent.com/clulab/eidos/master/' + \
                 'src/main/resources/org/clulab/wm/eidos/toy_ontology.yml'
 
-eidos_ns = Namespace('http://github.com/clulab/eidos/')
+eidos_ns = Namespace('https://github.com/clulab/eidos/wiki/JSON-LD/Grounding#')
 indra_ns = 'http://sorger.med.harvard.edu/indra/'
 indra_rel_ns = Namespace(indra_ns + 'relations/')
 isa = indra_rel_ns.term('isa')
@@ -23,19 +23,22 @@ def save_hierarchy(g, path):
         out_file.write(g_bytes)
 
 
-def get_term(node):
-    return eidos_ns.term(node)
+def get_term(node, prefix):
+    node = node.replace(' ', '_')
+    path = prefix + '/' + node if prefix else node
+    return eidos_ns.term(path)
 
 
-def build_relations(G, node, tree):
-    print(node)
-    this_term = get_term(node)
+def build_relations(G, node, tree, prefix):
+    this_term = get_term(node, prefix)
+    this_prefix = prefix + '/' + node if prefix else node
     for entry in tree:
         if isinstance(entry, str):
-            continue
-        child = list(entry.keys())[0]
-        build_relations(G, child, entry[child])
-        child_term = get_term(child)
+            child = entry
+        elif isinstance(entry, dict):
+            child = list(entry.keys())[0]
+            build_relations(G, child, entry[child], this_prefix)
+        child_term = get_term(child, this_prefix)
         rel = (child_term, isa, this_term)
         G.add(rel)
 
@@ -44,6 +47,8 @@ if __name__ == '__main__':
     yml = requests.get(eidos_ont_url).content
     root = yaml.load(yml)
     G = Graph()
-    build_relations(G, 'root', root)
+    for top_entry in root:
+        node = list(top_entry.keys())[0]
+        build_relations(G, node, top_entry[node], None)
     rdf_path = join(dirname(abspath(__file__)), 'eidos_ontology.rdf')
     save_hierarchy(G, rdf_path)
