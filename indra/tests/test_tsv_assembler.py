f861dbd61401a09b485393f10c1420b37fd8443e
John Bachman <bachmanjohn@gmail.com>
2018-03-21 17:58:41 -0400

    Update tests

--- indra/tests/test_tsv_assembler.py
+++ indra/tests/test_tsv_assembler.py
@@ -3,12 +3,8 @@ from indra.assemblers.tsv_assembler import TsvAssembler
 from indra.sources.signor import SignorProcessor
 
 # Get some statements from Signor
-#sp = SignorProcessor()
-#stmts = sp.statements
-
-from indra.tools import assemble_corpus as ac
-#ac.dump_statements(stmts, 'test_stmts.pkl')
-stmts = ac.load_statements('test_stmts.pkl')
+sp = SignorProcessor()
+stmts = sp.statements
 
 def test_tsv_init():
     ta = TsvAssembler(stmts)
@@ -21,12 +17,7 @@ def test_tsv_add_stmts():
 
 def test_make_model():
     ta = TsvAssembler(stmts)
-    ta.make_model('tsv_test')
-    assert os.path.exists('tsv_test_mods.tsv')
-
-if __name__ == '__main__':
-    test_tsv_init()
-    test_tsv_add_stmts()
-    test_make_model()
+    ta.make_model('tsv_test.tsv')
+    assert os.path.exists('tsv_test.tsv')
 
 
