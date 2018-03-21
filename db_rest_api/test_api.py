5f6a71e25417428424e9d3e0a98d35c117ef27da
pagreene <patrick.anton.greene@gmail.com>
2018-03-21 14:44:00 -0400

    Update tests to check for preassembled statements.

--- db_rest_api/test_api.py
+++ db_rest_api/test_api.py
@@ -3,6 +3,10 @@ import json
 
 from db_rest_api import api
 from datetime import datetime
+from indra.statements import stmts_from_json
+from itertools import combinations
+
+TIMELIMIT = 20
 
 class DbApiTestCase(unittest.TestCase):
 
@@ -25,9 +29,15 @@ class DbApiTestCase(unittest.TestCase):
         resp, dt = self.__time_get_query(query_str)
         assert resp.status_code == 200, \
             'Got error code %d: \"%s\".' % (resp.status_code, resp.data.decode())
-        assert len(json.loads(resp.data.decode())) is not 0, \
+        json_stmts = json.loads(resp.data.decode('utf8'))
+        assert len(json_stmts) is not 0, \
             'Did not get any statements.'
-        assert dt.seconds <= 25, \
+        stmts = stmts_from_json(json_stmts)
+        assert len(stmts) == 1 or any([s.supports + s.supported_by for s in stmts]),\
+            "Some statements lack support: %s." % str([str(s) for s in stmts if not s.supports + s.supported_by])
+        assert all([not s1.matches(s2) for s1, s2 in combinations(stmts, 2)]),\
+            "Some statements match: %s." % str([(s1, s2) for s1, s2 in combinations(stmts, 2) if s1.matches(s2)])
+        assert dt.seconds <= TIMELIMIT, \
             "Query took %d seconds. Must be less than 25." % dt.seconds
         return resp
 
@@ -37,7 +47,7 @@ class DbApiTestCase(unittest.TestCase):
         assert resp.status_code == 400, \
             ('Got unexpected response with code %d: %s.'
              % (resp.status_code, resp.data.decode()))
-        assert dt.seconds <= 25, \
+        assert dt.seconds <= TIMELIMIT, \
             "Query took %d seconds. Must be less than 25." % dt.seconds
 
     def test_specific_query(self):
