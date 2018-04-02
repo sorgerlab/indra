cb2c1ea41503a2c491c23006bc2f214f69730322
pagreene <patrick.anton.greene@gmail.com>
2018-04-02 17:37:40 -0400

    Create methods for loading and analyzing reach logs of db run.

--- indra/util/aws.py
+++ indra/util/aws.py
@@ -1,5 +1,6 @@
 import re
 import boto3
+from os.path import join
 
 
 def kill_all(job_queue, reason='None given', states=None):
@@ -132,30 +133,75 @@ def dump_logs(job_queue='run_reach_queue', job_status='RUNNING'):
 def analyze_reach_log(log_fname=None, log_str=None):
     """Return unifinished PMIDs given a log file name."""
     assert bool(log_fname) ^ bool(log_str), 'Must specify log_fname OR log_str'
+    started_patt = re.compile('Starting ([\d]+)')
+    # TODO: it might be interesting to get the time it took to read
+    # each paper here
+    finished_patt = re.compile('Finished ([\d]+)')
+
     def get_content_nums(txt):
         pat = 'Retrieved content for ([\d]+) / ([\d]+) papers to be read'
         res = re.match(pat, txt)
         has_content, total = res.groups() if res else None, None
         return has_content, total
 
-    def get_started(txt):
-        pat = 'Starting ([\d]+)'
-        pmids = re.findall(pat, txt)
-        return pmids
-
-    def get_finished(txt):
-        # TODO: it might be interesting to get the time it took to read
-        # each paper here
-        pat = 'Finished ([\d]+)'
-        pmids = re.findall(pat, txt)
-        return pmids
-
     if log_fname:
         with open(log_fname, 'r') as fh:
             log_str = fh.read()
-    has_content, total = get_content_nums(log_str)
-    pmids_started = get_started(log_str)
-    pmids_finished = get_finished(log_str)
+    # has_content, total = get_content_nums(log_str)  # unused
+    pmids_started = started_patt.findall(log_str)
+    pmids_finished = finished_patt.findall(log_str)
     pmids_not_done = set(pmids_started) - set(pmids_finished)
     return pmids_not_done
 
+
+#==============================================================================
+# Functions for analyzing a db reading submission
+#==============================================================================
+
+
+def get_logs_from_db_reading(job_prefix, reading_queue='run_db_reading_queue'):
+    """Get the logs stashed on s3 for a particular reading."""
+    s3 = boto3.client('s3')
+    gen_prefix = 'reading_results/%s/logs/%s' % (job_prefix, reading_queue)
+    job_log_data = s3.list_objects_v2(Bucket='bigmech',
+                                      Prefix=join(gen_prefix, job_prefix))
+    # TODO: Track success/failure
+    log_strs = []
+    for fdict in job_log_data['Contents']:
+        resp = s3.get_object(Bucket='bigmech', Key=fdict['Key'])
+        log_strs.append(resp['Body'].read().decode('utf-8'))
+    return log_strs
+
+
+def extract_reach_logs(log_str):
+    """Get the list of reach logs from the overall logs."""
+    log_lines = log_str.splitlines()
+    reach_logs = []
+    reach_lines = []
+    adding_reach_lines = False
+    for l in log_lines:
+        if not adding_reach_lines and 'Beginning reach' in l:
+            adding_reach_lines = True
+        elif adding_reach_lines and 'Reach finished' in l:
+            adding_reach_lines = False
+            reach_logs.append(('SUCCEEDED', '\n'.join(reach_lines)))
+            reach_lines = []
+        elif adding_reach_lines:
+            reach_lines.append(l.split('readers - ')[1])
+    if adding_reach_lines:
+        reach_logs.append(('FAILURE', '\n'.join(reach_lines)))
+    return reach_logs
+
+
+def analyze_db_reading(job_prefix, reading_queue='run_db_reading_queue'):
+    """Run various analysis on a particular reading job."""
+    # Analyze reach failures
+    log_strs = get_logs_from_db_reading(job_prefix, reading_queue)
+    reach_logs = [extract_reach_logs(log_str) for log_str in log_strs]
+    failed_reach_logs = [reach_log_str for job_reach_logs in reach_logs
+                         for result, reach_log_str in job_reach_logs
+                         if result == 'FAILURE']
+    tcids_unfinished = {tcid for reach_log in failed_reach_logs
+                        for tcid in analyze_reach_log(log_str=reach_log)}
+    print("Found %d unfinished tcids." % len(tcids_unfinished))
+    return reach_logs, failed_reach_logs, tcids_unfinished
