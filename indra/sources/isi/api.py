a3afe0b98b22ec6c18d8ef246a5bb3b1f394ec8d
Daniel Milstein <djmilstein@gmail.com>
2018-04-25 09:34:03 -0400

    ISI: fix docker invocation

--- indra/sources/isi/api.py
+++ indra/sources/isi/api.py
@@ -3,6 +3,10 @@ from builtins import dict, str
 import tempfile
 import shutil
 from indra.sources.isi.processor import IsiProcessor
+import subprocess
+import logging
+
+logger = logging.getLogger('isi')
 
 def process_directory(dir_name):
     """Process a directory filled with text files (it is sufficient for them
@@ -20,15 +24,23 @@ def process_directory(dir_name):
     """
 
     # Create a temporary directory to store the output
-    output_dir = tempfile.mkstemp('indra_isi_processor_output')
-    tmp_dir = tempfile.mkstemp('indra_isi_processor_tmp')
+    output_dir = tempfile.mkdtemp('indra_isi_processor_output')
+    tmp_dir = tempfile.mkdtemp('indra_isi_processor_tmp')
 
+    # Form the command to invoke the ISI reader via Docker
     input_binding = dir_name + ':/input:ro'
     output_binding = output_dir + ':/output:rw'
     tmp_binding = tmp_dir + ':/temp:rw'
     command = ['docker', 'run', '-it', '--rm',
                '-v', input_binding, '-v', output_binding, '-v', tmp_binding,
                'sahilgar/bigmechisi', './myprocesspapers.sh']
+
+    # Invoke the ISI reader
+    print('Running command:')
+    print(' '.join(command))
+    ret = subprocess.call(command)
+    if ret != 0:
+        logger.error('Docker returned non-zero status code')
 
     # Process ISI output
     ip = IsiProcessor(output_dir)
