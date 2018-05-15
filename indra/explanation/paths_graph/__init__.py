f0b14e4cf93ab14f17c16a098c2bba5abb5e3ea2
John Bachman <bachmanjohn@gmail.com>
2018-05-15 16:11:28 -0400

    Remove paths_graph module from indra

--- indra/explanation/paths_graph/__init__.py
+++
@@ -1,5 +0,0 @@
-from .paths_graph import PathsGraph, CombinedPathsGraph, get_reachable_sets
-from .pre_cfpg import PreCFPG
-from .cfpg import CFPG, CombinedCFPG
-from .paths_tree import PathsTree
-from .api import *
