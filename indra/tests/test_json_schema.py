0772e81a278a516627d82bfbdcf9fab408b7260a
Daniel Milstein <djmilstein@gmail.com>
2018-05-24 09:01:23 -0400

    JSON schema: unit tests for invalid phosphorylation statements

--- indra/tests/test_json_schema.py
+++ indra/tests/test_json_schema.py
@@ -2,6 +2,7 @@ import json
 import jsonschema
 import os
 from nose.tools import assert_raises
+from jsonschema.exceptions import ValidationError
 
 dir_this = os.path.dirname(__file__)
 schema_file = os.path.join(dir_this, '../../schemas/statements_schema.json')
@@ -13,9 +14,37 @@ valid_agent2 = {'name': 'RAS', 'db_refs': {'TEXT': 'RAS'}}
 
 invalid_agent1 = {'name': 'RAS', 'db_refs': 2}
 invalid_agent2 = {'db_refs': {'TEXT': 'RAS'}}
-invalid_agent3 = {'name': 'cow', 'db_refs': {'TEXT': 'RAS'}, 'bound_conditions': 'mooooooooooo!'}
+invalid_agent3 = {'name': 'cow', 'db_refs': {'TEXT': 'RAS'},
+                  'bound_conditions': 'mooooooooooo!'}
+
 
 def test_valid_phosphorylation():
     # Calls to validate() in this function should not raise exceptions
-    s = {'enz': valid_agent1, 'sub': valid_agent2, 'type': 'Phosphorylation', 'id': '5'}
+    s = {'enz': valid_agent1, 'sub': valid_agent2, 'type': 'Phosphorylation',
+         'id': '5'}
     jsonschema.validate([s], schema)
+
+    s['residue'] = 'S'
+    jsonschema.validate([s], schema)
+
+    s['position'] = '10'
+    jsonschema.validate([s], schema)
+
+
+def test_invalid_phosphorylation():
+    s = {'enz': valid_agent1, 'sub': valid_agent2, 'type': 'Phosphorylation',
+         'id': '5', 'residue': 5}  # residue should be a string
+    val = lambda s: jsonschema.validate([s], schema)
+    assert_raises(ValidationError, val, s)
+
+    s = {'enz': valid_agent1, 'sub': invalid_agent1, 'type': 'Phosphorylation',
+         'id': '5'}
+    assert_raises(ValidationError, val, s)
+
+    s = {'enz': valid_agent1, 'sub': invalid_agent2, 'type': 'Phosphorylation',
+         'id': '5'}
+    assert_raises(ValidationError, val, s)
+
+    s = {'enz': valid_agent1, 'sub': invalid_agent3, 'type': 'Phosphorylation',
+         'id': '5'}
+    assert_raises(ValidationError, val, s)
