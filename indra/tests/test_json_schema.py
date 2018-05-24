9023c597ac775a4f9132b12c3c097eb755c7e656
Daniel Milstein <djmilstein@gmail.com>
2018-05-24 09:21:30 -0400

    JSON schema: add unit tests for Conversion

--- indra/tests/test_json_schema.py
+++ indra/tests/test_json_schema.py
@@ -11,6 +11,7 @@ with open(schema_file, 'r') as f:
 
 valid_agent1 = {'name': 'RAF', 'db_refs': {'TEXT': 'RAF'}}
 valid_agent2 = {'name': 'RAS', 'db_refs': {'TEXT': 'RAS'}}
+valid_agent3 = {'name': 'ERK', 'db_refs': {'TEXT': 'ERK'}}
 
 invalid_agent1 = {'name': 'RAS', 'db_refs': 2}
 invalid_agent2 = {'db_refs': {'TEXT': 'RAS'}}
@@ -130,4 +131,28 @@ def test_invalid_influence():
 
     s = {'subj': valid_concept1, 'obj': valid_concept2, 'subj_delta': None,
          'obj_delta': None, 'type': 'Influence', 'id': 10}
+    assert_raises(ValidationError, val, s)
+
+
+def test_valid_conversion():
+    s = {'type': 'Conversion', 'id': '11', 'subj': valid_agent1,
+         'obj_from': [valid_agent2, valid_agent3], 'obj_to': [valid_agent3]}
+    jsonschema.validate([s], schema)
+
+
+def test_invalid_conversion():
+    s = {'type': 'Conversion', 'id': '11', 'subj': valid_agent1,
+         'obj_from': [12, valid_agent3], 'obj_to': [valid_agent3]}
+    assert_raises(ValidationError, val, s)
+
+    s = {'type': 'Conversion', 'id': '11', 'subj': valid_agent1,
+         'obj_from': [valid_agent2, valid_agent3], 'obj_to': [valid_agent3, 12]}
+    assert_raises(ValidationError, val, s)
+
+    s = {'type': 'Conversion', 'id': '11', 'subj': 'dog',
+         'obj_from': [valid_agent2, valid_agent3], 'obj_to': [valid_agent3]}
+    assert_raises(ValidationError, val, s)
+
+    s = {'type': 'Conversion', 'id': '11', 'subj': valid_agent1,
+         'obj_from': 'banana', 'obj_to': [valid_agent3]}
     assert_raises(ValidationError, val, s)
