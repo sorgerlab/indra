c7748ece47c1f00f327b3f0730318ec7676b31b8
Ben Gyori <ben.gyori@gmail.com>
2018-06-09 16:06:27 -0400

    Improve testing of getting synonyms

--- indra/tests/test_uniprot_client.py
+++ indra/tests/test_uniprot_client.py
@@ -60,13 +60,16 @@ def test_get_gene_name_no_table_entry():
 
 
 def test_get_synonyms():
-    synonyms = uniprot_client.get_gene_alt_labels('P31938')
-    assert synonyms, synonyms
-    other_names = uniprot_client.get_gene_alt_names('P31938')
-    assert other_names, other_names
-    all_names = uniprot_client.get_all_gene_names('P31938')
-    assert (set(synonyms) & set(other_names)).issubset(set(all_names)), \
-        all_names
+    upid = 'Q02750'  # This is MAP2K1
+    gene_synonyms = uniprot_client.get_gene_synonyms(upid)
+    assert gene_synonyms, gene_synonyms
+    assert 'MEK1' in gene_synonyms
+    protein_synonyms = uniprot_client.get_protein_synonyms(upid)
+    assert protein_synonyms, protein_synonyms
+    assert 'MKK1' in protein_synonyms
+    all_synonyms = uniprot_client.get_synonyms(upid)
+    assert set(gene_synonyms + protein_synonyms) == set(all_synonyms)
+    assert 'MAP2K1' in all_synonyms
 
 
 def test_get_gene_name_nonhuman():
