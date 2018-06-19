e7501d63924f048b8116fa19801a19dc036029fb
Ben Gyori <ben.gyori@gmail.com>
2018-06-19 11:06:17 -0700

    Import and run EMELI in the demo script

--- models/bmi_demo/bmi_models.py
+++ models/bmi_demo/bmi_models.py
@@ -3,9 +3,10 @@ import shutil
 from indra.sources import eidos
 from indra.assemblers.bmi_wrapper import BMIModel
 from indra.assemblers import PysbAssembler
-
+from topoflow.framework import emeli
 
 def text_to_stmts(text):
+    """Run Eidos reading on a given text and return INDRA Statements."""
     fname = text.replace(' ', '_') + '.jsonld'
     if os.path.exists(fname):
         ep = eidos.process_json_ld_file(fname)
@@ -16,6 +17,7 @@ def text_to_stmts(text):
 
 
 def make_component_repo(bmi_models):
+    """Generate files representing each component for EMELI to discover."""
     for m in bmi_models:
         m.export_into_python()
     comps = [m.make_repository_component() for m in bmi_models]
@@ -26,6 +28,11 @@ def make_component_repo(bmi_models):
         for m in bmi_models:
             fh.write('%s %s\n' % (m.model.name, m.model.name))
 
+# Variables of interest in topoflow/components/met_base.py
+# _output_var_names
+# 'atmosphere_water__rainfall_volume_flux'
+# 'land_surface__temperature'
+# 'land_surface_net-shortwave-radiation__energy_flux'
 
 if __name__ == '__main__':
     model_txts = ['rainfall causes floods', 'floods cause displacement']
@@ -40,3 +47,8 @@ if __name__ == '__main__':
         bm = BMIModel(model, root_vars=root_vars[idx])
         bmi_models.append(bm)
     make_component_repo(bmi_models)
+
+    # Make the EMELI framework and run it
+    f = emeli.framework()
+    f.run_model(cfg_prefix='component', cfg_directory='.',
+                driver_comp_name=bmi_models[0].model.name)
