be11e1ccb38f4593c99df12943ea28248dd0b8d1
Ben Gyori <ben.gyori@gmail.com>
2018-06-19 13:09:42 -0700

    Plot results of simulation when done

--- models/bmi_demo/bmi_models.py
+++ models/bmi_demo/bmi_models.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import matplotlib.pyplot as plt
 from indra.sources import eidos
 from indra.assemblers.bmi_wrapper import BMIModel
 from indra.assemblers import PysbAssembler
@@ -7,7 +8,7 @@ from topoflow.framework import emeli
 
 def text_to_stmts(text):
     """Run Eidos reading on a given text and return INDRA Statements."""
-    fname = text.replace(' ', '_') + '.jsonld'
+    fname = text.replace(' ', '_').replace(',', '_') + '.jsonld'
     if os.path.exists(fname):
         ep = eidos.process_json_ld_file(fname)
     else:
@@ -28,6 +29,22 @@ def make_component_repo(bmi_models):
         for m in bmi_models:
             fh.write('%s %s\n' % (m.model.name, m.model.name))
 
+
+def plot_results(model_dict):
+    plt.figure()
+    plt.ion()
+    for model_name, bmi_model in model_dict.items():
+        state_dict = {}
+        times = [tc[0] for tc in bmi_model.time_course]
+        for var_name, var_id in bmi_model.species_name_map.items():
+            state_dict[var_name] = [tc[1][var_id] for tc in
+                                    bmi_model.time_course]
+            plt.plot(times, state_dict[var_name], label=var_name)
+    #plt.xlim([1, 1001])
+    plt.legend()
+    plt.show()
+
+
 # Variables of interest in topoflow/components/met_base.py
 # _output_var_names
 # 'atmosphere_water__rainfall_volume_flux'
@@ -35,7 +52,8 @@ def make_component_repo(bmi_models):
 # 'land_surface_net-shortwave-radiation__energy_flux'
 
 if __name__ == '__main__':
-    model_txts = ['rainfall causes floods', 'floods cause displacement']
+    model_txts = ['rainfall causes floods',
+        'floods cause displacement, and displacement reduces access to food']
     stmts = [text_to_stmts(t) for t in model_txts]
     bmi_models = []
     root_vars = [['rainfall'], []]
@@ -44,7 +62,7 @@ if __name__ == '__main__':
         pa.add_statements(model_stmts)
         model = pa.make_model()
         model.name = 'model%d' % idx
-        bm = BMIModel(model, root_vars=root_vars[idx])
+        bm = BMIModel(model, root_vars=root_vars[idx], stop_time=5000)
         bmi_models.append(bm)
     make_component_repo(bmi_models)
 
@@ -52,3 +70,4 @@ if __name__ == '__main__':
     f = emeli.framework()
     f.run_model(cfg_prefix='component', cfg_directory='.',
                 driver_comp_name=bmi_models[0].model.name)
+    plot_results(f.comp_set)
