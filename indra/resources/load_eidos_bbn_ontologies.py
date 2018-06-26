0fd1248d64d11d065ebc2b13b114f128f4d8db2b
pagreene <patrick.anton.greene@gmail.com>
2018-06-26 14:40:00 -0400

    Merge eidos and bbn ontologie loaders.

--- indra/resources/load_eidos_bbn_ontologies.py
+++ indra/resources/load_eidos_bbn_ontologies.py
@@ -1,15 +1,20 @@
+"""This file contains the methods needed to load the ontologies for Eidos.
+
+It also loads BBN and can easly handle any other ontology which uses the same
+format (yaml ontology following the namespace defined at `eidos_ns`).
+"""
+
 import yaml
 import requests
 from os.path import join, dirname, abspath
 from rdflib import Graph, Namespace, Literal
 
-bb_ont_url = ('https://raw.githubusercontent.com/BBN-E/Hume/master/resource/'
-              'ontologies/hume_ontology.yaml')
 
 eidos_ns = Namespace('https://github.com/clulab/eidos/wiki/JSON-LD/Grounding#')
 indra_ns = 'http://sorger.med.harvard.edu/indra/'
 indra_rel_ns = Namespace(indra_ns + 'relations/')
 isa = indra_rel_ns.term('isa')
+
 
 def save_hierarchy(g, path):
     with open(path, 'wb') as out_file:
@@ -50,12 +55,31 @@ def build_relations(G, node, tree, prefix):
             G.add(rel)
 
 
-if __name__ == '__main__':
-    yml = requests.get(bb_ont_url).content
+def load_ontology(ont_url, rdf_path):
+    """Load an ontology formatted like Eidos' from github."""
+    yml = requests.get(ont_url).content
     root = yaml.load(yml)
     G = Graph()
     for top_entry in root:
-        for node in top_entry.keys():
-            build_relations(G, node, top_entry[node], None)
-    rdf_path = join(dirname(abspath(__file__)), 'bbn_ontology.rdf')
+        assert len(top_entry) == 1
+        node = list(top_entry.keys())[0]
+        build_relations(G, node, top_entry[node], None)
     save_hierarchy(G, rdf_path)
+
+
+if __name__ == '__main__':
+    # Eidos
+    from indra.sources import eidos
+    eidos_ont_url = ('https://raw.githubusercontent.com/clulab/eidos/master/'
+                     'src/main/resources/org/clulab/wm/eidos/ontologies/'
+                     'un_ontology.yml')
+    eidos_rdf_path = join(dirname(abspath(eidos.__file__)),
+                          'eidos_ontology.rdf')
+    load_ontology(eidos_ont_url, eidos_rdf_path)
+
+    # BBN
+    from indra.sources import bbn
+    bbn_ont_url = ('https://raw.githubusercontent.com/BBN-E/Hume/master/'
+                   'resource/ontologies/hume_ontology.yaml')
+    bbn_rdf_path = join(dirname(abspath(bbn.__file__)), 'bbn_ontology.rdf')
+    load_ontology(bbn_ont_url, bbn_rdf_path)
