1f76ebb0adcdba30fc2e4d9f370bac84cc2882f4
Ben Gyori <ben.gyori@gmail.com>
2018-07-12 23:26:21 -0400

    Add reader initializer for Eidos

--- indra/sources/eidos/__init__.py
+++ indra/sources/eidos/__init__.py
@@ -33,4 +33,5 @@ Finally, rerun sbt assembly.
 
 from .eidos_api import process_text, \
     process_json_str, process_json_file, process_json, \
-    process_json_ld_str, process_json_ld_file, process_json_ld
+    process_json_ld_str, process_json_ld_file, process_json_ld, \
+    initialize_reader
