78e9bf89412e8cf0f68a749f97ca5801d349303d
John Bachman <bachmanjohn@gmail.com>
2018-07-13 11:59:23 -0400

    Add subj/obj pruning method to model checker

--- indra/explanation/model_checker.py
+++ indra/explanation/model_checker.py
@@ -773,6 +773,32 @@ class ModelChecker(object):
         # Now remove all the edges to be removed with a single call
         im.remove_edges_from(edges_to_remove)
 
+    def prune_influence_map_subj_obj(self):
+        """Prune influence map to include only edges where the object of the
+        upstream rule matches the subject of the downstream rule."""
+        def get_rule_subj_obj(r):
+            result = {}
+            for ann in self.model.annotations:
+                if ann.subject == r:
+                    if ann.predicate == 'rule_has_subject':
+                        result['subject'] = ann.object
+                    elif ann.predicate == 'rule_has_object':
+                        result['object'] = ann.object
+            return result
+        im = self.get_im()
+        rules = im.nodes()
+        edges_to_prune = []
+        for r1, r2 in itertools.permutations(rules, 2):
+            if (r1, r2) not in im.edges():
+                continue
+            r1_info = get_rule_info(r1)
+            r2_info = get_rule_info(r2)
+            if 'object' not in r1_info or 'subject' not in r2_info:
+                continue
+            if r1_info['object'] != r2_info['subject']:
+                logger.info("Removing edge %s --> %s" % (r1, r2))
+                edges_to_prune.append((r1, r2))
+        im.remove_edges_from(edges_to_prune)
 
 def _find_sources_sample(im, target, sources, polarity, rule_obs_dict,
                          agent_to_obs, agents_values):
