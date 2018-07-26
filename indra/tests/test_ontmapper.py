5a188f0a3ee97ee220cf626872ce0b171aa2a696
Ben Gyori <ben.gyori@gmail.com>
2018-07-26 16:02:47 -0400

    Update BBN grounding to be treated as list in tests

--- indra/tests/test_ontmapper.py
+++ indra/tests/test_ontmapper.py
@@ -5,13 +5,13 @@ from indra.preassembler.ontology_mapper import OntologyMapper, wm_ontomap
 
 def test_map():
     c1 = Concept('x', db_refs={'UN': [('entities/x', 1.0)]})
-    c2 = Concept('y', db_refs={'BBN': 'entities/y'})
+    c2 = Concept('y', db_refs={'BBN': [('entities/y', 1.0)]})
     c3 = Concept('z')
     stmts = [Influence(c1, c3), Influence(c2, c3)]
     om = OntologyMapper(stmts)
     om.map_statements()
     assert len(om.statements) == 2
-    assert om.statements[0].subj.db_refs['BBN'] == 'entities/y', \
+    assert om.statements[0].subj.db_refs['BBN'] == [('entities/y', 1.0)] \
         om.statements[0].subj.db_refs
     assert om.statements[1].subj.db_refs['UN'] == [('entities/x', 1.0)], \
         om.statements[1].subj.db_refs
