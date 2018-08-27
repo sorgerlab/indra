903f3af2ce7a580d4ed401d5ff7326d99cc6e339
kkaris <karis.klas@gmail.com>
2018-08-27 12:33:11 -0400

    Refactor: eidos_api to api

--- indra/sources/eidos/__init__.py
+++ indra/sources/eidos/__init__.py
@@ -31,7 +31,7 @@ Next, set the property "useW2V" to true in src/main/resources/eidos.conf.
 Finally, rerun sbt assembly.
 """
 
-from .eidos_api import process_text, \
+from .api import process_text, \
     process_json_str, process_json_file, process_json, \
     process_json_ld_str, process_json_ld_file, process_json_ld, \
     initialize_reader
