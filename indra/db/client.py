ee239ed912dec3728417717a03bd9410813663b0
pagreene <patrick.anton.greene@gmail.com>
2018-09-14 08:54:24 -0400

    Record content source in annotations.

--- indra/db/client.py
+++ indra/db/client.py
@@ -650,7 +650,7 @@ def _get_pa_stmt_jsons_w_mkhash_subquery(db, mk_hashes_q, best_first=True,
 
     selection = (select([mk_hashes_al.c.mk_hash, mk_hashes_al.c.ev_count,
                          json_content_al.c.raw_json, json_content_al.c.pa_json,
-                         db.ReadingRefLink.pmid])
+                         db.ReadingRefLink.pmid, db.ReadingRefLink.source])
                  .select_from(stmts_q))
     logger.debug("Executing sql to get statements:\n%s" % str(selection))
 
@@ -660,7 +660,7 @@ def _get_pa_stmt_jsons_w_mkhash_subquery(db, mk_hashes_q, best_first=True,
     stmts_dict = {}
     total_evidence = 0
     returned_evidence = 0
-    for mk_hash, ev_count, raw_json_bts, pa_json_bts, pmid in res:
+    for mk_hash, ev_count, raw_json_bts, pa_json_bts, pmid, src in res:
         returned_evidence += 1
         raw_json = json.loads(raw_json_bts.decode('utf-8'))
         ev_json = raw_json['evidence'][0]
@@ -692,6 +692,9 @@ def _get_pa_stmt_jsons_w_mkhash_subquery(db, mk_hashes_q, best_first=True,
         if 'prior_uuids' not in ev_json['annotations'].keys():
             ev_json['annotations']['prior_uuids'] = []
         ev_json['annotations']['prior_uuids'].append(raw_json['id'])
+
+        if src:
+            ev_json['annotations']['content_source'] = src
 
         stmts_dict[mk_hash]['evidence'].append(ev_json)
 
