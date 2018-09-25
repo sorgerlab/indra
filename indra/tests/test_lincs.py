e4ead0b33bea8a741b89814d5cdf5376e91cbcc6
pagreene <patrick.anton.greene@gmail.com>
2018-09-25 13:10:56 -0400

    Test processor.

--- indra/tests/test_lincs.py
+++ indra/tests/test_lincs.py
@@ -1,9 +1,16 @@
 from __future__ import absolute_import, print_function, unicode_literals
 from builtins import dict, str
 
-from indra.sources.lincs.api import _get_lincs_drug_target_data
+from indra.sources.lincs.api import _get_lincs_drug_target_data, \
+    process_from_web
 
 
 def test_get_drug_target_data():
     data_list = _get_lincs_drug_target_data()
     assert len(data_list) > 100, len(data_list)
+
+
+def test_process_from_web():
+    lincs_p = process_from_web()
+    assert lincs_p is not None
+    assert lincs_p.statements
