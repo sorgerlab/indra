f069115e44ccf3d63476890e33e19ebbc6001785
Ben Gyori <ben.gyori@gmail.com>
2018-09-29 01:42:38 -0400

    Don't use os.path.join for URL construction
    Because os.sep is a backslash on Windows

--- indra/databases/lincs_client.py
+++ indra/databases/lincs_client.py
@@ -6,7 +6,7 @@ __all__ = ['get_drug_target_data', 'get_small_molecule_data',
 
 import csv
 import requests
-from os import path
+
 
 LINCS_URL = 'http://lincs.hms.harvard.edu/db'
 
@@ -128,7 +128,7 @@ def get_drug_target_data():
         A list of dicts, each keyed based on the header of the csv, with values
         as the corresponding column values.
     """
-    url = path.join(LINCS_URL, 'datasets/20000/results')
+    url = LINCS_URL + '/datasets/20000/results'
     return _load_lincs_csv(url)
 
 
@@ -142,7 +142,7 @@ def get_small_molecule_data():
         contained in a dict of row values keyed by the column headers extracted
         from the csv.
     """
-    url = path.join(LINCS_URL, 'sm/')  # The trailing / is deliberate
+    url = LINCS_URL + '/sm/'  # The trailing / is deliberate
     sm_data = _load_lincs_csv(url)
     sm_dict = {d['HMS LINCS ID']: d.copy() for d in sm_data}
     assert len(sm_dict) == len(sm_data), "We lost data."
@@ -158,7 +158,7 @@ def get_protein_data():
         A dict keyed by HMS LINCS protein ids, with the metadata contained in a
         dict of row values keyed by the column headers extracted from the csv.
     """
-    url = path.join(LINCS_URL, 'proteins/')
+    url = LINCS_URL + '/proteins/'
     prot_data = _load_lincs_csv(url)
     prot_dict = {d['HMS LINCS ID']: d.copy() for d in prot_data}
     assert len(prot_dict) == len(prot_data), "We lost data."
