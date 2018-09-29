7a8daa11d692aa405f39cf23b9d32a0ca533a6b8
Ben Gyori <ben.gyori@gmail.com>
2018-09-29 18:34:43 -0400

    Update tests to match new LincsClient API

--- indra/tests/test_lincs_client.py
+++ indra/tests/test_lincs_client.py
@@ -1,8 +1,10 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 from nose.plugins.attrib import attr
-from indra.databases.lincs_client import get_drug_target_data, \
-    get_small_molecule_data, get_protein_data, LincsClient
+from indra.databases.lincs_client import get_drug_target_data, LincsClient
+
+
+lc = LincsClient()
 
 
 @attr('webservice')
@@ -11,31 +13,25 @@ def test_get_drug_target_data():
     assert len(data_list) > 100, len(data_list)
 
 
-@attr('webservice')
-def test_get_small_molecule_data():
-    sm_data = get_small_molecule_data()
-    assert len(sm_data) > 100, len(sm_data)
+def test_get_protein_refs():
+    prot_refs = lc.get_protein_refs('200020')
+    assert prot_refs.get('UP') == 'P00519'
+    assert prot_refs.get('EGID') == '25'
+    assert prot_refs.get('HMS-LINCS') == '200020'
 
 
-@attr('webservice')
-def test_get_protein_data():
-    prot_data = get_protein_data()
-    assert len(prot_data) > 100, len(prot_data)
-
-
-@attr('webservice')
-def test_lincs_client():
-    lc = LincsClient()
-    prot_refs = lc.get_protein_ref('25', id_type='entrez')
-    assert prot_refs
-    assert '200020' in prot_refs.keys()
-    prot_ref_2 = lc.get_protein_ref('200020')  # id_type='hms-lincs'
-    assert prot_refs['200020'] == prot_ref_2
-    sm_name = lc.get_small_molecule_name('10001', id_type='short-hms-lincs')
+def test_get_sm_name():
+    sm_name = lc.get_small_molecule_name('10001')
     assert sm_name == 'Seliciclib', sm_name
-    sm_refs = lc.get_small_molecule_ref('10001', id_type='short-hms-lincs')
-    assert sm_refs
-    sm_ref = lc.get_small_molecule_ref('10001-101')  # id_type='hms-lincs'
-    assert sm_ref == list(sm_refs.values())[0]
-    sm_refs_2 = lc.get_small_molecule_ref('14762', id_type='chembl')
-    assert sm_refs_2 == sm_refs
+
+
+def test_get_sm_refs():
+    sm_refs = lc.get_small_molecule_refs('10001')
+    assert sm_refs.get('HMS-LINCS') == '10001'
+    assert sm_refs.get('PUBCHEM') == '160355'
+    assert sm_refs.get('CHEMBL') == '14762'
+
+    sm_ref = lc.get_small_molecule_refs('10001-101')
+    assert sm_refs.get('HMS-LINCS') == '10001'
+    assert sm_refs.get('PUBCHEM') == '160355'
+    assert sm_refs.get('CHEMBL') == '14762'
--- indra/tests/test_lincs_drug.py
+++ indra/tests/test_lincs_drug.py
@@ -12,7 +12,10 @@ def test_process_from_web():
     assert lincs_p.statements
     data_len = len(get_drug_target_data())
     num_stmts = len(lincs_p.statements)
-    assert num_stmts == data_len, \
+    # Note that due to an erroneous entry in the HMS LINCS protein table,
+    # one Statement is not extracted from the table, hence the condition
+    # below, which should be kept as long as the error persists
+    assert num_stmts >= data_len - 1, \
         ("Did not convert all statements: expected %d, got %d."
          % (data_len, num_stmts))
     assert all(len(s.evidence) > 0 for s in lincs_p.statements),\
