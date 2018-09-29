486033818e83c689a79a34bddb15bd277db9f9db
Ben Gyori <ben.gyori@gmail.com>
2018-09-29 18:54:08 -0400

    Name source_api for LINCS drugs lincs_drug

--- indra/sources/lincs_drug/processor.py
+++ indra/sources/lincs_drug/processor.py
@@ -92,7 +92,7 @@ class LincsProcessor(object):
                 pmid = m.groups()[0]
             annotations = {'reference': ref}
             annotations.update(generic_notes)
-            ev = Evidence('lincs', pmid=pmid, annotations=annotations,
+            ev = Evidence('lincs_drug', pmid=pmid, annotations=annotations,
                           epistemics={'direct': True})
             ev_list.append(ev)
         return ev_list
