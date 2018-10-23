0cd084550fc5c1315fe33fcb00e57c1c332be6ab
John Bachman <bachmanjohn@gmail.com>
2018-10-23 13:04:09 -0400

    Add test for invalid MESH ID

--- indra/tests/test_mesh.py
+++ indra/tests/test_mesh.py
@@ -4,3 +4,8 @@ def test_mesh_id_lookup():
     mesh_id = 'D003094'
     mesh_name = mesh_client.get_mesh_name(mesh_id)
     assert mesh_name == 'Collagen'
+
+def test_invalid_id():
+    mesh_name = mesh_client.get_mesh_name('34jkgfh')
+    assert mesh_name is None
+
