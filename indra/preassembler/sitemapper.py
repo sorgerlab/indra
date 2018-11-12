16ceb4ad5f17f31a33b9165516bf4068752f34e9
John Bachman <bachmanjohn@gmail.com>
2018-11-12 14:55:54 -0500

    Treat known errors the same as missing sites

--- indra/preassembler/sitemapper.py
+++ indra/preassembler/sitemapper.py
@@ -167,8 +167,6 @@ class SiteMapper(object):
                     invalid_sites += agent_invalid_sites
                     new_agent.bound_conditions[ind].agent = new_b
 
-
-
                 new_agent_list.append(new_agent)
             else:
                 new_agent_list.append(agent)
@@ -380,7 +378,8 @@ class SiteMapper(object):
         list
             A list of invalid sites, where each entry in the list has two
             elements: ((gene_name, residue, position), mapped_site).  If the
-            invalid position was not found in the site map, mapped_site is
+            invalid position was not found in the site map, or if it is curated
+            in the site map as a known error with no mapping, mapped_site is
             None; otherwise it is a tuple consisting of (residue, position,
             comment).
         """
@@ -480,8 +479,17 @@ class SiteMapper(object):
                         continue
             # Now check the site map
             mapped_site = self.site_map.get(site_key, None)
+            # If there is an entry in the site map that is intended to flag
+            # a site as incorrect, with no viable mapping (i.e., either of the
+            # the entries in the mapping tuple are None), then set the
+            # mapped_site to None (not a tuple) so that it doesn't get
+            # misinterpreted as an actual mapping
+            if mapped_site is not None and \
+               (mapped_site[0] is None or mapped_site[1] is None):
+                mapped_site = None
+            # No entry in the site map, or no valid mapping; set site info
+            # to None
             if mapped_site is None:
-                # No entry in the site map--set site info to None
                 self._cache[site_key] = None
                 invalid_sites.append((site_key, None))
             # Manually mapped in the site map
