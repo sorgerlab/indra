bbbaf8af9138fd58af90bded16840a4f593d0c47
Ben Gyori <ben.gyori@gmail.com>
2018-11-20 23:05:58 -0500

    Refactor script to make Hume examples for reusability

--- indra/sources/hume/make_hume_tsv.py
+++ indra/sources/hume/make_hume_tsv.py
@@ -6,8 +6,7 @@ import requests
 from indra.sources import hume
 
 
-
-def build_examples(node, tree, prefix):
+def build_examples(node, tree, prefix, all_examples):
     if not prefix or prefix in ('entity', 'event'):
         this_prefix = node
     else:
@@ -19,7 +18,10 @@ def build_examples(node, tree, prefix):
             for child in entry.keys():
                 if child[0] != '_':
                     if isinstance(entry[child], (list, dict)):
-                        build_examples(child, entry[child], this_prefix)
+                        build_examples(child, entry[child], this_prefix,
+                                       all_examples)
+        else:
+            continue
         if child[0] != '_':
             if this_prefix in all_examples:
                 all_examples[this_prefix].add(child)
@@ -28,7 +30,7 @@ def build_examples(node, tree, prefix):
                 all_examples[this_prefix] = set(parts + [child])
 
 
-if __name__ == '__main__':
+def make_file(fname):
     hume_ont_url = ('https://raw.githubusercontent.com/BBN-E/Hume/master/'
                     'resource/ontologies/hume_ontology.yaml')
 
@@ -38,9 +40,14 @@ if __name__ == '__main__':
     root = yaml.load(yml)
     for top_entry in root:
         node = list(top_entry.keys())[0]
-        build_examples(node, top_entry[node], None)
+        build_examples(node, top_entry[node], None, all_examples)
 
 
-    with open('hume_ontology_examples.tsv', 'w') as fh:
+    with open(fname, 'w') as fh:
         for k, v in sorted(all_examples.items(), key=lambda x: x[0]):
             fh.write('%s\t%s\n' % (k, ','.join(sorted(list(v)))))
+
+
+if __name__ == '__main__':
+    fname = 'hume_ontology_examples.tsv'
+    make_file(fname)
