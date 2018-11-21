28a44f5a7d15916f302ba0fd837b59e942cd7bee
Ben Gyori <ben.gyori@gmail.com>
2018-11-20 19:13:24 -0500

    Add Influence test and fix spaces

--- indra/assemblers/english/assembler.py
+++ indra/assemblers/english/assembler.py
@@ -339,14 +339,14 @@ def _assemble_influence(stmt):
 
     # Note that n is prepended to increase to make it "an increase"
     if stmt.subj_delta['polarity'] is not None:
-        subj_delta_str = 'decrease' if stmt.subj_delta['polarity'] == -1 \
+        subj_delta_str = ' decrease' if stmt.subj_delta['polarity'] == -1 \
             else 'n increase'
-        subj_str = 'a %s in %s' % (subj_delta_str, subj_str)
+        subj_str = 'a%s in %s' % (subj_delta_str, subj_str)
 
     if stmt.obj_delta['polarity'] is not None:
-        obj_delta_str = 'decrease' if stmt.obj_delta['polarity'] == -1 \
+        obj_delta_str = ' decrease' if stmt.obj_delta['polarity'] == -1 \
             else 'n increase'
-        obj_str = 'a %s in %s' % (obj_delta_str, obj_str)
+        obj_str = 'a%s in %s' % (obj_delta_str, obj_str)
 
     stmt_str = '%s causes %s' % (subj_str, obj_str)
     return _make_sentence(stmt_str)
