823b08a2605a9caaf1b64e5295e547aa94a08504
Ben Gyori <ben.gyori@gmail.com>
2018-11-28 13:22:32 -0500

    Remove old tests using old processor

--- indra/tests/test_hume.py
+++ indra/tests/test_hume.py
@@ -9,46 +9,8 @@ from indra.sources.hume.api import *
 
 # Path to the HUME test files
 path_this = os.path.dirname(os.path.abspath(__file__))
-test_file_simple = os.path.join(path_this, 'bbn_test_simple.json-ld')
-test_file_negatedCause = os.path.join(path_this,
-                                      'bbn_test_negatedCause.json-ld')
-test_file_negatedEffect = os.path.join(path_this,
-                                       'bbn_test_negatedEffect.json-ld')
 
-
-def test_simple_extraction():
-    """Verify that processor extracts a simple causal assertion correctly from
-    a JSON-LD file."""
-    bp = process_jsonld_file(test_file_simple)
-    statements = bp.statements
-
-    assert(len(statements) == 1)
-    s0 = statements[0]
-
-    assert(isinstance(s0, Influence))
-    assert(s0.subj.name == 'cow')
-    assert(s0.subj.db_refs['HUME'] == 'Bovine')
-    assert(s0.obj.name == 'moo')
-    assert(s0.obj.db_refs['HUME'] == 'MooSound')
-
-    assert(len(s0.evidence) == 1)
-    ev0 = s0.evidence[0]
-    assert(ev0.source_api == 'hume')
-    assert(ev0.text == 'Cow causes moo.')
-
-
-def test_negated_cause():
-    """We only want to extract causal relations between two positive events.
-    The processor should give no statements for a negated cause."""
-    bp = process_json_file_old(test_file_negatedCause)
-    assert(len(bp.statements) == 0)
-
-
-def test_negated_effect():
-    """We only want to extract causal relations between two positive events.
-    The processor should give no statements for a negated effect."""
-    bp = process_json_file_old(test_file_negatedEffect)
-    assert(len(bp.statements) == 0)
+test_file_new_simple = os.path.join(path_this, 'wm_m12.ben_sentence.json-ld')
 
 
 @unittest.skip('Need updated JSON-LD file')
@@ -74,8 +36,7 @@ def test_large_bbn_corpus():
 
 
 def test_for_context():
-    bp = process_jsonld_file(os.path.join(path_this,
-                             'wm_m12.ben_sentence.json-ld'))
+    bp = process_jsonld_file(test_file_new_simple)
     assert bp, "Processor is none."
     assert len(bp.statements) == 1, len(bp.statements)
     stmt = bp.statements[0]
