d47d33b89d6163cc97e4b578719e94777dcf2563
kkaris <karis.klas@gmail.com>
2019-01-08 17:24:54 -0500

    Wrote function that returns processed text from Sofia API.

--- indra/sources/sofia/api.py
+++ indra/sources/sofia/api.py
@@ -1,4 +1,8 @@
+import time
+
 import openpyxl
+import requests
+
 from .processor import SofiaProcessor
 
 
@@ -25,3 +29,39 @@ def process_table(fname):
     entities_sheet = book['Entities']
     sp = SofiaProcessor(rel_sheet.rows, event_sheet.rows, entities_sheet.rows)
     return sp
+
+
+def _sofia_api_post(api, option, json, auth):
+    return requests.post(url=api + option, json=json, auth=auth)
+
+
+def _text_processing(text_json, user, password):
+    assert len(text_json) > 0
+
+    sofia_api = 'https://sofia.worldmodelers.com'
+    auth = (user, password)
+
+    # Initialize process
+    resp = _sofia_api_post(api=sofia_api, option='/process_text',
+                           json=text_json, auth=auth)
+    res_json = resp.json()
+
+    # Get status
+    status = _sofia_api_post(api=sofia_api, option='/status',
+                             json=res_json, auth=auth)
+
+    # Check status every two seconds
+    while status.json()['Status'] == 'Processing':
+        time.sleep(2.0)
+        status = _sofia_api_post(api=sofia_api, option='/status',
+                                 json=res_json, auth=auth)
+        if status.json()['Status'] == 'Done':
+            # Get results when processing is done
+            results = _sofia_api_post(api=sofia_api, option='/results',
+                                      json=res_json, auth=auth)
+            return results.json()
+
+    # The while loop exited without 'Done' status; return the api response
+    results = _sofia_api_post(api=sofia_api, option='/results',
+                              json=res_json, auth=auth)
+    return results.json()
