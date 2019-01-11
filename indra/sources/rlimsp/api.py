fa357bbe60c3fb984567a510881de24b14237c85
John Bachman <bachmanjohn@gmail.com>
2019-01-11 12:00:38 -0500

    Initial commit of placeholder init/api files

---
+++ indra/sources/rlimsp/api.py
@@ -0,0 +1,17 @@
+import json
+import logging
+import requests
+
+
+logger = logging.getLogger(__name__)
+
+
+default_output_fname = 'rlimsp_output.json'
+
+
+def process_pmc(pmc_id, output_fname=default_output_fname):
+
+
+def process_text(text, citation=None):
+    raise NotImplementedError()
+
