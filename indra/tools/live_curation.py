e9f40d8eaaabbe25c55e7850657038466ccda9d3
Ben Gyori <ben.gyori@gmail.com>
2019-01-15 09:01:51 -0500

    Implement corpus as class and assert curations

--- indra/tools/live_curation.py
+++ indra/tools/live_curation.py
@@ -9,9 +9,19 @@ scorer = wm_scorer.get_eidos_counts()
 app = Flask(__name__)
 
 
-corpora = {
-    '1': {}
-    }
+corpora = {}
+
+
+class Corpus(object):
+    def __init__(self, statements):
+        self.statements = {st.uuid: st for st in statements}
+        self.curations = {}
+
+    def __str__(self):
+        return 'Corpus(%s -> %s)' % (str(self.statements), str(self.curations))
+
+    def __repr__(self):
+        return str(self)
 
 
 @app.route('/update_beliefs', methods=['POST'])
@@ -23,7 +33,8 @@ def update_beliefs():
     subtype_counts = {}
     corpus = corpora.get(corpus_id)
     for uuid, correct in curations.items():
-        stmt = corpus.get(uuid)
+        corpus.curations[uuid] = correct
+        stmt = corpus.statements.get(uuid)
         for ev in stmt.evidence:
             print(ev)
             extraction_rule = ev.annotations.get('found_by')
@@ -49,8 +60,12 @@ def update_beliefs():
         return jsonify({})
     else:
         be = BeliefEngine(scorer)
-        stmts = list(corpus.values())
+        stmts = list(corpus.statements.values())
         be.set_prior_probs(stmts)
+        # Here we set beliefs based on actual curation
+        for uuid, correct in corpus.curations.items():
+            stmt = corpus.statements.get(uuid)
+            stmt.belief = correct
         belief_dict = _get_belief_dict(stmts)
         print(belief_dict)
         return jsonify(belief_dict)
@@ -64,6 +79,6 @@ if __name__ == '__main__':
     corpus_path = sys.argv[1]
     with open(corpus_path, 'rb') as fh:
         stmts = pickle.load(fh)
-        corpora['1'] = {st.uuid: st for st in stmts}
+        corpora['1'] = Corpus(stmts)
     print(corpora)
     app.run()
