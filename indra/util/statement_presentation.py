c991b886fd8ad249b82f7eefa0d6123a387309ba
pagreene <patrick.anton.greene@gmail.com>
2019-01-30 17:09:19 -0500

    Move code to order statements from Bioagents to INDRA.
    See 00fafafc4ab13cf5d53c82c55cc4847a90608b31 in bioagents.

---
+++ indra/util/statement_presentation.py
@@ -0,0 +1,67 @@
+from indra.assemblers.english import EnglishAssembler
+from indra.statements import Agent, get_statement_by_name
+
+
+def get_row_data(stmt_list, ev_totals=None):
+    def name(agent):
+        return 'None' if agent is None else agent.name
+
+    stmt_rows = {}
+    for s in stmt_list:
+        # Create a key.
+        verb = s.__class__.__name__
+        key = (verb,)
+
+        ags = s.agent_list()
+        if verb == 'Complex':
+            ag_ns = {name(ag) for ag in ags}
+            key += tuple(sorted(ag_ns))
+        elif verb == 'Conversion':
+            subj = name(ags[0])
+            objs_from = {name(ag) for ag in ags[1]}
+            objs_to = {name(ag) for ag in ags[2]}
+            key += (subj, tuple(sorted(objs_from)), tuple(sorted(objs_to)))
+        elif verb == 'ActiveForm':
+            key += (name(ags[0]), s.activity, s.is_active)
+        elif verb == 'HasActivity':
+            key += (name(ags[0]), s.activity, s.has_activity)
+        else:
+            key += tuple([name(ag) for ag in ags])
+
+        # Update the counts, and add key if needed.
+        if key not in stmt_rows.keys():
+            stmt_rows[key] = []
+        stmt_rows[key].append(s)
+
+    # Sort the rows by count and agent names.
+    def process(tpl):
+        key, stmts = tpl
+        if ev_totals is None:
+            count = sum(len(s.evidence) for s in stmts)
+        else:
+            count = sum(ev_totals[s.get_hash()] for s in stmts)
+        new_key = (count,)
+        new_key += tuple(key[1:])
+        return new_key, key[0], stmts
+
+    row_data = sorted((process(t) for t in stmt_rows.items()),
+                      key=lambda tpl: tpl[0], reverse=True)
+
+    return row_data
+
+
+def make_statement_string(key, verb):
+    """Make a Statement string via EnglishAssembler from `get_row_data` key."""
+    inp = key[1:]
+    StmtClass = get_statement_by_name(verb)
+    if verb == 'Complex':
+        stmt = StmtClass([Agent(name) for name in inp])
+    elif verb == 'Conversion':
+        stmt = StmtClass(Agent(inp[0]), [Agent(name) for name in inp[1]],
+                         [Agent(name) for name in inp[2]])
+    elif verb == 'ActiveForm' or verb == 'HasActivity':
+        stmt = StmtClass(Agent(inp[0]), inp[1], inp[2])
+    else:
+        stmt = StmtClass(*[Agent(name) for name in inp])
+    ea = EnglishAssembler([stmt])
+    return ea.make_model()[:-1]
