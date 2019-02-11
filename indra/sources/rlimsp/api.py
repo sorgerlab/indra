bbf2dd316058bc398c5e45dfb2d04eac1bbca550
pagreene <patrick.anton.greene@gmail.com>
2019-02-11 17:04:00 -0500

    Implement request to rlims-p rest api.

--- indra/sources/rlimsp/api.py
+++ indra/sources/rlimsp/api.py
@@ -7,11 +7,22 @@ logger = logging.getLogger(__name__)
 
 
 default_output_fname = 'rlimsp_output.json'
+RLIMSP_URL = 'https://research.bioinformatics.udel.edu/itextmine/api/data/rlims/pmc'
 
 
-def process_pmc(pmc_id, output_fname=default_output_fname):
+class RLIMSP_Error(Exception):
+    pass
 
 
-def process_text(text, citation=None):
-    raise NotImplementedError()
+def process_pmc(pmc_id, output_fname=default_output_fname, with_grounding=True):
+    """Get an output from RLIMS-p for the given pmic id."""
+    if with_grounding:
+        resp = requests.get(RLIMSP_URL + '.normed/pmcid/%s' % pmc_id)
+    else:
+        resp = requests.get(RLIMSP_URL + '/pmcid/%s' % pmc_id)
 
+    if resp.status_code != 200:
+        raise RLIMSP_Error("Bad status code: %d - %s"
+                           % (resp.status_code, resp.reason))
+
+    return
