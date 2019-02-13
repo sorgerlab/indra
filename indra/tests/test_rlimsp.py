77f820fe1286a5d39f2704c3821251bcbe20a2ba
pagreene <patrick.anton.greene@gmail.com>
2019-02-13 13:39:31 -0500

    Make basic test more particular.

--- indra/tests/test_rlimsp.py
+++ indra/tests/test_rlimsp.py
@@ -5,6 +5,12 @@ def test_simple_usage():
     rp = rlimsp.process_pmc('PMC3717945')
     stmts = rp.statements
     assert len(stmts) == 6, len(stmts)
+    for s in stmts:
+        assert len(s.evidence) == 1, "Wrong amount of evidence."
+        ev = s.evidence[0]
+        assert ev.annotations, "Missing annotations."
+        assert 'agents' in ev.annotations.keys()
+        assert 'trigger' in ev.annotations.keys()
 
 
 def test_ungrounded_usage():
