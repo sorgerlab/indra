921225181fc1d0242d61226c7b10663ddba1a1a2
pagreene <patrick.anton.greene@gmail.com>
2019-02-13 11:33:03 -0500

    Update test and add test for ungrounded endpoint.

--- indra/tests/test_rlimsp.py
+++ indra/tests/test_rlimsp.py
@@ -2,4 +2,11 @@ from indra.sources import rlimsp
 
 
 def test_simple_usage():
-    stmts = rlimsp.process_pmc('PMC3717945')
+    rp = rlimsp.process_pmc('PMC3717945')
+    stmts = rp.statements
+    assert len(stmts) == 6, len(stmts)
+
+
+def test_ungrounded_usage():
+    rp = rlimsp.process_pmc('PMC3717945', with_grounding=False)
+    assert len(rp.statements)
