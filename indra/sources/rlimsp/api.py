377fa94c2963a9c2522164ff374431dbe836217e
pagreene <patrick.anton.greene@gmail.com>
2019-02-13 15:20:31 -0500

    Add capability to read pmids and get medline.

--- indra/sources/rlimsp/api.py
+++ indra/sources/rlimsp/api.py
@@ -1,4 +1,4 @@
-__all__ = ['process_pmc']
+__all__ = ['process_from_webservice']
 
 import logging
 import requests
@@ -9,29 +9,38 @@ from .processor import RlimspProcessor
 logger = logging.getLogger(__name__)
 
 
-RLIMSP_URL = 'https://research.bioinformatics.udel.edu/itextmine/api/data/rlims/pmc'
+RLIMSP_URL = 'https://research.bioinformatics.udel.edu/itextmine/api/data/rlims/'
 
 
 class RLIMSP_Error(Exception):
     pass
 
 
-def process_pmc(pmcid, with_grounding=True):
+def process_from_webservice(id_val, id_type='pmcid', source='pmc',
+                            with_grounding=True):
     """Get an output from RLIMS-p for the given pmic id.
 
     Parameters
     ----------
-    pmcid : str
-        A PMCID, with the prefix PMC, of the paper to be "read".
+    id_val : str
+        A PMCID, with the prefix PMC, or pmid, with no prefix, of the paper to
+        be "read".
+    id_type : str
+        Either 'pmid' or 'pmcid'. The default is 'pmcid'.
+    source : str
+        Either 'pmc' or 'medline', whether you want pmc fulltext or medline
+        abstracts.
     with_grounding : bool
         The RLIMS-P web service provides two endpoints, one pre-grounded, the
         other not so much. The grounded endpoint returns far less content, and
         may perform some grounding that can be handled by the grounding mapper.
     """
     if with_grounding:
-        resp = requests.get(RLIMSP_URL + '.normed/pmcid/%s' % pmcid)
+        fmt = '%s.normed/%s/%s'
     else:
-        resp = requests.get(RLIMSP_URL + '/pmcid/%s' % pmcid)
+        fmt = '%s/%s/%s'
+
+    resp = requests.get(RLIMSP_URL + fmt % (source, id_type, id_val))
 
     if resp.status_code != 200:
         raise RLIMSP_Error("Bad status code: %d - %s"
