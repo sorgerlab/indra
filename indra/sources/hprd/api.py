0f406890c26ce3556a684b4a9854a5d170ffdb4d
John Bachman <bachmanjohn@gmail.com>
2019-02-14 13:34:13 -0500

    Get complex statements

--- indra/sources/hprd/api.py
+++ indra/sources/hprd/api.py
@@ -1,16 +1,43 @@
+import pandas as pd
 from collections import namedtuple
+from indra.sources.hprd.processor import HprdProcessor
 
-HprdComplexRow = namedtuple('HprdComplexRow',
-                        ['HPRD_COMPLEX_ID', 'HPRD_PROTEIN_ID', 'HGNC_SYMBOL',
-                         'REFSEQ_ID', 'EXPT_TYPES', 'PMIDS'])
+_hprd_id_cols = ['HPRD_ID', 'HGNC_SYMBOL', 'REFSEQ_GENE', 'REFSEQ_PROTEIN',
+                 'EGID', 'OMIM', 'UNIPROT', 'NAME']
 
+_cplx_cols = ['CPLX_ID', 'HPRD_ID', 'HGNC_SYMBOL', 'REFSEQ_PROTEIN',
+              'EVIDENCE', 'PMIDS']
 
-def process_from_flat_files(complexes_file=None, ptm_file=None):
+"""
+_cplx_dtype = {
+    'CPLX_ID': 'str',
+    'HPRD_ID': 'str',
+    'HGNC_SYMBOL': 'str',
+    'REFSEQ_PROTEIN': 'str',
+    'EVIDENCE': 'str',
+    'PMIDS': 'str',
+    }
+"""
+
+def process_from_flat_files(id_mappings_file, complexes_file=None,
+                            ptm_file=None):
+    id_df = pd.read_csv(id_mappings_file, delimiter='\t', names=_hprd_id_cols,
+                        dtype='str')
+    id_df = id_df.set_index('HPRD_ID')
     if complexes_file is None and phosphorylation_file is None:
         raise ValueError('At least one of complexes_file or '
                          'phosphorylation_file must be specified.')
-    hprd_rows = []
-    if ptm_file:
-        pass
     if complexes_file:
-        pass
+        cplx_df = pd.read_csv(complexes_file, delimiter='\t', names=_cplx_cols,
+                              dtype='str')
+    hprd_processor = HprdProcessor(id_df, cplx_df)
+    return hprd_processor
+
+if __name__ == '__main__':
+    from os.path import join
+    pd.set_option('display.max_columns', 15)
+    pd.set_option('expand_frame_repr', False)
+    hp = process_from_flat_files(
+                            join(hprd_path, 'HPRD_ID_MAPPINGS.txt'),
+                            join(hprd_path, 'PROTEIN_COMPLEXES.txt'))
+
--- indra/sources/hprd/processor.py
+++ indra/sources/hprd/processor.py
@@ -1,13 +1,76 @@
-from indra.statements import Complex, Agent
-from indra.sources.hprd.api import HprdComplexRow
+import logging
+from indra.databases import hgnc_client
+from indra.statements import Complex, Agent, Evidence
+
+
+logger = logging.getLogger('hprd')
+
 
 class HprdProcessor(object):
-    def __init__(self, hprd_rows):
-        hprd_complex_rows = [r for r in hprd_rows
-                             if isinstance(r, HprdComplexRow)]
-        self.statements = []
-        self.get_complexes(hprd_complex_rows)
+    """Get INDRA Statements from HPRD data row objects.
 
-    def get_complexes(self, hprd_complex_rows):
-        stmt = Complex([Agent('ASCL1'), Agent('TCF3'), Agent('MEF2C')])
-        self.statements.append(stmt)
+    Parameters
+    ----------
+
+    Attributes
+    ----------
+    statements : list of INDRA Statements
+    """
+
+    def __init__(self, id_df, cplx_df=None, phospho_df=None):
+        if cplx_df is None and phospho_df is None:
+            raise ValueError('At least one of cplx_df or phospho_df must be '
+                             'specified.')
+        self.statements = []
+        self.id_df = id_df
+        if cplx_df is not None:
+            self.get_complexes(cplx_df)
+
+
+    def get_complexes(self, cplx_df):
+        # Bring the ID information from the ID table into the dataframe of
+        # complexes
+        cplx_id_join = cplx_df.join(self.id_df, 'HPRD_ID', how='left',
+                                    lsuffix='CPLX') 
+        for cplx_id, this_cplx in cplx_id_join.groupby('CPLX_ID'):
+            agents = []
+            evidence = []
+            for egid in this_cplx.EGID:
+                ag = _make_agent_from_egid(egid)
+                if ag is not None:
+                    agents.append(ag)
+            # Get info from first member of complex
+            row0 = this_cplx.iloc[0]
+            pmids = row0.PMIDS.split(';')
+            ev_types = row0.EVIDENCE
+            ev_list = []
+            for pmid in pmids:
+                ev = Evidence(source_api='hprd',
+                              source_id=_hprd_url(row0.HPRD_ID, 'interactions'),
+                              pmid=pmid)
+                ev_list.append(ev)
+            stmt = Complex(agents, evidence=ev_list)
+            self.statements.append(stmt)
+
+
+def _hprd_url(hprd_id, info_type):
+    if info_type == 'interactions':
+        return ('http://hprd.org/interactions?hprd_id=%s&isoform_id=%s_1'
+                '&isoform_name=' % (hprd_id, hprd_id))
+
+
+def _make_agent_from_egid(egid):
+    # Make sure the EGID is a string
+    egid = str(egid)
+    hgnc_id = hgnc_client.get_hgnc_from_entrez(egid)
+    if not hgnc_id:
+        return None
+    hgnc_name = hgnc_client.get_hgnc_name(hgnc_id)
+    assert hgnc_name is not None
+    up_id = hgnc_client.get_uniprot_id(hgnc_id)
+    if not up_id:
+        logger.info("Skipping entry for gene %s with no Uniprot ID" %
+                    hgnc_name)
+    db_refs = {'HGNC': hgnc_id, 'UP': up_id, 'EGID': egid}
+    return Agent(hgnc_name, db_refs=db_refs)
+
