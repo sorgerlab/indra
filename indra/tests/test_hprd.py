ef9d7cd3d3e165d416ecccc6c6e4668bc46a06e4
John Bachman <bachmanjohn@gmail.com>
2019-02-16 20:54:40 -0500

    Add test for binary PPIs

--- indra/tests/test_hprd.py
+++ indra/tests/test_hprd.py
@@ -62,6 +62,33 @@ def test_process_ptms():
     assert s0.evidence[0].annotations['site_motif'] == \
                     {'motif': 'NFSSSPSTPVGSPQG', 'respos': 8}
 
+
+def test_process_ppis():
+    ppi_file = join(test_dir, 'BINARY_PROTEIN_PROTEIN_INTERACTIONS.txt')
+    hp = hprd.process_flat_files(id_file, ppi_file=ppi_file)
+    assert isinstance(hp, hprd.HprdProcessor)
+    assert isinstance(hp.statements, list)
+    assert len(hp.statements) == 5
+    s0 = hp.statements[0]
+    assert isinstance(s0, Complex)
+    assert len(s0.members) == 2
+    assert set([ag.name for ag in s0.members]) == set(['ITGA7', 'CHRNA1'])
+    assert s0.members[0].db_refs == \
+            {'HGNC': '6143', 'UP': 'Q13683', 'EGID': '3679',
+             'REFSEQ_PROT': 'NP_001138468.1'}
+    assert s0.members[1].db_refs == \
+            {'HGNC': '1955', 'UP': 'P02708', 'EGID': '1134',
+             'REFSEQ_PROT': 'NP_001034612.1'}
+    assert len(s0.evidence) == 1
+    assert s0.evidence[0].pmid == '10910772'
+    assert s0.evidence[0].source_api == 'hprd'
+    assert s0.evidence[0].annotations['evidence'] == ['in vivo']
+    assert s0.evidence[0].source_id == ('http://hprd.org/interactions?'
+                                     'hprd_id=02761&isoform_id=02761_1'
+                                     '&isoform_name=')
+
+
+
 @raises(ValueError)
 def test_process_ptms_no_seq():
     ptm_file = join(test_dir, 'POST_TRANSLATIONAL_MODIFICATIONS.txt')
