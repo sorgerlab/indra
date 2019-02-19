92d23c145c450f8ff0345aa6a5ea000c856e764d
Ben Gyori <ben.gyori@gmail.com>
2019-02-18 20:54:17 -0500

    Add citation to TAS docs

--- indra/sources/tas/__init__.py
+++ indra/sources/tas/__init__.py
@@ -1,6 +1,11 @@
 """This module provides and API and processor to the Target Affinity Spectrum
 data set compiled by N. Moret in the Laboratory of Systems Pharmacology at HMS.
 This data set is based on experiments as opposed to the manually curated
-drug-target relationships provided in the LINCS small molecule dataset."""
+drug-target relationships provided in the LINCS small molecule dataset.
+
+Moret, N., et al. (2018). Cheminformatics tools for analyzing and designing
+optimized small molecule libraries. BioRxiv, (617), 358978.
+https://doi.org/10.1101/358978
+"""
 
 from .api import *
