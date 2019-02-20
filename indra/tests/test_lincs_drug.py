986d4d45994688df429d51e8702047a08f0e2946
Ben Gyori <ben.gyori@gmail.com>
2019-02-20 01:44:59 -0500

    Skip LINCS webservice test due to reliability

--- indra/tests/test_lincs_drug.py
+++ indra/tests/test_lincs_drug.py
@@ -1,11 +1,13 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
+import unittest
 from nose.plugins.attrib import attr
 from indra.databases.lincs_client import get_drug_target_data
 from indra.sources.lincs_drug import process_from_web
 
 
 @attr('webservice')
+@unittest.skip('LINCS web service very unreliable.')
 def test_process_from_web():
     lincs_p = process_from_web()
     assert lincs_p is not None
