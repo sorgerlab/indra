8766147067a4db0fe2c4a5cce9d21240a0bd9fd6
John Bachman <bachmanjohn@gmail.com>
2019-03-12 15:18:08 -0400

    Additional tests using medscan terms

--- indra/tests/test_mesh.py
+++ indra/tests/test_mesh.py
@@ -63,3 +63,15 @@ def test_mesh_term_name_norm():
     assert mesh_name == 'Cellular Senescence'
 
 
+def test_mesh_term_lookups():
+    queries = {'Breast Cancer': ('D001943', 'Breast Neoplasms'),
+               'Neoplasms': ('D009369', 'Neoplasms'),
+               'Colorectal Cancer': ('D015179', 'Colorectal Neoplasms'),
+               'Intestinal Neoplasms': ('D007414', 'Intestinal Neoplasms'),
+               'Carcinoma, Non-Small-Cell Lung':
+                                ('D002289', 'Carcinoma, Non-Small-Cell Lung'),
+               'Prostate Cancer': ('D011471', 'Prostatic Neoplasms')}
+    for query_term, (correct_id, correct_name) in queries.items():
+        mesh_id, mesh_name = mesh_client.get_mesh_id_name(query_term)
+        assert mesh_id == correct_id
+        assert mesh_name == correct_name
