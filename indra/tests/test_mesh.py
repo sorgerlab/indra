8c00a3f0ae40363a446eb1e86274d509c69876f0
John Bachman <bachmanjohn@gmail.com>
2019-03-12 15:17:44 -0400

    Implementation of mesh lookup from REST API

--- indra/tests/test_mesh.py
+++ indra/tests/test_mesh.py
@@ -30,19 +30,36 @@ def test_mesh_id_fallback_to_rest():
     assert mesh_name == 'Ofloxacin'
 
 
-def test_mesh_name_lookup_local():
-    mesh_name = 'Glucosylceramides'
-    mesh_id = mesh_client.get_mesh_id(mesh_name, offline=True)
+def test_mesh_term_lookup_local():
+    mesh_term = 'Glucosylceramides'
+    (mesh_id, mesh_name) = mesh_client.get_mesh_id_name(mesh_term, offline=True)
     assert mesh_id == 'D005963'
+    assert mesh_name == mesh_term
 
 
-def test_mesh_name_lookup_local():
-    mesh_name = 'Glucosylceramides'
-    mesh_id = mesh_client.get_mesh_id(mesh_name, offline=True)
-    assert mesh_id == 'D005963'
-
-
-def test_mesh_name_local_missing():
-    mesh_name = 'Prostate Cancer'
-    mesh_id = mesh_client.get_mesh_id(mesh_name, offline=True)
+def test_mesh_term_local_missing():
+    mesh_term = 'Prostate Cancer'
+    mesh_id, mesh_name = mesh_client.get_mesh_id_name(mesh_term, offline=True)
     assert mesh_id is None
+    assert mesh_name is None
+
+
+def test_mesh_term_name_norm():
+    # For this one, the corresponding descriptor is D016922, which is in the
+    # INDRA resource file; however, the descriptor name is "Cellular
+    # Senescence".  This test verifies the expected behavior that in
+    # offline-only mode, "Cellular Senescence" will return the correct
+    # descriptor ID, but "Cell Aging" will not, unless using the REST service.
+    query_name = 'Cellular Senescence'
+    mesh_id, mesh_name = mesh_client.get_mesh_id_name(query_name, offline=True)
+    assert mesh_id == 'D016922'
+    assert mesh_name == query_name
+    query_name = 'Cell Aging'
+    mesh_id, mesh_name = mesh_client.get_mesh_id_name(query_name, offline=True)
+    assert mesh_id is None
+    assert mesh_name is None
+    mesh_id, mesh_name = mesh_client.get_mesh_id_name(query_name, offline=False)
+    assert mesh_id == 'D016922'
+    assert mesh_name == 'Cellular Senescence'
+
+
