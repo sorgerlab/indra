d76f0f158cfc920d522d8ad640d46a6d6c2de5b4
Ben Gyori <ben.gyori@gmail.com>
2019-03-13 08:50:57 +0900

    Move docs to module level and add to README

--- indra/sources/hprd/api.py
+++ indra/sources/hprd/api.py
@@ -1,28 +1,3 @@
-"""
-Get content from HPRD as INDRA Statements.
-
-Processes post-translational modifications, protein complexes, and (binary)
-protein-protein interactions from the Human Protein Reference Database, a
-curated protein data resource.
-
-More information about HPRD can be obtained at
-`http://www.hprd.org <http://www.hprd.org>`_ and in these publications:
-
-* Peri, S. et al. (2003). Development of Human Protein Reference Database as an
-  initial platform for approaching systems biology in humans. Genome Research.
-  13, 2363-2371.
-* Prasad, T. S. K. et al. (2009). Human Protein Reference Database - 2009
-  Update. Nucleic Acids Research. 37, D767-72.
-
-Data from the final release of HPRD (version 9) can be obtained at the
-following URLs:
-
-* http://www.hprd.org/RELEASE9/HPRD_FLAT_FILES_041310.tar.gz (text files)
-* http://www.hprd.org/RELEASE9/HPRD_XML_041310.tar.gz (XML)
-
-This module is designed to process the text files obtained from the first
-link listed above.
-"""
 import pandas as pd
 from collections import namedtuple
 from protmapper.uniprot_client import load_fasta_sequences
