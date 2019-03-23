e151466d2bf2a5843d683cada3c3d2563c1ab9d5
Ben Gyori <ben.gyori@gmail.com>
2019-03-23 00:20:07 -0400

    Add test for basic consistency

---
+++ indra/tests/test_trrust.py
@@ -0,0 +1,16 @@
+from nose.plugins.attrib import attr
+from indra.sources import trrust
+from indra.statements import RegulateAmount
+
+
+@attr('slow', 'webservice')
+def test_process_from_web():
+    tp = trrust.process_from_web()
+    assert len(tp.statements) > 6200
+    for stmt in tp.statements:
+        assert isinstance(stmt, RegulateAmount)
+        assert len(stmt.evidence) == 1
+        assert stmt.obj.db_refs.get('HGNC')
+        assert stmt.subj.db_refs.get('HGNC')
+        assert stmt.evidence[0].source_api == 'trrust'
+        assert stmt.evidence[0].pmid is not None
