783d4393d21092eeb2e24a162a62c9b6a943b7b7
Ben Gyori <ben.gyori@gmail.com>
2019-04-03 14:19:45 -0400

    Refactor doc pre-processing into separate class

--- indra/sources/eidos/processor.py
+++ indra/sources/eidos/processor.py
@@ -1,12 +1,9 @@
-from __future__ import absolute_import, print_function, unicode_literals
-from builtins import dict, str
 import re
 import logging
 import datetime
 import objectpath
 from indra.statements import Influence, Association, Concept, Evidence, \
     WorldContext, TimeContext, RefContext
-
 
 logger = logging.getLogger(__name__)
 
@@ -21,90 +18,34 @@ class EidosProcessor(object):
 
     Attributes
     ----------
-    tree : objectpath.Tree
-        The objectpath Tree object representing the extractions.
     statements : list[indra.statements.Statement]
         A list of INDRA Statements that were extracted by the processor.
     """
     def __init__(self, json_dict):
-        self.tree = objectpath.Tree(json_dict)
+        self.doc = EidosDocument(json_dict)
         self.statements = []
-        self.extractions = []
-        self.sentences = {}
-        self.entities = {}
-        self.documents = {}
-        self.coreferences = {}
-        self.timexes = {}
-        self.geolocs = {}
-        self.dct = None
-        self._preprocess_extractions()
-
-    def _preprocess_extractions(self):
-        extractions = \
-            self.tree.execute("$.extractions[(@.@type is 'Extraction')]")
-        if not extractions:
-            return
-        # Listify for multiple reuse
-        self.extractions = list(extractions)
-
-        # Build a dictionary of entities
-        entities = [e for e in self.extractions if 'Concept' in
-                    e.get('labels', [])]
-        self.entities = {entity['@id']: entity for entity in entities}
-
-        # Build a dictionary of sentences and document creation times (DCTs)
-        documents = self.tree.execute("$.documents[(@.@type is 'Document')]")
-        self.sentences = {}
-        for document in documents:
-            dct = document.get('dct')
-            title = document.get('title')
-            self.documents[document['@id']] = {'title': title}
-            # We stash the DCT here as a TimeContext object
-            if dct is not None:
-                self.dct = self.time_context_from_dct(dct)
-                self.timexes[dct['@id']] = self.dct
-            sentences = document.get('sentences', [])
-            for sent in sentences:
-                self.sentences[sent['@id']] = sent
-                timexes = sent.get('timexes')
-                if timexes:
-                    for timex in timexes:
-                        tc = self.time_context_from_timex(timex)
-                        self.timexes[timex['@id']] = tc
-                geolocs = sent.get('geolocs')
-                if geolocs:
-                    for geoloc in geolocs:
-                        rc = self.ref_context_from_geoloc(geoloc)
-                        self.geolocs[geoloc['@id']] = rc
-
-        # Build a dictionary of coreferences
-        for extraction in self.extractions:
-            if 'Coreference' in extraction['labels']:
-                reference = self.find_arg(extraction, 'reference')
-                anchor = self.find_arg(extraction, 'anchor')
-                self.coreferences[reference] = anchor
 
     def get_causal_relations(self):
         """Extract causal relations as Statements."""
         # Get the events that are labeled as directed and causal
-        events = [e for e in self.extractions if
+        events = [e for e in self.doc.extractions if
                   'DirectedRelation' in e['labels'] and
                   'Causal' in e['labels']]
         for event in events:
             # For now, just take the first source and first destination.
             # Later, might deal with hypergraph representation.
-            subj_id = self.find_arg(event, 'source')
-            obj_id = self.find_arg(event, 'destination')
+            subj_id = find_arg(event, 'source')
+            obj_id = find_arg(event, 'destination')
             if subj_id is None or obj_id is None:
                 continue
 
             # Resolve coreferences by ID
-            subj_id = self.coreferences.get(subj_id, subj_id)
-            obj_id = self.coreferences.get(obj_id, obj_id)
+            subj_id = self.doc.coreferences.get(subj_id, subj_id)
+            obj_id = self.doc.coreferences.get(obj_id, obj_id)
 
             # Get the actual entities
-            subj = self.entities[subj_id]
-            obj = self.entities[obj_id]
+            subj = self.doc.entities[subj_id]
+            obj = self.doc.entities[obj_id]
 
             subj_delta = self.extract_entity_states(subj.get('states', []))
             obj_delta = self.extract_entity_states(obj.get('states', []))
@@ -141,22 +82,22 @@ class EidosProcessor(object):
             self.statements.append(st)
 
     def get_correlations(self):
-        events = [e for e in self.extractions if
+        events = [e for e in self.doc.extractions if
                   'UndirectedRelation' in e['labels'] and
                   'Correlation' in e['labels']]
         for event in events:
             # For now, just take the first source and first destination.
             # Later, might deal with hypergraph representation.
-            arg_ids = self.find_args(event, 'argument')
+            arg_ids = find_args(event, 'argument')
             if len(arg_ids) != 2:
                 logger.warning('Skipping correlation with not 2 arguments.')
 
             # Resolve coreferences by ID
-            arg_ids = [self.coreferences.get(arg_id, arg_id)
+            arg_ids = [self.doc.coreferences.get(arg_id, arg_id)
                        for arg_id in arg_ids]
 
             # Get the actual entities
-            args = [self.entities[arg_id] for arg_id in arg_ids]
+            args = [self.doc.entities[arg_id] for arg_id in arg_ids]
             # Make Concepts from the entities
             members = [self.get_concept(arg) for arg in args]
             # Get the evidence
@@ -176,7 +117,7 @@ class EidosProcessor(object):
             sentence_tag = provenance[0].get('sentence')
             if sentence_tag and '@id' in sentence_tag:
                 sentence_id = sentence_tag['@id']
-                sentence = self.sentences.get(sentence_id)
+                sentence = self.doc.sentences.get(sentence_id)
                 if sentence is not None:
                     text = _sanitize(sentence['text'])
                 # Get temporal constraints if available
@@ -184,13 +125,13 @@ class EidosProcessor(object):
                 if timexes:
                     # We currently handle just one timex per statement
                     timex = timexes[0]
-                    tc = self.time_context_from_timex(timex)
+                    tc = time_context_from_timex(timex)
                     context = WorldContext(time=tc)
                 # Get geolocation if available
                 geolocs = sentence.get('geolocs', [])
                 if geolocs:
                     geoloc = geolocs[0]
-                    rc = self.ref_context_from_geoloc(geoloc)
+                    rc = ref_context_from_geoloc(geoloc)
                     if context:
                         context.geo_location = rc
                     else:
@@ -200,14 +141,14 @@ class EidosProcessor(object):
             # in the provenance
             doc_id = provenance[0].get('document', {}).get('@id')
             if doc_id:
-                title = self.documents.get(doc_id, {}).get('title')
+                title = self.doc.documents.get(doc_id, {}).get('title')
                 if title:
                     provenance[0]['document']['title'] = title
 
         annotations = {'found_by': event.get('rule'),
                        'provenance': provenance}
-        if self.dct is not None:
-            annotations['document_creation_time'] = self.dct.to_json()
+        if self.doc.dct is not None:
+            annotations['document_creation_time'] = self.doc.dct.to_json()
 
         epistemics = {}
         negations = self.get_negation(event)
@@ -333,24 +274,86 @@ class EidosProcessor(object):
         concept = Concept(name, db_refs=db_refs)
         return concept
 
-    @staticmethod
-    def find_arg(event, arg_type):
-        """Return ID of the first argument of a given type"""
-        obj_ids = EidosProcessor.find_args(event, arg_type)
-        if not obj_ids:
-            return None
-        else:
-            return obj_ids[0]
+    def time_context_from_ref(self, timex):
+        """Return a time context object given a timex reference entry."""
+        # If the timex has a value set, it means that it refers to a DCT or
+        # a TimeExpression e.g. "value": {"@id": "_:DCT_1"} and the parameters
+        # need to be taken from there
+        value = timex.get('value')
+        if value:
+            # Here we get the TimeContext directly from the stashed DCT
+            # dictionary
+            tc = self.doc.timexes.get(value['@id'])
+            return tc
+        return None
 
-    @staticmethod
-    def find_args(event, arg_type):
-        """Return IDs of all arguments of a given type"""
-        args = event.get('arguments', {})
-        obj_tags = [arg for arg in args if arg['type'] == arg_type]
-        if obj_tags:
-            return [o['value']['@id'] for o in obj_tags]
-        else:
-            return []
+    def geo_context_from_ref(self, ref):
+        """Return a ref context object given a location reference entry."""
+        value = ref.get('value')
+        if value:
+            # Here we get the RefContext from the stashed geoloc dictionary
+            rc = self.doc.geolocs.get(value['@id'])
+            return rc
+        return None
+
+
+class EidosDocument(object):
+    def __init__(self, json_dict):
+        self.tree = objectpath.Tree(json_dict)
+        self.extractions = []
+        self.sentences = {}
+        self.entities = {}
+        self.documents = {}
+        self.coreferences = {}
+        self.timexes = {}
+        self.geolocs = {}
+        self.dct = None
+        self._preprocess_extractions()
+
+    def _preprocess_extractions(self):
+        extractions = \
+            self.tree.execute("$.extractions[(@.@type is 'Extraction')]")
+        if not extractions:
+            return
+        # Listify for multiple reuse
+        self.extractions = list(extractions)
+
+        # Build a dictionary of entities
+        entities = [e for e in self.extractions if 'Concept' in
+                    e.get('labels', [])]
+        self.entities = {entity['@id']: entity for entity in entities}
+
+        # Build a dictionary of sentences and document creation times (DCTs)
+        documents = self.tree.execute("$.documents[(@.@type is 'Document')]")
+        self.sentences = {}
+        for document in documents:
+            dct = document.get('dct')
+            title = document.get('title')
+            self.documents[document['@id']] = {'title': title}
+            # We stash the DCT here as a TimeContext object
+            if dct is not None:
+                self.dct = self.time_context_from_dct(dct)
+                self.timexes[dct['@id']] = self.dct
+            sentences = document.get('sentences', [])
+            for sent in sentences:
+                self.sentences[sent['@id']] = sent
+                timexes = sent.get('timexes')
+                if timexes:
+                    for timex in timexes:
+                        tc = time_context_from_timex(timex)
+                        self.timexes[timex['@id']] = tc
+                geolocs = sent.get('geolocs')
+                if geolocs:
+                    for geoloc in geolocs:
+                        rc = ref_context_from_geoloc(geoloc)
+                        self.geolocs[geoloc['@id']] = rc
+
+        # Build a dictionary of coreferences
+        for extraction in self.extractions:
+            if 'Coreference' in extraction['labels']:
+                reference = find_arg(extraction, 'reference')
+                anchor = find_arg(extraction, 'anchor')
+                self.coreferences[reference] = anchor
 
     @staticmethod
     def time_context_from_dct(dct):
@@ -362,48 +365,6 @@ class EidosProcessor(object):
         tc = TimeContext(text=time_text, start=start, end=end,
                          duration=duration)
         return tc
-
-    def time_context_from_ref(self, timex):
-        """Return a time context object given a timex reference entry."""
-        # If the timex has a value set, it means that it refers to a DCT or
-        # a TimeExpression e.g. "value": {"@id": "_:DCT_1"} and the parameters
-        # need to be taken from there
-        value = timex.get('value')
-        if value:
-            # Here we get the TimeContext directly from the stashed DCT
-            # dictionary
-            tc = self.timexes.get(value['@id'])
-            return tc
-        return None
-
-    def geo_context_from_ref(self, ref):
-        """Return a ref context object given a location reference entry."""
-        value = ref.get('value')
-        if value:
-            # Here we get the RefContext from the stashed geoloc dictionary
-            rc = self.geolocs.get(value['@id'])
-            return rc
-        return None
-
-    @staticmethod
-    def time_context_from_timex(timex):
-        """Return a TimeContext object given a timex entry."""
-        time_text = timex.get('text')
-        constraint = timex['intervals'][0]
-        start = _get_time_stamp(constraint.get('start'))
-        end = _get_time_stamp(constraint.get('end'))
-        duration = constraint['duration']
-        tc = TimeContext(text=time_text, start=start, end=end,
-                         duration=duration)
-        return tc
-
-    @staticmethod
-    def ref_context_from_geoloc(geoloc):
-        """Return a RefContext object given a geoloc entry."""
-        text = geoloc.get('text')
-        geoid = geoloc.get('geoID')
-        rc = RefContext(name=text, db_refs={'GEOID': geoid})
-        return rc
 
 
 def _sanitize(text):
@@ -425,3 +386,42 @@ def _get_time_stamp(entry):
         logger.debug('Could not parse %s format' % entry)
         return None
     return dt
+
+
+def ref_context_from_geoloc(geoloc):
+    """Return a RefContext object given a geoloc entry."""
+    text = geoloc.get('text')
+    geoid = geoloc.get('geoID')
+    rc = RefContext(name=text, db_refs={'GEOID': geoid})
+    return rc
+
+
+def time_context_from_timex(timex):
+    """Return a TimeContext object given a timex entry."""
+    time_text = timex.get('text')
+    constraint = timex['intervals'][0]
+    start = _get_time_stamp(constraint.get('start'))
+    end = _get_time_stamp(constraint.get('end'))
+    duration = constraint['duration']
+    tc = TimeContext(text=time_text, start=start, end=end,
+                     duration=duration)
+    return tc
+
+
+def find_arg(event, arg_type):
+    """Return ID of the first argument of a given type"""
+    obj_ids = find_args(event, arg_type)
+    if not obj_ids:
+        return None
+    else:
+        return obj_ids[0]
+
+
+def find_args(event, arg_type):
+    """Return IDs of all arguments of a given type"""
+    args = event.get('arguments', {})
+    obj_tags = [arg for arg in args if arg['type'] == arg_type]
+    if obj_tags:
+        return [o['value']['@id'] for o in obj_tags]
+    else:
+        return []
