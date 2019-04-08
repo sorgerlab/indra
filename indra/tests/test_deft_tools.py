2df7c8b1885ffa37cdd80d7242a3db21a688a9c4
Albert Steppi <albertsteppi@Alberts-MacBook-Pro.local>
2019-04-08 14:34:56 -0400

    Update deft tool tests

--- indra/tests/test_deft_tools.py
+++ indra/tests/test_deft_tools.py
@@ -1,57 +1,54 @@
+import re
 from nose.plugins.attrib import attr
 
-from indra.literature.deft_tools import universal_extract_text
+from indra.literature.deft_tools import universal_extract_paragraphs, \
+    filter_paragraphs
 from indra.literature import pmc_client, elsevier_client, pubmed_client
 
 
 @attr('nonpublic', 'webservice')
-def test_universal_extract_text_elsevier():
+def test_universal_extract_paragraphs_elsevier():
     doi = '10.1016/B978-0-12-416673-8.00004-6'
     xml_str = elsevier_client.download_article(doi)
-    text = universal_extract_text(xml_str)
-    assert text is not None
-    assert ' ER ' in text
+    paragraphs = universal_extract_paragraphs(xml_str)
+    assert len(paragraphs) > 1
 
 
 @attr('webservice')
-def test_universal_extract_text_pmc():
+def test_universal_extract_paragraphs_pmc():
     pmc_id = 'PMC3262597'
     xml_str = pmc_client.get_xml(pmc_id)
-    text = universal_extract_text(xml_str)
-    assert text is not None
-    assert ' ER ' in text
+    paragraphs = universal_extract_paragraphs(xml_str)
+    assert len(paragraphs) > 1
 
 
 @attr('webservice')
-def test_universal_extract_text_abstract():
+def test_universal_extract_paragraphs_abstract():
     pmid = '16511588'
     abstract = pubmed_client.get_abstract(pmid)
-    result = universal_extract_text(abstract)
-    assert result == abstract + '\n'
+    result = universal_extract_paragraphs(abstract)
+    assert result[0] == abstract
 
 
-@attr('webservice')
-def test_universal_extract_text_contains():
-    pmc_id = 'PMC3262597'
-    xml_str = pmc_client.get_xml(pmc_id)
-    text1 = universal_extract_text(xml_str)
-    text2 = universal_extract_text(xml_str, contains='ER')
-    assert text1 is not None
-    assert text2 is not None
-    assert ' ER ' in text1 and ' ER ' in text2
-    assert len(text2) < len(text1)
+def test_universal_extract_texts_contains():
+    example = ['eeeeeeeeeeeeeEReeeeeeeeee',
+               'eeeeeee-ER-eeeeeeeeeeeeee',
+               'eeeeeee ER eeeeeeeeeeeeee',
+               'eeeeeeeER eeeeeeeeeeeeeee']
+    result = ('eeeeeee-ER-eeeeeeeeeeeeee\n'
+              'eeeeeee ER eeeeeeeeeeeeee\n')
+    text = filter_paragraphs(example, contains='ER')
+    assert text == result
 
 
-@attr('webservice')
-def test_universal_extract_text_contains_union():
-    pmc_id = 'PMC4954987'
-    xml_str = pmc_client.get_xml(pmc_id)
-    text1 = universal_extract_text(xml_str)
-    text2 = universal_extract_text(xml_str, contains='NP')
-    text3 = universal_extract_text(xml_str, contains='NPs')
-    text4 = universal_extract_text(xml_str, contains=['NP', 'NPs'])
-    assert text1 is not None
-    assert text2 is not None
-    assert text3 is not None
-    assert text4 is not None
-    assert len(text2) < len(text3) < len(text4) < len(text1)
+def test_universal_extract_texts_contains_union():
+    example = ['eeeeeeeeeeNPeeeeeeeeeeeeee',
+               'eeeeeee-NPs-eeeeeeeeeeeeee',
+               'eeeeeee NP eeeeeeeeeeeeeee',
+               'eeeeeeeNPseeeee NP-eeeeeee',
+               'eeeeeeeeeeeeeeeeeeeeeeeeee']
+    result = ('eeeeeee-NPs-eeeeeeeeeeeeee\n'
+              'eeeeeee NP eeeeeeeeeeeeeee\n'
+              'eeeeeeeNPseeeee NP-eeeeeee\n')
+    text = filter_paragraphs(example, contains=['NP', 'NPs'])
+    assert text == result
