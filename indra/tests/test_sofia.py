6fda59c2970fb94c7abcbc86c1bc6f71349454be
dianakolusheva <dianakolusheva@gmail.com>
2019-04-25 16:09:31 -0400

    Extend Sofia test file and tests to test Events

--- indra/tests/test_sofia.py
+++ indra/tests/test_sofia.py
@@ -6,6 +6,14 @@ import json
 from nose.plugins.attrib import attr
 
 from indra.sources import sofia
+from indra.statements.statements import Influence, Event
+from indra.statements.context import WorldContext
+
+
+# Tell nose to not run tests in the imported modules
+Influence.__test__ = False
+Event.__test__ = False
+WorldContext.__test__ = False
 
 
 @attr('webservice', 'nonpublic')
@@ -23,6 +31,13 @@ def test_process_json():
     with open(test_file, 'r') as fh:
         js = json.load(fh)
     sp = sofia.process_json(js)
-    assert len(sp.statements) == 1
+    assert len(sp.statements) == 2
+    assert isinstance(sp.statements[0], Influence)
     assert sp.statements[0].subj.concept.name == 'rainfall'
     assert sp.statements[0].obj.concept.name == 'floods'
+    assert len(sp.statements[0].evidence) == 1, len(sp.statements[0].evidence)
+    assert isinstance(sp.statements[1], Event)
+    assert sp.statements[1].concept.name == 'inflation'
+    assert isinstance(sp.statements[1].context, WorldContext)
+    assert sp.statements[1].context.time.text == '28, JULY, 2016'
+    assert sp.statements[1].context.geo_location.name == 'South Sudan'
