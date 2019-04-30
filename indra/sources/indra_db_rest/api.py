8b0200aaf671e8fd01ec2c154669526888608774
pagreene <patrick.anton.greene@gmail.com>
2019-04-30 15:06:30 -0400

    Add Conversion to the list.

--- indra/sources/indra_db_rest/api.py
+++ indra/sources/indra_db_rest/api.py
@@ -1,6 +1,6 @@
 from indra.util import clockit
 from indra.statements import stmts_from_json, Complex, SelfModification, \
-    ActiveForm, Translocation
+    ActiveForm, Translocation, Conversion
 
 from indra.sources.indra_db_rest.processor import IndraDBRestProcessor
 from indra.sources.indra_db_rest.util import submit_statement_request, \
@@ -256,7 +256,7 @@ def get_statement_queries(stmts, **params):
     queries = []
     url_base = get_url_base('statements/from_agents')
     non_binary_statements = [Complex, SelfModification, ActiveForm,
-                             Translocation]
+                             Translocation, Conversion]
     for stmt in stmts:
         kwargs = {}
         if type(stmt) not in non_binary_statements:
