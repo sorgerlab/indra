591bb061aafce8d5e4b6f1a15e58565630e80cc9
pagreene <patrick.anton.greene@gmail.com>
2019-06-04 14:10:19 -0400

    Create true test of basic liveliness.

--- indra/tests/test_rest_api.py
+++ indra/tests/test_rest_api.py
@@ -1,10 +1,35 @@
 import json
 import requests
+from datetime import datetime
+
 from nose.plugins.attrib import attr
+
+BASE = 'http://api.indra.bio:8000/'
+
+
+def _call_api(method, route, *args, **kwargs):
+    route = route.lstrip('/')
+    req_meth = getattr(requests, method)
+    start = datetime.now()
+    print("Submitting request to '%s' at %s." % ('/' + route, start))
+    print("\targs:", args)
+    print("\tkwargs:", kwargs)
+    res = req_meth(BASE + route, *args, **kwargs)
+    end = datetime.now()
+    print("Got result with %s at %s after %s seconds."
+          % (res.status_code, end, (end-start).total_seconds()))
+    assert res.status_code == 200, res.status_code
+    return res
 
 
 @attr('webservice')
-def test_rest_api_responsive():
+def test_responsive():
+    res = _call_api('get', '')
+    assert res.content.startswith(b'This is the INDRA REST API.')
+
+
+@attr('webservice')
+def test_assemblers_cyjs():
     stmt_json = {
         "statements": [{
             "sbo": "http://identifiers.org/sbo/SBO:0000526",
@@ -18,7 +43,8 @@ def test_rest_api_responsive():
         }]
     }
     stmt_str = json.dumps(stmt_json)
-    url = 'http://api.indra.bio:8000/' + \
-        'assemblers/cyjs'
-    res = requests.post(url, stmt_str)
-    assert res.status_code == 200
+    res = _call_api('post', 'assemblers/cyjs', stmt_str)
+    res_json = res.json()
+    assert len(res_json['edges']) == 1, len(res_json['edges'])
+    assert len(res_json['nodes']) == 2, len(res_json['nodes'])
+    return
