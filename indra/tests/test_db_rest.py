87cc17c50a6441c06ebc6c5917864330fcc835cb
pagreene <patrick.anton.greene@gmail.com>
2019-07-09 10:20:23 -0400

    Allow for removal of restricted hash.

--- indra/tests/test_db_rest.py
+++ indra/tests/test_db_rest.py
@@ -169,7 +169,7 @@ def test_get_statements_by_hash():
     hash_list = [30674674032092136, -22289282229858243, -25056605420392180]
     stmts = dbr.get_statements_by_hash(hash_list)
     print({s.get_hash(shallow=True): s for s in stmts})
-    assert len(stmts) == len(hash_list), \
+    assert len(stmts) >= 2, \
         "Wrong number of statements: %s vs. %s" % (len(stmts), len(hash_list))
     return
 
