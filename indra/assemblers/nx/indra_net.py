60f0514959feb0266509151d96eecee81ca14b45
dianakolusheva <dianakolusheva@gmail.com>
2019-07-17 15:13:46 -0400

    Start creating IndraNet from df

--- indra/assemblers/nx/indra_net.py
+++ indra/assemblers/nx/indra_net.py
@@ -29,12 +29,21 @@ class IndraNet(nx.MultiDiGraph):
         :param multi:
         :return:
         """
-        if not {'agA_name', 'agB_name', 'agA_ns', 'agA_id', 'agB_ns', 'agB_id',
-                'stmt_type', 'evidence_count', 'hash', 'belief',
-                'evidence'}.issubset(
-             set(df.columns)):
+        mandatory_columns = ['agA_name', 'agB_name', 'agA_ns', 'agA_id',
+                             'agB_ns', 'agB_id', 'stmt_type', 'evidence_count',
+                             'hash', 'belief', 'evidence']
+        if not set(mandatory_columns).issubset(set(df.columns)):
             raise ValueError('Missing required columns in data frame')
-
+        node_keys = {'agA': set(), 'agB': set()}
+        edge_keys = set()
+        for key in df.columns:
+            if key not in mandatory_columns:
+                if key.startswith('agA_'):
+                    node_keys['agA'].add(key)
+                if key.startswith('agB_'):
+                    node_keys['agB'].add(key)
+                if not key.startswith('ag'):
+                    edge_keys.add(key)
         index = 0
         skipped = 0
         for index, row in df.iterrows():
@@ -42,7 +51,21 @@ class IndraNet(nx.MultiDiGraph):
                 skipped += 1
                 logger.warning('None found as node (index %d)' % index)
                 continue
-            pass
+            # Add non-existing nodes
+            if row['agA_name'] not in cls.nodes:
+                cls._add_node(row['agA_name'], row['agA_ns'], row['agA_id'])
+            if row['agB_name'] not in cls.nodes:
+                cls._add_node(row['agB_name'], row['agB_ns'], row['agB_id'])
+            # Add edges
+            ed = {'u': row['agA_name'],
+                  'v': row['agB_name'],
+                  'stmt_type': row['stmt_type'],
+                  'evidence_count': row['evidence_count'],
+                  'evidence': row['evidence'],
+                  'belief': row['belief']}
+            cls._add_edge(**ed)
+        if skipped:
+            logger.warning('Skipped %d edges with None as node' % skipped)
         return cls
 
     @classmethod
