8fa1abf73b5fbaa8d7a9a05b8c4868c813ac63bc
dianakolusheva <dianakolusheva@gmail.com>
2019-07-23 15:39:09 -0400

    Update IndraNetAssembler tests

--- indra/tests/test_indranet_assembler.py
+++ indra/tests/test_indranet_assembler.py
@@ -14,11 +14,6 @@ st3 = Activation(Agent('b'), Agent('d'))
 st4 = ActiveForm(Agent('e'), None, True)  # 1 agent
 st5 = Complex([Agent('c'), Agent('f'), Agent('g')])
 st6 = Complex([Agent('h'), Agent('i'), Agent('j'), Agent('b')])
-hash_ab = st1.get_hash()
-hash_ac = st2.get_hash()
-hash_bd = st3.get_hash()
-hash_fg = st5.get_hash()
-hash_hi = st6.get_hash()
 
 
 def test_simple_assembly():
@@ -30,17 +25,17 @@ def test_simple_assembly():
     # Stmt with 1 agent should not be added
     assert 'e' not in g.nodes
     # Complex with more than 3 agents should not be added
-    assert ('f', 'g', hash_fg) in g.edges
-    assert ('h', 'i', hash_hi) not in g.edges
+    assert ('f', 'g', 0) in g.edges
+    assert ('h', 'i', 0) not in g.edges
     # Test node attributes
     assert g.nodes['a']['ns'] == 'HGNC', g.nodes['a']['ns']
     assert g.nodes['a']['id'] == 1
     # Test edge attributes
-    e = g['a']['c'][hash_ac]
+    e = g['a']['c'][0]
     assert e['stmt_type'] == 'Inhibition'
     assert e['belief'] == 0.76
     assert e['evidence_count'] == 3
-    assert g['b']['d'][hash_bd]['evidence_count'] == 0
+    assert g['b']['d'][0]['evidence_count'] == 0
 
 
 def test_exclude_stmts():
@@ -49,7 +44,7 @@ def test_exclude_stmts():
     assert len(g.nodes) == 3
     assert len(g.edges) == 2
     assert 'c' not in g.nodes
-    assert ('a', 'c', hash_ac) not in g.edges
+    assert ('a', 'c', 0) not in g.edges
 
 
 def test_complex_members():
@@ -57,8 +52,8 @@ def test_complex_members():
     g = ia.make_model(complex_members=4)
     assert len(g.nodes) == 5
     assert len(g.edges) == 13, len(g.edges)
-    assert ('h', 'i', hash_hi) in g.edges
-    assert ('i', 'h', hash_hi) in g.edges
+    assert ('h', 'i', 0) in g.edges
+    assert ('i', 'h', 0) in g.edges
 
 
 def test_make_df():
@@ -68,4 +63,4 @@ def test_make_df():
     assert len(df) == 9
     assert set(df.columns) == {
         'agA_name', 'agB_name', 'agA_ns', 'agA_id', 'agB_ns', 'agB_id',
-        'stmt_type', 'evidence_count', 'hash', 'belief'}
+        'stmt_type', 'evidence_count', 'stmt_hash', 'belief'}
