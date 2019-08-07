2876e1fedca9052239b2c16b06cd4cd975bf3cf7
dianakolusheva <dianakolusheva@gmail.com>
2019-08-07 12:50:15 -0400

    Add methods to flatten IndraNet

--- indra/assemblers/indranet/net.py
+++ indra/assemblers/indranet/net.py
@@ -1,11 +1,15 @@
-from networkx import MultiDiGraph
+import networkx as nx
 import pandas as pd
 import logging
 
 logger = logging.getLogger(__name__)
+default_sign_dict = {'Activation': 0,
+                     'Inhibition': 1,
+                     'IncreaseAmount': 0,
+                     'DecreaseAmount': 1}
 
 
-class IndraNet(MultiDiGraph):
+class IndraNet(nx.MultiDiGraph):
     """A Networkx representation of INDRA Statements."""
     def __init__(self, incoming_graph_data=None, **attr):
         super().__init__(incoming_graph_data, **attr)
@@ -92,3 +96,47 @@ class IndraNet(MultiDiGraph):
         if skipped:
             logger.warning('Skipped %d edges with None as node' % skipped)
         return graph
+
+    def to_digraph(self):
+        """Flatten the IndraNet to a DiGraph."""
+        G = nx.DiGraph()
+        for u, v, data in self.edges(data=True):
+            if G.has_edge(u, v):
+                G[u][v]['statements'].append(data)
+            else:
+                G.add_edge(u, v, statements=[data])
+        return G
+
+    def to_signed_graph(self, sign_dict=default_sign_dict):
+        """Flatten the IndraNet to a signed graph.
+        
+        Parameters
+        ----------
+        sign_dict : dict
+            A dictionary mapping a Statement type to a sign to be used for
+            the edge. By default only Activation and IncreaseAmount are added
+            as positive edges and Inhibition and DecreaseAmount are added as
+            negative edges, but a user can pass any other Statement types in
+            a dictionary."""
+        SG = nx.MultiDiGraph()
+        for u, v, data in self.edges(data=True):
+            if data['stmt_type'] not in sign_dict:
+                continue
+            sign = sign_dict[data['stmt_type']]
+            if SG.has_edge(u, v, sign):
+                SG[u][v][sign]['statements'].append(data)
+            else:
+                SG.add_edge(u, v, sign, statements=[data], sign=sign)
+        return SG
+
+    @classmethod
+    def digraph_from_df(cls, df):
+        """Create a digraph from a pandas DataFrame."""
+        net = cls.from_df(df)
+        return net.to_digraph()
+
+    @classmethod
+    def signed_from_df(cls, df, sign_dict=default_sign_dict):
+        """Create a signed graph from a pandas DataFrame."""
+        net = cls.from_df(df)
+        return net.to_signed_graph(sign_dict=sign_dict)
