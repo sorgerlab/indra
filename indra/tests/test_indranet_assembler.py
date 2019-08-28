7130d75b69e16c2781889715faeb120f38e67a48
dianakolusheva <dianakolusheva@gmail.com>
2019-08-28 15:46:08 -0400

    Add test for initial signs from df

--- indra/tests/test_indranet_assembler.py
+++ indra/tests/test_indranet_assembler.py
@@ -67,7 +67,8 @@ def test_make_df():
     assert len(df) == 9
     assert set(df.columns) == {
         'agA_name', 'agB_name', 'agA_ns', 'agA_id', 'agB_ns', 'agB_id',
-        'stmt_type', 'evidence_count', 'stmt_hash', 'belief', 'source_counts'}
+        'stmt_type', 'evidence_count', 'stmt_hash', 'belief', 'source_counts',
+        'initial_sign'}
 
 
 # Test assembly from IndraNet directly
@@ -167,3 +168,25 @@ def _weight_mapping(G):
     for edge in G.edges:
         G.edges[edge]['weight'] = 1 - G.edges[edge]['belief']
     return G
+
+
+def test_initial_signs():
+    a = Event(Concept('a'), QualitativeDelta(polarity=1))
+    b = Event(Concept('b'), QualitativeDelta(polarity=1))
+    c = Event(Concept('c'), QualitativeDelta(polarity=-1))
+    d = Event(Concept('d'), QualitativeDelta(polarity=-1))
+    st1 = Influence(a, b)
+    st2 = Influence(b, c)
+    st3 = Influence(c, d)
+    st4 = Influence(b, d)
+    ia = IndraNetAssembler([st1, st2, st3, st4])
+    sg = ia.make_model(graph_type='signed')
+    assert len(sg.nodes) == 4
+    assert len(sg.edges) == 4
+    assert ('a', 'b', 0) in sg.edges
+    assert ('b', 'c', 0) not in sg.edges
+    assert ('b', 'c', 1) in sg.edges
+    assert ('c', 'd', 0) in sg.edges
+    assert ('c', 'd', 1) not in sg.edges
+    assert ('b', 'd', 0) not in sg.edges
+    assert ('b', 'd', 1) in sg.edges
