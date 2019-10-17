ad8bddbf77ac31c1ef53b174359be27bb4619e09
Ben Gyori <ben.gyori@gmail.com>
2019-10-17 15:20:56 -0400

    Bump version to 1.15.1

--- indra/__init__.py
+++ indra/__init__.py
@@ -2,7 +2,7 @@ from __future__ import print_function, unicode_literals
 import logging
 import os
 import sys
-__version__ = '1.15.0'
+__version__ = '1.15.1'
 
 __all__ = ['assemblers', 'belief', 'databases', 'explanation', 'literature',
            'mechlinker', 'preassembler', 'sources', 'tools', 'util']
--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ def main():
                                   for dep in deps})
 
     setup(name='indra',
-          version='1.15.0',
+          version='1.15.1',
           description='Integrated Network and Dynamical Reasoning Assembler',
           long_description=long_description,
           long_description_content_type='text/markdown',
