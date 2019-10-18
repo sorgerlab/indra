47d3593a6ea8e6164e36a2833e47e2debcd6317d
Ben Gyori <ben.gyori@gmail.com>
2019-10-18 11:36:32 -0400

    Add upper constraint on PySB version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(readme_path, 'r', encoding='utf-8') as fh:
 
 
 def main():
-    install_list = ['pysb>=1.3.0', 'objectpath', 'rdflib',
+    install_list = ['pysb>=1.3.0,<=1.9.1', 'objectpath', 'rdflib',
                     'requests>=2.11', 'lxml', 'ipython', 'future',
                     'networkx>=2,<=2.3', 'pandas', 'ndex2==2.0.1', 'jinja2',
                     'protmapper>=0.0.14']
