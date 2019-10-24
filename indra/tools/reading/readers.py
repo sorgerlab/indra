13c7d8ed9af7bc6f65c0ecbc0457ab04a9f7c5a0
pagreene <patrick.anton.greene@gmail.com>
2019-10-24 12:22:19 -0400

    Switch to using existing base_dir tree for ISI reading.

--- indra/tools/reading/readers.py
+++ indra/tools/reading/readers.py
@@ -730,20 +730,24 @@ class IsiReader(Reader):
 
     name = 'ISI'
 
-    def read(self, read_list, verbose=False, log=False, n_per_proc=None):
-        # Define some temp directories
-        nxml_tmp = tempfile.mkdtemp('isi_nxml')
-        inp_tmp = tempfile.mkdtemp('isi_input')
-        out_tmp = tempfile.mkdtemp('isi_output')
-        tmp_tmp = tempfile.mkdtemp('isi_temp')
+    def __init__(self, *args, **kwargs):
+        super(IsiReader, self).__init__(*args, **kwargs)
 
+        # Define some extra directories
+        self.nxml_dir = _get_dir(self.tmp_dir, 'nxmls')
+        self.isi_temp_dir = _get_dir(self.tmp_dir, 'temp')
+        self.output_dir = _get_dir(self.tmp_dir, 'output')
+
+        return
+
+    def read(self, read_list, verbose=False, log=False, n_per_proc=None):
         # Create a preprocessor
-        pp = IsiPreprocessor(inp_tmp)
+        pp = IsiPreprocessor(self.input_dir)
 
         # Preprocess all the content.
         for content in read_list:
             if content.is_format('nxml'):
-                content.copy_to(nxml_tmp)
+                content.copy_to(self.nxml_dir)
                 pp.preprocess_nxml_file(content.get_filepath(renew=True),
                                         content.get_id(), {})
             elif content.is_format('txt', 'text'):
@@ -754,13 +758,14 @@ class IsiReader(Reader):
                                  % content.get_format())
 
         # Run ISI
-        run_isi(inp_tmp, out_tmp, tmp_tmp, self.n_proc)
+        run_isi(self.input_dir, self.output_dir, self.isi_temp_dir,
+                self.n_proc)
 
         # Process the outputs
-        for fname, content_id, extra_annots in pp.iter_outputs(out_tmp):
+        for fname, cid, extra_annots in pp.iter_outputs(self.output_dir):
             with open(fname, 'rb') as f:
                 content = json.load(f)
-            self.add_result(content_id, content)
+            self.add_result(cid, content)
 
         return self.results
 
