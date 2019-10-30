59499e823425aafd01182da0f7b33306975b521f
Ben Gyori <ben.gyori@gmail.com>
2019-10-30 09:55:06 -0400

    Bump version to 1.15.2

--- indra/__init__.py
+++ indra/__init__.py
@@ -2,7 +2,7 @@ from __future__ import print_function, unicode_literals
 import logging
 import os
 import sys
-__version__ = '1.15.0'
+__version__ = '1.15.2'
 
 __all__ = ['assemblers', 'belief', 'databases', 'explanation', 'literature',
            'mechlinker', 'preassembler', 'sources', 'tools', 'util']
