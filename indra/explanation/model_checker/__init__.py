c633112d6336c37e15577eb6d035488cc42bfd59
kkaris <karis.klas@gmail.com>
2019-11-05 13:57:37 -0500

    Add get_path_iter to model_checker importables

--- indra/explanation/model_checker/__init__.py
+++ indra/explanation/model_checker/__init__.py
@@ -1,4 +1,4 @@
-from .model_checker import ModelChecker, PathResult, PathMetric
+from .model_checker import ModelChecker, PathResult, PathMetric, get_path_iter
 from .pysb import PysbModelChecker
 from .signed_graph import SignedGraphModelChecker
 from .unsigned_graph import UnsignedGraphModelChecker
