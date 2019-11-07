85c321de1c7ee5b8437026cfee3aeaf80ecb1a5a
kkaris <karis.klas@gmail.com>
2019-11-07 11:30:10 -0500

    Move model checker methods out as functions

--- indra/explanation/model_checker/model_checker.py
+++ indra/explanation/model_checker/model_checker.py
@@ -384,68 +384,6 @@ class ModelChecker(object):
         # There was no path; this will produce an empty generator
         return
 
-    def signed_edges_to_signed_nodes(self, graph, prune_nodes=True,
-                                     edge_signs={'pos': 0, 'neg': 1}):
-        """Convert a graph with signed edges to a graph with signed nodes.
-
-        Each pair of nodes linked by an edge in an input graph are represented
-        as four nodes and two edges in the new graph. For example, an edge (a,
-        b, 0), where a and b are nodes and 0 is a sign of an edge (positive),
-        will be represented as edges ((a, 0), (b, 0)) and ((a, 1), (b, 1)),
-        where (a, 0), (a, 1), (b, 0), (b, 1) are signed nodes. An edge (a, b,
-        1) with sign 1 (negative) will be represented as edges ((a, 0), (b,
-        1)) and ((a, 1), (b, 0)).
-
-        Parameters
-        ----------
-        graph : networkx.MultiDiGraph
-            Graph with signed edges to convert. Can have multiple edges between
-            a pair of nodes.
-        prune_nodes : Optional[bool]
-            If True, iteratively prunes negative (with sign 1) nodes without
-            predecessors.
-        edge_signs : dict
-            A dictionary representing the signing policy of incoming graph. The
-            dictionary should have strings 'pos' and 'neg' as keys and integers
-            as values.
-
-        Returns
-        -------
-        signed_nodes_graph : networkx.DiGraph
-        """
-        signed_nodes_graph = nx.DiGraph()
-        nodes = []
-        for node, node_data in graph.nodes(data=True):
-            nodes.append(((node, 0), node_data))
-            nodes.append(((node, 1), node_data))
-        signed_nodes_graph.add_nodes_from(nodes)
-        edges = set()
-        for u, v, edge_data in graph.edges(data=True):
-            edge_sign = edge_data.get('sign')
-            if edge_sign == edge_signs['pos']:
-                edges.add(((u, 0), (v, 0)))
-                edges.add(((u, 1), (v, 1)))
-            elif edge_sign == edge_signs['neg']:
-                edges.add(((u, 0), (v, 1)))
-                edges.add(((u, 1), (v, 0)))
-        signed_nodes_graph.add_edges_from(edges)
-        if prune_nodes:
-            signed_nodes_graph = self.prune_nodes(signed_nodes_graph)
-        return signed_nodes_graph
-
-    def prune_nodes(self, graph):
-        """Prune nodes with sign (1) if they do not have predecessors."""
-        nodes_to_prune = [node for node, in_deg
-                          in graph.in_degree()
-                          if in_deg == 0 and node[1] == 1]
-        while nodes_to_prune:
-            graph.remove_nodes_from(nodes_to_prune)
-            # Make a list of nodes whose in degree is now 0
-            nodes_to_prune = [node for node, in_deg
-                              in graph.in_degree()
-                              if in_deg == 0 and node[1] == 1]
-        return graph
-
     def make_false_result(self, result_code, max_paths, max_path_length):
         return PathResult(False, result_code, max_paths, max_path_length)
 
@@ -516,3 +454,67 @@ def get_path_iter(graph, source, target):
                 yield path
         except nx.NetworkXNoPath:
             pass
+
+
+def signed_edges_to_signed_nodes(graph, prune_nodes=True,
+                                 edge_signs={'pos': 0, 'neg': 1}):
+    """Convert a graph with signed edges to a graph with signed nodes.
+
+    Each pair of nodes linked by an edge in an input graph are represented
+    as four nodes and two edges in the new graph. For example, an edge (a,
+    b, 0), where a and b are nodes and 0 is a sign of an edge (positive),
+    will be represented as edges ((a, 0), (b, 0)) and ((a, 1), (b, 1)),
+    where (a, 0), (a, 1), (b, 0), (b, 1) are signed nodes. An edge (a, b,
+    1) with sign 1 (negative) will be represented as edges ((a, 0), (b,
+    1)) and ((a, 1), (b, 0)).
+
+    Parameters
+    ----------
+    graph : networkx.MultiDiGraph
+        Graph with signed edges to convert. Can have multiple edges between
+        a pair of nodes.
+    prune_nodes : Optional[bool]
+        If True, iteratively prunes negative (with sign 1) nodes without
+        predecessors.
+    edge_signs : dict
+        A dictionary representing the signing policy of incoming graph. The
+        dictionary should have strings 'pos' and 'neg' as keys and integers
+        as values.
+
+    Returns
+    -------
+    signed_nodes_graph : networkx.DiGraph
+    """
+    signed_nodes_graph = nx.DiGraph()
+    nodes = []
+    for node, node_data in graph.nodes(data=True):
+        nodes.append(((node, 0), node_data))
+        nodes.append(((node, 1), node_data))
+    signed_nodes_graph.add_nodes_from(nodes)
+    edges = set()
+    for u, v, edge_data in graph.edges(data=True):
+        edge_sign = edge_data.get('sign')
+        if edge_sign == edge_signs['pos']:
+            edges.add(((u, 0), (v, 0)))
+            edges.add(((u, 1), (v, 1)))
+        elif edge_sign == edge_signs['neg']:
+            edges.add(((u, 0), (v, 1)))
+            edges.add(((u, 1), (v, 0)))
+    signed_nodes_graph.add_edges_from(edges)
+    if prune_nodes:
+        signed_nodes_graph = prune_signed_nodes(signed_nodes_graph)
+    return signed_nodes_graph
+
+
+def prune_signed_nodes(graph):
+    """Prune nodes with sign (1) if they do not have predecessors."""
+    nodes_to_prune = [node for node, in_deg
+                      in graph.in_degree()
+                      if in_deg == 0 and node[1] == 1]
+    while nodes_to_prune:
+        graph.remove_nodes_from(nodes_to_prune)
+        # Make a list of nodes whose in degree is now 0
+        nodes_to_prune = [node for node, in_deg
+                          in graph.in_degree()
+                          if in_deg == 0 and node[1] == 1]
+    return graph
