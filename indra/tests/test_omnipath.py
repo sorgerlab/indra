b760c4a8fc5353d898d962071b2bcdaba571bd84
kkaris <karis.klas@gmail.com>
2019-11-14 16:23:45 -0500

    Add mods test

--- indra/tests/test_omnipath.py
+++ indra/tests/test_omnipath.py
@@ -18,11 +18,18 @@ def test_omnipath_web_api():
     res = requests.get(query_url)
     assert res.status_code == 200
 
-def test_query_ptms():
-    stmts = op.get_ptms(['Q13873'])
-    assert len(stmts) == 1
-    assert isinstance(stmts[0], Phosphorylation)
-    assert stmts[0].enz.name == 'CSNK2A1'
-    assert stmts[0].sub.name == 'BMPR2'
-    assert stmts[0].residue == 'S'
-    assert stmts[0].position == '757'
+
+def test_mods_from_web():
+    params = {'format': 'json', 'substrates': JAK2_UPID,
+              'fields': ['sources', 'references']}
+    ptm_url = '%s/ptms' % op_url
+    res = requests.get(ptm_url, params=params)
+    assert res.status_code == 200
+    assert res.text
+    ptm_json = res.json()
+    assert ptm_json[0]['substrate'] == JAK2_UPID, ptm_json[0]['substrate']
+    stmts = OmniPathModificationProcessor(ptm_json).statements
+    assert JAK2_AG.name in [a.name for a in stmts[0].agent_list()],\
+        stmts[0].agent_list()
+    assert 'omnipath' == stmts[0].evidence[0].source_api,\
+        stmts[0].evidence[0].source_api
