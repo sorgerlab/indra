a8601d8a17c9ba8e87b8336870e0d52f79e0ffa2
kkaris <karis.klas@gmail.com>
2019-11-14 16:23:06 -0500

    Update imports, test general web api

--- indra/tests/test_omnipath.py
+++ indra/tests/test_omnipath.py
@@ -1,7 +1,22 @@
-from __future__ import unicode_literals
-from builtins import dict, str
-from indra.statements import Phosphorylation
-from indra.databases import omnipath as op
+import requests
+from indra.sources.omnipath import OmniPathModificationProcessor,\
+    OmniPathLiganReceptorProcessor
+from indra.sources.omnipath.api import op_url
+from indra.statements import Agent, Phosphorylation
+from indra.preassembler.grounding_mapper import GroundingMapper
+
+BRAF_UPID = 'P15056'
+JAK2_UPID = 'O60674'
+BRAF_AG = Agent(None, db_refs={'UP': BRAF_UPID})
+GroundingMapper.standardize_agent_name(BRAF_AG)
+JAK2_AG = Agent(None, db_refs={'UP': JAK2_UPID})
+GroundingMapper.standardize_agent_name(JAK2_AG)
+
+
+def test_omnipath_web_api():
+    query_url = '%s/queries'
+    res = requests.get(query_url)
+    assert res.status_code == 200
 
 def test_query_ptms():
     stmts = op.get_ptms(['Q13873'])
