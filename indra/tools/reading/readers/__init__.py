923c52b51af7046d1b994cd957e6d8a20772398b
pagreene <patrick.anton.greene@gmail.com>
2019-11-18 13:11:04 -0500

    Hedge on loading readers.

--- indra/tools/reading/readers/__init__.py
+++ indra/tools/reading/readers/__init__.py
@@ -5,13 +5,30 @@ from .core import Reader, ReadingError, ReadingData, get_reader, \
 
 from .util import get_dir
 
-from .isi import IsiReader
-from .trips import TripsReader
-from .reach import ReachReader
-from .sparser import SparserReader
-
 from .content import Content
-
 
 logger = logging.getLogger(__name__)
 
+err_msg = ("Could not load {reader} reader: \"{err}\". {reader} will not be "
+           "available.")
+
+# Try loading each reader, and raise a warning if it not available.
+try:
+    from .isi import IsiReader
+except Exception as e:
+    logger.warning(err_msg.format(reader="ISI", err=str(e)))
+
+try:
+    from .trips import TripsReader
+except Exception as e:
+    logger.warning(err_msg.format(reader="Trips", err=str(e)))
+
+try:
+    from .reach import ReachReader
+except Exception as e:
+    logger.warning(err_msg.format(reader="REACH", err=str(e)))
+
+try:
+    from .sparser import SparserReader
+except Exception as e:
+    logger.warning(err_msg.format(reader="Sparser", err=str(e)))
