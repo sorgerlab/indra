b72c728c3ed21322f5eb878660016414b1004743
kkaris <karis.klas@gmail.com>
2019-11-19 16:03:01 -0500

    Update __init__

--- indra/sources/omnipath/__init__.py
+++ indra/sources/omnipath/__init__.py
@@ -1,3 +1,2 @@
 from .api import process_from_web, process_from_pypath
-from .processor import OmniPathModificationProcessor,\
-    OmniPathLiganReceptorProcessor
+from .processor import OmniPathProcessor
