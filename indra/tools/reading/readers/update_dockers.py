221590f449c53a27bad642f848f19abbc460faab
pagreene <patrick.anton.greene@gmail.com>
2019-11-21 13:26:17 -0500

    Add build triggers to script.

--- indra/tools/reading/readers/update_dockers.py
+++ indra/tools/reading/readers/update_dockers.py
@@ -4,8 +4,8 @@ import logging
 import inspect
 
 from os import path
-from zipfile import ZipFile
 from io import BytesIO
+from zipfile import ZipFile
 
 from indra.tools.reading.readers import get_reader_classes
 
@@ -20,7 +20,9 @@ class DockerTemplateError(Exception):
     pass
 
 
-def make_dockerfile(template_path):
+def _make_dockerfile_rec(template_path):
+    """Recursive function to generate a dockerfile from templates."""
+
     # Get the list of templates used
     with open(template_path, 'r') as f:
         template = f.read()
@@ -33,48 +35,77 @@ def make_dockerfile(template_path):
             raise DockerTemplateError("Template %s does not exist."
                                       % other_template_path)
 
-        other_template_string = make_dockerfile(other_template_path)
+        other_template_string = _make_dockerfile_rec(other_template_path)
         template = template.replace("{%% %s %%}" % other_template_name,
                                     other_template_string)
 
     return template
 
 
+def make_zip_package(rc):
+    # Make the dockerfile from the template
+    template_path = path.join(path.dirname(inspect.getfile(rc)),
+                              'docker_template')
+    if not path.exists(template_path):
+        logger.info("%s does not have a dockerfile. Continuing." % rc.name)
+        return
+    logger.info("Forming dockerfile for %s." % rc.name)
+    dockerfile = _make_dockerfile_rec(template_path)
+    dockerfile += "\n# Set in-{reader} environment variable\n" \
+                  "ENV IN_{reader}_DOCKER=true\n".format(reader=rc.name)
+
+    # Create the buildspec
+    build_spec_path = path.join(DOCKER_TEMPLATE_DIR, 'buildspec_fmt.yml')
+    with open(build_spec_path, 'r') as f:
+        build_spec_fmt = f.read()
+    arg_list = re.findall('^ARG[ \t]+(.*?)$', dockerfile, re.MULTILINE)
+    args = ' '.join('--build-arg {arg}=${arg}'.format(arg=arg)
+                    for arg in arg_list)
+    build_spec = build_spec_fmt.format(args=args)
+
+    # Zip up the buildspec and the dockerfile
+    zip_output = BytesIO()
+    with ZipFile(zip_output, 'w') as zf:
+        zf.writestr('Dockerfile', dockerfile)
+        zf.writestr('buildspec.yml', build_spec)
+    zip_output.seek(0)
+
+    return zip_output, arg_list
+
+
 def main():
+    from sys import argv
+
+    # Get the AWS clients.
+    s3 = boto3.client('s3')
+    cb = boto3.client('codebuild')
+
     for rc in get_reader_classes():
-        # Make the dockerfile from the template
-        template_path = path.join(path.dirname(inspect.getfile(rc)),
-                                  'docker_template')
-        if not path.exists(template_path):
-            logger.info("%s does not have a dockerfile. Continuing." % rc.name)
-            continue
-        logger.info("Forming dockerfile for %s." % rc.name)
-        dockerfile = make_dockerfile(template_path)
-        dockerfile += "\n# Set in-{reader} environment variable\n" \
-                      "ENV IN_{reader}_DOCKER=true\n".format(reader=rc.name)
-
-        # Create the buildspec
-        build_spec_path = path.join(DOCKER_TEMPLATE_DIR, 'buildspec_fmt.yml')
-        with open(build_spec_path, 'r') as f:
-            build_spec_fmt = f.read()
-        arg_list = re.findall('^ARG[ \t]+(.*?)$', dockerfile, re.MULTILINE)
-        args = ' '.join('--build-arg {arg}=${arg}'.format(arg=arg)
-                        for arg in arg_list)
-        build_spec = build_spec_fmt.format(args=args)
-
-        # Zip up the buildspec and the dockerfile
-        zip_output = BytesIO()
-        with ZipFile(zip_output, 'w') as zf:
-            zf.writestr('Dockerfile', dockerfile)
-            zf.writestr('buildspec.yml', build_spec)
-        zip_output.seek(0)
-
-        # Put the dockerfile on s3
-        s3 = boto3.client('s3')
+        # Put the latest dockerfile etc on s3
+        zip_output, arg_list = make_zip_package(rc)
         s3_key = 'indra-db/{rdr}-dockerfile/{rdr}-autogen.zip'.format(rdr=rc.name.lower())
         logger.info("Writing %s to s3." % s3_key)
         s3.put_object(Bucket='bigmech', Key=s3_key, Body=zip_output)
 
+        # Trigger the builds. ENV vars are overwritten based on CLI input.
+        # ex: --indra_branch=dev will set the env var INDRA_BRANCH=dev
+        env_overrides = []
+        for arg in arg_list:
+            cli_arg = '--%s' % arg.lower()
+            if cli_arg in argv:
+                cli_idx = argv.index(cli_arg)
+                env_overrides.append({'name': arg, 'value': argv[cli_idx + 1],
+                                      'type': 'PLAINTEXT'})
+
+        logger.info("Triggering build for %s with env overrides:\n%s"
+                    % (rc.name, env_overrides))
+        project_name = 'indra_%s_reading_docker' % rc.name.lower()
+        try:
+            cb.start_build(projectName=project_name,
+                           environmentVariablesOverride=env_overrides)
+        except cb.exceptions.ResourceNotFoundException:
+            logger.error("Project %s does not exist on AWS. Cannot trigger "
+                         "build." % project_name)
     return
 
 
