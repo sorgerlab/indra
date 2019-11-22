979d84f965b0118f86a8df7aa0311f65f8e36170
pagreene <patrick.anton.greene@gmail.com>
2019-11-22 15:54:33 -0500

    Implement the basics of the TRIPS reader.

--- indra/tools/reading/readers/trips/__init__.py
+++ indra/tools/reading/readers/trips/__init__.py
@@ -1,9 +1,13 @@
-from indra.tools.reading.readers.core import EmptyReader
+import os
+import subprocess as sp
 
-from indra.sources import trips
+from indra.tools.reading.readers.core import Reader
+
+from indra.sources.trips import client, process_xml
+from indra_db import formats
 
 
-class TripsReader(EmptyReader):
+class TripsReader(Reader):
     """A stand-in for TRIPS reading.
 
     Currently, we do not run TRIPS (more specifically DRUM) regularly at large
@@ -11,21 +15,39 @@ class TripsReader(EmptyReader):
     a while ago.
     """
     name = 'TRIPS'
+    result_format = formats.XML
 
     def __init__(self, *args, **kwargs):
         self.version = self.get_version()
         return
 
-    def _read(self, *args, **kwargs):
-        return []
+    def _read(self, content_iter, verbose=False, log=False, n_per_proc=None):
+        # Start trips running
+        if os.environ.get("IN_TRIPS_DOCKER", 'false') != 'true':
+            return []
+
+        p = sp.Popen('/sw/drum/bin/startup.sh', stdout=sp.PIPE,
+                     stderr=sp.STDOUT)
+        service_endpoint = 'http://localhost:80/cgi/'
+        service_host = 'drum'
+
+        # Process all the content.
+        for content in content_iter:
+            html = client.send_query(content.get_text(),
+                                     service_endpoint=service_endpoint,
+                                     service_host=service_host)
+            xml = client.get_xml(html)
+            self.add_result(content.get_id(), xml)
+
+        return self.results
 
     @classmethod
     def get_version(cls):
         return 'STATIC'
 
     @staticmethod
-    def get_processor(content):
-        return trips.process_xml(content)
+    def get_processor(reading_content):
+        return process_xml(reading_content)
 
 
 
