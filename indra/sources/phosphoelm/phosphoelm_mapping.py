1360bedfe41757e0fb566d844bbb3d2a5f9f79a0
kkaris <karis.klas@gmail.com>
2019-11-27 12:40:07 -0500

    Fix missing mappings except PDKC

--- indra/sources/phosphoelm/phosphoelm_mapping.py
+++ indra/sources/phosphoelm/phosphoelm_mapping.py
@@ -1,7 +1,7 @@
 phosphoelm_mapping = {
     'Aurora A': ('HGNC', '11393'),
     'Aurora B': ('HGNC', '11390'),
-    # 'CCDPK': ('', ''),
+    'CCDPK': (None, None),  # Non-human gene: https://www.uniprot.org/uniprot/Q9U5L0
     'CK1_delta': ('HGNC', '2452'),  # CSNK1D?? https://www.ncbi.nlm.nih.gov/gene/1453/
     'CK1_epsilon': ('HGNC', '2453'),  # CSNK1E?? https://www.ncbi.nlm.nih.gov/gene/1454
     'CK2_alpha': ('HGNC', '2459'),  # CSNK2A2?? https://www.ncbi.nlm.nih.gov/gene/1459/
@@ -16,6 +16,6 @@ phosphoelm_mapping = {
     'PKC_eta': ('HGNC', '9403'),  # PRKCH?? https://www.ncbi.nlm.nih.gov/gene/5583
     'PKC_iota': ('HGNC', '9404'),  # PRKCI?? https://www.uniprot.org/uniprot/P41743
     'PKC_zeta': ('HGNC', '9412'),  # PRKCZ?? https://www.ncbi.nlm.nih.gov/gene/5590
-    # 'RSK-5': ('', ''),
+    'RSK-5': ('HGNC', '10434'),  # RPS6KA5: https://www.uniprot.org/uniprot/O75582
     'Titin kinase': ('HGNC', '12403')
 }
