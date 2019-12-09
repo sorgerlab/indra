75f0600543a2c30a5b07650b1a091cbe63629776
pagreene <patrick.anton.greene@gmail.com>
2019-12-09 17:19:20 -0500

    Don't remove parentheticals.

--- indra/tools/reading/readers/trips/__init__.py
+++ indra/tools/reading/readers/trips/__init__.py
@@ -121,12 +121,10 @@ class TripsReader(Reader):
 
             # Clean up the text string a bit.
             # - remove all excess white space.
-            # - remove parentheticals
             # - remove special greek letters
             # - remove all special unicode, replace with ascii
             raw_text = content.get_text()
             raw_text = re.sub('\s+', ' ', raw_text)
-            raw_text = re.sub('\s+[[(].*?[\])]', '', raw_text)
             for greek_letter, spelled_letter in greek_alphabet.items():
                 raw_text = raw_text.replace(greek_letter, spelled_letter)
             text = unidecode(raw_text)
