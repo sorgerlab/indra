0706b7fea4c8ee557fdc06f9e4a6492824a5bc94
Ben Gyori <ben.gyori@gmail.com>
2019-12-31 10:41:09 -0500

    Refactor TAS API to allow S3 download and CSV processing

--- indra/sources/tas/api.py
+++ indra/sources/tas/api.py
@@ -1,45 +1,33 @@
 from __future__ import absolute_import, print_function, unicode_literals
 from builtins import dict, str
 
-__all__ = ['process_csv']
+__all__ = ['process_csv', 'process_from_web']
 
-from os import path
+import csv
+import logging
+import requests
 from .processor import TasProcessor
-from indra.util import read_unicode_csv
+from indra.util import read_unicode_csv, read_unicode_csv_fileobj
+
+tas_data_url = 'https://bigmech.s3.amazonaws.com/indra-db/indra_tas.csv'
 
 
-HERE = path.dirname(path.abspath(__file__))
-DATAFILE_NAME = 'indra_tas.csv'
+logger = logging.getLogger(__name__)
 
 
-def _load_data():
-    """Load the data from the csv in data.
-
-    The "gene_id" is the Entrez gene id, and the "approved_symbol" is the
-    standard gene symbol.
-
-    Returns
-    -------
-    data : list[dict]
-        A list of dicts of row values keyed by the column headers extracted from
-        the csv file, described above.
-    """
-    # Get the cwv reader object.
-    csv_path = path.join(HERE, path.pardir, path.pardir, 'resources',
-                         DATAFILE_NAME)
-    data_iter = list(read_unicode_csv(csv_path))
-
+def _load_data(data_iter):
     # Get the headers.
     headers = data_iter[0]
 
     # For some reason this heading is oddly formatted and inconsistent with the
     # rest, or with the usual key-style for dicts.
-    return [{header: val for header, val in zip(headers, line)}
+    data = [{header: val for header, val in zip(headers, line)}
             for line in data_iter[1:]]
+    return data
 
 
-def process_csv(affinity_class_limit=2):
-    """Return a TasProcessor for the contents of the csv contained in data.
+def process_from_web(affinity_class_limit=2):
+    """Return a TasProcessor for the contents of the TAS dump online.
 
     Interactions are classified into the following classes based on affinity:
       | 1  -- Kd < 100nM
@@ -62,4 +50,39 @@ def process_csv(affinity_class_limit=2):
         from the CSV file representing drug-target inhibitions in its
         statements attribute.
     """
-    return TasProcessor(_load_data(), affinity_class_limit)
+    logger.info('Downloading TAS data from %s' % tas_data_url)
+    res = requests.get(tas_data_url)
+    res.raise_for_status()
+    logger.info('Finished downloading TAS data from %s' % tas_data_url)
+    data_iter = list(csv.reader(res.text.splitlines(), delimiter=','))
+    return TasProcessor(_load_data(data_iter), affinity_class_limit)
+
+
+def process_csv(fname, affinity_class_limit=2):
+    """Return a TasProcessor for the contents of a given CSV file..
+
+    Interactions are classified into the following classes based on affinity:
+      | 1  -- Kd < 100nM
+      | 2  -- 100nM < Kd < 1uM
+      | 3  -- 1uM < Kd < 10uM
+      | 10 -- Kd > 10uM
+    By default, only classes 1 and 2 are extracted but the affinity_class_limit
+    parameter can be used to change the upper limit of extracted classes.
+
+    Parameters
+    ----------
+    fname : str
+        The path to a local CSV file containing the TAS data.
+    affinity_class_limit : Optional[int]
+        Defines the highest class of binding affinity that is included in the
+        extractions. Default: 2
+
+    Returns
+    -------
+    TasProcessor
+        A TasProcessor object which has a list of INDRA Statements extracted
+        from the CSV file representing drug-target inhibitions in its
+        statements attribute.
+    """
+    data_iter = list(read_unicode_csv(fname))
+    return TasProcessor(_load_data(data_iter), affinity_class_limit)
--- indra/tests/test_tas.py
+++ indra/tests/test_tas.py
@@ -1,16 +1,8 @@
-from __future__ import absolute_import, print_function, unicode_literals
-from builtins import dict, str
-
-from indra.sources.tas.api import _load_data, process_csv
-
-
-def test_load_data():
-    data = _load_data()
-    assert len(data) > 100, len(data)
+from indra.sources.tas import process_from_web
 
 
 def test_processor():
-    tp = process_csv(affinity_class_limit=10)
+    tp = process_from_web(affinity_class_limit=10)
     assert tp
     assert tp.statements
     num_stmts = len(tp.statements)
