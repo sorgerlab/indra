ae0e2a481f91e94cf05ac2df63f1d66f76a5e442
Ben Gyori <ben.gyori@gmail.com>
2020-01-14 15:26:51 -0500

    Refactor Gilda module and add function to get models

--- indra/preassembler/grounding_mapper/gilda.py
+++ indra/preassembler/grounding_mapper/gilda.py
@@ -4,7 +4,50 @@ and contains functions to help apply it during the course of INDRA assembly."""
 import requests
 from .mapper import GroundingMapper
 
-grounding_service_url = 'http://grounding.indra.bio/ground'
+grounding_service_url = 'http://grounding.indra.bio'
+
+
+def get_gilda_models():
+    """Return a list of strings for which Gilda has a disambiguation model.
+
+    Returns
+    -------
+    list[str]
+        A list of entity strings.
+    """
+    res = requests.post(grounding_service_url + '/models')
+    models = res.json()
+    return models
+
+
+def ground_statement(stmt):
+    """Set grounding for Agents in a given Statement using Gilda.
+
+    This function modifies the original Statement/Agents in place.
+
+    Parameters
+    ----------
+    stmt : indra.statements.Statement
+        A Statement to ground
+    """
+    if stmt.evidence and stmt.evidence[0].text:
+        context = stmt.evidence[0].text
+    else:
+        context = None
+    for agent in stmt.agent_list():
+        if agent is not None and 'TEXT' in agent.db_refs:
+            txt = agent.db_refs['TEXT']
+            resp = requests.post(grounding_service_url + '/ground',
+                                 json={'text': txt,
+                                       'context': context})
+            results = resp.json()
+            if results:
+                db_refs = {'TEXT': txt,
+                           results[0]['term']['db']:
+                               results[0]['term']['id']}
+                agent.db_refs = db_refs
+                GroundingMapper.standardize_agent_name(agent,
+                                                       standardize_refs=True)
 
 
 def ground_statements(stmts):
@@ -14,25 +57,8 @@ def ground_statements(stmts):
 
     Parameters
     ----------
-    stmts : list[indra.statements.Statements]
+    stmts : list[indra.statements.Statement]
         A list of Statements to ground
     """
     for stmt in stmts:
-        if stmt.evidence and stmt.evidence[0].text:
-            context = stmt.evidence[0].text
-        else:
-            context = None
-        for agent in stmt.agent_list():
-            if agent is not None and 'TEXT' in agent.db_refs:
-                txt = agent.db_refs['TEXT']
-                resp = requests.post(grounding_service_url,
-                                     json={'text': txt,
-                                           'context': context})
-                results = resp.json()
-                if results:
-                    db_refs = {'TEXT': txt,
-                               results[0]['term']['db']:
-                                   results[0]['term']['id']}
-                    agent.db_refs = db_refs
-                    GroundingMapper.standardize_agent_name(agent,
-                                                        standardize_refs=True)
+        ground_statement(stmt)
