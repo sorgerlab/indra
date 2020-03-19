bb27b536193fcc6ada7ab6a4193ac1bf889569d7
Ben Gyori <ben.gyori@gmail.com>
2020-03-19 17:17:26 -0400

    Implement fetching annotations for a given group

--- indra/sources/hypothesis/api.py
+++ indra/sources/hypothesis/api.py
@@ -1,1 +1,27 @@
 import requests
+from indra.config import get_config
+from .processor import HypothesisProcessor
+
+
+base_url = 'https://api.hypothes.is/api/'
+api_key = get_config('HYPOTHESIS_API_KEY')
+headers = {'Authorization': 'Bearer %s' % api_key,
+           'Accept': 'application/vnd.hypothesis.v1+json',
+           'content-type': 'application/json'}
+
+
+def send_request(endpoint, **params):
+    if api_key is None:
+        return ValueError('No API key set in HYPOTHESIS_API_KEY')
+    res = requests.get(base_url + endpoint, headers=headers,
+                       params=params)
+    res.raise_for_status()
+    return res.json()
+
+
+def process_annotations(group):
+    res = send_request('search', group=group)
+    annotations = res.get('rows', [])
+    hp = HypothesisProcessor(annotations)
+    hp.get_statements()
+    return hp
