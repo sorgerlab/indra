bb8ed532f6a371a148cacdb46c7038555a175354
Ben Gyori <ben.gyori@gmail.com>
2020-04-07 17:44:25 -0400

    Make sure protmapper version is high enough in setup

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def main():
     install_list = ['pysb>=1.3.0,<=1.9.1', 'objectpath', 'rdflib',
                     'requests>=2.11', 'lxml', 'ipython', 'future',
                     'networkx>=2', 'pandas', 'ndex2==2.0.1', 'jinja2',
-                    'protmapper>=0.0.14', 'obonet']
+                    'protmapper>=0.0.16', 'obonet']
 
     extras_require = {
                       # Inputs and outputs
