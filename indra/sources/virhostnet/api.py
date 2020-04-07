99ec449a7f00f28dc6fd474e37f01dfb68cd33c1
Ben Gyori <ben.gyori@gmail.com>
2020-04-07 09:45:16 -0400

    Add query argument, docstrings, and logging

--- indra/sources/virhostnet/api.py
+++ indra/sources/virhostnet/api.py
@@ -1,8 +1,12 @@
 import pandas
+import logging
 from .processor import VirhostnetProcessor
 
+logger = logging.getLogger(__name__)
+
+
 vhn_url = ('http://virhostnet.prabi.fr:9090/psicquic/webservices/current/'\
-           'search/query/*')
+           'search/query/')
 
 
 data_columns = [
@@ -13,19 +17,66 @@ data_columns = [
 ]
 
 
-def process_from_web():
-    df = pandas.read_csv(vhn_url, delimiter='\t', names=data_columns,
+def process_from_web(query=None):
+    """Process host-virus interactions from the VirHostNet website.
+
+    Parameters
+    ----------
+    query : Optional[str]
+        A query that constrains the results to a given subset of the VirHostNet
+        database. Example: "taxid:2697049" to search for interactions for
+        SARS-CoV-2. If not provided, By default, the "*" query is used which
+        returns the full database.
+
+    Returns
+    -------
+    VirhostnetProcessor
+        A VirhostnetProcessor object which contains a list of extracted
+        INDRA Statements in its statements attribute.
+    """
+    # Search for everything to get the full download by default
+    url = vhn_url + ('*' if query is None else query)
+    logger.info('Processing VirHostNet data from %s' % url)
+    df = pandas.read_csv(url, delimiter='\t', names=data_columns,
                          header=None)
     return process_df(df)
 
 
 def process_tsv(fname):
+    """Process a TSV data file obtained from VirHostNet.
+
+    Parameters
+    ----------
+    fname : str
+        The path to the VirHostNet tabular data file (in the same format as
+        the web service).
+
+    Returns
+    -------
+    VirhostnetProcessor
+        A VirhostnetProcessor object which contains a list of extracted
+        INDRA Statements in its statements attribute.
+    """
     df = pandas.read_csv(fname, delimiter='\t', names=data_columns,
                          header=None)
     return process_df(df)
 
 
 def process_df(df):
+    """Process a VirHostNet pandas DataFrame.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        A DataFrame representing VirHostNet interactions (in the same format as
+        the web service).
+
+    Returns
+    -------
+    VirhostnetProcessor
+        A VirhostnetProcessor object which contains a list of extracted
+        INDRA Statements in its statements attribute.
+    """
     vp = VirhostnetProcessor(df)
     vp.extract_statements()
     return vp
