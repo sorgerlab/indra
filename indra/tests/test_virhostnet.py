3cd6e5be02db76e0dc9e5b65f6c9a3e4cb4c32d1
Ben Gyori <ben.gyori@gmail.com>
2020-04-07 10:25:28 -0400

    Implement API tests

--- indra/tests/test_virhostnet.py
+++ indra/tests/test_virhostnet.py
@@ -1,8 +1,40 @@
+import pandas
+from nose.plugins.attrib import attr
 from indra.statements import Complex
 from indra.sources import virhostnet
 from indra.sources.virhostnet.api import data_columns
 from indra.sources.virhostnet.processor import parse_psi_mi, parse_source_ids, \
     parse_text_refs, get_agent_from_grounding, process_row
+
+
+test_row_str = ('uniprotkb:Q6P5R6	uniprotkb:Q1K9H5	'
+                'uniprotkb:RL22L_HUMAN	uniprotkb:Q1K9H5_I33A0	'
+                'uniprotkb:RL22L_HUMAN	uniprotkb:Q1K9H5_I33A0	'
+                'psi-mi:"MI:0004"(affinity chromatography technology)	'
+                '-	pubmed:26651948	taxid:9606	taxid:381518	'
+                'psi-mi:"MI:0915"(physical association)	'
+                'psi-mi:"MI:1114"(virhostnet)	'
+                'virhostnet-rid:19809|virhostnet-nrid:18603	'
+                'virhostnet-miscore:0.32715574')
+test_row = {k: v for k, v in zip(data_columns, test_row_str.split('\t'))}
+test_df = pandas.DataFrame.from_dict({k: [v] for k, v in test_row.items()})
+
+
+@attr('webservice', 'slow')
+def test_process_from_web():
+    vp = virhostnet.process_from_web(query='pubmed:8553588')
+    assert len(vp.statements) == 5, len(vp.statements)
+    assert all(isinstance(st, Complex) for st in vp.statements)
+    assert all(stmt.members[1].db_refs['REFSEQ_PROT'] == 'NP_040311'
+               for stmt in vp.statements)
+    assert all(stmt.evidence[0].pmid == '8553588'
+               for stmt in vp.statements)
+
+
+def test_process_df():
+    vp = virhostnet.process_df(test_df)
+    assert len(vp.statements) == 1
+    _assert_test_stmt(vp.statements[0])
 
 
 def test_get_agent_from_grounding():
@@ -44,17 +76,11 @@ def test_parse_psi_mi():
 
 
 def test_process_row():
-    test_row_str = ('uniprotkb:Q6P5R6	uniprotkb:Q1K9H5	'
-                    'uniprotkb:RL22L_HUMAN	uniprotkb:Q1K9H5_I33A0	'
-                    'uniprotkb:RL22L_HUMAN	uniprotkb:Q1K9H5_I33A0	'
-                    'psi-mi:"MI:0004"(affinity chromatography technology)	'
-                    '-	pubmed:26651948	taxid:9606	taxid:381518	'
-                    'psi-mi:"MI:0915"(physical association)	'
-                    'psi-mi:"MI:1114"(virhostnet)	'
-                    'virhostnet-rid:19809|virhostnet-nrid:18603	'
-                    'virhostnet-miscore:0.32715574')
-    row = {k: v for k, v in zip(data_columns, test_row_str.split('\t'))}
-    stmt = process_row(row)
+    stmt = process_row(test_row)
+    _assert_test_stmt(stmt)
+
+
+def _assert_test_stmt(stmt):
     assert isinstance(stmt, Complex)
     host_ag = stmt.members[0]
     assert host_ag.name == 'RPL22L1'
@@ -77,3 +103,4 @@ def test_process_row():
     assert ev.annotations['exp_method'] == {'id': 'MI:0004',
                                             'name': ('affinity chromatography '
                                                      'technology')}
+
