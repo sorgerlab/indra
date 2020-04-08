789a94c5a601bbd78cfef4e5a1498a82fac488fb
Ben Gyori <ben.gyori@gmail.com>
2020-04-07 21:16:31 -0400

    Apply new UPPRO-based grounding in test

--- indra/tests/test_virhostnet.py
+++ indra/tests/test_virhostnet.py
@@ -43,10 +43,9 @@ def test_get_agent_from_grounding():
     assert ag.db_refs['UP'] == 'P15056', ag.db_refs
 
     ag = get_agent_from_grounding('uniprotkb:P15056-PRO_0000085665')
-    # This is the name of the chain in UniProt, it will have to be uncommented
-    # once normalization for chains is merged
-    # assert ag.name == 'Serine/threonine-protein kinase B-raf'
-    assert ag.name == 'BRAF'
+    # This is the name of the chain in UniProt which takes precedence
+    # if we have a feature ID
+    assert ag.name == 'Serine/threonine-protein kinase B-raf'
     assert ag.db_refs['UP'] == 'P15056'
     assert ag.db_refs['UPPRO'] == 'PRO_0000085665'
 
