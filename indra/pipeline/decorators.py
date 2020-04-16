402fb8c93be37f58ba2bb601e3d2584a7748b2b6
dianakolusheva <dianakolusheva@gmail.com>
2020-04-16 15:39:28 -0400

    Fix a name

--- indra/pipeline/decorators.py
+++ indra/pipeline/decorators.py
@@ -1,7 +1,7 @@
 pipeline_functions = {}
 
 
-def pipeline(function):
+def register_pipeline(function):
     if function.__name__ not in pipeline_functions:
         pipeline_functions[function.__name__] = function
     return function
