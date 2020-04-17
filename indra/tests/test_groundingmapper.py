584e7f8036db352a90e7241620344154905051e6
Ben Gyori <ben.gyori@gmail.com>
2020-04-17 18:55:32 -0400

    Update name standardization test with new name

--- indra/tests/test_groundingmapper.py
+++ indra/tests/test_groundingmapper.py
@@ -429,8 +429,8 @@ def test_standardize_name_efo_hp_doid():
 
     ag = Agent('x', db_refs={'DOID': 'DOID:0014667'})
     standardize_agent_name(ag)
-    # Name based on HP itself
-    assert ag.name == 'disease of metabolism'
+    # Name based on MESH mapping
+    assert ag.name == 'Metabolic Diseases'
 
     ag = Agent('x', db_refs={'EFO': '1002050'})
     standardize_agent_name(ag)
