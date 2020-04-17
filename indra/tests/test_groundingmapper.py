48ea078b923dd961f02fe8acbf896dedc26a17f8
Ben Gyori <ben.gyori@gmail.com>
2020-04-17 18:52:12 -0400

    Add test for new mappings

--- indra/tests/test_groundingmapper.py
+++ indra/tests/test_groundingmapper.py
@@ -399,15 +399,21 @@ def test_standardize_db_refs_efo_hp_doid():
     refs = standardize_db_refs({'MESH': 'D064706'})
     assert refs.get('HP') == 'HP:0031801', refs
 
-    # There would be a one-to-many mapping from this DOID
-    # so we make sure we don't add a mapping for it.
-    refs = standardize_db_refs({'DOID': 'DOID:0060695'})
-    assert 'MESH' not in refs
+    # Currently there is no one-to-many mapping in the direction towards MeSH
+    # (there used to be) if there is again, we should test it here
+    #refs = standardize_db_refs({'DOID': 'DOID:0060695'})
+    #assert 'MESH' not in refs
+
+    # One-to-many mappings away from MESH
     refs = standardize_db_refs({'MESH': 'D000071017'})
-    assert refs.get('DOID') == 'DOID:0060695'
+    assert 'DOID' not in refs
 
     refs = standardize_db_refs({'DOID': 'DOID:0060495'})
     assert refs.get('MESH') == 'D000067208'
+
+    # This is an xrefs-based mapping that isn't in Gilda's resource file
+    refs = standardize_db_refs({'EFO': '0000694'})
+    assert refs.get('MESH') == 'D045169'
 
 
 def test_standardize_name_efo_hp_doid():
