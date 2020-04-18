698dde672d47b87dcd9e4c4ac0b568ed62bf5161
Ben Gyori <ben.gyori@gmail.com>
2020-04-18 17:03:12 -0400

    Add HGNC/UP mappings to graph

--- indra/resources/ontology_graph.py
+++ indra/resources/ontology_graph.py
@@ -10,13 +10,34 @@ class IndraOntology(networkx.MultiDiGraph):
         super().__init__()
         self.add_hgnc_nodes()
         self.add_uniprot_nodes()
+        self.add_hgnc_uniprot_links()
 
     def add_hgnc_nodes(self):
-        nodes = [('HGNC:%s' % hid, {'name': hname}) for (hid, hname)
-                 in hgnc_client.hgnc_names.items()]
+        nodes = [(label('HGNC', hid),
+                  {'name': hname, 'ns': 'HGNC', 'id': hid})
+                 for (hid, hname) in hgnc_client.hgnc_names.items()]
         self.add_nodes_from(nodes)
 
     def add_uniprot_nodes(self):
-        nodes = [('UP:%s' % uid, {'name': uname}) for (uid, uname)
+        nodes = [(label('UP', uid), {'name': uname, 'ns': 'UP', 'id': uid})
+                 for (uid, uname)
                  in uniprot_client.um.uniprot_gene_name.items()]
         self.add_nodes_from(nodes)
+
+    def add_hgnc_uniprot_links(self):
+        edges = []
+        for hid, uid in hgnc_client.uniprot_ids.items():
+            uids = uid.split(', ')
+            for uid in uids:
+                edges.append((label('HGNC', hid), label('UP', uid),
+                              {'type': 'xref', 'source': 'hgnc'}))
+        self.add_edges_from(edges)
+
+        edges = [(label('UP', uid), label('HGNC', hid),
+                  {'type': 'xref', 'source': 'hgnc'})
+                 for uid, hid in uniprot_client.um.uniprot_hgnc.items()]
+        self.add_edges_from(edges)
+
+
+def label(ns, id):
+    return '%s:%s' % (ns, id)
