6d746a8b0ec1584f70c2878b73c9504b30f8982b
Ben Gyori <ben.gyori@gmail.com>
2020-04-19 15:55:05 -0400

    Add FamPlex to ontology graph

--- indra/resources/ontology_graph.py
+++ indra/resources/ontology_graph.py
@@ -1,8 +1,12 @@
+import os
 import networkx
+from indra.util import read_unicode_csv
 from indra.databases import hgnc_client
 from indra.databases import uniprot_client
 from indra.databases import chebi_client
 from indra.databases import mesh_client
+
+HERE = os.path.dirname(os.path.abspath(__file__))
 
 
 class IndraOntology(networkx.MultiDiGraph):
@@ -10,7 +14,10 @@ class IndraOntology(networkx.MultiDiGraph):
         super().__init__()
         self.add_hgnc_nodes()
         self.add_uniprot_nodes()
-        self.add_hgnc_uniprot_links()
+        self.add_hgnc_uniprot_xrefs()
+        self.add_famplex_nodes()
+        self.add_famplex_hierarchy()
+        self.add_famplex_xrefs()
 
     def add_hgnc_nodes(self):
         nodes = [(label('HGNC', hid),
@@ -24,7 +31,7 @@ class IndraOntology(networkx.MultiDiGraph):
                  in uniprot_client.um.uniprot_gene_name.items()]
         self.add_nodes_from(nodes)
 
-    def add_hgnc_uniprot_links(self):
+    def add_hgnc_uniprot_xrefs(self):
         edges = []
         for hid, uid in hgnc_client.uniprot_ids.items():
             uids = uid.split(', ')
@@ -36,6 +43,44 @@ class IndraOntology(networkx.MultiDiGraph):
         edges = [(label('UP', uid), label('HGNC', hid),
                   {'type': 'xref', 'source': 'hgnc'})
                  for uid, hid in uniprot_client.um.uniprot_hgnc.items()]
+        self.add_edges_from(edges)
+
+    def add_famplex_nodes(self):
+        nodes = []
+        for row in read_unicode_csv(os.path.join(HERE, 'famplex',
+                                                 'entities.csv'),
+                                    delimiter=','):
+            entity = row[0]
+            nodes.append((label('FPLX', entity),
+                          {'name': entity, 'id': entity}))
+        self.add_nodes_from(nodes)
+
+    def add_famplex_hierarchy(self):
+        edges = []
+        for row in read_unicode_csv(os.path.join(HERE, 'famplex',
+                                                 'relations.csv'),
+                                    delimiter=','):
+            ns1, id1, rel, ns2, id2 = row
+            if ns1 == 'HGNC':
+                id1 = hgnc_client.get_hgnc_id(id1)
+            edges.append((label(ns1, id1), label(ns2, id2), {'type': rel}))
+        self.add_edges_from(edges)
+
+    def add_famplex_xrefs(self):
+        edges = []
+        include_refs = {'PF', 'IP', 'GO', 'NCIT'}
+        for row in read_unicode_csv(os.path.join(HERE, 'famplex',
+                                                 'equivalences.csv'),
+                                    delimiter=','):
+            ref_ns, ref_id, fplx_id = row
+            if ref_ns not in include_refs:
+                continue
+            edges.append((label(ref_ns, ref_id),
+                          label('FPLX', fplx_id),
+                          {'type': 'xref', 'source': 'fplx'}))
+            edges.append((label('FPLX', fplx_id),
+                          label(ref_ns, ref_id),
+                          {'type': 'xref', 'source': 'fplx'}))
         self.add_edges_from(edges)
 
 
