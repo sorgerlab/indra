fd5c61bbb5972e251aa194413ad0deeabda138ed
Ben Gyori <ben.gyori@gmail.com>
2020-05-08 11:09:32 -0400

    Use OBO to update ChEBI names/hierarchy

--- indra/resources/update_resources.py
+++ indra/resources/update_resources.py
@@ -354,66 +354,17 @@ def update_bel_chebi_map():
                       (chebi_name, chebi_id)).encode('utf-8'))
 
 
-def _get_chebi_obo_terms():
-    def process_term(term):
-        lines = term.split('\n')[1:]
-        term_id = None
-        parents = []
-        secondaries = []
-        name = None
-        for line in lines:
-            k, v = line.split(': ', maxsplit=1)
-            if k == 'id':
-                term_id = v
-            elif k == 'is_a':
-                parents.append(v)
-            elif k == 'relationship':
-                rel, target = v.split(' ')
-                if rel in ('is_conjugate_acid_of', 'has_functional_parent',
-                           'has_parent_hydride', 'has_role'):
-                    parents.append(target)
-            elif k == 'alt_id':
-                secondaries.append(v)
-            elif k == 'name':
-                name = v
-        return term_id, name, secondaries, parents
-    url = 'ftp://ftp.ebi.ac.uk/pub/databases/chebi/ontology/chebi_lite.obo'
-    fname = 'chebi_lite.obo'
-    logger.info('Downloading %s' % url)
-    urlretrieve(url, fname)
-    with open(fname, 'r') as fh:
-        logger.info('Loading %s' % fname)
-        content = fh.read()
-    chunks = content.split('\n\n')
-    terms = [c for c in chunks if c.startswith('[Term]')]
-    logger.info('Found %d terms' % len(terms))
-    term_entries = [process_term(term) for term in terms]
-    return term_entries
-
-
-def update_chebi_entries():
-    term_entries = _get_chebi_obo_terms()
-    # Make the name and secondary table
-    fname = os.path.join(path, 'chebi_entries.tsv')
-    rows = [['CHEBI_ID', 'NAME', 'SECONDARIES']]
-    for term_id, name, secondaries, parents in term_entries:
-        term_id = term_id.replace('CHEBI:', '')
-        rows.append([term_id, name, ','.join(secondaries)])
-    with open(fname, 'wb') as fh:
-        write_unicode_csv(fname, rows, '\t')
-
-
 def make_chebi_hierarchy():
-    term_entries = _get_chebi_obo_terms()
-    terms_with_parents = [(t, p) for t, _, _, p in term_entries if p]
+    from indra.databases.chebi_client import _obo_client as oc
     g = rdflib.Graph()
     indra_rn = \
         rdflib.Namespace('http://sorger.med.harvard.edu/indra/relations/')
     chebi_ns = rdflib.Namespace('http://identifiers.org/chebi/')
     isa = indra_rn.term('isa')
-    for child, parents in terms_with_parents:
-        for parent in parents:
-            g.add((chebi_ns.term(child), isa, chebi_ns.term(parent)))
+    for db_id, entry in oc.entries.items():
+        for rel, targets in entry.get('relations', {}).items():
+            for target in targets:
+                g.add((chebi_ns.term(db_id), isa, chebi_ns.term(target)))
     return g
 
 
@@ -738,13 +689,19 @@ def update_go():
     OboClient.update_resource(path, url, 'go', remove_prefix=False)
 
 
+def update_chebi_obo():
+    """Update disease ontology."""
+    url = 'ftp://ftp.ebi.ac.uk/pub/databases/chebi/ontology/chebi_lite.obo'
+    OboClient.update_resource(path, url, 'chebi', remove_prefix=False)
+
+
 def main():
     update_famplex()
     update_famplex_map()
     update_hgnc_entries()
     update_kinases()
     update_uniprot_subcell_loc()
-    update_chebi_entries()
+    update_chebi_obo()
     update_chebi_references()
     update_chebi_accessions()
     update_hmdb_chebi_map()
