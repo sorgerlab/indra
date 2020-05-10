02525b69e1342877d46aa57fa2ff62d79ac4a25f
Ben Gyori <ben.gyori@gmail.com>
2020-05-10 11:30:28 -0400

    Add MeSH nodes and hierarchy

--- indra/resources/ontology_graph.py
+++ indra/resources/ontology_graph.py
@@ -1,4 +1,5 @@
 import os
+import logging
 import networkx
 from indra.util import read_unicode_csv
 from indra.databases import hgnc_client, uniprot_client, chebi_client, \
@@ -6,6 +7,8 @@ from indra.databases import hgnc_client, uniprot_client, chebi_client, \
 from .shortest_path import bidirectional_shortest_path
 
 HERE = os.path.dirname(os.path.abspath(__file__))
+
+logger = logging.getLogger(__name__)
 
 
 class IndraOntology(networkx.MultiDiGraph):
@@ -16,6 +19,7 @@ class IndraOntology(networkx.MultiDiGraph):
         self.add_uniprot_nodes()
         self.add_famplex_nodes()
         self.add_obo_nodes()
+        self.add_mesh_nodes()
         # Add xrefs
         self.add_hgnc_uniprot_xrefs()
         self.add_famplex_xrefs()
@@ -23,6 +27,7 @@ class IndraOntology(networkx.MultiDiGraph):
         # Add hierarchies
         self.add_famplex_hierarchy()
         self.add_obo_hierarchies()
+        self.add_mesh_hierarchy()
 
     def _check_path(self, ns1, id1, ns2, id2, edge_types):
         try:
@@ -137,7 +142,8 @@ class IndraOntology(networkx.MultiDiGraph):
                 for rel, targets in entry.get('relations', {}).items():
                     for target in targets:
                         edges.append((label(ns.upper(), db_id),
-                                      target, {'type': rel}))
+                                      label(ns.upper(), target),
+                                      {'type': rel}))
         self.add_edges_from(edges)
 
     def add_chemical_xrefs(self):
@@ -178,6 +184,33 @@ class IndraOntology(networkx.MultiDiGraph):
                           label('CHEBI', chebi_id),
                           {'type': 'xref', 'source': 'chebi'}))
 
+        self.add_edges_from(edges)
+
+    def add_mesh_nodes(self):
+        nodes = [(label('MESH', mesh_id),
+                  {'name': name, 'ns': 'MESH', 'id': mesh_id})
+                 for mesh_id, name in
+                 mesh_client.mesh_id_to_name.items()]
+        self.add_nodes_from(nodes)
+
+    def add_mesh_hierarchy(self):
+        mesh_tree_numbers_to_id = {}
+        for mesh_id, tns in mesh_client.mesh_id_to_tree_numbers.items():
+            for tn in tns:
+                mesh_tree_numbers_to_id[tn] = mesh_id
+        edges = []
+        for mesh_id, tns in mesh_client.mesh_id_to_tree_numbers.items():
+            parents_added = set()
+            for tn in tns:
+                if '.' not in tn:
+                    continue
+                parent_tn, _ = tn.rsplit('.', maxsplit=1)
+                parent_id = mesh_tree_numbers_to_id[parent_tn]
+                if parent_id in parents_added:
+                    continue
+                edges.append((label('MESH', mesh_id),
+                              label('MESH', parent_id),
+                              {'type': 'isa'}))
         self.add_edges_from(edges)
 
 
