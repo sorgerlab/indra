b695c3698431081aeca55062ad8ff3710ef7f509
kkaris <karis.klas@gmail.com>
2020-05-19 12:52:51 -0400

    Immigrate sign helpers from depmap_analysis

--- indra/explanation/pathfinding_util.py
+++ indra/explanation/pathfinding_util.py
@@ -1,6 +1,9 @@
+import logging
 from copy import deepcopy
 
 import networkx as nx
+
+logger = logging.getLogger(__name__)
 
 
 def signed_edges_to_signed_nodes(graph, prune_nodes=True,
@@ -77,3 +80,59 @@ def prune_signed_nodes(graph):
                           in graph.in_degree()
                           if in_deg == 0 and node[1] == 1]
     return graph
+
+
+def path_sign_to_signed_nodes(source, target, edge_sign):
+    """Translates a signed edge or path to valid signed nodes
+
+    Pairs with a negative source node are filtered out.
+
+    Paramters
+    ---------
+    source : str|int
+        The source node
+    target : str|int
+        The target node
+    edge_sign : int
+        The sign of the edge
+
+    Returns
+    -------
+    sign_tuple : (a, sign), (b, sign)
+        Tuple of tuples of the valid combination of signed nodes
+    """
+    # Sign definitions: + == 0, - == 1
+    # + path -> (a+, b+)
+    # - path -> (a+, b-)
+    # (a-, b-) and (a-, b+) are also technically valid but not in this context
+    try:
+        if int(edge_sign) == 0:
+            return (source, 0), (target, 0)
+        else:
+            return (source, 1), (target, 0)
+    except ValueError:
+        logger.warning('Invalid sign %s when translating edge sign to int'
+                       % edge_sign)
+        return (None, None), (None, None)
+
+
+def signed_nodes_to_signed_edge(source, target):
+    """Create the triple (node, node, sign) from a pair of signed nodes
+
+    Assuming source, target forms an edge of signed nodes:
+    edge = (a, sign), (b, sign), return the corresponding signed edge triple
+    """
+    # Sign definitions: + == 0, - == 1
+    # + edge/path -> (a+, b+) and (a-, b-)
+    # - edge/path -> (a-, b+) and (a+, b-)
+    source_name, source_sign = source
+    target_name, target_sign = target
+    try:
+        if int(source_sign) == int(target_sign):
+            return source_name, target_name, 0
+        else:
+            return source_name, target_name, 1
+    except ValueError:
+        logger.warning('Error translating signed nodes to signed edge using '
+                       '(%s, %s)' % (source, target))
+        return None, None, None
