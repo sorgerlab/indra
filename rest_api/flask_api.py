e4cd671185b1a2d7472a143ea6a9cb3f190282b5
dianakolusheva <dianakolusheva@gmail.com>
2020-05-21 11:40:01 -0400

    Implement preassembly resources with flask-restx

--- rest_api/flask_api.py
+++ rest_api/flask_api.py
@@ -1,11 +1,11 @@
 import argparse
-import json
-from functools import partial
+import inspect
+from collections import namedtuple
 from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
+from flask_restx import Api, Resource, reqparse, Namespace, inputs
 from indra.pipeline import AssemblyPipeline, pipeline_functions
 from indra.tools.assemble_corpus import *
-from indra.statements import stmts_from_json
+from indra.statements import stmts_from_json, get_statement_by_name
 
 
 boolean_args = [
@@ -22,9 +22,10 @@ dict_args = [
 float_args = ['score_threshold', 'belief_cutoff']
 int_args = ['poolsize', 'size_cutoff']
 app = Flask(__name__)
-api = Api(app)
-parser = reqparse.RequestParser()
-parser.add_argument('statements')
+api = Api(app, title='INDRA REST API')
+preassembly = Namespace('preassembly', path='/preassembly/')
+api.add_namespace(preassembly)
+parsers = {}
 
 
 def _return_stmts(stmts):
@@ -53,20 +54,56 @@ def add_arg_to_parser(arg, parser):
 
 
 class PreassembleStatements(Resource):
-    def post(self, func_name):
-        args = request.form.to_dict()
-        # args = parser.parse_args()
-        stmts = stmts_from_json(json.loads(args['statements']))
-        stmts_out = pipeline_functions[func_name](stmts)
+    func_name = None
+
+    def process_args(self, args_json):
+        args = parsers[self.func_name].parse_args()
+        for arg in args_json:
+            if arg not in args:
+                add_arg_to_parser(arg, parsers[self.func_name])
+        args = parsers[self.func_name].parse_args()
+        for arg in args:
+            if arg == 'stmt_type':
+                args[arg] = get_statement_by_name(args[arg])
+            elif arg in ['matches_fun', 'refinement_fun']:
+                args[arg] = pipeline_functions[args[arg]]
+            elif arg == 'curations':
+                Curation = namedtuple(
+                    'Curation', ['pa_hash', 'source_hash', 'tag'])
+                args[arg] = [
+                    Curation(cur['pa_hash'], cur['source_hash'], cur['tag'])
+                    for cur in args[arg]]
+        return args
+
+    def post(self):
+        args = self.process_args(request.json)
+        stmts = stmts_from_json(args.pop('statements'))
+        print(args)
+        stmts_out = pipeline_functions[self.func_name](stmts, **args)
         return _return_stmts(stmts_out)
 
 
-api.add_resource(PreassembleStatements, '/preassembly/<func_name>')
+for func_name, func in pipeline_functions.items():
+    if func.__module__ == 'indra.tools.assemble_corpus':
+        doc = ''
+        if func.__doc__:
+            doc = func.__doc__.split('\n')[0]
+        parser = reqparse.RequestParser()
+        parser.add_argument('statements', type=list, location='json')
+        for arg in inspect.getargspec(func).args:
+            if arg != 'stmts_in':
+                add_arg_to_parser(arg, parser)
+        parsers[func_name] = parser
+
+        @preassembly.expect(parsers[func_name])
+        @preassembly.route(f'/{func_name}', doc={'description': doc})
+        class NewFunction(PreassembleStatements):
+            func_name = func_name
 
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser('Run the INDRA REST API')
     argparser.add_argument('--host', default='0.0.0.0')
     argparser.add_argument('--port', default=8081, type=int)
-    args = argparser.parse_args()
-    app.run(host=args.host, port=args.port)
+    argparserargs = argparser.parse_args()
+    app.run(host=argparserargs.host, port=argparserargs.port)
