fa7a528ea0bb83f9e024a4675f5906f039db580e
Ben Gyori <ben.gyori@gmail.com>
2020-06-10 15:40:12 -0400

    Enable configuration of virtual ontology

--- indra/ontology/bio/__init__.py
+++ indra/ontology/bio/__init__.py
@@ -1,4 +1,11 @@
 """Module containing the implementation of an IndraOntology for the
  general biology use case."""
+__all__ = ['bio_ontology', 'BioOntology']
+
+from indra.config import get_config
 from .ontology import BioOntology
-bio_ontology = BioOntology()
+from ..virtual import VirtualOntology
+
+indra_ontology_url = get_config('INDRA_ONTOLOGY_URL')
+bio_ontology = BioOntology() if not indra_ontology_url else \
+    VirtualOntology(url=indra_ontology_url)
