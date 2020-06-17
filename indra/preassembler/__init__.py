8ef57092fbf1997a8b980d691ab9f659bd12d01c
Ben Gyori <ben.gyori@gmail.com>
2020-06-17 10:57:33 -0400

    Make sure the ontology is initialized before grouping

--- indra/preassembler/__init__.py
+++ indra/preassembler/__init__.py
@@ -350,6 +350,7 @@ class Preassembler(object):
     def _generate_id_maps(self, unique_stmts, poolsize=None,
                           size_cutoff=100, split_idx=None):
         """Connect statements using their refinement relationships."""
+        self.ontology.initialize()
         if len(unique_stmts) > 10000:
             self.ontology._build_transitive_closure()
         # Check arguments relating to multiprocessing
