5d1d1f7d84c42a7632980ff7c191d4371ab5332b
Ben Gyori <ben.gyori@gmail.com>
2020-06-23 17:52:05 -0400

    Add DrugBank xrefs to ontology graph

--- indra/ontology/bio/ontology.py
+++ indra/ontology/bio/ontology.py
@@ -207,13 +207,16 @@ class BioOntology(IndraOntology):
         self.add_edges_from(edges)
 
     def add_chemical_xrefs(self):
-        from indra.databases import chebi_client
+        from indra.databases import chebi_client, drugbank_client
         mappings = [
             (chebi_client.chebi_chembl, 'CHEBI', 'CHEMBL', True),
             (chebi_client.chebi_pubchem, 'CHEBI', 'PUBCHEM', False),
             (chebi_client.pubchem_chebi, 'PUBCHEM', 'CHEBI', False),
             (chebi_client.hmdb_chebi, 'HMDB', 'CHEBI', True),
             (chebi_client.cas_chebi, 'CAS', 'CHEBI', True),
+            (drugbank_client.drugbank_to_db, 'DRUGBANK', None, False),
+            (drugbank_client.db_to_drugbank, None, 'DRUGBANK', False),
+
         ]
         edges = []
         data = {'type': 'xref', 'source': 'chebi'}
@@ -225,6 +228,12 @@ class BioOntology(IndraOntology):
 
         for map_dict, from_ns, to_ns, symmetric in mappings:
             for from_id, to_id in map_dict.items():
+                # Here we assume if no namespace is given, then
+                # we're dealing with a (namespace, id) tuple
+                if from_ns is None:
+                    from_ns, from_id = from_id
+                if to_ns is None:
+                    to_ns, to_id = to_id
                 source = label_fix(from_ns, from_id)
                 target = label_fix(to_ns, to_id)
                 edges.append((source, target, data))
