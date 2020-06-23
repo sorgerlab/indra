6b7e220cdaa403354104aa0fbeabdce8ce37ff13
steppi <albert_steppi@hms.harvard.edu>
2020-06-23 10:35:12 -0400

    Update expected number of tas statements in test

--- indra/tests/test_tas.py
+++ indra/tests/test_tas.py
@@ -7,6 +7,6 @@ def test_processor():
     assert tp.statements
     num_stmts = len(tp.statements)
     # This is the total number of statements about human genes
-    assert num_stmts == 51722, num_stmts
+    assert num_stmts == 1601159, num_stmts
     assert all(len(s.evidence) == 1 for s in tp.statements), \
         "Some statements lack evidence, or have extra evidence."
