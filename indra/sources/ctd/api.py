259b393802491afdc1f79de17602456c50a76fb0
Ben Gyori <ben.gyori@gmail.com>
2020-06-24 21:22:47 -0400

    Implement more subsets and relation types

--- indra/sources/ctd/api.py
+++ indra/sources/ctd/api.py
@@ -1,19 +1,38 @@
 import pandas
-from .processor import CTDProcessor
+from .processor import CTDChemicalDiseaseProcessor, \
+    CTDGeneDiseaseProcessor, CTDChemicalGeneProcessor
+
+base_url = 'http://ctdbase.org/reports/'
+
+urls = {
+    'chemical_gene': base_url + 'CTD_chem_gene_ixns.tsv.gz',
+    'chemical_disease': base_url + 'CTD_chemicals_diseases.tsv.gz',
+    'gene_disease': base_url + 'CTD_genes_diseases.tsv.gz',
+}
+
+processors = {
+    'chemical_gene': CTDChemicalGeneProcessor,
+    'chemical_disease': CTDChemicalDiseaseProcessor,
+    'gene_disease': CTDGeneDiseaseProcessor,
+}
 
 
-def process_from_web():
-    url = 'http://ctdbase.org/reports/CTD_chem_gene_ixns.tsv.gz'
-    df = pandas.read_csv(url, sep='\t', skiprows=29)
+def process_from_web(subset):
+    if subset not in urls:
+        raise ValueError('%s is not a valid CTD subset.')
+    df = pandas.read_csv(urls[subset], sep='\t', comment='#',
+                         header=None)
     return process_dataframe(df)
 
 
-def process_tsv(fname):
-    df = pandas.read_csv(fname, sep='\t', skiprows=29)
-    return process_dataframe(df)
+def process_tsv(fname, subset):
+    df = pandas.read_csv(fname, sep='\t', comment='#', header=None)
+    return process_dataframe(df, subset)
 
 
-def process_dataframe(df):
-    cp = CTDProcessor(df)
-    cp.extract_chemical_gene()
+def process_dataframe(df, subset):
+    if subset not in processors:
+        raise ValueError('%s is not a valid CTD subset.')
+    cp = processors[subset](df)
+    cp.extract_statements()
     return cp
--- indra/sources/ctd/processor.py
+++ indra/sources/ctd/processor.py
@@ -7,25 +7,100 @@ from indra.ontology.standardize import standardize_db_refs, \
 
 
 rel_mapping = {
-    'increases^expression': IncreaseAmount,
-    'decreases^expression': DecreaseAmount,
+    # Activity regulation
     'increases^activity': Activation,
     'decreases^activity': Inhibition,
+    # Amount regulation
+    'increases^expression': IncreaseAmount,
+    'decreases^expression': DecreaseAmount,
+    'increases^chemical synthesis': IncreaseAmount,
+    'decreases^chemical synthesis': DecreaseAmount,
+    'increases^degradation': DecreaseAmount,
+    'decreases^degradation': IncreaseAmount,
+    'increases^abundance': IncreaseAmount,
+    'decreases^abundance': DecreaseAmount,
+    'increases^stability': IncreaseAmount,
+    'decreases^stability': DecreaseAmount,
+    # Modification
+    'increases^phosphorylation': Phosphorylation,
+    'decreases^phosphorylation': Dephosphorylation,
+    'increases^acetylation': Acetylation,
+    'decreases^acetylation': Deacetylation,
+    'increases^ubiquitination': Ubiquitination,
+    'decreases^ubiquitination': Deubiquitination,
+    'increases^hydroxylation': Hydroxylation,
+    'decreases^hydroxylation': Dehydroxylation,
+    'increases^methylation': Methylation,
+    'decreases^methylation': Demethylation,
+    'increases^farnesylation': Farnesylation,
+    'decreases^farnesylation': Defarnesylation,
+    'increases^palmitoylation': Palmitoylation,
+    'decreases^palmitoylation': Depalmitoylation,
+    'increases^ribosylation': Ribosylation,
+    'decreases^ribosylation': Deribosylation,
+    'increases^sumoylation': Sumoylation,
+    'decreases^sumoylation': Desumoylation,
+    # For gene/disease and chemical/disease effects
+    'therapeutic': Inhibition
 }
 
 
-class CTDProcessor:
+class CTDChemicalDiseaseProcessor:
     def __init__(self, df):
         self.df = df
         self.statements = []
 
-    def extract_chemical_gene(self):
+    def extract_statements(self):
         for _, row in tqdm.tqdm(self.df.iterrows()):
-            chem_name, chem_mesh_id, _, gene_name, gene_entrez_id, \
-                gene_type, organism_name, organism_tax_id, txt, \
+            chem_name, chem_mesh_id, chem_cas_id, disease_name, disease_id,\
+                direct_ev, inf_gene, inf_score, omim_ids, pmids = list(row)
+            if not direct_ev:
+                continue
+            chem_agent = get_chemical_agent(chem_name, chem_mesh_id,
+                                            chem_cas_id)
+            disease_agent = get_disease_agent(disease_name, disease_id)
+            stmt_types = get_statement_types(direct_ev)
+            evs = [Evidence(pmid=pmid) for pmid in pmids.split('|')]
+            for stmt_type in stmt_types:
+                stmt = stmt_type(chem_agent, disease_agent,
+                                 evidence=evs)
+                self.statements.append(stmt)
+
+
+class CTDGeneDiseaseProcessor:
+    def __init__(self, df):
+        self.df = df
+        self.statements = []
+
+    def extract_statements(self):
+        for _, row in tqdm.tqdm(self.df.iterrows()):
+            gene_name, gene_entrez_id, disease_name, disease_id, direct_ev, \
+                inf_chem, inf_score, omim_ids, pmids = list(row)
+            if not direct_ev:
+                continue
+            disease_agent = get_disease_agent(disease_name, disease_id)
+            gene_agent = get_gene_agent(gene_name, gene_entrez_id)
+            stmt_types = get_statement_types(direct_ev)
+            evs = [Evidence(pmid=pmid) for pmid in pmids.split('|')]
+            for stmt_type in stmt_types:
+                stmt = stmt_type(gene_agent, disease_agent,
+                                 evidence=evs)
+                self.statements.append(stmt)
+
+
+class CTDChemicalGeneProcessor:
+    def __init__(self, df):
+        self.df = df
+        self.statements = []
+
+    def extract_statements(self):
+        for _, row in tqdm.tqdm(self.df.iterrows()):
+            chem_name, chem_mesh_id, chem_cas_id, gene_name, gene_entrez_id, \
+                gene_forms, organism_name, organism_tax_id, txt, \
                 rels, pmids = list(row)
 
-            chem_agent = get_chemical_agent(chem_name, chem_mesh_id)
+            chem_agent = get_chemical_agent(chem_name, chem_mesh_id,
+                                            chem_cas_id)
             gene_agent = get_gene_agent(gene_name, gene_entrez_id)
             stmt_types = get_statement_types(rels)
             context = get_context(organism_name, organism_tax_id)
@@ -49,6 +124,18 @@ def get_statement_types(rel_str):
     return [rel_mapping[rel] for rel in rels if rel in rel_mapping]
 
 
+def get_disease_agent(name, disease_id):
+    groundings = disease_id.split('|')
+    db_refs = {}
+    for gr in groundings:
+        db_ns, db_id = gr.split(':')
+        db_refs[db_ns] = db_id
+    standard_name, db_refs = standardize_name_db_refs(db_refs)
+    if standard_name:
+        name = standard_name
+    return Agent(name, db_refs=db_refs)
+
+
 def get_gene_agent(name, gene_entrez_id):
     db_refs = {'EGID': gene_entrez_id}
     hgnc_id = hgnc_client.get_hgnc_id(name)
@@ -60,6 +147,9 @@ def get_gene_agent(name, gene_entrez_id):
     return Agent(name, db_refs=db_refs)
 
 
-def get_chemical_agent(name, mesh_id):
-    db_refs = standardize_db_refs({'MESH': mesh_id})
+def get_chemical_agent(name, mesh_id, cas_id):
+    db_refs = {'MESH': mesh_id}
+    if cas_id:
+        db_refs['CAS'] = cas_id
+    db_refs = standardize_db_refs(db_refs)
     return Agent(name, db_refs=db_refs)
