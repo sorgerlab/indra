e5b08d16ed7a7cb70ceca407642e16b03a383e0d
Ben Gyori <ben.gyori@gmail.com>
2020-07-04 14:05:52 -0400

    Add docstrings

--- indra/sources/drugbank/__init__.py
+++ indra/sources/drugbank/__init__.py
@@ -1,1 +1,7 @@
+"""This module provides an API and processor for DrugBank content.
+It builds on the XML-formatted data schema of DrugBank and expects
+the XML file to be available locally. The full DrugBank download
+can be obtained at: https://www.drugbank.ca/releases/latest. Once the
+XML file is decompressed, it can be processed using the process_xml
+function."""
 from .api import process_xml
--- indra/sources/drugbank/api.py
+++ indra/sources/drugbank/api.py
@@ -6,6 +6,20 @@ logger = logging.getLogger(__name__)
 
 
 def process_xml(fname):
+    """Return a processor by extracting Statement from DrugBank XML.
+
+    Parameters
+    ----------
+    fname : str
+        The path to a DrugBank XML file to process.
+
+    Returns
+    -------
+    DrugbankProcessor
+        A DrugbankProcessor instance which contains a list of INDRA
+        Statements in its statements attribute that were extracted
+        from the given XML file.
+    """
     logger.info('Loading %s...' % fname)
     et = ElementTree.parse(fname)
     logger.info('Extracting DrugBank statements...')
