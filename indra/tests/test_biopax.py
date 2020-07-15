d2ed8c3fa025fea260eb1aba87f8e100583f5221
Ben Gyori <ben.gyori@gmail.com>
2020-07-15 17:51:33 -0400

    Overhaul tests for BioPAX processor

--- indra/tests/test_biopax.py
+++ indra/tests/test_biopax.py
@@ -1,16 +1,20 @@
 import os
+from collections import defaultdict
 from indra.sources import biopax
+from indra.statements import *
 import indra.sources.biopax.processor as bpc
-from indra.databases import uniprot_client
 from indra.util import unicode_strs
 from nose.plugins.attrib import attr
 
 model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                           'biopax_test.owl')
 
-
 bp = biopax.process_owl(model_path)
-uri_prefix = 'http://purl.org/pc2/7/'
+
+stmts_by_source_id = defaultdict(set)
+for stmt in bp.statements:
+    for ev in stmt.evidence:
+        stmts_by_source_id[ev.source_id.split('/')[-1]].add(stmt)
 
 
 def test_listify():
@@ -19,43 +23,58 @@ def test_listify():
     assert bpc._listify([1] == [1])
 
 
-def test_protein_agent():
-    bpe = bp.model.objects['Protein_b48159c1c65acd16a40bd7ea46e50e52']
-    agents = bp._get_agents_from_singular_entity(bpe)
-    assert len(agents) == 1
-    agent = agents[0]
-    assert agent.name == 'ARHGAP35'
-    assert len(agent.mods) == 1
-    assert agent.mods[0].position == '1105'
-    assert agent.db_refs['UP'] == 'Q9NRY4'
-    assert agent.db_refs['HGNC'] == '4591'
+def test_protein_family_agent():
+    bpe = bp.model.objects['Protein_da79d1a005a8eb259b0c09278ae9230e']
+    agents = bp._get_agents_from_entity(bpe)
+    assert len(agents) == 2
+    assert {a.name for a in agents} == {'MAPK1', 'MAPK3'}
+
+    mapk3 = agents[0]
+    assert mapk3.name == 'MAPK3'
+    assert len(mapk3.mods) == 2
+    assert mapk3.mods[0].position == '202'
+    assert mapk3.db_refs['UP'] == 'P27361', mapk3.db_refs
+    assert mapk3.db_refs['HGNC'] == '6877', mapk3.db_refs
 
 
-def test_get_hgnc_id():
-    bpe = bp.model.objects['http://identifiers.org/uniprot/Q86YV0']
-    hgnc_id = bp._get_hgnc_id(bpe)
-    assert hgnc_id == '26129', hgnc_id
+def test_phosphorylation_extraction():
+    cat = 'Catalysis_12411c77e2fd2252f2a8b52bbee6eeb9'
+    stmts = list(stmts_by_source_id[cat])
+    assert len(stmts) == 2
+    assert all(isinstance(stmt, Phosphorylation) for stmt in stmts)
+    assert all(stmt.enz.name == 'BRAF' for stmt in stmts)
+    assert all(stmt.sub.name == 'MAP2K1' for stmt in stmts)
+    assert {stmt.position for stmt in stmts} == {'218', '222'}
 
 
-def test_get_mod_feature():
-    bpe = bp.model.objects[
-            'ModificationFeature_9b4c5a6e2db0be17ac63ae2adb2e5313']
-    mc = bpc.BiopaxProcessor.mod_condition_from_mod_feature(bpe)
-    assert mc.mod_type == 'phosphorylation'
-    assert mc.residue == 'T'
-    assert mc.position == '42'
+def test_active_form_extraction():
+    br = 'BiochemicalReaction_5271ab5d568f463bffa8bb378e8aa257'
+    stmts = list(stmts_by_source_id[br])
+    assert len(stmts) == 2, stmts
+    stmt = stmts[0]
+    assert isinstance(stmt, ActiveForm)
+    assert stmt.agent.name in {'MAPK1', 'MAPK3'}
+    assert len(stmt.agent.mods) == 2
 
 
-def test_get_entity_mods():
-    bpe = bp.model.objects['Protein_fb17cc169251fd5c9e0d02fe62df6d33']
-    mods = bpc.BiopaxProcessor._get_entity_mods(bpe)
-    assert len(mods) == 5, mods
-    mod_types = set([m.mod_type for m in mods])
-    assert mod_types == {'phosphorylation'}
-    residues = {m.residue for m in mods}
-    assert residues == {'Y', 'T', 'S'}, mods
-    mod_pos = {m.position for m in mods}
-    assert mod_pos == {'302', '452', '576', '299', '455'}
+def test_amount_regulation_extraction():
+    br = 'TemplateReactionRegulation_77553a64e29e82b517d0be230363b757'
+    stmts = list(stmts_by_source_id[br])
+    assert len(stmts) == 1, stmts
+    stmt = stmts[0]
+    assert isinstance(stmt, IncreaseAmount)
+    assert stmt.subj.name == 'reactive oxygen species'
+    assert stmt.subj.db_refs['CHEBI'] == 'CHEBI:26523'
+    assert stmt.obj.name == 'TP53'
+
+
+def test_activity_regulation_extraction():
+    br = 'Catalysis_5710598d0317be9d4742b469d1322a48'
+    stmts = list(stmts_by_source_id[br])
+    assert len(stmts) == 3, stmts
+    assert {s.subj.name for s in stmts} == {'HRAS', 'NRAS', 'KRAS'}
+    assert isinstance(stmts[0], Activation)
+    assert all(s.obj.name == 'BRAF' for s in stmts)
 
 
 @attr('webservice', 'slow')
@@ -66,40 +85,6 @@ def test_pathsfromto():
     assert unicode_strs(bp.statements)
     num_unique = len({s.get_hash(shallow=False) for s in bp.statements})
     assert len(bp.statements) == num_unique
-
-
-def test_all_uniprot_ids():
-    for obj in bp.model.objects.values():
-        if bpc._is_protein(obj):
-            uniprot_id = bp._get_uniprot_id(obj)
-            if uniprot_id is not None:
-                assert not uniprot_client.is_secondary(uniprot_id)
-                assert unicode_strs(uniprot_id)
-
-
-def test_all_hgnc_ids():
-    for obj in bp.model.objects.values():
-        if bpc._is_protein(obj):
-            hgnc_id = bp._get_hgnc_id(obj)
-            if hgnc_id is not None:
-                assert unicode_strs(hgnc_id)
-
-
-def test_all_protein_db_refs():
-    unmapped_uniprot_ids = []
-    for obj in bp.model.objects.values():
-        if bpc._is_protein(obj):
-            db_refs = bpc.BiopaxProcessor._get_db_refs(obj)
-            uniprot_id = db_refs.get('UP')
-            hgnc_id = db_refs.get('HGNC')
-            if uniprot_id:
-                if uniprot_client.is_human(uniprot_id):
-                    if not hgnc_id:
-                        unmapped_uniprot_ids.append(uniprot_id)
-    unmapped_uniprot_ids = sorted(list(set(unmapped_uniprot_ids)))
-    # The number of unmapped entries should not increase
-    # so we check for an upper limit here
-    assert len(unmapped_uniprot_ids) < 95
 
 
 def assert_pmids(stmts):
