0912f6910ac436f8f09848b4485e39e5c308f70e
Ben Gyori <ben.gyori@gmail.com>
2020-07-23 19:48:06 -0400

    Fix the index dumper code

--- indra/tools/live_curation/dump_index.py
+++ indra/tools/live_curation/dump_index.py
@@ -2,16 +2,18 @@
 
 import boto3
 
-res = s3.list_objects(Bucket='world-modelers', Prefix='indra_models')
-s3 = boto3.session.Session(profile_name='wm').client('s3')
 
-corpora = []
-for entry in res['Content']:
-    if entry['Key'].endswith('/statements.json'):
-        corpus_id = entry['Key'].split('/')[1]
-        mod = entry['LastModified']
-        corpora.append((corpus_id, mod.strftime('%Y-%m-%d-%H-%M-%S')))
+if __name__ == '__main__':
+    s3 = boto3.session.Session(profile_name='wm').client('s3')
+    res = s3.list_objects(Bucket='world-modelers', Prefix='indra_models')
 
-with open('index.csv', 'w') as fh:
-    for corpus_id, mod in sorted(corpora, key=lambda x: x[1]):
-        fh.write('%s,%s\n' % (corpus_id, mod))
+    corpora = []
+    for entry in res['Content']:
+        if entry['Key'].endswith('/statements.json'):
+            corpus_id = entry['Key'].split('/')[1]
+            mod = entry['LastModified']
+            corpora.append((corpus_id, mod.strftime('%Y-%m-%d-%H-%M-%S')))
+
+    with open('index.csv', 'w') as fh:
+        for corpus_id, mod in sorted(corpora, key=lambda x: x[1]):
+            fh.write('%s,%s\n' % (corpus_id, mod))
