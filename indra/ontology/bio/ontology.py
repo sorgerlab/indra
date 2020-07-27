7290344b22c1696ad8f9910866a66d936f35c084
Ben Gyori <ben.gyori@gmail.com>
2020-07-27 14:18:13 -0400

    Add DrugBank nodes to ontology

--- indra/ontology/bio/ontology.py
+++ indra/ontology/bio/ontology.py
@@ -59,6 +59,7 @@ class BioOntology(IndraOntology):
         self.add_mirbase_nodes()
         self.add_chembl_nodes()
         self.add_hms_lincs_nodes()
+        self.add_drugbank_nodes()
         # Add xrefs
         logger.info('Adding xrefs...')
         self.add_hgnc_uniprot_xrefs()
@@ -380,6 +381,14 @@ class BioOntology(IndraOntology):
                               self.label('HMS-LINCS', hmsl_id),
                               {'type': 'xref', 'source': 'hms-lincs'}))
         self.add_edges_from(edges)
+
+    def add_drugbank_nodes(self):
+        from indra.databases import drugbank_client
+        nodes = []
+        for db_id, db_name in drugbank_client.drugbank_names.items():
+            nodes.append((self.label('DRUGBANK', db_id),
+                          {'name': db_name}))
+        self.add_nodes_from(nodes)
 
     def add_activity_hierarchy(self):
         rels = [
