35f270c28749bb15cd0d114b1f48eb9ce978d886
dianakolusheva <dianakolusheva@gmail.com>
2020-08-05 10:58:07 -0400

    Add mk_str to statements __all__

--- indra/statements/statements.py
+++ indra/statements/statements.py
@@ -215,7 +215,7 @@ __all__ = [
     'modtype_to_modclass',
     'modclass_to_modtype', 'modtype_conditions', 'modtype_to_inverse',
     'modclass_to_inverse', 'get_statement_by_name', 'make_hash', 'stmt_type',
-    'default_ns_order'
+    'default_ns_order', 'mk_str'
     ]
 
 import abc
