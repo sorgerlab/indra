4b10355d256f1a0bf6b7cc8eae1eda4b8794cb09
kkaris <karis.klas@gmail.com>
2020-08-19 13:35:03 -0400

    Write short blurb about module and its usage

--- indra/sources/omnipath/__init__.py
+++ indra/sources/omnipath/__init__.py
@@ -1,2 +1,18 @@
+"""
+The OmniPath module accesses biomolecular interaction data from various
+curated databases using the OmniPath API (see
+https://saezlab.github.io/pypath/html/index.html#webservice) and processes
+the returned data into statements using the OmniPathProcessor.
+
+Currently, the following data is collected:
+  - Modifications from the PTMS endpoint https://saezlab.github.io/pypath/html/index.html#enzyme-substrate-interactions
+  - Ligand-Receptor data from the interactions endpoint https://saezlab.github.io/pypath/html/index.html#interaction-datasets
+
+To process all statements, use the function `process_from_web`:
+
+>>> from indra.sources.omnipath import process_from_web
+>>> omnipath_processor = process_from_web()
+>>> stmts = omnipath_processor.statements
+"""
 from .api import process_from_web
 from .processor import OmniPathProcessor
