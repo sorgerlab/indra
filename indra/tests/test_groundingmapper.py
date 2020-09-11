2afe8fa71ee2b02e740c66fe591a3d040758c55b
Ben Gyori <ben.gyori@gmail.com>
2020-09-10 21:56:03 -0400

    Add test for ignores and disambiguation

--- indra/tests/test_groundingmapper.py
+++ indra/tests/test_groundingmapper.py
@@ -490,3 +490,29 @@ def test_gilda_disambiguation_local():
         annotations
     # This is to make sure the to_json of the ScoredMatches works
     assert annotations['agents']['gilda'][1][0]['term']['db'] == 'HGNC'
+
+
+def test_text_and_norm_text():
+    gm.gilda_mode = 'local'
+
+    # We should filter out ignores in both TEXT and TEXT_NORM
+    ag = Agent('x', db_refs={'TEXT': 'XREF_BIBR', 'TEXT_NORM': 'ERK'})
+    stmt = Phosphorylation(None, ag)
+    res = gm.map_stmts([stmt])
+    assert not res
+    ag = Agent('x', db_refs={'TEXT': 'ERK', 'TEXT_NORM': 'XREF_BIBR'})
+    stmt = Phosphorylation(None, ag)
+    res = gm.map_stmts([stmt])
+    assert not res
+
+    # We should disambiguate based on both TEXT and TEXT_NORM
+    ag = Agent('x', db_refs={'TEXT': 'AA', 'TEXT_NORM': 'XXX'},)
+    stmt = Phosphorylation(None, ag,
+                           evidence=Evidence(text='Arachidonic acid (AA)'))
+    res = gm.map_stmts([stmt])
+    assert res[0].sub.name == 'arachidonic acid', res[0]
+    ag = Agent('x', db_refs={'TEXT': 'XXX', 'TEXT_NORM': 'AA'})
+    stmt = Phosphorylation(None, ag,
+                           evidence=Evidence(text='Arachidonic acid (AA)'))
+    res = gm.map_stmts([stmt])
+    assert res[0].sub.name == 'arachidonic acid', res[0]
