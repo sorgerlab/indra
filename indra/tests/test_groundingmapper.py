6822d2e0c344c9e82aaa98cb4cab0502ee999eb7
Ben Gyori <ben.gyori@gmail.com>
2020-09-10 22:13:36 -0400

    Add test for basic grounding mapping

--- indra/tests/test_groundingmapper.py
+++ indra/tests/test_groundingmapper.py
@@ -516,3 +516,13 @@ def test_text_and_norm_text():
                            evidence=Evidence(text='Arachidonic acid (AA)'))
     res = gm.map_stmts([stmt])
     assert res[0].sub.name == 'arachidonic acid', res[0]
+
+    ag = Agent('x', db_refs={'TEXT': 'XXX', 'TEXT_NORM': 'ERK'})
+    stmt = Phosphorylation(None, ag)
+    res = gm.map_stmts([stmt])
+    assert res[0].sub.name == 'ERK', res[0]
+
+    ag = Agent('x', db_refs={'TEXT': 'ERK', 'TEXT_NORM': 'XXX'})
+    stmt = Phosphorylation(None, ag)
+    res = gm.map_stmts([stmt])
+    assert res[0].sub.name == 'ERK', res[0]
