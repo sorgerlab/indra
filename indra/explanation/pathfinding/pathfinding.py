6157dc305e6f042a3764928c5a31ff7b7170e846
kolusask <19ak97@gmail.com>
2020-09-14 18:19:37 +0200

    Select open dijkstra weight attribute based on hashes presence

--- indra/explanation/pathfinding/pathfinding.py
+++ indra/explanation/pathfinding/pathfinding.py
@@ -768,7 +768,7 @@ def open_dijkstra_search(g, start, reverse=False, depth_limit=2, path_limit=None
     else:
         proper_path = lambda x: True
     paths = list(nx.single_source_dijkstra_path(g, start,
-                                                weight='weight').values())[1:]
+                                                weight=('context_weight' if hashes else weight)).values())[1:]
     if path_limit is not None:
         for p in paths:
             path_limit -= 1
