751c39ae69ff114c6bc3deaedca9e95bfe4c2b78
Ben Gyori <ben.gyori@gmail.com>
2020-10-06 00:00:31 -0400

    Add some resource functions

--- indra/resources/__init__.py
+++ indra/resources/__init__.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 RESOURCES_PATH = os.path.dirname(os.path.abspath(__file__))
 
@@ -11,3 +12,14 @@ def open_resource_file(resource_name, *args, **kwargs):
         resource_path = resource_name
 
     return open(resource_path, *args, **kwargs)
+
+
+def get_resource_path(fname):
+    """Return the absolute path to a file in the resource folder."""
+    return os.path.join(RESOURCES_PATH, fname)
+
+
+def load_resource_json(fname):
+    """Return a loaded INDRA JSON resource file."""
+    with open(get_resource_path(fname), 'r') as fh:
+        return json.load(fh)
