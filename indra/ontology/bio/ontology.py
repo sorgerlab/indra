ab499bc8cf13c06d992ba0a3ae2028ba52bf647e
Ben Gyori <ben.gyori@gmail.com>
2020-10-13 09:41:17 -0400

    Update bio ontology version

--- indra/ontology/bio/ontology.py
+++ indra/ontology/bio/ontology.py
@@ -19,7 +19,7 @@ class BioOntology(IndraOntology):
     # should be incremented to "force" rebuilding the ontology to be consistent
     # with the underlying resource files.
     name = 'bio'
-    version = '1.3'
+    version = '1.4'
 
     def __init__(self):
         super().__init__()
