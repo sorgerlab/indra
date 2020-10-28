19fc141fbe1a2bb5c7ca9684e8d20f9f7819e35b
Ben Gyori <ben.gyori@gmail.com>
2020-10-28 11:59:55 -0400

    Add link to PyBioPAX

--- indra/sources/biopax/__init__.py
+++ indra/sources/biopax/__init__.py
@@ -1,9 +1,9 @@
 """This module allows processing BioPAX content into INDRA Statements. It uses
-the pybiopax package to process OWL files or strings, or to obtain
-BioPAX content by querying the PathwayCommons web service. The module has
-been tested with BioPAX content from PathwayCommons
-https://www.pathwaycommons.org/archives/PC2/v12/. BioPAX from other sources
-may not adhere to the same conventions and could result in processing issues,
+the pybiopax package (https://github.com/indralab/pybiopax) to process OWL
+files or strings, or to obtain BioPAX content by querying the PathwayCommons
+web service. The module has been tested with BioPAX content from PathwayCommons
+https://www.pathwaycommons.org/archives/PC2/v12/. BioPAX from other sources may
+not adhere to the same conventions and could result in processing issues,
 though these can typically be addressed with minor changes in the processor's
 logic.
 """
