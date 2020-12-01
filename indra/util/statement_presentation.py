74b50f141805bddb73d4b9e93c0ef93ddc5e444a
pagreene <patrick.anton.greene@gmail.com>
2020-12-01 18:35:06 -0500

    Re-implement statemetn sorting with belief option.

--- indra/util/statement_presentation.py
+++ indra/util/statement_presentation.py
@@ -69,17 +69,86 @@ def _get_keyed_stmts(stmt_list):
         yield key, s
 
 
-def group_and_sort_statements(stmt_list, ev_totals=None, source_counts=None):
+class StmtMetricAgg:
+    """A class to aggregate statement metrics efficiently."""
+    def __init__(self, ev_counts=None, beliefs=None,
+                 source_count_arrays=None, source_count_cols=None):
+        self.__src_arrays = source_count_arrays
+        self.__source_cols = source_count_cols
+        n_sources = len(next(iter(self.__src_arrays.values())))
+        self.__ev_counts = ev_counts
+        self.__beliefs = beliefs
+
+        # Init the results.
+        self.src_count = zeros(n_sources)
+        self.ev_count = 0
+        self.belief = 0
+
+    def add(self, stmt):
+        sh = stmt.get_hash()
+
+        # Update the evidence count.
+        if self.__ev_counts is None or sh not in self.__ev_counts:
+            n_ev = len(stmt.evidence)
+        else:
+            n_ev = self.__ev_counts[sh]
+        self.ev_count += n_ev
+
+        # Update the source counts.
+        if self.__src_arrays and sh in self.__src_arrays:
+            self.src_count += self.__src_arrays[sh]
+
+        # Update the belief.
+        if self.__beliefs is None or sh not in self.__beliefs:
+            belief = stmt.belief
+        else:
+            belief = self.__beliefs[sh]
+        self.belief = max(self.belief, belief)
+        return
+
+    def get_source_counts(self):
+        if self.__source_cols is None:
+            assert not len(self.src_count)
+            return {}
+        return dict(zip(self.__source_cols, self.src_count.astype(int)))
+
+    @classmethod
+    def get_prepped_constructor(cls, ev_counts=None, beliefs=None,
+                                source_counts=None):
+        # Turn the source dicts into arrays.
+        if source_counts:
+            src_arrays = {}
+            for h, counts in source_counts.items():
+                src_arrays[h] = array(list(counts.values()))
+            source_cols = list(counts.keys())
+        else:
+            src_arrays = {}
+            source_cols = None
+
+        # Define the constructor function.
+        def constructor():
+            return cls(ev_counts, beliefs, src_arrays, source_cols)
+        return constructor
+
+
+def group_and_sort_statements(stmt_list, sort_by='ev_count', ev_counts=None,
+                              source_counts=None, beliefs=None):
     """Group statements by type and arguments, and sort by prevalence.
 
     Parameters
     ----------
     stmt_list : list[Statement]
         A list of INDRA statements.
-    ev_totals : dict{int: int}
+    sort_by : str
+        Indicate which parameter to sort by, either 'belief' or 'ev_count'. The
+        default is 'ev_count'.
+    ev_counts : dict{int: int}
         A dictionary, keyed by statement hash (shallow) with counts of total
         evidence as the values. Including this will allow statements to be
-        better sorted.
+        sorted by evidence count..
+    beliefs : dict{int: float}
+        A dictionary, keyed by statement hash (shallow) with the belief of each
+        statement. Including this will allow statements to be sorted by belief.
     source_counts : dict{int: OrderedDict}
         A dictionary, keyed by statement hash, with an OrderedDict of
         counts per source (ordered).
@@ -91,41 +160,24 @@ def group_and_sort_statements(stmt_list, ev_totals=None, source_counts=None):
         of statements, also sorted by evidence count, for that key and type.
         The sort key contains a count of statements with those argument, the
         arguments (normalized strings), the count of statements with those
-        arguements and type, and then the statement type.
+        arguments and type, and then the statement type.
     """
-    if source_counts:
-        src_arrays = {}
-        for h, counts in source_counts.items():
-            src_arrays[h] = array(list(counts.values()))
-        n_counts = len(counts) + 1
-        cols = list(counts.keys())
-    else:
-        src_arrays = {}
-        n_counts = 1
-        cols = None
-
-    def _counts(stmt):
-        sh = stmt.get_hash()
-        if ev_totals is None or sh not in ev_totals:
-            tot = len(stmt.evidence)
-        else:
-            tot = ev_totals[sh]
-
-        if src_arrays and sh in src_arrays:
-            return concatenate([array([tot]), src_arrays[sh]])
-        else:
-            return array([tot])
+    if sort_by not in ['belief', 'ev_count']:
+        raise ValueError(f"Parameter `sort_by` must be 'belief' or 'ev_count'!")
 
     stmt_rows = defaultdict(list)
-    stmt_counts = defaultdict(lambda: zeros(n_counts))
-    arg_counts = defaultdict(lambda: zeros(n_counts))
+
+    counter_maker = StmtMetricAgg\
+        .get_prepped_constructor(ev_counts, beliefs, source_counts)
+    stmt_counts = defaultdict(counter_maker)
+    arg_counts = defaultdict(counter_maker)
     for key, s in _get_keyed_stmts(stmt_list):
         # Update the counts, and add key if needed.
         stmt_rows[key].append(s)
 
-        # Keep track of the total evidence counts for this statement and the
-        # arguments.
-        stmt_counts[key] += _counts(s)
+        # Keep track of the evidence counts, source counts, and max belief for
+        # this statement and the arguments.
+        stmt_counts[key].add(s)
 
         # Add up the counts for the arguments, pairwise for Complexes and
         # Conversions. This allows, for example, a complex between MEK, ERK,
@@ -134,30 +186,45 @@ def group_and_sort_statements(stmt_list, ev_totals=None, source_counts=None):
         if key[0] == 'Conversion':
             subj = key[1]
             for obj in key[2] + key[3]:
-                arg_counts[(subj, obj)] += _counts(s)
+                arg_counts[(subj, obj)].add(s)
         else:
-            arg_counts[key[1:]] += _counts(s)
+            arg_counts[key[1:]].add(s)
+
+    def _sort_key_func(stmt):
+        sh = stmt.get_hash()
+        n_agents = len(s.agent_list())
+        if sort_by == 'ev_count':
+            if ev_counts and sh in ev_counts:
+                cnt = ev_counts[sh]
+            else:
+                cnt = len(stmt.evidence)
+            return cnt + 1/(1 + n_agents)
+        elif sort_by == 'belief':
+            if beliefs and sh in beliefs:
+                b = beliefs[sh]
+            else:
+                b = stmt.belief
+            return b + 0.01/(1 + n_agents)
+        else:
+            assert False, "Invalid sort_by, should not be possible here."
 
     # Sort the rows by count and agent names.
     def processed_rows(stmt_rows):
         for key, stmts in stmt_rows.items():
             verb = key[0]
             inps = key[1:]
-            sub_count = stmt_counts[key][0]
-            arg_count = arg_counts[inps][0]
+            sub_count = stmt_counts[key].ev_count
+            arg_count = arg_counts[inps].ev_count
             if verb == 'Complex' and sub_count == arg_count and len(inps) <= 2:
                 if all([len(set(ag.name for ag in s.agent_list())) > 2
                         for s in stmts]):
                     continue
             new_key = (arg_count, inps, sub_count, verb)
-            stmts = sorted(stmts,
-                           key=(lambda s:
-                                _counts(s)[0] + 1 / (1+len(s.agent_list()))),
-                           reverse=True)
+            stmts = sorted(stmts, key=_sort_key_func, reverse=True)
             if source_counts:
-                yield new_key, verb, stmts,\
-                      dict(zip(cols, arg_counts[inps][1:].astype(int))),\
-                      dict(zip(cols, stmt_counts[key][1:].astype(int)))
+                yield new_key, verb, stmts, \
+                      arg_counts[inps].get_source_counts(), \
+                      stmt_counts[key].get_source_counts()
             else:
                 yield new_key, verb, stmts
 
@@ -275,6 +342,10 @@ def standardize_counts(counts):
 
 def get_available_ev_counts(stmts):
     return {stmt.get_hash(): len(stmt.evidence) for stmt in stmts}
+
+
+def get_available_beliefs(stmts):
+    return {stmt.get_hash(): stmt.belief for stmt in stmts}
 
 
 def get_available_source_counts(stmts):
