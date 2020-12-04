f976f2ebcad39374b7060619df6b78e815e71522
Ben Gyori <ben.gyori@gmail.com>
2020-12-04 14:19:34 -0500

    Update test to tab3.0 format

--- indra/tests/test_biogrid.py
+++ indra/tests/test_biogrid.py
@@ -8,14 +8,15 @@ test_file = os.path.join(this_dir, 'biogrid_tests_data/biogrid_test.txt')
 
 def test_biogrid_tsv():
     # Download biogrid file form the web and process it
-    bp = BiogridProcessor(test_file)
+    bp = BiogridProcessor(test_file, physical_only=False)
 
     # There are 50 statements in that file
     statements = bp.statements
-    assert len(statements) == 50
+    assert len(statements) == 50, len(statements)
 
     # Any given statement should be a complex, with appropriate evidence
-    s0 = statements[0]
+    # This one is a human protein complex
+    s0 = statements[33]
     assert isinstance(s0, Complex)
     ev = s0.evidence[0]
     assert ev.source_api == 'biogrid'
@@ -23,5 +24,7 @@ def test_biogrid_tsv():
     assert ev.pmid is not None
 
     # The first statement in the file involves MAP2K4 and FLNC
-    assert str(s0.members[0]) == 'MAP2K4()'
-    assert str(s0.members[1]) == 'FLNC()'
+    assert s0.members[0].name == 'OTUB1', s0
+    assert s0.members[1].name == 'DDX23', s0
+    assert set(s0.members[0].db_refs) == {'HGNC', 'UP', 'EGID'}
+    assert set(s0.members[1].db_refs) == {'HGNC', 'UP', 'EGID'}
