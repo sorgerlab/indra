af8c488ebcd4074ff877e15495d257fa9eafe608
steppi <albert_steppi@hms.harvard.edu>
2020-12-18 02:00:00 -0500

    Add test to check grounding map preferred to gilda

--- indra/tests/test_groundingmapper.py
+++ indra/tests/test_groundingmapper.py
@@ -501,6 +501,22 @@ def test_gilda_disambiguation_local():
     assert annotations['agents']['gilda'][1][0]['term']['db'] == 'HGNC'
 
 
+def test_grounding_map_gilda_priority():
+    gm.gilda_mode = 'web'
+    fetal_bovine_serum = Agent('FBS', db_refs={'TEXT': 'FBS'})
+    pmid = '28536624'
+    stmt = Phosphorylation(None, fetal_bovine_serum,
+                           evidence=[Evidence(pmid=pmid,
+                                              text_refs={'PMID': pmid})])
+    mapped_stmts = gm.map_stmts([stmt])
+    annotations = mapped_stmts[0].evidence[0].annotations
+    # agents should not be in annotations if gilda is run. Second condition
+    # added as future proofing in case some future change causes this mapping
+    # to add agent annotations in the future.
+    assert 'agents' not in annotations or \
+        'gilda' not in annotations['agents']
+
+
 def test_text_and_norm_text():
     gm.gilda_mode = 'local'
 
