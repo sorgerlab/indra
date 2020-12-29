eb9e9af4552c3ec8284ce1c3720cf64adff7b120
Ben Gyori <ben.gyori@gmail.com>
2020-12-28 21:44:20 -0500

    Update Famplex and ontology version

--- indra/ontology/bio/ontology.py
+++ indra/ontology/bio/ontology.py
@@ -19,7 +19,7 @@ class BioOntology(IndraOntology):
     # should be incremented to "force" rebuilding the ontology to be consistent
     # with the underlying resource files.
     name = 'bio'
-    version = '1.6'
+    version = '1.7'
 
     def __init__(self):
         super().__init__()
