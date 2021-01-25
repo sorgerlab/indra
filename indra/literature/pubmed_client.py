923a05cc5f2416b106735a78048bd75f126bfe51
dianakolusheva <dianakolusheva@gmail.com>
2021-01-25 16:38:02 -0500

    Get more complex titles

--- indra/literature/pubmed_client.py
+++ indra/literature/pubmed_client.py
@@ -263,7 +263,7 @@ def _get_title_from_article_element(article):
     title = None
     if title_tag is not None:
         title = title_tag.text
-        if title is None and hasattr(title_tag, 'itertext'):
+        if hasattr(title_tag, 'itertext'):
             title = ' '.join(list(title_tag.itertext()))
     return title
 
