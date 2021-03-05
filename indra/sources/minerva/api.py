af72879c42993f6f0883ef11688d15908da7a9a0
dianakolusheva <dianakolusheva@gmail.com>
2021-03-05 11:40:38 -0500

    Refactor api for new processor input

--- indra/sources/minerva/api.py
+++ indra/sources/minerva/api.py
@@ -1,26 +1,25 @@
 from .processor import SifProcessor
 
 
-def process_file(filename):
-    with open(filename, 'r') as f:
-        sif_strs = f.readlines()
-    return process_sif_strs(sif_strs)
+
+def process_file(filename, model_id):
+    return process_files({model_id: filename})
 
 
-def process_files(filenames):
-    sif_strs = []
-    for filename in filenames:
+def process_files(ids_to_filenames):
+    model_id_to_sif_strs = {}
+    for model_id, filename in ids_to_filenames.items():
         with open(filename, 'r') as f:
-            strs = f.readlines()
-        sif_strs += strs
-    return process_sif_strs(sif_strs)
+            sif_strs = f.readlines()
+        model_id_to_sif_strs[model_id] = sif_strs
+    return process_sif_strs(model_id_to_sif_strs)
 
 
 def process_from_web(url):
     pass
 
 
-def process_sif_strs(sif_strs):
-    sp = SifProcessor(sif_strs)
+def process_sif_strs(model_id_to_sif_strs):
+    sp = SifProcessor(model_id_to_sif_strs)
     sp.extract_statements()
     return sp
