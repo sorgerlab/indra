d179d9a184a9ee472cea80100ec8b6ca72a6b5b8
dianakolusheva <dianakolusheva@gmail.com>
2021-03-10 17:16:16 -0500

    Add docstring in init

--- indra/sources/minerva/__init__.py
+++ indra/sources/minerva/__init__.py
@@ -1,1 +1,10 @@
+"""
+This module implements extracting INDRA Statements from COVID-19 Disease Map
+models (https://covid19map.elixir-luxembourg.org/minerva/).
+Currently it supports a processor that extracts statements from SIF export of
+the models.
+
+More information about COVID-19 Disease Map project can be found at 
+https://covid.pages.uni.lu
+"""
 from .api import *
