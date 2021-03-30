47215ccd8aa1d60f5855b86fa2c606e6931d9264
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-03-30 17:05:25 +0200

    Get version with versions if possible

--- indra/sources/dgi/api.py
+++ indra/sources/dgi/api.py
@@ -2,9 +2,12 @@
 
 """Code for downloading `Drug Gene Interaction Database (DGI-DB) <http://www.dgidb.org>`_."""
 
+import logging
 from typing import Optional, Tuple
 
 import pandas as pd
+
+logger = logging.getLogger(__name__)
 
 USECOLS = [
     'gene_name', 'entrez_id', 'interaction_claim_source',
@@ -15,9 +18,14 @@ USECOLS = [
 def get_version_df(version: Optional[str] = None) -> Tuple[str, pd.DataFrame]:
     """Get the latest version of the DGI interaction dataframe."""
     if version is None:
-        version = '2021-Jan'  # TODO use bioversions to look up with following two lines
-        # import bioregistry
-        # version = bioregistry.get_version('dgi')
+        try:
+            import bioversions
+            version = bioversions.get_version('Drug Gene Interaction Database')
+        except ImportError:
+            version = None
+    if version is None:
+        logger.warning('could not find version with bioregistry')
+        version = '2021-Jan'
     url = f'https://www.dgidb.org/data/monthly_tsvs/{version}/interactions.tsv'
     df = pd.read_csv(url, usecols=USECOLS, sep='\t', dtype=str)
     return version, df
