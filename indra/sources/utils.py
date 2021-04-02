8ea90a83318e4c1cb01b773435ef4861a459ac0f
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-04-02 21:10:53 +0200

    Implement autoloading and summary function

--- indra/sources/utils.py
+++ indra/sources/utils.py
@@ -2,6 +2,8 @@
 
 """Processor for remote INDRA JSON files."""
 
+from collections import Counter
+
 import requests
 from typing import List
 
@@ -24,16 +26,31 @@ class RemoteProcessor:
     #: The URL of the data
     url: str
 
-    #: A list of statements
-    statements: List[Statement]
-
     def __init__(self, url: str):
         self.url = url
-        self.statements = []
+        self._statements = None
+
+    @property
+    def statements(self) -> List[Statement]:
+        """The extracted statements."""
+        if self._statements is None:
+            self.extract_statements()
+        return self._statements
 
     def extract_statements(self) -> List[Statement]:
         """Extract statements from the remote JSON file."""
         res = requests.get(self.url)
         res.raise_for_status()
-        self.statements = stmts_from_json(res.json())
-        return self.statements
+        self._statements = stmts_from_json(res.json())
+        return self._statements
+
+    def print_summary(self) -> None:
+        """print a summary of the statements."""
+        from tabulate import tabulate
+        print(tabulate(
+            Counter(
+                statement.__class__.__name__
+                for statement in self.statements
+            ).most_common(),
+            headers=["Statement Type", "Count"],
+        ))
