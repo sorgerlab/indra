213990a027e4807fd44283d1ce6e829a2027d35a
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-04-05 15:37:07 +0200

    Update test

--- indra/sources/bel/api.py
+++ indra/sources/bel/api.py
@@ -161,7 +161,7 @@ def process_text(bel: str, squeeze: bool = False):
     >>> from indra.sources.bel import process_text
     >>> bel_s = 'kin(p(FPLX:MEK)) -> kin(p(FPLX:ERK))'
     >>> indra_s = process_text(bel_s, squeeze=True)
-    IncreaseAmount(MEK(), ERK())
+    Activation(MEK(kinase), ERK(), kinase)
     """
     r = pybel.parse(bel)
     graph = pybel.BELGraph()
