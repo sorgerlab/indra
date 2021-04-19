a68c3c76aed802b3d5420fd85217e36d2f88a803
Ben Gyori <ben.gyori@gmail.com>
2021-04-19 01:00:53 -0400

    Remove WM-specific reader modules

--- indra/sources/hume/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Hume is a general purpose reading system developed by BBN.
-
-Currently, INDRA can process JSON-LD files produced by Hume. When available,
-the API will be extended with access to the reader as a service.
-"""
-
-from .api import *
