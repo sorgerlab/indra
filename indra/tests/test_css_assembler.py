6704ce68b33157861fd2e816f47cf6f6edd0be6c
kkaris <karis.klas@gmail.com>
2021-05-25 13:37:41 -0400

    Make helper to check source badge styling

--- indra/tests/test_css_assembler.py
+++ indra/tests/test_css_assembler.py
@@ -1,6 +1,23 @@
+from typing import List, Tuple, Dict, Any
+
 from indra.assemblers.html.assembler import DEFAULT_SOURCE_COLORS
 from indra.assemblers.html.css_assembler import SourceBadgeStyleSheet, \
     StmtsViewStyleSheet, BaseTemplateStyleSheet
+
+
+def _source_badge_in_str(css_str: str,
+                         source_colors: List[Tuple[str, Dict[str, Any]]]) ->\
+        bool:
+    # Loop all the colors and test if they are in the generated stylesheet str
+    for category, data in source_colors:
+        for source, font_color in data['sources'].items():
+            style_str = f".source-{source} " \
+                        "{\n" \
+                        f"    background-color: {font_color};\n" \
+                        f"    color: {data['color']};\n" \
+                        "}"
+            assert style_str in css_str
+    return True
 
 
 def test_source_badge_sheet():
