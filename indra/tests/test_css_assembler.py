9e4857c9bfca730a890b7b82e5762d7cb6e02cc1
kkaris <karis.klas@gmail.com>
2021-05-25 13:37:56 -0400

    Start test for base template styling

--- indra/tests/test_css_assembler.py
+++ indra/tests/test_css_assembler.py
@@ -24,12 +24,10 @@ def test_source_badge_sheet():
     sbss = SourceBadgeStyleSheet()
     css_str = sbss.make_model()
 
-    # Loop all the colors and test if they are in the generated stylesheet str
-    for category, data in DEFAULT_SOURCE_COLORS:
-        for source, font_color in data['sources'].items():
-            style_str = f".source-{source} " \
-                        "{\n" \
-                        f"    background-color: {font_color};\n" \
-                        f"    color: {data['color']};\n" \
-                        "}"
-            assert style_str in css_str
+    assert _source_badge_in_str(css_str, DEFAULT_SOURCE_COLORS)
+
+
+def test_base_template_sheet():
+    btss_simple = BaseTemplateStyleSheet(simple=False)
+    css_str = btss_simple.make_model()
+    assert _source_badge_in_str(css_str, DEFAULT_SOURCE_COLORS)
