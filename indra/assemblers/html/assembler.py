cbdce8d9e3e7035b25c45706c2283f7f6e50343b
kkaris <karis.klas@gmail.com>
2021-06-02 17:43:22 -0400

    Add function generating a stylesheet from color mapping

--- indra/assemblers/html/assembler.py
+++ indra/assemblers/html/assembler.py
@@ -213,6 +213,35 @@ def get_default_source_colors(force_reload: bool = False) -> SourceColors:
 DEFAULT_SOURCE_COLORS = get_default_source_colors()
 
 
+def generate_source_css(fname: str,
+                        source_colors: SourceColors = None):
+    """Save a stylesheet defining color, background-color for the given sources
+
+    Parameters
+    ----------
+    fname :
+        Where to save the stylesheet
+    source_colors :
+        Colors defining the styles
+    """
+    if source_colors is None:
+        source_colors = DEFAULT_SOURCE_COLORS
+
+    rule_string = '.source-{src} {{\n    background-color: {src_bg};\n    ' \
+                  'color: {src_txt};\n}}\n\n'
+
+    stylesheet_str = ''
+    for _, info in source_colors:
+        text_color = info['color']
+        for source_name, bg_color in info['sources'].items():
+            stylesheet_str += rule_string.format(src=source_name,
+                                                 src_bg=bg_color,
+                                                 src_txt=text_color)
+
+    with open(fname, 'w') as fh:
+        fh.write(stylesheet_str)
+
+
 class HtmlAssembler(object):
     """Generates an HTML-formatted report from INDRA Statements.
 
