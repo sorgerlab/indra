8e0940244a19e4ebabfa7c7bb267ef959421118a
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-06-28 16:06:53 -0400

    Blacken

--- indra/databases/owl_client.py
+++ indra/databases/owl_client.py
@@ -19,46 +19,43 @@ class OwlClient(OntologyClient):
     """A base client for data that's been grabbed via OWL."""
 
     @staticmethod
-    def entry_from_term(term: 'pronto.Term') -> Mapping[str, Any]:
+    def entry_from_term(term: "pronto.Term") -> Mapping[str, Any]:
         """Create a data dictionary from a Pronto term."""
         rels_dict = defaultdict(list)
         xrefs = []
         for xref in term.xrefs:
             try:
-                xref_db, xref_id = xref.id.split(':')
+                xref_db, xref_id = xref.id.split(":")
             except ValueError:
                 continue
             else:
                 xrefs.append(dict(namespace=xref_db, id=xref_id))
         for child in term.subclasses(distance=1, with_self=False):
-            rels_dict['is_a'].append(child.id)
+            rels_dict["is_a"].append(child.id)
 
-        namespace, identifier = term.id.split(':')
+        namespace, identifier = term.id.split(":")
 
         return {
-            'namespace': namespace,
-            'id': identifier,
-            'name': term.name,
-            'synonyms': [
-                s.description
-                for s in term.synonyms
-            ],
-            'xrefs': xrefs,
-            'alt_ids': sorted(term.alternate_ids),
-            'relations': dict(rels_dict),
+            "namespace": namespace,
+            "id": identifier,
+            "name": term.name,
+            "synonyms": [s.description for s in term.synonyms],
+            "xrefs": xrefs,
+            "alt_ids": sorted(term.alternate_ids),
+            "relations": dict(rels_dict),
         }
 
     @classmethod
     def entries_from_ontology(
         cls,
         prefix: str,
-        ontology: 'pronto.Ontology',
+        ontology: "pronto.Ontology",
         *,
         skip_obsolete: bool = True,
     ):
         """"""
         rv = []
-        for term in tqdm(ontology.terms(), desc=f'[{prefix}]'):
+        for term in tqdm(ontology.terms(), desc=f"[{prefix}]"):
             if term.obsolete and skip_obsolete:
                 continue
             if not term.id.startswith(prefix):
@@ -70,42 +67,44 @@ class OwlClient(OntologyClient):
     def update_resource(
         cls,
         prefix: str,
-        ontology: 'pronto.Ontology',
+        ontology: "pronto.Ontology",
         skip_obsolete: bool = True,
     ):
-        entries = cls.entries_from_ontology(prefix=prefix, ontology=ontology, skip_obsolete=skip_obsolete)
+        entries = cls.entries_from_ontology(
+            prefix=prefix, ontology=ontology, skip_obsolete=skip_obsolete
+        )
         entries = prune_empty_entries(
             entries,
-            {'synonyms', 'xrefs', 'alt_ids', 'relations'},
+            {"synonyms", "xrefs", "alt_ids", "relations"},
         )
-        entries = sorted(entries, key=lambda x: int(x['id']))
+        entries = sorted(entries, key=lambda x: int(x["id"]))
 
-        resource_path = get_resource_path(f'{prefix.lower()}.json')
-        with open(resource_path, 'w') as file:
+        resource_path = get_resource_path(f"{prefix.lower()}.json")
+        with open(resource_path, "w") as file:
             json.dump(entries, file, indent=1, sort_keys=True)
 
     @classmethod
     def update_from_obo_library(
         cls,
         prefix: str,
-        extension: str = 'owl',
+        extension: str = "owl",
         **kwargs,
     ):
-        cache_path = get_resource_path(f'{prefix.lower()}.{extension}.pkl')
+        cache_path = get_resource_path(f"{prefix.lower()}.{extension}.pkl")
 
         if os.path.exists(cache_path):
-            with cache_path.open('rb') as file:
+            with cache_path.open("rb") as file:
                 ontology = pickle.load(file)
         else:
             try:
                 import pronto
             except ImportError:
                 raise ImportError(
-                    'To use the INDRA OWL Client, you must first'
-                    'install Pronto with `pip install pronto`.'
+                    "To use the INDRA OWL Client, you must first"
+                    "install Pronto with `pip install pronto`."
                 )
-            ontology = pronto.Ontology.from_obo_library(f'{prefix}.{extension}')
-            with cache_path.open('wb') as file:
+            ontology = pronto.Ontology.from_obo_library(f"{prefix}.{extension}")
+            with cache_path.open("wb") as file:
                 pickle.dump(ontology, file, protocol=pickle.HIGHEST_PROTOCOL)
 
         cls.update_resource(prefix=prefix, ontology=ontology, **kwargs)
@@ -121,12 +120,12 @@ class OwlClient(OntologyClient):
             import pronto
         except ImportError:
             raise ImportError(
-                'To use the INDRA OWL Client, you must first'
-                'install Pronto with `pip install pronto`.'
+                "To use the INDRA OWL Client, you must first"
+                "install Pronto with `pip install pronto`."
             )
         ontology = pronto.Ontology(file)
         cls.update_resource(prefix=prefix, ontology=ontology, **kwargs)
 
 
-if __name__ == '__main__':
-    OwlClient.update_from_obo_library('IDO')
+if __name__ == "__main__":
+    OwlClient.update_from_obo_library("IDO")
