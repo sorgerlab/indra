51bb8f8b7eb89e1c40ec9b3e8c8f2911feb63abb
Ben Gyori <ben.gyori@gmail.com>
2021-06-27 22:11:56 -0400

    Fix bugs and refactor processors

--- indra/sources/gnbr/processor.py
+++ indra/sources/gnbr/processor.py
@@ -34,14 +34,11 @@ class GnbrGeneGeneProcessor:
                                    (self.df1['V+'] > 0)]
         df = df1_activations.join(self.df2.set_index('path'), on='path')
         for index, row in df.iterrows():
-            agent1 = self.standardize_agent(row['nm_1_raw'], row['nm_1_dbid'])
-            agent2 = self.standardize_agent(row['nm_2_raw'], row['nm_2_dbid'])
-            evidence = Evidence(source_api='gnbr',
-                                pmid=row['id'],
-                                text=row['sentence'],
-                                text_refs={'PMID': row['id']}
-                                )
-            self.statements.append(Activation(agent1, agent2, evidence))
+            agent1 = get_standard_gene(row['nm_1_raw'], row['nm_1_dbid'])
+            agent2 = get_standard_gene(row['nm_2_raw'], row['nm_2_dbid'])
+            evidence = get_evidence(row)
+            self.statements.append(Activation(agent1, agent2,
+                                              evidence=evidence))
 
     def extract_increase_amount(self) -> None:
         """Make IncreaseAmount Statements from the dataframes."""
@@ -51,50 +48,23 @@ class GnbrGeneGeneProcessor:
                                          (self.df1['Q'] > 0))]
         df = df1_increase_amounts.join(self.df2.set_index('path'), on='path')
         for index, row in df.iterrows():
-            agent1 = self.standardize_agent(row['nm_1_raw'], row['nm_1_dbid'])
-            agent2 = self.standardize_agent(row['nm_2_raw'], row['nm_2_dbid'])
-            evidence = Evidence(source_api='gnbr',
-                                pmid=row['id'],
-                                text=row['sentence'],
-                                text_refs={'PMID': row['id']}
-                                )
-            self.statements.append(IncreaseAmount(agent1, agent2, evidence))
+            agent1 = get_standard_gene(row['nm_1_raw'], row['nm_1_dbid'])
+            agent2 = get_standard_gene(row['nm_2_raw'], row['nm_2_dbid'])
+            evidence = get_evidence(row)
+            self.statements.append(IncreaseAmount(agent1, agent2,
+                                                  evidence=evidence))
 
     def extract_complexes(self):
         """Make Complex Statements from the dataframes."""
         df1_complexes = self.df1[(self.df1['H.ind'] == 1) &
-                                        (self.df1['H'] > 0)]
+                                 (self.df1['H'] > 0)]
         df = df1_complexes.join(self.df2.set_index('path'), on='path')
         for index, row in df.iterrows():
-            agent1 = self.standardize_agent(row['nm_1_raw'], row['nm_1_dbid'])
-            agent2 = self.standardize_agent(row['nm_2_raw'], row['nm_2_dbid'])
-            evidence = Evidence(source_api='gnbr',
-                                pmid=row['id'],
-                                text=row['sentence'],
-                                text_refs={'PMID': row['id']}
-                                )
-            self.statements.append(Complex([agent1, agent2], evidence))
-
-
-    @staticmethod
-    def standardize_agent(raw_string: str, db_id: str) -> Agent:
-        """Standardize agent names.
-
-        Parameters
-        ----------
-        raw_string :
-            Name of the agent in the GNBR dataset.
-        db_id :
-            Entrez identifier of the agent.
-
-        Returns
-        -------
-        agent :
-            A standardized Agent object.
-        """
-        agent = Agent(raw_string, db_refs={'EGID': db_id, 'TEXT': raw_string})
-        standardize_agent_name(agent)
-        return agent
+            agent1 = get_standard_gene(row['nm_1_raw'], row['nm_1_dbid'])
+            agent2 = get_standard_gene(row['nm_2_raw'], row['nm_2_dbid'])
+            evidence = get_evidence(row)
+            self.statements.append(Complex([agent1, agent2],
+                                           evidence=evidence))
 
 
 class GnbrChemicalGeneProcessor:
@@ -123,14 +93,11 @@ class GnbrChemicalGeneProcessor:
                                    (self.df1['A+'] > 0)]
         df = df1_activations.join(self.df2.set_index('path'), on='path')
         for index, row in df.iterrows():
-            agent1 = self.std_chemical(row['nm_1_raw'], row['nm_1_dbid'])
-            agent2 = self.std_gene(row['nm_2_raw'], row['nm_2_dbid'])
-            evidence = Evidence(source_api='gnbr',
-                                pmid=row['id'],
-                                text=row['sentence'],
-                                text_refs={'PMID': row['id']}
-                                )
-            self.statements.append(Activation(agent1, agent2, evidence))
+            agent1 = get_standard_chemical(row['nm_1_raw'], row['nm_1_dbid'])
+            agent2 = get_standard_gene(row['nm_2_raw'], row['nm_2_dbid'])
+            evidence = get_evidence(row)
+            self.statements.append(Activation(agent1, agent2,
+                                              evidence=evidence))
 
     def extract_inhibition(self):
         """Make Inhibition Statements from the dataframes."""
@@ -140,14 +107,11 @@ class GnbrChemicalGeneProcessor:
                                     (self.df1['N'] > 0))]
         df = df1_inhibitions.join(self.df2.set_index('path'), on='path')
         for index, row in df.iterrows():
-            agent1 = self.std_chemical(row['nm_1_raw'], row['nm_1_dbid'])
-            agent2 = self.std_gene(row['nm_2_raw'], row['nm_2_dbid'])
-            evidence = Evidence(source_api='gnbr',
-                                pmid=row['id'],
-                                text=row['sentence'],
-                                text_refs={'PMID': row['id']}
-                                )
-            self.statements.append(Inhibition(agent1, agent2, evidence))
+            agent1 = get_standard_chemical(row['nm_1_raw'], row['nm_1_dbid'])
+            agent2 = get_standard_gene(row['nm_2_raw'], row['nm_2_dbid'])
+            evidence = get_evidence(row)
+            self.statements.append(Inhibition(agent1, agent2,
+                                              evidence=evidence))
 
     def extract_complexes(self):
         """Make Complex Statements from the dataframes."""
@@ -155,14 +119,10 @@ class GnbrChemicalGeneProcessor:
                                  (self.df1['B'] > 0)]
         df = df1_complexes.join(self.df2.set_index('path'), on='path')
         for index, row in df.iterrows():
-            agent1 = self.std_chemical(row['nm_1_raw'], row['nm_1_dbid'])
-            agent2 = self.std_gene(row['nm_2_raw'], row['nm_2_dbid'])
-            evidence = Evidence(source_api='gnbr',
-                                pmid=row['id'],
-                                text=row['sentence'],
-                                text_refs={'PMID': row['id']}
-                                )
-            self.statements.append(Complex([agent1, agent2], evidence))
+            agent1 = get_standard_chemical(row['nm_1_raw'], row['nm_1_dbid'])
+            agent2 = get_standard_gene(row['nm_2_raw'], row['nm_2_dbid'])
+            evidence = get_evidence(row)
+            self.statements.append(Complex([agent1, agent2], evidence=evidence))
 
     def extract_increase_amount(self):
         """Make IncreaseAmount Statements from the dataframes."""
@@ -170,14 +130,11 @@ class GnbrChemicalGeneProcessor:
                                  (self.df1['E+'] > 0)]
         df = df1_complexes.join(self.df2.set_index('path'), on='path')
         for index, row in df.iterrows():
-            agent1 = self.std_chemical(row['nm_1_raw'], row['nm_1_dbid'])
-            agent2 = self.std_gene(row['nm_2_raw'], row['nm_2_dbid'])
-            evidence = Evidence(source_api='gnbr',
-                                pmid=row['id'],
-                                text=row['sentence'],
-                                text_refs={'PMID': row['id']}
-                                )
-            self.statements.append(IncreaseAmount(agent1, agent2, evidence))
+            agent1 = get_standard_chemical(row['nm_1_raw'], row['nm_1_dbid'])
+            agent2 = get_standard_gene(row['nm_2_raw'], row['nm_2_dbid'])
+            evidence = get_evidence(row)
+            self.statements.append(IncreaseAmount(agent1, agent2,
+                                                  evidence=evidence))
 
     def extract_decrease_amount(self):
         """Make DecreaseAmount Statements from the dataframes."""
@@ -185,66 +142,72 @@ class GnbrChemicalGeneProcessor:
                                  (self.df1['E-'] > 0)]
         df = df1_complexes.join(self.df2.set_index('path'), on='path')
         for index, row in df.iterrows():
-            agent1 = self.std_chemical(row['nm_1_raw'], row['nm_1_dbid'])
-            agent2 = self.std_gene(row['nm_2_raw'], row['nm_2_dbid'])
-            evidence = Evidence(source_api='gnbr',
-                                pmid=row['id'],
-                                text=row['sentence'],
-                                text_refs={'PMID': row['id']}
-                                )
-            self.statements.append(DecreaseAmount(agent1, agent2, evidence))
+            agent1 = get_standard_chemical(row['nm_1_raw'], row['nm_1_dbid'])
+            agent2 = get_standard_gene(row['nm_2_raw'], row['nm_2_dbid'])
+            evidence = get_evidence(row)
+            self.statements.append(DecreaseAmount(agent1, agent2,
+                                                  evidence=evidence))
 
-    @staticmethod
-    def std_gene(raw_string: str, db_id: str) -> Agent:
-        """Standardize agent (gene) names.
 
-        Parameters
-        ----------
-        raw_string :
-            Name of the agent in the GNBR dataset.
-        db_id :
-            Entrez identifier of the agent.
+def get_standard_gene(raw_string: str, db_id: str) -> Agent:
+    """Standardize agent (gene) names.
 
-        Returns
-        -------
-        agent :
-            A standardized Agent object.
-        """
-        agent: Agent = Agent(raw_string, db_refs={'EGID': db_id, 'TEXT': raw_string})
+    Parameters
+    ----------
+    raw_string :
+        Name of the agent in the GNBR dataset.
+    db_id :
+        Entrez identifier of the agent.
+
+    Returns
+    -------
+    agent :
+        A standardized Agent object.
+    """
+    agent: Agent = Agent(raw_string, db_refs={'EGID': db_id,
+                                              'TEXT': raw_string})
+    standardize_agent_name(agent)
+    return agent
+
+
+def get_standard_chemical(raw_string: str, db_id: str) -> Agent:
+    """Standardize agent (chemical) names.
+
+    Parameters
+    ----------
+    raw_string :
+        Name of the agent in the GNBR dataset.
+    db_id :
+        Entrez identifier of the agent.
+
+    Returns
+    -------
+    agent :
+        A standardized Agent object.
+    """
+    agent: Agent
+    if db_id == 'null':
+        agent = Agent(name=raw_string, db_refs={'TEXT': raw_string})
+    elif db_id.startswith('CHEBI:'):
+        agent = Agent(raw_string, db_refs={'CHEBI': db_id,
+                                           'TEXT': raw_string})
         standardize_agent_name(agent)
-        return agent
+    elif db_id.startswith('MESH:'):
+        agent = Agent(raw_string, db_refs={'MESH': db_id.split(':')[1],
+                                           'TEXT': raw_string})
+        standardize_agent_name(agent)
+    else:
+        agent = Agent(raw_string, db_refs={'MESH': db_id,
+                                           'TEXT': raw_string})
+        standardize_agent_name(agent)
 
-    @staticmethod
-    def std_chemical(raw_string: str, db_id: str) -> Agent:
-        """Standardize agent (chemical) names.
+    return agent
 
-        Parameters
-        ----------
-        raw_string :
-            Name of the agent in the GNBR dataset.
-        db_id :
-            Entrez identifier of the agent.
 
-        Returns
-        -------
-        agent :
-            A standardized Agent object.
-        """
-        agent: Agent
-        if db_id == 'null':
-            agent.name = raw_string
-            agent.db_refs = raw_string
-        elif db_id.startswith('CHEBI:'):
-            agent = Agent(raw_string, db_refs={'CHEBI': db_id,
-                                               'TEXT': raw_string})
-            standardize_agent_name(agent)
-        elif db_id.startswith('MESH:'):
-            agent = Agent(raw_string, db_refs={'MESH': db_id.split(':')[1],
-                                               'TEXT': raw_string})
-            standardize_agent_name(agent)
-        else:
-            agent = Agent(raw_string, db_refs={'MESH': db_id,
-                                               'TEXT': raw_string})
-            standardize_agent_name(agent)
-
-        return agent
+def get_evidence(row):
+    pmid = str(row['id']) if row['id'] else None
+    evidence = Evidence(source_api='gnbr',
+                        pmid=pmid,
+                        text=row['sentence'],
+                        text_refs={'PMID': pmid})
+    return evidence
