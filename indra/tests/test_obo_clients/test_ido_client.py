710f1a9908c8d897c4dccaaa34f29485b077c4e6
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-07-06 12:41:49 -0400

    Update test_ido_client.py

--- indra/tests/test_obo_clients/test_ido_client.py
+++ indra/tests/test_obo_clients/test_ido_client.py
@@ -1,6 +1,13 @@
 from indra.databases import ido_client
 
 
+def test_ido_client_loaded():
+    """Test the IDO client is loaded."""
+    assert 'ido' == ido_client._client.prefix
+    assert ido_client._client.entries
+    assert ido_client._client.name_to_id
+
+
 def test_lookup():
     """Test IDO name and identifier lookup."""
     name = ido_client.get_ido_name_from_ido_id("0000403")
