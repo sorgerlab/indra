6ab6b983a4e3486ba31768726bb60b55fbf61a9e
Ben Gyori <ben.gyori@gmail.com>
2021-10-12 14:24:24 +0200

    Bump version

--- indra/__init__.py
+++ indra/__init__.py
@@ -2,7 +2,7 @@ from __future__ import print_function, unicode_literals
 import logging
 import os
 import sys
-__version__ = '1.20.0'
+__version__ = '1.21.0'
 
 __all__ = ['assemblers', 'belief', 'databases', 'explanation', 'literature',
            'mechlinker', 'preassembler', 'sources', 'tools', 'util']
