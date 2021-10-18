a8777169ddad8edce59fb540283394023d168a99
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-10-18 15:24:45 +0200

    Add docs

--- indra/sources/creeds/__init__.py
+++ indra/sources/creeds/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-"""Processor for the `CREEDS
-<https://maayanlab.cloud/CREEDS>`_.
+"""Processor for the `CRowd Extracted Expression of Differential Signatures (CREEDS)
+<https://maayanlab.cloud/CREEDS>`_ from [wang2016]_.
 
 Contains results of differential gene expression experiments extracted
 from the Gene Expression Omnibus due to three types of perturbations:
@@ -9,6 +9,10 @@ from the Gene Expression Omnibus due to three types of perturbations:
 1. Single gene knockdown/knockout
 2. Drug
 3. Disease
+
+.. [wang2016] Wang, Z., *et al*. (2016). `Extraction and analysis of signatures from the Gene
+   Expression Omnibus by the crowd <https://doi.org/10.1038/ncomms12846>`_.
+   *Nature Communications*, **7**(1), 12846.
 """
 
 from .api import process_from_web
