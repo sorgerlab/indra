53a670884a378e948edfa4cc7193775b047a365e
Ben Gyori <ben.gyori@gmail.com>
2021-10-20 15:31:39 -0400

    Bump bioontology version

--- indra/ontology/bio/ontology.py
+++ indra/ontology/bio/ontology.py
@@ -19,7 +19,7 @@ class BioOntology(IndraOntology):
     # should be incremented to "force" rebuilding the ontology to be consistent
     # with the underlying resource files.
     name = 'bio'
-    version = '1.18'
+    version = '1.19'
 
     def __init__(self):
         super().__init__()
