a7dd813390f4699a5717d8d7002ad6c223181dc5
kkaris <karis.klas@gmail.com>
2021-10-20 13:29:28 -0400

    Fix _source_info_to_source_colors

--- indra/assemblers/html/assembler.py
+++ indra/assemblers/html/assembler.py
@@ -9,7 +9,7 @@ import uuid
 import logging
 import itertools
 from html import escape
-from typing import Union, Tuple, List, Dict
+from typing import Union, Tuple, List, Dict, Optional
 from collections import OrderedDict, defaultdict
 from os.path import abspath, dirname, join
 
@@ -50,20 +50,30 @@ READER_TEXT_COLOR = 'white'
 """The text color for reader sources when shown as source count badges"""
 
 
-def _source_info_to_source_colors(source_info: SourceInfo) -> SourceColors:
+def _source_info_to_source_colors(
+        source_info: Optional[SourceInfo] = None
+) -> SourceColors:
     """Returns a source color data structure with source names as they
-    appear in INDRA DB"""
+    appear in INDRA DB
+    """
+    if source_info is None:
+        source_info = SOURCE_INFO
+
     # Initialize dicts for source: background-color for readers and databases
     database_colors = {}
     reader_colors = {}
     for source in all_sources:
-        mapped_source = reverse_source_mappings.get(source, source)
-        info = SOURCE_INFO.get(mapped_source)
+        # Get name as it is registered in source_info.json and get info
+        src_info_name = reverse_source_mappings.get(source, source)
+        info = source_info.get(src_info_name)
         if not info:
             logger.error('Source info missing for %s' % source)
             continue
+        # Get color from info
         color = info['default_style']['background-color']
-        mapped_source = internal_source_mappings.get(source, source)
+
+        # Map back to db name, use original name from all_sources as default
+        mapped_source = internal_source_mappings.get(src_info_name, source)
         if info['type'] == 'reader':
             reader_colors[mapped_source] = color
         else:
