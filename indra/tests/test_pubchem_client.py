06c48f42ac20eaca7181468048e6db6aa7d15d68
Samuel Bunga <sambunga094@gmail.com>
2021-11-12 10:59:11 -0500

    adding test for get_pmids

--- indra/tests/test_pubchem_client.py
+++ indra/tests/test_pubchem_client.py
@@ -1,6 +1,13 @@
 from indra.databases import pubchem_client
 
 
+
 def test_get_inchi_key():
     ik = pubchem_client.get_inchi_key('5280613')
     assert ik == 'JLVSPVFPBBFMBE-HXSWCURESA-O', ik
+
+
+def test_get_pmids():
+    pmids = pubchem_client.get_pmids('2244')
+    example_pmid = '19036898'
+    assert pmids[0] == example_pmid
