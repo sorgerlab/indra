089b4098ed4bb84a23768afbff4c396a3130f11c
Samuel Bunga <sambunga094@gmail.com>
2021-11-12 12:50:10 -0500

    update tests

--- indra/tests/test_pubchem_client.py
+++ indra/tests/test_pubchem_client.py
@@ -9,4 +9,4 @@ def test_get_inchi_key():
 def test_get_pmids():
     pmids = pubchem_client.get_pmids('2244')
     example_pmid = '19036898'
-    assert pmids[0] == example_pmid
+    assert example_pmid in pmids
