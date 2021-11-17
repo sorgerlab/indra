0ca7892505a47f4530a969f7184fad5914027143
Samuel Bunga <sambunga094@gmail.com>
2021-11-17 09:36:40 -0500

    tests for substance_mesh_mapping

--- indra/tests/test_pubchem_client.py
+++ indra/tests/test_pubchem_client.py
@@ -18,3 +18,11 @@ def test_mesh_mappings():
     mesh_id = pubchem_client.get_mesh_id('56649450')  # Alpelisib
     assert mesh_id == 'C585539', mesh_id
     assert pubchem_client.get_mesh_id('abcd') is None
+
+
+def test_get_substance_mesh_id():
+    mesh_ids = pubchem_client.get_substance_mesh_id('27959613')
+    example_mesh_id = 'D009570'
+    wrong_mesh_id = 'D0074447'
+    assert example_mesh_id in mesh_ids
+    assert wrong_mesh_id not in mesh_ids
