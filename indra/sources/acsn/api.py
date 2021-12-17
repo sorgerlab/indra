fa0be4e266358cadbafdcdd83a6556d2ea8a07bb
Samuel Bunga <sambunga094@gmail.com>
2021-12-16 22:32:04 -0500

    initial push of processor and api for acsn

--- indra/sources/acsn/api.py
+++ indra/sources/acsn/api.py
@@ -1,28 +1,53 @@
-__all__ = ['process_from_web', 'process_files', 'process_df']
+__all__ = ['process_from_web', 'process_df']
 
-
-import pandas
+import os
+import pandas as pd
+from collections import defaultdict
 from .processor import AcsnProcessor
+from indra.ontology.bio import bio_ontology
 
 ACSN_URL = 'https://acsn.curie.fr/ACSN2/downloads/'
 ACSN_RELATIONS_URL = ACSN_URL + \
-    'ACSN2_binary_relations_between_proteins_with_PMID.txt'
-ACSN_CORRESPONDENCE_URL = ACSN_URL + 'ACSN2_HUGO_Correspondence.gmt'
+                     'ACSN2_binary_relations_between_proteins_with_PMID.txt'
+ACSN_CORRESPONDENCE = os.path.join(os.path.realpath(os.path.dirname(__file__)),
+                                   'ACSN2_HUGO_Correspondence.gmt')
+
+
+def transform_gmt(gmt):
+    # Convert the GMT file into a dictionary
+    acsn_gmt_dict = defaultdict(set)
+    with open(gmt, 'r') as fh:
+        for line in fh:
+            line = line.rstrip('\n')
+            line = line.rstrip('\t')
+            interactors = line.split('\t')
+            for ag2 in interactors[2:]:
+                acsn_gmt_dict[(interactors[0])].add(ag2)
+    return acsn_gmt_dict
 
 
 def process_from_web():
-    relations_df = pandas.read_csv(ACSN_RELATIONS_URL)
-    correspondence_df = pandas.read_csv(ACSN_CORRESPONDENCE_URL)
-    return process_df(relations_df, correspondence_df)
+    relations_df = pd.read_csv(ACSN_RELATIONS_URL, sep='\t')
+    correspondence_dict = transform_gmt(ACSN_CORRESPONDENCE)
+    return process_df(relations_df, correspondence_dict)
 
 
-def process_files(relations_path: str, correspondence_path: str):
-    relations_df = pandas.read_csv(relations_path)
-    correspondence_df = pandas.read_csv(correspondence_path)
-    return process_df(relations_df, correspondence_df)
-
-
-def process_df(relations_df, correspondence_df):
-    ap = AcsnProcessor(relations_df, correspondence_df)
+def process_df(relations_df, correspondence_dict):
+    fplx_lookup = get_famplex_lookup()
+    ap = AcsnProcessor(relations_df, correspondence_dict,
+                       fplx_lookup)
     ap.extract_statements()
     return ap
+
+
+def get_famplex_lookup():
+    fplx_lookup = {}
+    bio_ontology.initialize()
+    for node in bio_ontology.nodes:
+        ns, id = bio_ontology.get_ns_id(node)
+        if ns == 'FPLX':
+            children = bio_ontology.get_children(ns, id)
+            hgnc_children = [bio_ontology.get_name(*c)
+                             for c in children if c[0] == 'HGNC']
+            fplx_lookup[tuple(sorted(hgnc_children))] = id
+    return fplx_lookup
--- indra/sources/acsn/processor.py
+++ indra/sources/acsn/processor.py
@@ -1,12 +1,68 @@
 from indra.statements import *
+from indra.databases.hgnc_client import get_hgnc_id
+from indra.ontology.standardize import get_standard_agent
+
+rel_mapping = {
+    'CATALYSIS': Activation,
+    'INHIBITION': Inhibition,
+    'HETERODIMER_ASSOCIATION': Complex,
+    'CATALYSIS;HETERODIMER_ASSOCIATION': Complex
+}
 
 
 class AcsnProcessor:
-    def __init__(self, relations_df, correspondence_df):
+    def __init__(self, relations_df, correspondence_dict,
+                 fplx_lookup):
         self.relations_df = relations_df
-        self.correspondence_df = correspondence_df
+        self.correspondence_dict = correspondence_dict
+        self.fplx_lookup = fplx_lookup
         self.statements = []
 
     def extract_statements(self):
         # This is where we implement the Statement extraction
-        pass
+        for _, row in self.relations_df.iterrows():
+            agA, stmt_types, agB, pmids = list(row)
+
+            agA = get_agent(agA, self.correspondence_dict,
+                            self.fplx_lookup)
+            agB = get_agent(agB, self.correspondence_dict,
+                            self.fplx_lookup)
+            stmt_type = get_stmt_type(stmt_types)
+            if stmt_type:
+                if agA and agB:
+                    if str(pmids) == 'nan':
+                        evs = [Evidence(source_api='acsn')]
+
+                    else:
+                        evs = [Evidence(source_api='acsn', pmid=pmid)
+                               for pmid in pmids.split(';')]
+
+                    if stmt_type == Complex:
+                        stmt = stmt_type([agA, agB], evidence=evs)
+                    else:
+                        stmt = stmt_type(agA, agB, evidence=evs)
+
+                    self.statements.append(stmt)
+
+
+def get_agent(ag_name, correspondence_dict, fplx_lookup):
+    db_refs = {}
+    if ag_name in correspondence_dict and len(correspondence_dict[ag_name]) == 1:
+        grounded_gene = next(iter(correspondence_dict[ag_name]))
+        hgnc_id = get_hgnc_id(grounded_gene)
+        if hgnc_id:
+            db_refs['HGNC'] = hgnc_id
+        db_refs['TEXT'] = grounded_gene
+        return get_standard_agent(grounded_gene, db_refs)
+
+    elif ag_name in correspondence_dict and len(correspondence_dict) > 1:
+        fplx_rel = fplx_lookup.get(tuple(sorted(correspondence_dict[ag_name])))
+        if fplx_rel:
+            db_refs['FPLX'] = fplx_rel
+            return get_standard_agent(fplx_rel, db_refs)
+
+
+def get_stmt_type(stmt_type):
+    for r in rel_mapping:
+        if stmt_type in rel_mapping:
+            return rel_mapping[stmt_type]
