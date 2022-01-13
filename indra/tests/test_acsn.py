3cb998fc6e2fd94ab08e08266cbf7cb6b65e6351
Samuel Bunga <sambunga094@gmail.com>
2022-01-13 14:39:21 -0500

    add tests for get_stmt_type and get_agent

--- indra/tests/test_acsn.py
+++ indra/tests/test_acsn.py
@@ -1,6 +1,18 @@
+import pandas
 import requests
 from indra.sources.acsn import api
 from indra.sources.acsn import processor
+from indra.ontology.standardize import get_standard_agent
+
+# Agents
+VEGFA = get_standard_agent('VEGFA', db_refs={'HGNC': '12680'})
+MIRLET7A = get_standard_agent('MIRLET7A', db_refs={'FPLX': 'MIRLET7A'})
+
+
+relations_df = pandas.read_csv(api.ACSN_RELATIONS_URL, sep='\t')
+correspondence_dict = api._transform_gmt(
+    requests.get(api.ACSN_CORRESPONDENCE_URL).text.split('\n'))
+acsn_proc = processor.AcsnProcessor(relations_df, correspondence_dict)
 
 
 def test_acsn_web_api():
@@ -24,3 +36,19 @@ def test_famplex_lookup():
     fplx_lookup = processor._make_famplex_lookup()
     assert 'USPL' in fplx_lookup[('CYLD', 'USPL1')]
     assert 'VEGFRR' not in fplx_lookup[('FLT1', 'FLT4', 'KDR')]
+
+
+def test_get_stmt_type():
+    assert processor.get_stmt_type('CATALYSIS').__name__ == 'Activation'
+    assert processor.get_stmt_type('INHIBITION').__name__ == 'Inhibition'
+    assert processor.get_stmt_type('HETERODIMER_ASSOCIATION').__name__ == 'Complex'
+    assert processor.get_stmt_type('CATALYSIS;HETERODIMER_ASSOCIATION').__name__ == \
+           'Complex'
+    assert not processor.get_stmt_type('Activation')
+
+
+def test_get_agent():
+    assert acsn_proc.get_agent('VEGFA').db_refs == VEGFA.db_refs, VEGFA.db_refs
+    assert acsn_proc.get_agent('MIRLET7A*').db_refs == MIRLET7A.db_refs, MIRLET7A.db_refs
+    assert acsn_proc.get_agent('XyZ') == None
+
