51479ca43a03329ec563a6218b44fc062712be4d
Ben Gyori <ben.gyori@gmail.com>
2022-09-08 20:48:48 -0400

    Add some tests for MGI client

---
+++ indra/tests/test_mgi_client.py
@@ -0,0 +1,9 @@
+from indra.databases import mgi_client
+
+
+def test_lookups():
+    assert mgi_client.get_id_from_name('Braf') == '88190'
+    assert mgi_client.get_name_from_id('1926283') == 'Pgap6'
+    assert mgi_client.get_id_from_name_synonym('Pgap6') == '1926283'
+    assert mgi_client.get_id_from_name_synonym('Tmem8') == '1926283'
+    assert isinstance(mgi_client.get_id_from_name_synonym('EGF-TM7'), list)
