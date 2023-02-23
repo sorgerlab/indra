de3b43f6334e36c98c44787e5df415d831114cac
Ben Gyori <ben.gyori@gmail.com>
2023-02-23 15:32:39 -0500

    Switch to local mode by default

--- indra/sources/eidos/bio_processor.py
+++ indra/sources/eidos/bio_processor.py
@@ -74,5 +74,5 @@ def get_agent_bio(concept, context=None, grounder: Optional[Grounder] = None):
 def default_grounder_wrapper(text: str, context: Optional[str]) -> GrounderResult:
     # Import here to avoid this when working in INDRA World context
     from indra.preassembler.grounding_mapper.gilda import get_grounding
-    grounding, _ = get_grounding(text, context=context)
+    grounding, _ = get_grounding(text, context=context, mode='local')
     return grounding
