842c48f19d352355306826edf04d2ece4a350e34
Ben Gyori <ben.gyori@gmail.com>
2023-03-22 17:30:41 -0400

    Mark individual tests to skip

--- indra/tests/test_elsevier_client.py
+++ indra/tests/test_elsevier_client.py
@@ -4,11 +4,10 @@ import pytest
 
 logger = logging.getLogger(__name__)
 
-pytest.skip('Elsevier credentials currently not operational.')
-
 
 @pytest.mark.nonpublic
 @pytest.mark.webservice
+@pytest.mark.nogha
 def test_get_fulltext_article():
     # This article is not open access so in order to get a full text response
     # with a body element requires full text access keys to be correctly
@@ -20,6 +19,7 @@ def test_get_fulltext_article():
 
 @pytest.mark.nonpublic
 @pytest.mark.webservice
+@pytest.mark.nogha
 def test_get_abstract():
     # If we have an API key but are not on an approved IP or don't have a
     # necessary institution key, we should still be able to get the abstract.
@@ -32,6 +32,7 @@ def test_get_abstract():
 
 @pytest.mark.nonpublic
 @pytest.mark.webservice
+@pytest.mark.nogha
 def test_get_converted_article_body():
     """Make sure we can get fulltext of an article that has
     ja:converted-article as its principal sub-element."""
@@ -47,6 +48,7 @@ def test_get_converted_article_body():
 
 @pytest.mark.nonpublic
 @pytest.mark.webservice
+@pytest.mark.nogha
 def test_get_rawtext():
     """Make sure we can get content of an article that has content in
     xocs:rawtext"""
@@ -62,6 +64,7 @@ def test_get_rawtext():
 
 @pytest.mark.nonpublic
 @pytest.mark.webservice
+@pytest.mark.nogha
 def test_article():
     # PMID: 11302724
     doi = '10.1006/bbrc.2001.4693'
