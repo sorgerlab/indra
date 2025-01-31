5eb089372bdf7a4f4315919f888530dc150b5592
Bradley-Buchner <bradbu@live.unc.edu>
2025-01-30 19:30:01 -0500

    Add description

--- indra/sources/wormbase/__init__.py
+++ indra/sources/wormbase/__init__.py
@@ -1,1 +1,6 @@
+"""This module provides an interface to the WormBase knowledge base and
+extracts C. elegans gene interactions as INDRA Statements.
 
+WormBase can be accessed at https://wormbase.org/#012-34-5.
+"""
+from .api import *
