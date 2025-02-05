2c8b3510986f59f411df1735fceeef741df9db8e
Bradley-Buchner <bradbu@live.unc.edu>
2025-02-05 14:04:20 -0500

    Update __init__.py
    Update docstring description

--- indra/sources/wormbase/__init__.py
+++ indra/sources/wormbase/__init__.py
@@ -1,5 +1,5 @@
 """This module provides an interface to the WormBase knowledge base and
-extracts C. elegans gene interactions as INDRA Statements.
+extracts C. elegans genetic and molecular interactions as INDRA Statements.
 
 WormBase can be accessed at https://wormbase.org/#012-34-5.
 """
