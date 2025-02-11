5484768442c8001027ce98536596f047f119e1ad
Ben Gyori <ben.gyori@gmail.com>
2025-02-11 10:15:17 -0500

    Clean up some of the API and processor

--- indra/sources/wormbase/__init__.py
+++ indra/sources/wormbase/__init__.py
@@ -1,6 +1,6 @@
 """This module provides an interface to the WormBase knowledge base and
 extracts C. elegans genetic and molecular interactions as INDRA Statements.
 
-WormBase can be accessed at https://wormbase.org/#012-34-5.
+WormBase can be accessed at https://wormbase.org/.
 """
 from .api import *
