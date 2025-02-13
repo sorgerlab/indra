aadf0bff0dfff6ce15f7578710cb655e7b002385
Bradley-Buchner <bradbu@live.unc.edu>
2025-02-12 21:37:38 -0500

    Update test file to use process_from_files() instead of WormBaseProcessor()

--- indra/tests/test_wormbase_processor.py
+++ indra/tests/test_wormbase_processor.py
@@ -1,13 +1,14 @@
 from collections import Counter
 from unittest import TestCase
 import os
-import tqdm
-from indra.sources.wormbase.processor import WormBaseProcessor
+from indra.sources.wormbase import *
 
 
 this_dir = os.path.dirname(__file__)
 test_file_gen = os.path.join(this_dir, 'wormbase_tests_data/INTERACTION-GEN_WB_test.tsv')
 test_file_mol = os.path.join(this_dir, 'wormbase_tests_data/INTERACTION-MOL_WB_3_test.tsv')
+test_file_map = os.path.join(this_dir, 'wormbase_tests_data/wormbase_entrez_mappings.tsv')
+
 
 
 class TestWormBaseProcessor(TestCase):
@@ -17,8 +18,7 @@ class TestWormBaseProcessor(TestCase):
 
         try:
             # Create a WormBaseProcessor instance with test files
-            processor = WormBaseProcessor(test_file_gen, test_file_mol)
-            # processor = WormBaseProcessor()
+            processor = process_from_files(test_file_gen, test_file_mol. test_file_map)
 
             # Ensure `statements` has been populated
             self.assertGreater(len(processor.statements), 0, "The statements list should not be empty.")
