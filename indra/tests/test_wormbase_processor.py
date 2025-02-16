8ce1e62e2ef0ed9b8f9a765645775a47df72359f
Ben Gyori <ben.gyori@gmail.com>
2025-02-16 14:49:11 -0500

    Clean up wormbase tests

--- indra/tests/test_wormbase_processor.py
+++ indra/tests/test_wormbase_processor.py
@@ -10,25 +10,21 @@ test_file_mol = os.path.join(this_dir, 'wormbase_tests_data/INTERACTION-MOL_WB_3
 test_file_map = os.path.join(this_dir, 'wormbase_tests_data/wormbase_entrez_mappings.tsv')
 
 
-
 class TestWormBaseProcessor(TestCase):
 
     def test_statements_creation(self):
-        """Test attributes and properties of the `statements` list in WormBaseProcessor."""
+        """Test the `statements` list in WormBaseProcessor."""
 
-        try:
-            # Create a WormBaseProcessor instance with test files
-            processor = process_from_files(test_file_gen, test_file_mol. test_file_map)
+        # Create a WormBaseProcessor instance with test files
+        processor = process_from_files(test_file_gen, test_file_mol,
+                                       test_file_map)
 
-            # Ensure `statements` has been populated
-            self.assertGreater(len(processor.statements), 0, "The statements list should not be empty.")
+        # Ensure `statements` has been populated
+        self.assertGreater(len(processor.statements), 0,
+                           "The statements list should not be empty.")
 
-            # print(len(processor.statements))
-            # interaction_type_counts = Counter(type(statement).__name__ for statement in processor.statements)
-            # print(interaction_type_counts)
-
-        except Exception as e:
-            self.fail(f"Test failed due to an exception: {e}")
+        # interaction_type_counts = Counter(type(statement).__name__ for statement in processor.statements)
+        # print(interaction_type_counts)
 
 
 if __name__ == "__main__":
