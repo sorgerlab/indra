7d5d5864c8713dd08699429311a1f5af75c05bdd
haohangyan <hy2664@nyu.edu>
2025-06-06 15:29:12 -0400

    reserve the change in check_prior_probs

--- indra/belief/skl.py
+++ indra/belief/skl.py
@@ -657,9 +657,8 @@ class HybridScorer(BeliefScorer):
     ) -> None:
         """Check that sources in the set of statements are accounted for."""
         # Get all sources for the set of statements
-        sources = set()
-        for stmt in statements:
-            sources |= set([ev.source_api for ev in stmt.evidence])
+        sources = CountsScorer.get_all_sources(statements,
+                                               include_more_specific=True)
         non_cs_sources = set(sources).difference(
                                         set(self.counts_scorer.source_list))
         return self.simple_scorer._check_sources(non_cs_sources)
